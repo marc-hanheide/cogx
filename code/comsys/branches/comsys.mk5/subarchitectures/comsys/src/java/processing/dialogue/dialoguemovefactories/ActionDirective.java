4f91edc0e9c82535f6105b55442d33085460d7bc
gj <gj@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-05-26 10:51:19 +0000

    more dialogue move stuff, not compiling yet due to comsys.ice problems

---
+++ code/comsys/branches/comsys.mk5/subarchitectures/comsys/src/java/processing/dialogue/dialoguemovefactories/ActionDirective.java
@@ -0,0 +1,44 @@
+package comsys.processing.dialogue.dialoguemovefactories;
+
+import java.util.TreeMap;
+import lf.*;
+import comsys.*;
+import comsys.utils.datastructs.*;
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ * Dialogue Move factory for assertive utterances
+ * @author plison
+ *
+ */
+public class ActionDirective extends AbstractDialogueMoveFactory implements DialogueMoveFactory  {
+
+	public ActionDirective () {
+		super();
+		localInit();
+	}
+	
+
+	/** Initialization of class-local variables, and setting of 
+		super-class variables. */ 
+
+	protected void localInit () { 
+		_avm = new ArrayList<AbstractFeatureValue>();
+		_avm.add(new FeatureValue("mood", "imp"));
+	//	Vector<String> values = new Vector<String>();
+	//	values.add("action-motion");
+	//	values.add("action-non-motion");
+		_avm.add(new FeatureValue("sort", "action-non-motion"));
+	//	_avm.add(new FeatureValue("tense", "pres"));
+		_label = "ACTION-DIRECTIVE";
+	} // end 
+	
+	public DialogueMove produceDialogueMove (String SDRSFormulaId1, String SDRSFormulaId2, String dialogueMoveId) {
+		DialogueMove dm = new DialogueMove();
+		dm.mType = MoveType.ACTION_DIRECTIVE ;
+		dm.SDRSFormulaId1 = SDRSFormulaId1 ;
+		dm.SDRSFormulaId2 = SDRSFormulaId2 ;	
+		return dm;	
+	}
+}
