6e397543902d2d898cafe9a29a1b0ae58b726b7a
gj <gj@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-05-26 10:51:19 +0000

    more dialogue move stuff, not compiling yet due to comsys.ice problems

---
+++ comsys/branches/comsys.mk5/subarchitectures/comsys/src/java/processing/dialogue/dialoguemovefactories/DialogueMoveFactory.java
@@ -0,0 +1,63 @@
+//=================================================================
+//Copyright (C) 2007 Geert-Jan M. Kruijff (gj@dfki.de)
+//
+//This library is free software; you can redistribute it and/or
+//modify it under the terms of the GNU Lesser General Public License
+//as published by the Free Software Foundation; either version 2.1 of
+//the License, or (at your option) any later version.
+//
+//This library is distributed in the hope that it will be useful, but
+//WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//Lesser General Public License for more details.
+//
+//You should have received a copy of the GNU Lesser General Public
+//License along with this program; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//02111-1307, USA.
+//=================================================================
+
+//=================================================================
+//PACKAGE DEFINITION 
+//=================================================================
+
+package comsys.processing.dialogue.dialoguemovefactories;
+
+//=================================================================
+//IMPORTS
+//=================================================================
+
+
+//----------------------------------------------------------------
+//COMSYS imports
+//----------------------------------------------------------------
+import comsys.*;
+import comsys.utils.datastructs.*;
+
+import java.util.Iterator;
+import java.util.List;
+
+//=================================================================
+//JAVADOC CLASS DOCUMENTATION
+//=================================================================
+
+/**
+	The interface <b>EventNucleusFactory</b> describes the methods
+	which any method implementing a factory for producing event 
+	nuclei for a particular type of verbal predicate should implement. 
+	
+	@started 071026
+	@version 071026
+	@author	 Geert-Jan M. Kruijff (gj@dfki.de)
+*/ 
+
+public interface DialogueMoveFactory {
+
+
+	public List<AbstractFeatureValue> getProperties();
+	
+	public String getLabel();
+
+	public DialogueMove produceDialogueMove (String SDRSFormulaId1, String SDRSFormulaId2, String dialogueMoveId);
+
+} // end interface EventStructureFactory
---
+++ comsys/branches/comsys.mk5/subarchitectures/comsys/src/java/processing/dialogue/dialoguemovefactories/QuestionYN.java
@@ -0,0 +1,41 @@
+package comsys.processing.dialogue.dialoguemovefactories;
+
+import java.util.TreeMap;
+import org.cognitivesystems.comsys.autogen.ComsysEssentials.DialogueMove;
+import org.cognitivesystems.comsys.autogen.ComsysEssentials.MoveType;
+import org.cognitivesystems.repr.lf.autogen.LFPacking.PackedNominal;
+import org.cognitivesystems.comsys.util.datastructs.*;
+import java.util.ArrayList;
+
+/**
+ * Dialogue Move factory for assertive utterances
+ * @author plison
+ *
+ */
+public class QuestionYN extends AbstractDialogueMoveFactory implements DialogueMoveFactory  {
+
+	public QuestionYN () {
+		super();
+		localInit();
+	}
+	
+
+	/** Initialization of class-local variables, and setting of 
+		super-class variables. */ 
+
+	protected void localInit () { 
+		_avm = new ArrayList<AbstractFeatureValue>();
+		_avm.add(new FeatureValue("mood", "int"));
+		_avm.add(new FeatureValue("sort", "ascription"));
+		_avm.add(new FeatureValue("wh-restrictor", "no"));
+		_label = "QUESTION-YN";
+	} // end 
+	
+	public DialogueMove produceDialogueMove (String SDRSFormulaId1, String SDRSFormulaId2, String dialogueMoveId) {
+		DialogueMove dm = new DialogueMove();
+		dm.mType = MoveType.QUESTION_YN ;
+		dm.SDRSFormulaId1 = SDRSFormulaId1 ;
+		dm.SDRSFormulaId2 = SDRSFormulaId2 ;	
+		return dm;	
+	}
+}
