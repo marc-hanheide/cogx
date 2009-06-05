333e4c1f621f9d5e16c36252edf819e8f90671c4
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-06-05 13:07:37 +0000

    VisionUtils now builds

--- vision/trunk/subarchitectures/vision.sa/src/c++/vision/VisionUtils.h
+++ vision/trunk/subarchitectures/vision.sa/src/c++/vision/VisionUtils.h
@@ -6,19 +6,19 @@
 #ifndef VISION_UTILS_H
 #define VISION_UTILS_H
 
-#include <tools/hardware/video/src/c++/utils/VideoUtils.h>
-#include "VisionData.hpp"
+#include <VideoUtils.h>
+#include <VisionData.hpp>
 
 /**
  * Project a SOI in world co-ordinates to an image ROI.
  */
-inline VisionData::ROI projectSOI(const Video::CameraParameters &cam, const VisionData::SOI &soi)
+inline VisionData::ROIPtr projectSOI(const Video::CameraParameters &cam, const VisionData::SOI &soi)
 {
-   VisionData::ROI roi;
-   roi.rect.pos = projectPoint(cam, VisionData::SOI.boundingSphere.pos);
-   roi.rect.width = projectSize(cam, VisionData::SOI.boundingSphere.rad);
-   roi.rect.height = projectSize(cam, VisionData::SOI.boundingSphere.rad);
-   roi.time = soi.time;
+   VisionData::ROIPtr roi = new VisionData::ROI;
+   roi->rect.pos = projectPoint(cam, soi.boundingSphere.pos);
+   roi->rect.width = projectSize(cam, soi.boundingSphere.pos, soi.boundingSphere.rad);
+   roi->rect.height = projectSize(cam, soi.boundingSphere.pos, soi.boundingSphere.rad);
+   roi->time = soi.time;
    return roi;
 }
 
