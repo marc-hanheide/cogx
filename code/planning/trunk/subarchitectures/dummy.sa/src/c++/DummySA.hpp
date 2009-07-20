64f5aedac82e9418c8e67832f207ba5165775249
tkeller <tkeller@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-07-20 11:16:12 +0000

    added the dummy for testing purposes

---
+++ code/planning/trunk/subarchitectures/dummy.sa/src/c++/DummySA.hpp
@@ -0,0 +1,15 @@
+#ifndef DUMMY_SA_HPP_
+#define DUMMY_SA_HPP_
+
+#include <cast/architecture.hpp>
+
+class DummySA : public cast::ManagedComponent
+{
+ protected:
+  virtual void runComponent();
+
+ public:
+  virtual void planGenerated(const cast::cdl::WorkingMemoryChange& wmc);
+};
+
+#endif
--- code/planning/trunk/subarchitectures/planner.sa/src/python/PythonServer.py
+++ code/planning/trunk/subarchitectures/planner.sa/src/python/PythonServer.py
@@ -1,8 +1,8 @@
 import sys, traceback, Ice
 import autogen.Planner as Planner
-import core
+import cast.core
 
-class PythonServerI(Planner.PythonServer, core.CASTComponent):
+class PythonServerI(Planner.PythonServer, cast.core.CASTComponent):
   def __init__(self):
     self.client = None
 
@@ -19,6 +19,8 @@ class PythonServerI(Planner.PythonServer, core.CASTComponent):
     pass
 
   def addTask(self, task, current=None):
+    print "Planner PythonServer: New PlanningTask received"
+
     if(self.client is None):
       return -1
 
@@ -26,27 +28,5 @@ class PythonServerI(Planner.PythonServer, core.CASTComponent):
     return 1
 
   def registerClient(self, Client, current=None):
+    print "Planner PythonServer: running"
     self.client = Client
-
-#status = 0
-#ic = None
-
-#try:
-#  ic = Ice.initialize(sys.argv)
-#  adapter = ic.createObjectAdapterWithEndpoints("SimplePrinterAdapter", "default -p 10000")
-#  object = PythonServerI()
-#  adapter.add(object, ic.stringToIdentity("SimplePrinter"))
-#  adapter.activate()
-#  ic.waitForShutdown()
-#except:
-#  traceback.print_exc()
-#  status = 1
-#  if ic:
-  # Clean up
-#    try:
-#      ic.destroy()
-#    except:
-#      traceback.print_exc()
-#      status = 1
-#      sys.exit(status)
-    
