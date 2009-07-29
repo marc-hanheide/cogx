8ca20deafa3514956bb494ab1dea0373385fb3c1
brenner <brenner@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-07-29 20:09:49 +0000

    new version, still buggy

--- code/planning/trunk/subarchitectures/planner.sa/src/python/PythonServer.py
+++ code/planning/trunk/subarchitectures/planner.sa/src/python/PythonServer.py
@@ -1,11 +1,39 @@
 import sys, traceback, Ice
+
 import autogen.Planner as Planner
 import cast.core
-#import standalone.task
+
+# def extend_pythonpath():
+#   from os.path import abspath, dirname, join, isdir
+#   standalone_path = abspath(dirname(__file__))
+# extend_pythonpath()  
+
+# from standalone.task import Task
+# from standalone.planner import StandalonePlanner
+
+# # Using string templates only until move to the proper ICE types
+# MAPL_TASK_TMPL = """
+# (define (problem cosytask) (:domain coffee)
+# (:objects
+# %s
+# )
+# (:init
+# %s
+# )
+# (:goal (and
+# %s
+# )))
+# """
+
+# TEST_DOMAIN_FN = ""
+
+
 
 class PythonServerI(Planner.PythonServer, cast.core.CASTComponent):
   def __init__(self):
     self.client = None
+    self.planner = StandalonePlanner()
+    print "new PythonServer"
 
   def configure(self):
     pass
@@ -19,18 +47,29 @@ class PythonServerI(Planner.PythonServer, cast.core.CASTComponent):
   def runComponent(self):
     pass
 
-  def registerTask(self, task, current=None):
+  def registerTask(self, task_desc, current=None):
+    # MB: id?
     print "Planner PythonServer: New PlanningTask received:"
-    print "GOAL: " + task.goal;
-    print "OBJECTS: " + task.objects;
-    print "INIT: " + task.state;
+    print "GOAL: " + task_desc.goal;
+    print "OBJECTS: " + task_desc.objects;
+    print "INIT: " + task_desc.state;
 
-    task.plan = "SOMEDAY HERE WILL BE A PLAN!!!"
+#     print sys.path
+#     task = Task()
+#     task.read_mapl_domain(TEST_DOMAIN_FN)
+#     problem_str = MAPL_TASK_TMPL % (task_desc.objects, task_desc.state, task_desc.goal)
+#     task.read_mapl_problem(problem_str)
+#     self.planner.register_task(task)
+#     task.set_planning_status(PlanningStatusEnum.TASK_CHANGED)
+#     task.activate_change_dectection()
+
+
+    task_desc.plan = "SOMEDAY HERE WILL BE A PLAN!!!"
 
     if(self.client is None):
       print "ERROR!!"
 
-    self.client.deliverPlan(task);
+    self.client.deliverPlan(task_desc);
     # add task to some queue or start planning right away. when done call self.client.deliverPlan(string plan)
     
   def registerClient(self, Client, current=None):
