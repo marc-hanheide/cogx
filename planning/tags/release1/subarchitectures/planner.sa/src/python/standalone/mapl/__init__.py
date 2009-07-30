8c1e8a7276f71c3f4122cc384c6c0625b9983869
brenner <brenner@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-07-30 21:46:25 +0000

    release1 tag

---
+++ planning/tags/release1/subarchitectures/planner.sa/src/python/standalone/mapl/__init__.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: latin-1 -*-
+
+from mapl2pddl import translate, load_mapl_domain, load_mapl_task, compile_pddl_domain, create_subgoal_op, prepare_compiled_planning_domains, DomainData
+
