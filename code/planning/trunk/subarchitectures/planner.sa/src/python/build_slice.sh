b66fe7eeafb6935008b82b0f04b981c81bb78c48
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-08-25 07:33:51 +0000

    added extra include path

--- code/planning/trunk/subarchitectures/planner.sa/src/python/build_slice.sh
+++ code/planning/trunk/subarchitectures/planner.sa/src/python/build_slice.sh
@@ -1,3 +1,3 @@
 #
-slice2py ../slice/*.ice --output-dir . -I /usr/local/share -I ../../../binder/src/slice/ --all
+slice2py ../slice/*.ice --output-dir . -I /usr/local/share -I /opt/local/share -I ../../../binder/src/slice/ --all
 slice2py ../../../binder/src/slice/*ice --output-dir . -I /usr/local/share --all
