3f9f76c5899d51eaaaff5ccdbbfde4990cbe97b3
Nick Hawes <nickh@robots.ox.ac.uk>
2009-09-01 15:58:12 +0000

    needed to complile on os x

--- planning/branches/rel1cast2.1/subarchitectures/planner.sa/src/python/build_slice.sh
+++ planning/branches/rel1cast2.1/subarchitectures/planner.sa/src/python/build_slice.sh
@@ -1,3 +1,3 @@
 #
-#slice2py ../slice/*.ice --output-dir cogx_ice -I /usr/local/share/ --all
-slice2py ../slice/Planner.ice  -I /usr/local/share/ -I /usr/local/share/cast/slice
+#slice2py ../slice/*.ice --output-dir cogx_ice -I /usr/local/share/ -I /opt/local/share --all
+slice2py ../slice/Planner.ice  -I /usr/local/share/ -I /usr/local/share/cast/slice -I /opt/local/share
