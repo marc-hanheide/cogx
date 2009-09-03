b29d8ecff8d464c46c210e422f032b433df21c69
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-03 12:02:15 +0000

    castctrl-ansi

--- code/vision/trunk/tools/castctrl/core/messages.py
+++ code/vision/trunk/tools/castctrl/core/messages.py
@@ -8,8 +8,8 @@ import itertools, heapq
 import legacy
 from collections import deque
 
-reColorEscape = re.compile("\x1b\\[\\d+m")
-reColorEscapeSplit = re.compile("(\x1b\\[\\d+m)")
+reColorEscape = re.compile("\x1b\\[[0-9;]+m")
+reColorEscapeSplit = re.compile("(\x1b\\[[0-9;]+m)")
 
 class CMessage(object):
     MESSAGE=0
@@ -35,47 +35,55 @@ class CMessage(object):
         return self.message
 
 class CAnsiPainter(object):
-    ansiColor = {
-            1:  ("<b>", "</b>"),
-            2:  ("<font color=grey>", "</font>"),
-            3:  ("<i>", "</i>"),
-            4:  ("<u>", "</u>"),
-            31: ("<font color=darkred>", "</font>"),
-            32: ("<font color=darkgreen>", "</font>"),
-            33: ("<font color=orangered>", "</font>"),
-            34: ("<font color=blue>", "</font>"),
-            35: ("<font color=magenta>", "</font>"),
-            36: ("<font color=darkcyan>", "</font>"),
-            37: ("<font color=gray>", "</font>"),
-            38: ("<font color=gray>", "</font>")
-        }
+    colorName = ["black", "darkred", "darkgreen", "orangered",
+                 "blue", "magenta", "darkcyan", "gray", "gray"]
+    backgrName = ["lightgray", "lightred", "lightgreen", "yellow",
+                  "lightblue", "lightpink", "lightcyan", "lightgray", "lightgray"]
+
     def __init__(self):
         pass
 
     def paint(self, msg):
         parts = reColorEscapeSplit.split(msg)
         intag = None
+        styles = {}
+        laststyle = None
         for i, p in enumerate(parts):
             if not p.startswith("\x1b["):
-                parts[i] = parts[i].replace("<", "&lt;")
+                parts[i] = parts[i].replace("&", "&amp;").replace("<", "&lt;")
+                continue
+
+            codelist = p[2:-1].split(";")
+            for code in codelist:
+                code = int(codelist[0])
+                if code >= 90 and code < 109: code -= 60
+                if code < 30:
+                    if code == 0: styles = {}
+                    elif code == 1: styles["font-weight"] = "bold"
+                    elif code == 2: styles.pop("font-weight", "")
+                    elif code == 3: styles["font-style"] = "italic"
+                    elif code == 4: styles["text-decoration"] = "underline"
+                    elif code == 21: styles["text-decoration"] = "underline"
+                    elif code == 22: styles.pop("font-weight", "")
+                    elif code == 23: styles.pop("font-style", "")
+                    elif code == 24: styles.pop("text-decoration", "")
+                elif code >= 30 and code < 39: styles["color"] = CAnsiPainter.colorName[code-30]
+                elif code >= 40 and code < 49: styles["background"] = CAnsiPainter.backgrName[code-40]
+
+            newstyle = ";".join(["%s:%s" % (k, v) for k,v in styles.iteritems() if len(v) > 0])
+            if newstyle == "": newstyle=None
+            if newstyle == laststyle:
+                if i == 0: parts[i] = "<span>"
+                else: parts[i] = ""
             else:
-                code = int(p[2:-1])
-                if code >= 40 and code < 50: code -= 10
-                elif code >= 90 and code < 100: code -= 60
-                elif code >= 100 and code < 109: code -= 70
-                elif code > 20 and code < 30: code -= 20
-                if not CAnsiPainter.ansiColor.has_key(code): newtag = None
-                else: newtag = CAnsiPainter.ansiColor[code]
-                if newtag == None:
-                    if intag != None: parts[i] = intag[1] # close tag
-                    else: parts[i] = ""
-                else:
-                    if intag == None: parts[i] = newtag[0]
-                    elif intag[0] != newtag[0]: parts[i] = intag[1] + newtag[0]
-                    else: parts[i] = ""
-                intag = newtag
-        if intag != None: parts.append(intag[1])
-        msg = "".join(parts) + " "
+                if newstyle == None: stag = ""
+                else: stag = "<span style='%s'>" % newstyle
+                if laststyle == None: parts[i] = stag
+                else: parts[i] = "</span>" + stag
+                laststyle = newstyle
+        if laststyle != None: tail = "</span>"
+        else: tail = ""
+        msg = "".join(parts) + tail
         return msg
 
 class CMessageSource(object):
