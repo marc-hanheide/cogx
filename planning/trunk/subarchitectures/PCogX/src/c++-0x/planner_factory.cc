a5532ab9040838c4e0c2a65ed4ae9631af94f96e
grettonc <grettonc@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-03 12:41:19 +0000

    Added automatically generated stuff to SVN. Oops...

--- planning/trunk/subarchitectures/PCogX/src/c++-0x/planner_factory.cc
+++ planning/trunk/subarchitectures/PCogX/src/c++-0x/planner_factory.cc
@@ -22,15 +22,31 @@ Planner_Factory::Planner_Factory(Designator&& name)
 }
 
 void Planner_Factory::implement__obtainPlanner(PCogX::obtainPlannerPtr& input){
-    input->identityOfCreatedPlannerIsAReturn = "";
+
+    Designator new_designation;
+
+    decltype(random_number_generator()) candidate_designation;
+    
+    while(designators_already_in_use.end() !=
+              designators_already_in_use
+          .find(candidate_designation = random_number_generator())){
+    }
+        
+      
+    
+    
+    ostringstream oss;
+    oss<<candidate_designation;
+
+    auto designation_of_chosen_implementation
+        = {std::move(std::string(CLASSICAL_PLANNER_DESIGNATION))};
+    call<distinctPlanner>(candidate_designation.str(),
+                          designation_of_chosen_implementation);
 }
 
 
 void Planner_Factory::start() {
-    
-    implement<PCogX::obtainPlanner>(&Planner_Factory::implement__obtainPlanner);
-    implement<PCogX::distinctPlanner>(&Planner_Factory::implement__distinctPlanner);
-    
+    implement<PCogX::obtainPlanner>(&Planner_Factory::implement__obtainPlanner);   
 }
 
 void Planner_Factory::runComponent()
