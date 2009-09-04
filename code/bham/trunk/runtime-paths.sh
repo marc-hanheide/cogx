0b5ca795da9556b8d3e531ae0ddb9f34c52c1777
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-04 16:06:39 +0000

    source this for convenience

---
+++ code/bham/trunk/runtime-paths.sh
@@ -0,0 +1,4 @@
+export PYTHONPATH=output/python:${PYTHONPATH}
+export CLASSPATH=output/classes:${CLASSPATH}
+export DYLD_LIBRARY_PATH=output/lib:${DYLD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=output/lib:${LD_LIBRARY_PATH}
