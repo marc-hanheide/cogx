62df856e6ac7729194309f049d9c53491ab7f2db
plison <plison@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-06 17:20:17 +0000

    changes

--- code/binding/development/local-cast-server.sh
+++ code/binding/development/local-cast-server.sh
@@ -1,5 +1,5 @@
 
-export CLASSPATH=/opt/Ice-3.3.1/lib/Ice.jar:/opt/Ice-3.3.1/lib/ant-ice.jar:lib/cast.jar:output/classes
+export CLASSPATH=/opt/Ice-3.3.1/lib/Ice.jar:/opt/Ice-3.3.1/lib/ant-ice.jar:/usr/local/share/java/cast.jar:output/classes
 export DYLD_LIBRARY_PATH=/opt/Ice-3.3.1/lib:output/lib
 export PYTHONPATH=/opt/Ice-3.3.1/python
 cast-server
