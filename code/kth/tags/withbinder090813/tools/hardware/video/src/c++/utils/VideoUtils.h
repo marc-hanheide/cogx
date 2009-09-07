c184058e54df86b9d998e246a493b4eb6f607274
krsj <krsj@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-07 09:30:53 +0000

    Tag of spatial SA with some binding output working

---
+++ code/kth/tags/withbinder090813/tools/hardware/video/src/c++/utils/VideoUtils.h
@@ -0,0 +1,14 @@
+
+/**
+ * @author Michael Zillich
+ * @date February 2009
+ */
+
+#ifndef VIDEO_UTILS_H
+#define VIDEO_UTILS_H
+
+#include "CameraParameters.h"
+#include "ConvertImage.h"
+#include "AccessImage.h"
+
+#endif
