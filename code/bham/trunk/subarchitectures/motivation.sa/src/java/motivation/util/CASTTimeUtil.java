6e2a070675f4598b40d10089adf410fb0ef7472b
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-11 14:25:42 +0000

    added

---
+++ code/bham/trunk/subarchitectures/motivation.sa/src/java/motivation/util/CASTTimeUtil.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package motivation.util;
+
+import cast.cdl.CASTTime;
+
+/**
+ * @author marc
+ * 
+ */
+final public class CASTTimeUtil {
+	public static long diff(CASTTime t1, CASTTime t2) {
+		return (t1.s - t2.s) * 1000 + (t1.us - t2.us) / 1000;
+
+	}
+}
