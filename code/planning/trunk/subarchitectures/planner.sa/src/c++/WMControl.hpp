11851495073893ddc21def1e1658d0c4b3ac682c
goebelbe <goebelbe@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-11 14:57:29 +0000

    New and improved Planner<->WMControl interface Cleaned up Planning.ice data structures some bugfixes

--- code/planning/trunk/subarchitectures/planner.sa/src/c++/WMControl.hpp
+++ code/planning/trunk/subarchitectures/planner.sa/src/c++/WMControl.hpp
@@ -6,30 +6,45 @@
 #include "Planner.hpp"
 #include <map>
 
+using namespace autogen::Planner;
+
 class WMControl : public cast::ManagedComponent {
 public:
     virtual ~WMControl() {}
 
 protected:
-    autogen::Planner::PythonServerPrx pyServer;
+    PythonServerPrx pyServer;
     std::map<int,cast::cdl::WorkingMemoryChange> activeTasks;
 
+    virtual void configure(const cast::cdl::StringMap& _config, const Ice::Current& _current);
     virtual void start();
     virtual void runComponent();
 
     void connectToPythonServer();
-    void generateInitialState(autogen::Planner::PlanningTaskPtr& task);
-    void deliverPlan(const autogen::Planner::PlanningTaskPtr& task);
-    void receivePlannerCommands(const cast::cdl::WorkingMemoryChange& wmc);
+    void generateInitialState(PlanningTaskPtr& task);
+    //void deliverPlan(const autogen::Planner::PlanningTaskPtr& task);
+    void deliverPlan(int id, const ActionSeq& plan);
+    void updateStatus(int id, Completion status);
+    void setChangeFilter(int id, const StateChangeFilterPtr& filter);
 
-    class InternalCppServer : public autogen::Planner::CppServer {
+    void receivePlannerCommands(const cast::cdl::WorkingMemoryChange& wmc);
+    void actionChanged(const cast::cdl::WorkingMemoryChange& wmc);
+
+    class InternalCppServer : public CppServer {
     public:
-	InternalCppServer(WMControl* Parent);
-	virtual void deliverPlan(const autogen::Planner::PlanningTaskPtr& task, const Ice::Current&);
+        InternalCppServer(WMControl* Parent);
+        //virtual void deliverPlan(const PlanningTaskPtr& task, const Ice::Current&);
+        virtual void deliverPlan(int id, const ActionSeq& plan, const Ice::Current&);
+        virtual void updateStatus(int id, Completion status, const Ice::Current&);
+        virtual void setChangeFilter(int id, const StateChangeFilterPtr& filter, const Ice::Current&);
 
     protected:
-	WMControl* parent;
+        WMControl* parent;
     };
+
+private:
+    std::string m_python_server;
+
 };
 
 #endif
