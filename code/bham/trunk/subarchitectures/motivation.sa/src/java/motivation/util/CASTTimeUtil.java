e85403aa731c7ffdee82016880add6cd9918231b
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-13 20:49:13 +0000

    starting on execution manager

--- code/bham/trunk/subarchitectures/motivation.sa/src/java/motivation/util/CASTTimeUtil.java
+++ code/bham/trunk/subarchitectures/motivation.sa/src/java/motivation/util/CASTTimeUtil.java
@@ -4,6 +4,7 @@
 package motivation.util;
 
 import cast.cdl.CASTTime;
+import cast.core.CASTUtils;
 
 /**
  * @author marc
@@ -12,6 +13,5 @@ import cast.cdl.CASTTime;
 final public class CASTTimeUtil {
 	public static long diff(CASTTime t1, CASTTime t2) {
 		return (t1.s - t2.s) * 1000 + (t1.us - t2.us) / 1000;
-
 	}
 }
