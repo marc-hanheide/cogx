c40b8d5a0d29af93f734b1403bf1e1c296f5326a
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-18 16:04:05 +0000

    basic rules for combining beliefs and stf's

--- code/comsys/branches/comsys.mk5/subarchitectures/comsys/abduction/ctx_modality.m
+++ code/comsys/branches/comsys.mk5/subarchitectures/comsys/abduction/ctx_modality.m
@@ -72,7 +72,13 @@ stf_compose(now, now) = now.
 
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
 
+:- func belief_compose(belief::in, belief::in) = (belief::out) is semidet.
+
+belief_compose(private(A), private(B)) = attrib(A, B) :- A \= B.
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
+
 :- func ctx_modality_compose(ctx_modality::in, ctx_modality::in) = (ctx_modality::out) is semidet.
 
-ctx_modality_compose(_, _) = _ :-
-	fail.
+ctx_modality_compose(k(STF1, Bel1), k(STF2, Bel2)) = k(stf_compose(STF1, STF2), belief_compose(Bel1, Bel2)).
+ctx_modality_compose(t(STF1, Bel1), t(STF2, Bel2)) = t(stf_compose(STF1, STF2), belief_compose(Bel1, Bel2)).
