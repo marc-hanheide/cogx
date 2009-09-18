1732d5f089af3cfe8fad1cb403c5a198fa321bf5
plison <plison@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-18 08:21:56 +0000

    release 4

---
+++ binding/tags/release4/bin/test2.sh
@@ -0,0 +1,3 @@
+export CLASSPATH=/opt/Ice-3.3.1/lib/Ice.jar:/opt/Ice-3.3.1/lib/ant-Ice.jar:/usr/local/share/cast.jar:output/classes
+export DYLD_LIBRARY_PATH=/opt/Ice-3.3.1/lib
+cast-client subarchitectures/binder/config/testsuite/binder_test2.cast
---
+++ binding/tags/release4/subarchitectures/binder/src/java/binder/fakeproxygenerators/AbstractPhantomProxyGenerator.java
@@ -0,0 +1,63 @@
+package binder.fakeproxygenerators;
+
+import java.util.Map;
+import java.util.Random;
+
+import binder.abstr.AbstractBindingPredictor;
+import binder.autogen.core.Union;
+import binder.autogen.specialentities.PhantomProxy;
+
+abstract public class AbstractPhantomProxyGenerator extends AbstractBindingPredictor{
+
+	protected int nbOfProxiesToCreate = 0;
+	protected boolean reverted = false;
+
+	protected boolean pauses = false;
+
+	protected boolean deleteProxiesAfterBinding = true;
+	
+	
+	@Override
+	public void configure(Map<String, String> _config) {
+
+		if (_config.containsKey("--nbproxies")) {
+			nbOfProxiesToCreate = Integer.parseInt(_config.get("--nbproxies"));
+		} 
+		if (_config.containsKey("--reverted")) {
+			reverted = Boolean.parseBoolean(_config.get("--reverted"));
+		} 
+		if (_config.containsKey("--pauses")) {
+			pauses = Boolean.parseBoolean(_config.get("--pauses"));
+		} 
+		if (_config.containsKey("--deleteproxies")) {
+			deleteProxiesAfterBinding = Boolean.parseBoolean(_config.get("--deleteproxies"));
+		}
+	}
+	
+	
+
+	protected void randomPrediction() {
+
+		Random rand = new Random();
+		if (nbOfProxiesToCreate > 0) {
+			for (int i = 1 ; i < (nbOfProxiesToCreate +1) ; i++) {
+				if (pauses) {
+				sleepComponent(500 + rand.nextInt(2000));
+				}
+				PhantomProxy p;
+				if (!reverted) {
+					p = createPhantomProxy(i);
+				}
+				else {
+					p = createPhantomProxy(nbOfProxiesToCreate-i+1);
+				}
+				Union u = getBestPredictedUnion(p, deleteProxiesAfterBinding);
+				log("PREDICTED UNION: " + u.entityID);
+
+			}	
+		}
+	}
+	
+	abstract public PhantomProxy createPhantomProxy(int nb);
+	
+}
