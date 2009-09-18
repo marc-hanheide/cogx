a18159ec13ca48cf0c923e67f4472ba8f0740d39
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-18 18:42:57 +0000

    generalising i/o of logical forms

--- code/comsys/branches/comsys.mk5/subarchitectures/comsys/abduction/lf_io.m
+++ code/comsys/branches/comsys.mk5/subarchitectures/comsys/abduction/lf_io.m
@@ -4,18 +4,19 @@
 
 :- import_module lf.
 :- import_module string, formula.
+:- import_module stringable.
 
-:- func ground_term_to_id(ground_term::in) = (id(string, string)::out) is semidet.
-:- func ground_term_to_lf(ground_term::in) = (lf::out) is semidet.
+:- func ground_term_to_id(ground_term::in) = (id(I, S)::out) is semidet <= (parsable(I), parsable(S)).
+:- func ground_term_to_lf(ground_term::in) = (lf(I, S, R)::out) is semidet <= (parsable(I), parsable(S), parsable(R)).
 
-:- func ground_atomic_formula_to_lf(ground_atomic_formula::in) = (lf::out) is semidet.
-:- func det_ground_atomic_formula_to_lf(ground_atomic_formula) = lf.
+:- func ground_atomic_formula_to_lf(ground_atomic_formula::in) = (lf(I, S, R)::out) is semidet <= (parsable(I), parsable(S), parsable(R)).
+:- func det_ground_atomic_formula_to_lf(ground_atomic_formula) = lf(I, S, R) <= (parsable(I), parsable(S), parsable(R)).
 
-:- func lf_to_string(lf) = string.
+:- func lf_to_string(lf(I, S, R)) = string <= (stringable(I), stringable(S), stringable(R)).
 
-:- func id_to_ground_term(id(string, string)) = ground_term.
-:- func lf_to_ground_term(lf) = ground_term.
-:- func lf_to_ground_atomic_formula(lf) = ground_atomic_formula.
+:- func id_to_ground_term(id(I, S)) = ground_term <= (stringable(I), stringable(S)).
+:- func lf_to_ground_term(lf(I, S, R)) = ground_term <= (stringable(I), stringable(S), stringable(R)).
+:- func lf_to_ground_atomic_formula(lf(I, S, R)) = ground_atomic_formula <= (stringable(I), stringable(S), stringable(R)).
 
 %------------------------------------------------------------------------------%
 
@@ -25,7 +26,7 @@
 
 %------------------------------------------------------------------------------%
 
-ground_term_to_id(t("::", [t(Name, []), t(Sort, [])])) = of_sort(Name, Sort).
+ground_term_to_id(t("::", [t(Name, []), t(Sort, [])])) = of_sort(from_string(Name), from_string(Sort)).
 
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
 
@@ -35,7 +36,7 @@ ground_term_to_lf(t("@", [NameSortTerm, LFTerm])) =
 ground_term_to_lf(T) = i(ground_term_to_id(T)) :-
 	T = t("::", [_,_]).
 
-ground_term_to_lf(t(RelName, [LFTerm])) = r(RelName, ground_term_to_lf(LFTerm)).
+ground_term_to_lf(t(RelName, [LFTerm])) = r(from_string(RelName), ground_term_to_lf(LFTerm)).
 
 ground_term_to_lf(t(Prop, [])) = p(Prop).
 
@@ -53,25 +54,25 @@ det_ground_atomic_formula_to_lf(GF) = LF :-
 
 %------------------------------------------------------------------------------%
 
-:- func id_to_string(id(string, string)) = string.
+:- func id_to_string(id(I, S)) = string <= (stringable(I), stringable(S)).
 
-id_to_string(of_sort(I, S)) = I ++ ":" ++ S.
+id_to_string(of_sort(I, S)) = to_string(I) ++ ":" ++ to_string(S).
 
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
 
 lf_to_string(at(Id, LF)) = "@" ++ id_to_string(Id) ++ "(" ++ lf_to_string(LF) ++ ")".
 lf_to_string(i(Id)) = id_to_string(Id).
-lf_to_string(r(Rel, LF)) = "<" ++ Rel ++ ">" ++ "(" ++ lf_to_string(LF) ++ ")".
+lf_to_string(r(Rel, LF)) = "<" ++ to_string(Rel) ++ ">" ++ "(" ++ lf_to_string(LF) ++ ")".
 lf_to_string(p(Prop)) = Prop.
 lf_to_string(and(LF1, LF2)) = lf_to_string(LF1) ++ " ^ " ++ lf_to_string(LF2).
 
 %------------------------------------------------------------------------------%
 
-id_to_ground_term(of_sort(Idx, Sort)) = t("::", [t(Idx, []), t(Sort, [])]).
+id_to_ground_term(of_sort(Idx, Sort)) = t("::", [t(to_string(Idx), []), t(to_string(Sort), [])]).
 
 lf_to_ground_term(at(Id, LF)) = t("@", [id_to_ground_term(Id), lf_to_ground_term(LF)]).
 lf_to_ground_term(i(Id)) = id_to_ground_term(Id).
-lf_to_ground_term(r(Rel, LF)) = t(Rel, [lf_to_ground_term(LF)]).
+lf_to_ground_term(r(Rel, LF)) = t(to_string(Rel), [lf_to_ground_term(LF)]).
 lf_to_ground_term(p(Prop)) = t(Prop, []).
 lf_to_ground_term(and(LF1, LF2)) = t("^", [lf_to_ground_term(LF1), lf_to_ground_term(LF2)]).
 
