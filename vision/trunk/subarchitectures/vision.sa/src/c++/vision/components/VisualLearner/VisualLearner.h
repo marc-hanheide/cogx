99370d27040d438776010e9aa2063ec87672670f
alen <alen@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-18 17:41:42 +0000

    Added matlab code.

---
+++ vision/trunk/subarchitectures/vision.sa/src/c++/vision/components/VisualLearner/VisualLearner.h
@@ -0,0 +1,55 @@
+#ifndef __cast_FEATURE_EXTRACTOR_H__
+#define __cast_FEATURE_EXTRACTOR_H__
+
+#include <cast/architecture/ManagedComponent.hpp>
+#include <VisionData.hpp>
+
+#include <vector>
+#include <map>
+
+//default useful namespaces, fix to reflect your own code
+using namespace std;
+using namespace VisionData; // ROI
+using namespace cast;
+using namespace boost;
+using namespace Matlab; // Matrix
+
+// typedef map<string, shared_ptr<const CASTData<VisionData::SOI> > > SOIMap;
+typedef vector<shared_ptr<const CASTData<VisionData::SOI> > > SOIVector;
+
+namespace cast
+{
+
+class FeatureExtractor : public ManagedComponent {
+	public:
+		VisualLearner();
+		virtual ~VisualLearner();
+
+		virtual void runComponent();
+		virtual void configure(map<string,string> & _config);
+		virtual void start();
+                virtual void stop();
+
+	protected:
+		virtual void taskAdopted(const string &_taskID);
+		virtual void taskRejected(const string &_taskID);
+
+		void recogniseAttributes(ProtoObjectPtr _pData); //shared_ptr<const CASTData<SOI> > _pData);
+
+	private:
+
+		void newSOI(const cdl::WorkingMemoryChange & _wmc);
+//                string allowedSoiSource;
+//		void quip(CASTData<ComedyEssentials::Joke> *_pData);
+//		string generatePunchline(const string &_setup);
+
+		// Hashtable used to record the tasks we want to carry out
+//		SOIMap * m_pProposedProcessing;
+		SOIVector * m_pSOIs;
+//                float m_PerspectiveTrafo[9];
+//                bool m_bTrafoEnabled;
+}; // class FeatureExtractor
+
+}
+
+#endif
---
+++ vision/trunk/subarchitectures/vision.sa/src/matlab/Proxy/initlib.h
@@ -0,0 +1,3 @@
+
+extern "C" void InitFeatureLearningLib();
+extern "C" void TermFeatureLearningLib();
