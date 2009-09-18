d1650139556f3cdea3230ddc47c23300244a40fe
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-18 18:37:45 +0000

    testing simplified models

--- code/comsys/branches/comsys.mk5/subarchitectures/comsys/abduction/test_lf.m
+++ code/comsys/branches/comsys.mk5/subarchitectures/comsys/abduction/test_lf.m
@@ -10,34 +10,112 @@
 
 :- implementation.
 
-:- import_module solutions.
+:- import_module solutions, require.
 :- import_module string, map, set, list, pair.
-:- import_module lf, lf_io, formula.
+:- import_module lf, lf_io, formula, term_io, parser, formula_ops.
 :- import_module world_model.
 
 main(!IO) :-
-	test_lf( r("colour", p("red")), world_model.init, _, !IO),
-	test_lf( r("colour", i(of_sort("v", "thing"))), world_model.init, _, !IO),
-	test_lf( r("colour", and(r("tint", p("weird")), p("red"))), world_model.init, _, !IO),
-	test_lf( at(of_sort("v1", "object"), i(of_sort("v1", "object"))), world_model.init, _, !IO),
-	test_lf( at(of_sort("v1", "object"), i(of_sort("v2", "object"))), world_model.init, _, !IO),
+	io.command_line_arguments(CmdArgs, !IO),
+	(if
+		CmdArgs = [FileName]
+	then
+		read_file_as_lines(FileName, Strs0, !IO),
+		preprocess_file(Strs0, Strs),
 
-	test_lf( at(of_sort("v1", "object"), p("box")), world_model.init, _, !IO),
+		some [!WM] (
+			!:WM = world_model.init,
 
-	test_lf( at(of_sort("v1", "object"), and(p("box"), r("colour", and(i(of_sort("c", "colour")), p("something"))))), world_model.init, _, !IO),
+			list.foldl2((pred(Line::in, !.WM::in, !:WM::out, !.IO::di, !:IO::uo) is det :-
+				(if Line = "CLEAR"
+				then
+					print("-------------------------------------------------------------\n", !IO),
+					!:WM = world_model.init
+				else
+					(if Line = "PRINT"
+					then
+						print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n", !IO),
+						print_wm(!.WM, !IO)
+					else
+						(if Line = "SIMPLIFY"
+						then
+							print("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n", !IO),
+							print_simplified_models(!.WM, !IO)
+						else
+							LF = s2lf(Line),
+							print(lf_to_string(LF), !IO),
+							print(" ... ", !IO),
+							(if add_lf(LF, !WM)
+							then print("ok (" ++ from_int(count(simplified_models(!.WM))) ++")\n", !IO)
+							else print("FAIL, ignoring\n", !IO)
+							)
+						)
+					)
+				)
+					), Strs, !WM, !IO)
+		)
+	else
+		io.progname("?", ProgName, !IO),
+		format(stderr_stream, "Usage: %s TEST_FILE\n", [s(ProgName)], !IO)
+	).
 
-	test_lf( r("colour", and(r("tint", p("weird")), p("red"))), world_model.init, WM1, !IO),
-	test_lf( r("colour", i(of_sort("c", "colour"))), WM1, WM3, !IO),
-%	test_lf( r("colour", i(of_sort("d", "colour"))), WM2, WM3, !IO),  % this should make it inconsistent
-	test_lf( at(of_sort("c", "colour"), r("tint", i(of_sort("t", "tint")))), WM3, WM4, !IO),
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
 
-	print_simplified_models(WM4, !IO).
+:- pred read_file_as_lines(string::in, list(string)::out, io::di, io::uo) is det.
+
+read_file_as_lines(FileName, Lines, !IO) :-
+	see(FileName, SeeResult, !IO),
+	(
+		SeeResult = ok,
+		read_file_as_string(ReadResult, !IO),
+		(
+			ReadResult = ok(S),
+			Lines = string.words_separator((pred(C::in) is semidet :- C = '\n'), S)
+		;
+			ReadResult = error(_, _),
+			Lines = []
+		),
+		seen(!IO)
+	;
+		SeeResult = error(_),
+		Lines = []
+	).
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
+
+:- pred preprocess_file(list(string)::in, list(string)::out) is det.
+
+preprocess_file(LIn, LOut) :-
+	list.filter_map((pred(L0::in, L::out) is semidet :-
+		L1 = string.strip(L0),
+		L = L1,
+		not string.first_char(L, '#', _)
+			), LIn, LOut).
+
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
+
+:- func s2lf(string) = lf.
+
+s2lf(S) = LF :-
+	read_term_from_string_with_op_table(init_wabd_op_table, "", S, _, ReadResult),
+	(if ReadResult = term(_, T)
+	then LF = det_ground_atomic_formula_to_lf(det_formula_to_ground_formula(det_term_to_atomic_formula(T)))
+	else error("parsing error in s2lf in \"" ++ S ++ "\"")
+	).
 
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
 
 :- pred std_lf(lf::in) is det.
 
 std_lf(_).
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
+
+:- pred test_slf(string::in, world_model::in, world_model::out, io::di, io::uo) is det.
+
+test_slf(S, !WM, !IO) :-
+	test_lf(s2lf(S), !WM, !IO).
 
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
 
@@ -69,10 +147,9 @@ test_add_lf(LF, !WM, !IO) :-
 :- pred print_wm(world_model::in, io::di, io::uo) is det.
 
 print_wm(WM, !IO) :-
-	print("names:\n", !IO),
+	print("names:", !IO),
 	NamesStrs = list.map((func(Name-Sort) = S :- S = Name ++ ":" ++ Sort), map.to_assoc_list(WM^names)),
-	print("  " ++ string.join_list("\n  ", NamesStrs) ++ "\n", !IO),
-	nl(!IO),
+	print(string.join_list("\n  ", [""|NamesStrs]) ++ "\n\n", !IO),
 
 	print("reachability:\n", !IO),
 	set.fold((pred({Rel, Id1, Id2}::in, !.IO::di, !:IO::uo) is det :-
@@ -90,18 +167,25 @@ print_wm(WM, !IO) :-
 
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
 
+:- func simplified_models(world_model) = set(world_model).
+
+simplified_models(WM) = SWMs :-
+	SWMs = solutions_set((pred(SWM::out) is nondet :-
+		simplify_pred(WM, SWM)
+			)).
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
+
 :- pred print_simplified_models(world_model::in, io::di, io::uo) is det.
 
 print_simplified_models(WM, !IO) :-
-	SWMs = solutions((pred(SWM::out) is nondet :-
-		simplify_pred(WM, SWM)
-			)),
-
-	print("Simplified models: [\n", !IO),
-
-	list.foldl((pred(SWM::in, !.IO::di, !:IO::uo) is det :-
-		print_wm(SWM, !IO),
-		print("- - - - - - - - - - - - - -\n", !IO)
-			), SWMs, !IO),
-
-	print("].\n", !IO).
+	SWMs = set.to_sorted_list(simplified_models(WM)),
+	(if SWMs = []
+	then
+		print("no simplified models.\n", !IO)
+	else
+		list.foldl((pred(SWM::in, !.IO::di, !:IO::uo) is det :-
+			print_wm(SWM, !IO),
+			print("  = = = = = = = = = = = = = =\n", !IO)
+				), SWMs, !IO)
+	).
