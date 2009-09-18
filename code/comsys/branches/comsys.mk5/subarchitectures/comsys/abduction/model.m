7dcfc8ccb9e8615b11566a03ccd86dd8ba88a478
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-18 18:56:27 +0000

    adding support for "functional" accessibility relations

--- code/comsys/branches/comsys.mk5/subarchitectures/comsys/abduction/model.m
+++ code/comsys/branches/comsys.mk5/subarchitectures/comsys/abduction/model.m
@@ -22,7 +22,7 @@
 :- type model(Index, Sort, Rel).
 
 :- func worlds(model(I, S, R)) = map(I, S).
-:- func reach(model(I, S, R)) = set({R, world_id(I), world_id(I)}).
+:- func access(model(I, S, R)) = set({R, world_id(I), world_id(I)}).
 :- func props(model(I, S, R)) = map(world_id(I), set(proposition)).
 
 :- func unnamed_world_indices(model(I, S, R)) = set(int).
@@ -43,8 +43,9 @@
 		<= (isa_ontology(OS, S), accessibility(RT, R)).
 :- mode add_lf(in, in, in, in, out) is semidet.
 
-:- pred union(OS, model(I, S, R), model(I, S, R), model(I, S, R)) <= isa_ontology(OS, S).
-:- mode union(in, in, in, out) is semidet.
+:- pred union(OS, RT, model(I, S, R), model(I, S, R), model(I, S, R))
+		<= (isa_ontology(OS, S), accessibility(RT, R)).
+:- mode union(in, in, in, in, out) is semidet.
 
 	% satisfies(M, LF)
 	% True iff
@@ -87,7 +88,7 @@
 :- type model(Index, Sort, Rel)
 	--->	wm(
 		worlds :: map(Index, Sort),  % sort
-		reach :: set({Rel, world_id(Index), world_id(Index)}),  % accessibility
+		access :: set({Rel, world_id(Index), world_id(Index)}),  % accessibility
 		props :: map(world_id(Index), set(proposition)),  % valid propositions
 		next_unnamed :: int  % lowest unused index for unnamed worlds
 	).
@@ -102,8 +103,8 @@ init = wm(map.init, set.init, map.init, 0).
 
 unnamed_world_indices(M) = Ints :-
 	Ints = solutions_set((pred(Int::out) is nondet :-
-		( member({_, u(Int), _}, M^reach)
-		; member({_, _, u(Int)}, M^reach)
+		( member({_, u(Int), _}, M^access)
+		; member({_, _, u(Int)}, M^access)
 		; member(u(Int), keys(M^props))
 		)
 			)).
@@ -118,16 +119,37 @@ new_unnamed_world(Id, WM0, WM) :-
 
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
 
-:- pred rename_merge_world(OS::in, world_id(I)::in, world_id(I)::in, model(I, S, R)::in,
+:- pred well_formed(OS::in, RT::in, model(I, S, R)::in) is semidet
+		<= (isa_ontology(OS, S), accessibility(RT, R)).
+
+well_formed(Ont, RT, M) :-
+	UsedRels = set.to_sorted_list(solutions_set((pred(Rel::out) is nondet :-
+		set.member({Rel, _, _}, M^access)
+			))),
+	list.all_true((pred(Rel::in) is semidet :-
+		(if exclusive(RT, Rel)
+		then
+			Arcs = solutions_set((pred(W1-W2::out) is nondet :-
+				set.member({Rel, W1, W2}, M^access)
+					)),
+			singleton_set(Arcs, _)
+		else
+			true
+		)
+			), UsedRels).
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
+
+:- pred rename_merge_world(OS::in, int::in, world_id(I)::in, model(I, S, R)::in,
 		model(I, S, R)::out) is det <= isa_ontology(OS, S).
 
 rename_merge_world(Ont, Old, New, WM0, WM) :-
 	Reach = set.map((func({Rel, OldIdA, OldIdB}) = {Rel, NewIdA, NewIdB} :-
-		(OldIdA = Old -> NewIdA = New ; NewIdA = OldIdA),
-		(OldIdB = Old -> NewIdB = New ; NewIdB = OldIdB)
-			), WM0^reach),
+		(OldIdA = u(Old) -> NewIdA = New ; NewIdA = OldIdA),
+		(OldIdB = u(Old) -> NewIdB = New ; NewIdB = OldIdB)
+			), WM0^access),
 
-	(if map.search(WM0^props, Old, OldsProps0)
+	(if map.search(WM0^props, u(Old), OldsProps0)
 	then OldsProps = OldsProps0
 	else OldsProps = set.init
 	),
@@ -135,7 +157,7 @@ rename_merge_world(Ont, Old, New, WM0, WM) :-
 	then NewsProps = set.union(NewsProps0, OldsProps)
 	else NewsProps = OldsProps
 	),
-	DelProps = map.delete(WM0^props, Old),
+	DelProps = map.delete(WM0^props, u(Old)),
 	(if NewsProps = set.init
 	then Props = DelProps
 	else Props = map.set(DelProps, New, NewsProps)
@@ -143,10 +165,30 @@ rename_merge_world(Ont, Old, New, WM0, WM) :-
 
 	WM = wm(WM0^worlds, Reach, Props, WM0^next_unnamed).
 
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
+
+:- pred enforce_access_exclusivity(OS::in, RT::in, model(I, S, R)::in, model(I, S, R)::out)
+		is semidet <= (isa_ontology(OS, S), accessibility(RT, R)).
+
+enforce_access_exclusivity(Ont, RT, M0, M) :-
+	ExclRels = set.to_sorted_list(set.filter((pred({Rel, _, _}::in) is semidet :-
+		exclusive(RT, Rel)
+			), M0^access)),
+	first_mergable_worlds(ExclRels, Res),
+	(
+	 	Res = merge(W2, W3Num),
+		rename_merge_world(Ont, W3Num, W2, M0, M1),
+		enforce_access_exclusivity(Ont, RT, M1, M)
+	;
+		Res = none,
+		M = M0
+	).
+
 %------------------------------------------------------------------------------%
 
-add_lf(Ont, RT, !.WM, LF, !:WM) :-
-	add_lf0(Ont, RT, initial, _, LF, !WM).
+add_lf(Ont, RT, !.M, LF, !:M) :-
+	add_lf0(Ont, RT, initial, _, LF, !M),
+	enforce_access_exclusivity(Ont, RT, !M).
 
 % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
 
@@ -183,13 +225,34 @@ add_lf0(Ont, RT, Cur, i(WName), i(of_sort(WName, Sort)), WM0, WM) :-
 		else NewSort = Sort
 		),
 		WM1 = WM0^worlds := map.set(WM0^worlds, WName, NewSort),
-		rename_merge_world(Ont, u(Num), i(WName), WM1, WM)
+		rename_merge_world(Ont, Num, i(WName), WM1, WM)
 	).
 
 add_lf0(Ont, RT, Cur, Cur, r(Rel, LF), WM0, WM) :-
-	new_unnamed_world(Num, WM0, WM1),
-	WM2 = WM1^reach := set.insert(WM0^reach, {Rel, Cur, u(Num)}),
-	add_lf0(Ont, RT, u(Num), _, LF, WM2, WM).
+	(if
+		exclusive(RT, Rel)
+	then
+		Accessible = solutions((pred(W::out) is nondet :-
+			set.member({Rel, Cur, W}, WM0^access)
+				)),
+		(
+			Accessible = [],
+			new_unnamed_world(Num, WM0, WM1),
+			WM2 = WM1^access := set.insert(WM0^access, {Rel, Cur, u(Num)}),
+			add_lf0(Ont, RT, u(Num), _, LF, WM2, WM)
+		;
+			Accessible = [W],
+			add_lf0(Ont, RT, W, _, LF, WM0, WM)
+		;
+			% we're already inconsistent!
+			Accessible = [_|_],
+			fail
+		)
+	else
+		new_unnamed_world(Num, WM0, WM1),
+		WM2 = WM1^access := set.insert(WM0^access, {Rel, Cur, u(Num)}),
+		add_lf0(Ont, RT, u(Num), _, LF, WM2, WM)
+	).
 
 add_lf0(Ont, RT, Cur, Cur, p(Prop), WM0, WM) :-
 	(if OldProps = map.search(WM0^props, Cur)
@@ -205,12 +268,12 @@ add_lf0(Ont, RT, Cur0, Cur, and(LF1, LF2), WM0, WM) :-
 %------------------------------------------------------------------------------%
 
 	% merge M2 into M1
-union(Ont, M1, M2, M) :-
+union(Ont, RT, M1, M2, M) :-
 	ListUM2 = to_sorted_list(unnamed_world_indices(M2)),
 
 		% rename unnamed worlds so that we don't have any name clashes
-	list.foldr((pred(Int::in, Mx0::in, Mx::out) is det :-
-		rename_merge_world(Ont, u(Int), u(Int + M1^next_unnamed), Mx0, Mx)
+	list.foldr((pred(Int::in, Mx0::in, Mx::out) is semidet :-
+		rename_merge_world(Ont, Int, u(Int + M1^next_unnamed), Mx0, Mx)
 			), ListUM2, M2, M2R),
 
 		% increase the generator counter correspondingly
@@ -222,14 +285,15 @@ union(Ont, M1, M2, M) :-
 			), M1^worlds, M2R^worlds, Worlds),
 
 		% merge accessibility relations
-	set.union(M1^reach, M2R^reach, Reach),
+	set.union(M1^access, M2R^access, Reach),
 
 		% merge propositions
 	map_merge_op((pred(Ps1::in, Ps2::in, Ps::out) is det :-
 		set.union(Ps1, Ps2, Ps)
 			), M1^props, M2R^props, Props),
 
-	M = wm(Worlds, Reach, Props, NextUnnamed).
+	M3 = wm(Worlds, Reach, Props, NextUnnamed),
+	enforce_access_exclusivity(Ont, RT, M3, M).
 
 %------------------------------------------------------------------------------%
 
@@ -248,7 +312,7 @@ satisfies0(Ont, i(Idx), M, i(of_sort(Idx, LFSort))) :-
 	isa(Ont, Sort, LFSort).
 
 satisfies0(Ont, WCur, M, r(Rel, LF)) :-
-	set.member({Rel, WCur, WReach}, M^reach),
+	set.member({Rel, WCur, WReach}, M^access),
 	satisfies0(Ont, WReach, M, LF).
 
 satisfies0(Ont, WCur, M, p(Prop)) :-
@@ -261,15 +325,15 @@ satisfies0(Ont, WCur, M, and(LF1, LF2)) :-
 %------------------------------------------------------------------------------%
 	
 :- type mergable_result(I)
-	--->	merge(world_id(I), world_id(I))  % merge 2nd into 1st
+	--->	merge(world_id(I), int)  % merge 2nd into 1st
 	;	none
 	.
 
 reduced(Ont, WM) = RWM :-
-	first_mergable_worlds(set.to_sorted_list(WM^reach), Res),
+	first_mergable_worlds(set.to_sorted_list(WM^access), Res),
 	(
-	 	Res = merge(W2, W3),
-		rename_merge_world(Ont, W3, W2, WM, WM0),
+	 	Res = merge(W2, W3Num),
+		rename_merge_world(Ont, W3Num, W2, WM, WM0),
 		RWM = reduced(Ont, WM0)
 	;
 		Res = none,
@@ -294,9 +358,9 @@ first_mergable_worlds([{R, W1, W2} | Rest], Result) :-
 		list.find_first_map((pred({Rx, W1x, W3x}::in, W3x::out) is semidet :-
 			Rx = R, W1x = W1), Rest, W3)
 	then
-		(W2 = u(_), W3 = u(_) -> Result = merge(W2, W3) ;
-		(W2 = i(_), W3 = u(_) -> Result = merge(W2, W3) ;
-		(W2 = u(_), W3 = i(_) -> Result = merge(W3, W2) ;
+		(W2 = u(_), W3 = u(Num) -> Result = merge(W2, Num) ;
+		(W2 = i(_), W3 = u(Num) -> Result = merge(W2, Num) ;
+		(W2 = u(Num), W3 = i(_) -> Result = merge(W3, Num) ;
 		fail  % W2 and W3 are both indexed by nominals => non-reducible
 		)))
 	else
@@ -313,7 +377,7 @@ lfs(M) = LFs :-
 			set.member(Prop, SetProps),
 			LF = at(of_sort(Idx, Sort), p(Prop))
 		;
-			set.member({Rel, W1, W2}, M^reach),
+			set.member({Rel, W1, W2}, M^access),
 			W1 = i(Idx1), M^worlds^elem(Idx1) = Sort1,
 			W2 = i(Idx2), M^worlds^elem(Idx2) = Sort2,
 			LF = at(of_sort(Idx1, Sort1), r(Rel, i(of_sort(Idx2, Sort2))))
