ccc692c6c27e228bfadf1ef6f706f2fcac94e0a7
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-18 15:27:51 +0000

    correcting modality matching; adding module test_ctx

---
+++ code/comsys/branches/comsys.mk5/subarchitectures/comsys/abduction/test_ctx.m
@@ -0,0 +1,53 @@
+:- module test_ctx.
+
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+%------------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module list, string.
+:- import_module ctx, modality.
+:- import_module stringable.
+
+main(!IO) :-
+	test_ctx_match([i(next)], [any], !IO),
+	test_ctx_match([any], [i(next)], !IO),
+	test_ctx_match([i(next)], [i(next)], !IO),
+	test_ctx_match([any], [], !IO),
+	test_ctx_match([any], [any], !IO),
+	test_ctx_match([any], [any, any], !IO),
+
+	test_ctx_match([i(next)], [any, i(next), any], !IO),
+	test_ctx_match([i(next)], [any, i(this)], !IO),
+
+	test_ctx_match([], [], !IO).
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
+
+:- pred test_ctx_match(list(ctx)::in, list(ctx)::in, io::di, io::uo) is det.
+
+test_ctx_match(L, R, !IO) :-
+	test_match(L, R, !IO).
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
+
+:- pred test_match(list(T)::in, list(T)::in, io::di, io::uo) is det <= (modality(T), stringable(T)).
+
+test_match(L, R, !IO) :-
+	print(seq_to_string(L) ++ " ? " ++ seq_to_string(R) ++ " ... ", !IO),
+	(if match(L, R)
+	then print("yes", !IO)
+	else print("no", !IO)
+	),
+	nl(!IO).
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -%
+
+:- func seq_to_string(list(T)) = string <= stringable(T).
+
+seq_to_string(L) = "(" ++ string.join_list(", ", list.map(to_string, L)) ++ ")".
