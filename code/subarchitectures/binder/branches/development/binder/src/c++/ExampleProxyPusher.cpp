dd2b41d3652df70891df56caabbf7c4da0bfb377
plison <plison@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-21 14:51:28 +0000

    correction wrong directories

---
+++ code/subarchitectures/binder/branches/development/binder/src/c++/ExampleProxyPusher.cpp
@@ -0,0 +1,41 @@
+#include "ExampleProxyPusher.hpp"
+
+
+extern "C" {
+  cast::interfaces::CASTComponentPtr 
+  newComponent() {
+    return new ExampleProxyPusher();
+  }
+}
+
+
+
+using namespace binder::autogen::core;
+using namespace binder::autogen::featvalues;
+using namespace cast;
+
+
+void ExampleProxyPusher::start() {
+  //must call super start to ensure that the reader sets up change
+  //filters
+  BindingWorkingMemoryReader::start();
+}
+
+void ExampleProxyPusher::runComponent() {
+  sleepComponent(5000);
+  println("faking stuff up");
+
+  //copied from fake visual thingy
+    
+  OriginInfoPtr origin = createOriginInfo (getSubarchitectureID(), "blablabla", "anyDatatype");
+  
+  FeatureValuePtr relation = createStringValue ("relation", 0.8f);
+  FeaturePtr feat1 = createFeatureWithUniqueFeatureValue ("type", relation);
+  
+  ProxyPtr proxy = createNewProxy (origin, 0.75f);
+  
+  addFeatureToProxy (proxy, feat1);
+  
+  addProxyToWM(proxy);
+
+}
