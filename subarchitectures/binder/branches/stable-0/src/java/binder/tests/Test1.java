31e9c2f1ee7fb29a239962cfbe6a254d7044e3ca
Nick Hawes <nickh@robots.ox.ac.uk>
2009-09-22 15:41:31 +0000

    created stable 0 from 2704 of binder trunk

---
+++ subarchitectures/binder/branches/stable-0/src/java/binder/tests/Test1.java
@@ -0,0 +1,201 @@
+package binder.tests;
+
+import java.util.HashMap;
+
+import binder.autogen.core.Feature;
+import binder.autogen.core.FeatureValue;
+import binder.autogen.core.Proxy;
+import binder.autogen.core.Union;
+import binder.autogen.core.UnionConfiguration;
+import binder.utils.FeatureValueUtils;
+import cast.architecture.ChangeFilterFactory;
+import cast.architecture.WorkingMemoryChangeReceiver;
+import cast.cdl.WorkingMemoryChange;
+import cast.cdl.WorkingMemoryOperation;
+import cast.cdl.WorkingMemoryPointer;
+
+public class Test1 extends AbstractTester {
+	
+	protected static int testNumber = 1;
+	protected static String task = "Correct production of a binding union for a single proxy";
+	
+	
+	static Proxy lastProxy = new Proxy();
+	static HashMap<String, Union> predictedUnions = new HashMap<String, Union>();
+
+	
+	public Test1()
+	{
+		super(testNumber, task);
+	}
+	
+
+	public boolean performTest() {
+		
+		Proxy proxy = createProxy();
+		Union union = getBindingUnionForProxy(proxy);
+		
+		boolean check = isBindingUnionCorrect(union);
+		return check;
+	}
+	
+	
+	protected Proxy createProxy() {
+		WorkingMemoryPointer origin = createWorkingMemoryPointer("subarch1", "localDataID", "dataType");
+		Proxy newProxy = createNewProxy(origin, 0.8f);
+		
+		FeatureValue featvalue1a = createStringValue("featvalue1", 0.7f);
+		Feature feat1 = createFeatureWithUniqueFeatureValue("featlabel1", featvalue1a);
+		addFeatureToProxy(newProxy, feat1);
+
+		FeatureValue featvalue2a = createIntegerValue(3, 0.7f);
+		FeatureValue featvalue2b = createIntegerValue(4, 0.2f);
+		Feature feat2 = createFeature("featlabel2");
+		addFeatureValueToFeature(feat2, featvalue2a);
+		addFeatureValueToFeature(feat2, featvalue2b);
+		
+		addFeatureToProxy(newProxy, feat2);
+
+		return newProxy;
+	}
+
+	protected boolean isBindingUnionCorrect(Union union) {
+		if (union != null) {
+			if ((union.includedProxies.length == 1) && 
+					union.includedProxies[0] ==lastProxy) {
+			
+				 if (union.probExists > 0.0f) {
+					
+					 if (union.timeStamp.s <=getCASTTime().s) {
+						 
+						 if (union.features != null && 
+								 union.features.length == lastProxy.features.length) {
+							
+							 for (int i = 0 ; i < union.features.length ; i++) {
+							 if (union.features[i].featlabel.equals(lastProxy.features[i].featlabel)) {
+								 
+								 if (union.features[i].alternativeValues != null && 
+										 union.features[i].alternativeValues.length == 
+											 lastProxy.features[i].alternativeValues.length) {
+									 for (int j = 0; j < union.features[i].alternativeValues.length ; j++) {
+										 
+										 if (union.features[i].alternativeValues[j].getClass().equals(
+												 lastProxy.features[i].alternativeValues[j].getClass())) {
+											 
+											 if (!FeatureValueUtils.haveEqualValue(union.features[i].alternativeValues[j], lastProxy.features[i].alternativeValues[j])) {
+													 log("FAILED, feature values in the proxy and the union are different");
+											 }
+										 }
+										 else {
+											 log("FAILED, feature value subclasses are not similar");
+											 return false;
+										 }
+											 
+									 }
+								 }
+								 else {
+									 log("FAILED, problem with the feature values of feature " + i);
+									 if (union.features[i].alternativeValues != null) {
+										 log("number of feature values in proxy: " + 
+												 lastProxy.features[i].alternativeValues.length +
+												 " != number of feature values in union: " + 
+												 union.features[i].alternativeValues.length );	 
+									 }
+									 else {
+										 log("alternative feature values is null");
+									 }
+									 return false;
+								 }
+							 }
+							 else {
+								 log("FAILED, feature labels do not match");
+							 }
+							 }
+						 }
+						 
+						 else {
+							 log("FAILED, wrong number of features in proxy");
+						 }
+						 
+							if ((union.entityID == null)) {
+								log("FAILED, entity ID is null");
+								return false;
+							}
+							
+					 }
+					 else {
+						 log("FAILED, union timestamp not correctly specified");
+						 return false;
+					 }
+					 
+				
+				 }
+				 else {
+					 log("FAILED, probExists is not > 0");
+					 return false;
+				 }
+			}
+			else {
+				log("FAILED, wrong number of included proxies in union");
+				return false;
+			}
+		}
+		else {
+			log("FAILED, union is null");
+			return false;
+		}
+		
+		// Passed all tests!
+		return true;
+	}
+	
+	protected Union getBindingUnionForProxy (Proxy proxy) {
+		try {
+			
+			lastProxy = proxy;
+			activateChangeFilter(proxy);
+			addProxyToWM (proxy);
+
+			while (!predictedUnions.containsKey(proxy.entityID)) {
+				sleepComponent(20);
+			}
+			log("Predicted union for proxy is sucessfully retrieved");
+			
+			return predictedUnions.get(proxy.entityID);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+
+	private void activateChangeFilter (Proxy proxy) {
+
+
+		WorkingMemoryChangeReceiver receiverForPhantomProxies = 
+			new WorkingMemoryChangeReceiver() {
+			public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+				try {
+					UnionConfiguration config = 
+						getMemoryEntry(_wmc.address, UnionConfiguration.class);
+					for (int i = 0 ; i < config.includedUnions.length ; i++) {
+						Union u = config.includedUnions[i];
+						for (int j = 0 ; j < u.includedProxies.length ; j++) {
+							if (u.includedProxies[j].equals(lastProxy)) {
+								predictedUnions.put(lastProxy.entityID, u);
+							}
+						}
+					}
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			} 
+		};
+		addChangeFilter(ChangeFilterFactory.createGlobalTypeFilter(UnionConfiguration.class,
+				WorkingMemoryOperation.WILDCARD), receiverForPhantomProxies);
+	}
+
+}
---
+++ subarchitectures/binder/branches/stable-0/src/java/binder/tests/Test2.java
@@ -0,0 +1,173 @@
+package binder.tests;
+
+import java.util.Map;
+import java.util.Random;
+
+import cast.architecture.ChangeFilterFactory;
+import cast.architecture.WorkingMemoryChangeReceiver;
+import cast.cdl.WorkingMemoryAddress;
+import cast.cdl.WorkingMemoryChange;
+import cast.cdl.WorkingMemoryOperation;
+import cast.cdl.WorkingMemoryPointer;
+
+import binder.autogen.core.Feature;
+import binder.autogen.core.FeatureValue;
+import binder.autogen.core.Proxy;
+import binder.autogen.core.UnionConfiguration;
+import binder.autogen.featvalues.BooleanValue;
+import binder.autogen.featvalues.IntegerValue;
+import binder.autogen.featvalues.StringValue;
+
+
+public class Test2 extends AbstractTester{
+
+	static int testNumber = 2;
+	static String task = "Generate correct single-proxy unions for a large set of random proxies";
+	
+	
+	int NUMBER_OF_PROXIES = 30;
+	int MAX_NUMBER_OF_FEATURES = 6;
+	int MAX_NUMBER_OF_FEATUREVALUES = 4;
+	int NUMBER_SUBARCHS = 4;
+	
+	UnionConfiguration curConfig;
+	
+	Class[] featValueTypes = {StringValue.class, IntegerValue.class, BooleanValue.class};
+
+	
+	int proxyCount = 1;
+	int featurecount = 1;
+	int featurevaluecount = 1;
+	
+	
+	public Test2 () {
+		super(testNumber, task);
+	}
+	
+	
+	
+	@Override
+	public void start() {
+
+		// if best selected UnionConfiguration has been updated in the WM, update the
+		// reader accordingly
+		addChangeFilter(ChangeFilterFactory.createGlobalTypeFilter(UnionConfiguration.class,
+				WorkingMemoryOperation.WILDCARD), new WorkingMemoryChangeReceiver() {
+
+			public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+				try {
+					UnionConfiguration config = 
+						getMemoryEntry(_wmc.address, UnionConfiguration.class);
+					curConfig = config;
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			} 
+		});
+	}
+
+
+	/**
+	 * Set configuration parameters
+	 */
+
+	@Override
+	public void configure(Map<String, String> _config) {
+		
+		if (_config.containsKey("--nbproxies")) {
+			NUMBER_OF_PROXIES = Integer.parseInt(_config.get("--nbproxies"));
+		} 
+		if (_config.containsKey("--nbfeatures")) {
+			MAX_NUMBER_OF_FEATURES = Integer.parseInt(_config.get("--nbfeatures"));
+		}
+		if (_config.containsKey("--nbfeatvals")) {
+			MAX_NUMBER_OF_FEATUREVALUES = Integer.parseInt(_config.get("--nbfeatvals"));
+		}
+		if (_config.containsKey("--nbsubarchs")) {
+			NUMBER_SUBARCHS = Integer.parseInt(_config.get("--nbsubarchs"));
+		}		
+	}
+	
+	
+	@Override
+	public boolean performTest() {
+		
+		for (int i = 0 ; i < NUMBER_OF_PROXIES; i++) {
+			log("Now creating random proxy " + (i + 1));
+			Proxy proxy = createNewRandomProxy();
+			addProxyToWM(proxy);
+			sleepComponent(20);
+		}
+		
+		int counts = 0;
+		while ((curConfig.includedUnions.length < NUMBER_OF_PROXIES) && counts < (NUMBER_OF_PROXIES*2)) {
+		try {
+			sleepComponent(50);
+			counts++;
+		}
+		catch (Exception e){ }
+		}
+		log("Final number of unions: " + curConfig.includedUnions.length);
+		
+		boolean result = (curConfig.includedUnions.length == NUMBER_OF_PROXIES);
+		
+		if (!result) {
+			System.out.println("Final number of unions: " + curConfig.includedUnions.length);
+		}
+		
+		return result;
+	}
+	
+	
+	public Proxy createNewRandomProxy() {
+		Random rand = new Random();
+		int subarchNb = rand.nextInt(NUMBER_SUBARCHS) + 1;
+		WorkingMemoryPointer origin = createWorkingMemoryPointer("subarch"+subarchNb, "localID"+proxyCount, "localType");
+		proxyCount++;
+		float probExists = rand.nextFloat();
+		Proxy proxy = createNewProxy(origin, probExists);
+		
+		int nbFeatures = rand.nextInt(MAX_NUMBER_OF_FEATURES) ;
+		for (int i = 0 ; i < nbFeatures; i++) {
+			Feature feat = createFeature("featlabel"  + featurecount);
+			featurecount++;
+			int nbFeatureValues = rand.nextInt(MAX_NUMBER_OF_FEATUREVALUES) ;
+			
+			for (int j = 0; j < nbFeatureValues ;j++) {
+				FeatureValue featvalue = createRandomFeatureValue();
+				addFeatureValueToFeature(feat, featvalue);
+			}
+			if (feat.alternativeValues.length > 0) {
+				addFeatureToProxy(proxy, feat);
+			}
+		}
+		
+		return proxy;
+	}
+
+	
+	public FeatureValue createRandomFeatureValue() {
+
+		featurevaluecount++;
+		Random random = new Random();
+		int featvalueTypeChooser = random.nextInt(featValueTypes.length);
+		Class featureValueType = featValueTypes[featvalueTypeChooser];
+		
+		if (featureValueType.equals(StringValue.class)) {
+			return createStringValue("str"+ featurevaluecount , 
+					random.nextFloat());
+		}
+		else if (featureValueType.equals(IntegerValue.class)) {
+			return createIntegerValue(featurevaluecount, 
+					random.nextFloat());
+		}
+		else if (featureValueType.equals(BooleanValue.class)) {
+			return createBooleanValue(random.nextBoolean(), 
+					random.nextFloat());
+		}
+			
+		return null;
+		}
+	
+}
