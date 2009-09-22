aab72bc3c5e42e71dc49e952119d7e09466adbed
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-22 18:15:17 +0000

    pysiftcuda: homographies

--- subarchitectures/vision.sa/trunk/src/c++/vision/components/ObjectRecognizer/pysiftgpu/siftcuda/pysiftcuda.cpp
+++ subarchitectures/vision.sa/trunk/src/c++/vision/components/ObjectRecognizer/pysiftgpu/siftcuda/pysiftcuda.cpp
@@ -10,6 +10,7 @@
 #include <numpy/arrayobject.h>
 #include <dlfcn.h>
 #include <cutil.h>
+#include <vector>
 
 #include "cudaImage.h"
 #include "cudaSift.h"
@@ -128,6 +129,23 @@ int checkDescriptorArray(PyObject *arr)
 }
 
 static
+void copyPyArrayToSiftData(PyObject *array, int nPoints, SiftData &data)
+{
+#define ROUND(n) ((n + 7) / 8) * 8
+   InitSiftData(&data, ROUND(nPoints), true, true); 
+#undef ROUND
+
+   int lendata = 128 * sizeof(float);
+   memset((void*)&data.h_data[0], 0, lendata*nPoints);
+   unsigned char *dscr_data = (unsigned char*) PyArray_DATA((PyArrayObject*) array);
+   for (int i = 0; i < nPoints; i++) {
+      memcpy((void*)&data.h_data[i].data[0], dscr_data, lendata);
+      dscr_data += lendata;
+   }
+   data.numPts = nPoints;
+}
+
+static
 PyObject* PyCS_MatchDescriptors(PyObject *self, PyObject *args)
 {
    PyObject* ndaDescrA = NULL;
@@ -140,28 +158,8 @@ PyObject* PyCS_MatchDescriptors(PyObject *self, PyObject *args)
 
    checkInit();
    SiftData siftData1, siftData2;
-   int i;
-#define ROUND(n) ((n + 7) / 8) * 8
-   InitSiftData(&siftData1, ROUND(nf1), true, true); 
-   InitSiftData(&siftData2, ROUND(nf2), true, true); 
-#undef ROUND
-
-   int lendata = 128 * sizeof(float);
-   memset((void*)&siftData1.h_data[0], 0, lendata*nf1);
-   unsigned char *dscr_data = (unsigned char*) PyArray_DATA((PyArrayObject*) ndaDescrA);
-   for (i = 0; i < nf1; i++) {
-      memcpy((void*)&siftData1.h_data[i].data[0], dscr_data, lendata);
-      dscr_data += lendata;
-   }
-   siftData1.numPts = nf1;
-
-   memset((void*)&siftData2.h_data[0], 0, lendata*nf2);
-   dscr_data = (unsigned char*) PyArray_DATA((PyArrayObject*) ndaDescrB);
-   for (i = 0; i < nf2; i++) {
-      memcpy((void*)&siftData2.h_data[i].data[0], dscr_data, lendata);
-      dscr_data += lendata;
-   }
-   siftData2.numPts = nf2;
+   copyPyArrayToSiftData(ndaDescrA, nf1, siftData1);
+   copyPyArrayToSiftData(ndaDescrB, nf2, siftData2);
 
    // move data from host to device; we need a new .cu module
    UploadSiftData(&siftData1);
@@ -171,7 +169,7 @@ PyObject* PyCS_MatchDescriptors(PyObject *self, PyObject *args)
    FreeSiftData(&siftData2);
  
    // return a ndarray with matches
-   int count = 0;
+   int i, count = 0;
    for (i = 0; i < nf1; i++) {
       if (siftData1.h_data[i].match >= 0 && siftData1.h_data[i].match < nf2)
          count++;
@@ -203,6 +201,80 @@ PyObject* PyCS_MatchDescriptors(PyObject *self, PyObject *args)
    return pPairs;
 }
 
+static
+PyObject* PyCS_HomographyMatchDescriptors(PyObject *self, PyObject *args)
+{
+   PyObject* ndaDescrA = NULL;
+   PyObject* ndaDescrB = NULL;
+   if (!PyArg_ParseTuple(args, "OO", &ndaDescrA, &ndaDescrB))
+      return NULL;
+   int nf1 = checkDescriptorArray(ndaDescrA);
+   int nf2 = checkDescriptorArray(ndaDescrB);
+   if (nf1 < 0 || nf2 < 0) return NULL;
+
+   checkInit();
+   SiftData siftData1, siftData2;
+   copyPyArrayToSiftData(ndaDescrA, nf1, siftData1);
+   copyPyArrayToSiftData(ndaDescrB, nf2, siftData2);
+
+   // move data from host to device; we need a new .cu module
+   UploadSiftData(&siftData1);
+   UploadSiftData(&siftData2);
+  
+   MatchSiftData(&siftData1, &siftData2); // result in siftData1
+   float homography[9];
+   int numMatches;
+   FindHomography(&siftData1, homography, &numMatches, 1000, 0.85f, 0.95f, 5.0);
+
+   int numPts = siftData1.numPts;
+   SiftPoint *sift1 = siftData1.h_data;
+   SiftPoint *sift2 = siftData2.h_data;
+   std::vector<int> ma, mb;
+   for (int j=0; j<numPts; j++) { 
+      int k = sift1[j].match;
+      float x = sift1[j].xpos;
+      float y = sift1[j].ypos;
+      float den = homography[6]*x + homography[7]*y + homography[8]; 
+      float x2 = (homography[0]*x + homography[1]*y + homography[2]) / den;
+      float y2 = (homography[3]*x + homography[4]*y + homography[5]) / den;
+      float erx = x2 - sift2[k].xpos;
+      float ery = y2 - sift2[k].ypos;
+      float er2 = erx*erx + ery*ery;
+      if (er2<25.0f && sift1[j].score>0.85f && sift1[j].ambiguity<0.95f) {
+         ma.push_back(j);
+         mb.push_back(k); 
+      }
+   }
+   FreeSiftData(&siftData1);
+   FreeSiftData(&siftData2);
+
+   int i, count = ma.size();
+   if (count > 0) {
+      // copy ma, mb to PyObject
+      npy_intp dims[2] = { count, 2 };
+      PyObject *pPairs = PyArray_SimpleNew(2, dims, PyArray_FLOAT);
+      float *p_data = (float *) PyArray_DATA((PyArrayObject*) pPairs);
+      for (i = 0; i < count; i++) {
+         *p_data++ = ma[i];
+         *p_data++ = mb[i];
+      }
+
+      // copy homography to PyObject
+      dims[0] = 3;
+      dims[1] = 3;
+      PyObject *pHgr = PyArray_SimpleNew(2, dims, PyArray_FLOAT);
+      unsigned char *hgr_data = (unsigned char*) PyArray_DATA((PyArrayObject*) pHgr);
+      memcpy(hgr_data, homography, 9 * sizeof(float));
+
+      // pass the results to Python
+      PyObject *pRes = PyTuple_New(2);
+      PyTuple_SetItem(pRes, 0, pPairs);
+      PyTuple_SetItem(pRes, 1, pHgr);
+      return pRes;
+   }
+   return NULL;
+}
+
 /* ****************************************************************************** 
  * Python Module Stuff
  *
@@ -215,8 +287,14 @@ PyMethodDef methods[] = {
        "extracted from the image."
     },
     {"matchDescriptors", PyCS_MatchDescriptors, METH_VARARGS, 
-       "extractFeatures(descriptorsA, descriptorsB)\n   descriptorsX - a numpy ndaray Nx128\n"
+       "matchDescriptors(descriptorsA, descriptorsB)\n   descriptorsX - a numpy ndaray Nx128\n"
        "Returns an array Nx2 with indices of matching descriptors."
+    },
+    {"homographyMatchDescriptors", PyCS_HomographyMatchDescriptors, METH_VARARGS, 
+       "homographyMatchDescriptors(descriptorsA, descriptorsB)\n   descriptorsX - a numpy ndaray Nx128\n"
+       "Returns a tuple (matches, homography)\n"
+       "  matches - Nx2 array with indices of descriptors that match a homography.\n"
+       "  homography - calculated with RANSAC on the matching descriptors."
     },
     {NULL, NULL, 0, NULL}
 };
