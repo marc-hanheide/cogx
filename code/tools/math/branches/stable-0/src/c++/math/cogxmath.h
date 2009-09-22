3d23fefef9facd4e6ceecdeed2d23c418d75fe83
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-22 15:57:02 +0000

    created stable 0 from 2699 of math trunk

---
+++ code/tools/math/branches/stable-0/src/c++/math/cogxmath.h
@@ -0,0 +1,18 @@
+/**
+ * Various math stuff.
+ *
+ * @author Michael Zillich
+ * @date Februrary 2009
+ */
+
+#ifndef COGX_MATH_H
+#define COGX_MATH_H
+
+#include <cogxmath_base.h>
+#include <Vector2.h>
+#include <Vector3.h>
+#include <Matrix33.h>
+#include <Pose3.h>
+
+#endif
+
