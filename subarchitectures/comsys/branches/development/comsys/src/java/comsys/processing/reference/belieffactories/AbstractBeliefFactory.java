df4e4cedd91e529c157f2d06c9d71041ef793b48
gj <gj@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-24 14:04:43 +0000

    belief assimilation code

---
+++ subarchitectures/comsys/branches/development/comsys/src/java/comsys/processing/reference/belieffactories/AbstractBeliefFactory.java
@@ -0,0 +1,106 @@
+//
+//  AbstractBeliefFactory.java
+//  
+//
+//  Created by Geert-Jan Kruijff on 9/24/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+// ---------------------------------------------------------
+// PACKAGE
+// ---------------------------------------------------------
+
+package comsys.components.reference.belieffactories;
+
+// ---------------------------------------------------------
+// BELIEFMODEL imports
+// ---------------------------------------------------------
+
+import beliefmodel.adl.Agents; 
+import beliefmodel.adl.Belief; 
+import beliefmodel.adl.SpatialInterval; 
+import beliefmodel.adl.TemporalInterval; 
+import beliefmodel.adl.Perspective;
+import beliefmodel.adl.SpatioTemporalFrame; 
+import beliefmodel.adl.SpatioTemporalFrame; 
+
+
+
+// ---------------------------------------------------------
+// COMSYS / LF imports
+// ---------------------------------------------------------
+
+import comsys.processing.reference.BeliefFactory;
+
+
+public class AbstractBeliefFactory 
+	implements BeliefFactory
+	
+{
+
+	/** constructs a belief with default settings for agents (human) and spatiotemporal frame (here-and-now) */ 
+	
+	public Belief constructBelief (LogicalForm lf) { 
+		Agent[] agents = createAgents("human");
+		return constructBelief(lf,agents);
+	} // end method
+	
+	/** constructs a belief with default settings for spatiotemporal frame (here-and-now) */ 
+	
+	public Belief constructBelief (LogicalForm lf, Agents agents) { 
+		return constructBelief(lf,agents,createHereNowFrame(agents)); 
+	} // end method		
+	
+	/** creates an agents-list consisting of a single agent. */
+	
+	protected Agents createAgents(String id) { 
+		Agent[] agents = new Agent[1];
+		agents[0] = id;		
+		return agents; 
+	} // end method
+	
+	/** adds the given agent to the list of agents for the given belief. 
+	 
+	 @returns Belief the updated belief
+	 */
+	
+	protected Belief addAgent (Belief belief, String id) { 
+		Agent[] agents = belief.agents; 
+		if (agents == null) { 
+			agents = createAgents(id);
+		} else { 
+			Agent[] helper = new Agent[1+agents.length];
+			for (int i=0; i < agents.length; i++) { helper[i] = agents[i]; }
+			helper[agents.length] = id;
+			agents = helper;
+		} // end if..else
+		belief.agents = agents;
+		return belief;
+	} // end method
+	
+	/** 
+	 creates a spatiotemporal frame with a spatial interval identified with "here", and 
+	 a temporal interval identified as "now", starting and ending "now." the perspective 
+	 is initialized by the given agent. 
+	 */ 
+
+	protected SpatioTemporalFrame createHereNowFrame(Agents agents) { 
+		SpatialInterval spInt = new SpatialInterval();
+		spInt.id = "here";
+		TemporalInterval tmpInt = new TemporalInterval();
+		tmpInt.id = "now";
+		tmpInt.start = "now";
+		tmpInt.end   = "now";
+		Perspective persp = new Perspective();
+		persp.id = "here";
+		persp.agents = agents;
+		SpatioTemporalFrame frame = new SpatioTemporalFrame();
+		frame.spatialint = spInt;
+		frame.tempint = tmpInt;
+		frame.persp = persp;
+		return frame;
+	} // end method
+	
+	
+	
+} // end class
---
+++ subarchitectures/comsys/branches/development/comsys/src/java/comsys/processing/reference/belieffactories/ThingBelief.java
@@ -0,0 +1,68 @@
+//
+//  ThingBelief.java
+//  
+//
+//  Created by Geert-Jan Kruijff on 9/24/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+// ---------------------------------------------------------
+// PACKAGE
+// ---------------------------------------------------------
+
+package comsys.components.reference.belieffactories;
+
+// ---------------------------------------------------------
+// BELIEFMODEL imports
+// ---------------------------------------------------------
+
+import beliefmodel.adl.Belief; 
+
+import beliefmodel.domainmodel.cogx.MaterialObject; 
+
+// ---------------------------------------------------------
+// COMSYS / LF imports
+// ---------------------------------------------------------
+
+import comsys.datastructs.lf.LFNominal;
+import comsys.datastructs.lf.LogicalForm;
+import comsys.processing.reference.BeliefFactory;
+
+/** 
+ the class <b>ThingBelief</b> turns a logical form, describing a Thing object, 
+ into a Belief about a MaterialObject with several associated properties. 
+ 
+ @author	Geert-Jan Kruijff (gj@dfki.de)
+ @started	090924
+ @version	090924
+ */ 
+
+public class ThingBelief 
+	extends AbstractBeliefFactory
+{
+
+	private const String sort = "thing";
+	
+	public String getSort() { return sort; }
+	
+	
+
+	
+	public Belief constructBelief (LogicalForm lf, Agents agents, SpatioTemporalFrame frame) { 
+		// initialize the belief
+		LFNominal root = lf.root;
+		assert root.sort.equals(sort);
+		Belief belief = new Belief();
+		belief.id = "thing";
+		belief.sigma = frame;
+		belief.agents = agents; 
+		// create the formula
+		belief.phi = 
+		
+		
+		return belief;
+	
+	} // end method
+		
+	
+} // end class
