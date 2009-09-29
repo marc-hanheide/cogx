afa1325ce49b6c0cf707bf4393051462034a124a
krsj <krsj@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-09-29 15:04:14 +0000

    And the hpp...

--- code/subarchitectures/spatial.sa/branches/stable-0/src/c++/PlaceManager.hpp
+++ code/subarchitectures/spatial.sa/branches/stable-0/src/c++/PlaceManager.hpp
@@ -109,6 +109,9 @@ class PlaceManager : public cast::ManagedComponent
     void robotMoved(const cast::cdl::WorkingMemoryChange &objID);
     void processPlaceArrival(bool failed, NavData::FNodePtr newNavNode=0); 
 
+    // Helper function to create Gateway properties
+    void addNewGatewayProperty(int placeID);
+
     // Frontier based exploration path parameters
     double m_maxFrontierDist; // Culling distance for frontiers around a given node
     double m_minFrontierDist; // Culling distance for frontiers around a given node
