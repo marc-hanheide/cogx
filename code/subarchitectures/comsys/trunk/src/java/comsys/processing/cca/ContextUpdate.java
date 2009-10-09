9fd9f81ed1daae80b19a60a57a371e82cb92b95e
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-09 14:01:01 +0000

    adding forgotten ContextUpdate.java

---
+++ code/subarchitectures/comsys/trunk/src/java/comsys/processing/cca/ContextUpdate.java
@@ -0,0 +1,47 @@
+package comsys.processing.cca;
+
+import java.util.ArrayList;
+
+import Abducer.AssumedQuery;
+import Abducer.EventModality;
+import Abducer.MarkedQuery;
+import Abducer.ModalisedFormula;
+import Abducer.Predicate;
+
+
+public class ContextUpdate {
+
+	public Predicate intention;
+	public MarkedQuery[] proof;
+	
+	public ContextUpdate() {
+		this.proof = null;
+		this.intention = null;
+	}
+	
+	public ContextUpdate(MarkedQuery[] proof) {
+		this.proof = proof;
+		this.intention = proofToIntention(proof);
+	}
+	
+	public static Predicate proofToIntention(MarkedQuery[] proof) {
+		ModalisedFormula[] assumptions = ProofUtils.proofToFacts(ProofUtils.filterAssumed(proof));
+
+		// filter out just the event modality
+		ArrayList<ModalisedFormula> list = new ArrayList<ModalisedFormula>();
+		for (int i = 0; i < assumptions.length; i++) {
+			if (assumptions[i].m.length == 1 && assumptions[i].m[0] instanceof EventModality) {
+				list.add(assumptions[i]);
+			}
+		}
+		
+		ModalisedFormula[] fs = list.toArray(new ModalisedFormula[] { });
+		
+		if (fs.length == 1) {
+			return fs[0].p;
+		}
+		else {
+			return null;
+		}
+	}
+}
