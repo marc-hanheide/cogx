18d44cdbc86b1995380214b61fdbb93525127712
mxz <mxz@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-09 16:12:49 +0000

    first stable release

---
+++ tools/hardware/video/branches/stable-0/src/c++/utils/AccessImage.h
@@ -0,0 +1,38 @@
+/**
+ * @author Michael Zillich
+ * @date June 2009
+ */
+
+#ifndef ACCESS_IMAGE_H
+#define ACCESS_IMAGE_H
+
+#include <opencv/cv.h>
+#include "Video.hpp"
+
+namespace Video
+{
+
+/**
+ * Access raw image data at position (x,y), where x and y are not checked for
+ * valid range.
+ * const version.
+ */
+inline const char* cvAccessImageData(const IplImage *img, int x, int y)
+{
+  return img->imageData + y*img->widthStep + x*img->nChannels*img->depth/8;
+}
+
+/**
+ * Access raw image data at position (x,y), where x and y are not checked for
+ * valid range.
+ * non-const version.
+ */
+inline char* cvAccessImageData(IplImage *img, int x, int y)
+{
+  return img->imageData + y*img->widthStep + x*img->nChannels*img->depth/8;
+}
+
+}
+
+#endif
+
