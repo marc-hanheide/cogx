580b4e767680c9b9372411ea0d5b9f847265b0dc
mxz <mxz@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-09 16:12:49 +0000

    first stable release

---
+++ code/tools/hardware/video/branches/stable-0/src/c++/components/VideoServer.h
@@ -0,0 +1,230 @@
+/**
+ * @author Michael Zillich
+ * @date February 2009
+ */
+
+#ifndef VIDEO_SERVER_H
+#define VIDEO_SERVER_H
+
+#include <stdexcept>
+#include <vector>
+#include <string>
+#include <map>
+#include <cast/architecture/ManagedComponent.hpp>
+#include <VideoUtils.h>
+#include "Video.hpp"
+
+namespace cast
+{
+
+class VideoServer;
+
+/**
+ * Ice interface to a video server.
+ */
+class VideoServerI : public Video::VideoInterface
+{
+private:
+  VideoServer *vidSrv;
+
+public:
+  VideoServerI(VideoServer *_vid) : vidSrv(_vid) {}
+
+  /**
+   * Returns number of cameras this device manages.
+   */
+  virtual Ice::Int getNumCameras(const Ice::Current&);
+
+  /**
+   * Returns the devices image size, must be > 0.
+   * Note that all images have the same size (and colour format). This class is
+   * not intended to manage video sources of different sizes/formats.
+   */
+  virtual void getImageSize(Ice::Int& width, Ice::Int& height, const Ice::Current&);
+
+  /**
+   * Returns the frame rate in [ms], must be > 0.
+   * Note that all video sources have the same frame rate.
+   */
+  virtual Ice::Int getFramerateMilliSeconds(const Ice::Current&);
+
+  /**
+   * Get just image from one source.
+   * \param camId  (in) which video source (camera)
+   * \param frame  (out) image
+   */
+  virtual void getImage(Ice::Int camId, Video::Image& image, const Ice::Current&);
+
+  /**
+   * Get images from all sources.
+   * \param frames  (out) images
+   */
+  virtual void getImages(Video::ImageSeq& images, const Ice::Current&);
+
+  /**
+   * Get images from some sources.
+   * \param camIds  (in) which video sources (cameras)
+   * \param frames  (out) images
+   */
+  virtual void getScaledImages(Ice::Int width, Ice::Int height,
+      Video::ImageSeq& images, const Ice::Current&);
+
+  virtual void startReceiveImages(const std::string& receiverComponentId,
+      const std::vector<int> &camIds, Ice::Int width, Ice::Int height,
+      const Ice::Current&);
+
+  virtual void stopReceiveImages(const std::string& receiverComponentId,
+      const Ice::Current&);
+};
+
+/**
+ * Video Server to capture images from one or several (typically two) sources
+ * (= cameras).
+ *
+ * (In most cases basically just a wrapper around OpenCV capture functions.)
+ *
+ * If several sources are used, they all have the same image format and
+ * synchronisation between sources is handled by this class.
+ * If several sources are used, calls to Grab() shall aways fill the
+ * provided array in the same order.
+ */
+class VideoServer : virtual public ManagedComponent
+{
+protected:
+  class ImageReceiver
+  {
+  public:
+    /**
+     * our ICE proxy to the video client
+     */
+    Video::VideoClientInterfacePrx videoClient;
+    /**
+     * component ID of the client
+     */
+    std::string receiverComponentId;
+    /**
+     * image size of received images, 0 means the videos native image size
+     */
+    int imgWidth, imgHeight;
+    /**
+     * which images to receive
+     */
+    std::vector<int> camIds;
+  };
+
+  /**
+   * Camera IDs for the video sources: camIds[sourceNum]
+   * e.g. camIds[0] = 3; camIds[1] = 4;
+   */
+  std::vector<int> camIds;
+
+  /**
+   * Camera parameters for the video sources
+   */
+  std::vector<Video::CameraParameters> camPars;
+
+  /**
+   * list of clients which have registered via some startReceiveImage() and want
+   * to receive images from us
+   */
+  std::vector<ImageReceiver> imageReceivers;
+
+  /**
+   * return the index of the camera for a given camera ID
+   */
+  size_t getCamIndex(int camId) throw(std::runtime_error)
+  {
+    for(size_t i = 0; i < camIds.size(); i++)
+      if(camId == camIds[i])
+        return i;
+    throw std::runtime_error(exceptionMessage(__HERE__, "video has no camera %d", camId));
+  }
+
+  void receiveCameraParameters(const cdl::WorkingMemoryChange & _wmc);
+
+  virtual void configure(const std::map<std::string,std::string> & _config)
+    throw(std::runtime_error);
+
+  virtual void start();
+
+  virtual void runComponent();
+
+public:
+  VideoServer() {}
+  virtual ~VideoServer() {}
+
+  /**
+   * Grabs frames from all sources and stores them internally, including
+   * timestamps.
+   * This is typically very fast, little copying etc.
+   */
+  virtual void grabFrames() = 0;
+
+  /**
+   * Retrieves previously grabbed frames for given camera IDs.
+   * \param camIds (in) camera IDs
+   * \param width (in) size of output image. If 0 then use native image size of
+   *                   video.
+   * \param height (in)
+   * \param frames  (out) array of images, as many as size of camIds,
+   *                      including timestamps
+   */
+  virtual void retrieveFrames(const std::vector<int> &camIds,
+    int width, int height, std::vector<Video::Image> &frames) = 0;
+
+  /**
+   * Retrieves previously grabbed frames from all sources.
+   * \param width (in) size of output image. If 0 then use native image size of
+   *                   video.
+   * \param height (in)
+   * \param frames  (out) array of images, as many as num cameras,
+   *                      including timestamps
+   */
+  virtual void retrieveFrames(int width, int height,
+      std::vector<Video::Image> &frames) = 0;
+
+  /**
+   * Retrieve previously grabbed frame from just one source.
+   * \param camId  (in) which video source (camera)
+   * \param width (in) size of output image. If 0 then use native image size of
+   *                   video.
+   * \param height (in)
+   * \param frame  (out) image, including timestamp
+   */
+  virtual void retrieveFrame(int camId, int width, int height,
+      Video::Image &frame) = 0;
+
+  /**
+   * Returns number of cameras this device manages.
+   */
+  int getNumCameras() const;
+
+  /**
+   * Returns the devices image size, must be > 0.
+   * Note that all images have the same size (and colour format). This class is
+   * not intended to manage video sources of different sizes/formats.
+   * Note: not const (as one might assume) as we don't know what derived
+   * classes will have to do internally.
+   */
+  virtual void getImageSize(int &width, int &height) = 0;
+
+  /**
+   * Returns the frame rate in [ms], must be > 0.
+   * Note that all video sources have the same frame rate.
+   * Note: not const (as one might assume) as we don't know what derived
+   * classes will have to do internally.
+   */
+  virtual int getFramerateMilliSeconds() = 0;
+
+  void getImage(int camId, Video::Image &img);
+  void getImages(std::vector<Video::Image> &images);
+  void getScaledImages(int width, int height, std::vector<Video::Image> &images);
+  void startReceiveImages(const std::string &receiverComponentId,
+      const std::vector<int> &camIds, int width, int height);
+  void stopReceiveImages(const std::string &receiverComponentId);
+};
+
+}
+
+#endif
+
---
+++ code/tools/hardware/video/branches/stable-0/src/c++/utils/ConvertImage.cpp
@@ -0,0 +1,116 @@
+/**
+ * @author Michael Zillich
+ * @date February 2009
+ */
+
+#include <cassert>
+#include <cast/core/CASTUtils.hpp>
+#include "ConvertImage.h"
+
+// define this to use memcpy() instead of looping over image lines and pixels
+// memcpy() is a LOT faster, but will screw up the images if the memory layout
+// (byte per pixel, bytes per line) is not exactly the same.
+#define FAST_DIRTY_CONVERSION
+
+namespace Video
+{
+
+using namespace std;
+using namespace cast;
+
+void convertImageToIpl(const Video::Image & img, IplImage ** iplImg)
+{
+  if(*iplImg != 0)
+    if(img.width != (*iplImg)->width || img.height != (*iplImg)->height)
+      cvReleaseImage(iplImg);
+  if(*iplImg == 0)
+    *iplImg = cvCreateImage(cvSize(img.width,img.height), IPL_DEPTH_8U, 3);
+  assert(*iplImg != 0);
+#ifndef FAST_DIRTY_CONVERSION
+  int x, y, c;
+  // note: this neat triple loop might be somewhat slower than a memcpy, but
+  // makes sure images are copied correctly irrespective of memory layout and
+  // line padding.
+  for(y = 0; y < img.height; y++)
+    for(x = 0; x < img.width; x++)
+      for(c = 0; c < 3; c++)
+        (*iplImg)->imageData[y*(*iplImg)->widthStep + 3*x + c] =
+          img.data[3*(y*img.width + x) + c];
+#else
+  memcpy((*iplImg)->imageData, &img.data[0],
+      (*iplImg)->height*(*iplImg)->widthStep);
+#endif
+}
+
+IplImage* convertImageToIpl(const Video::Image & img)
+{
+  IplImage *iplImg = 0;
+  convertImageToIpl(img, &iplImg);
+  return iplImg;
+}
+
+void convertImageFromIpl(const IplImage *iplImg, Video::Image &img)
+  throw(runtime_error)
+{
+  assert(iplImg != 0);
+  if(iplImg->nChannels != 3)
+    throw runtime_error(exceptionMessage(__HERE__,
+      "can only handle 3 channel colour images, have %d channels",
+      iplImg->nChannels));
+
+  img.width = iplImg->width;
+  img.height = iplImg->height;
+  img.data.resize(iplImg->width*iplImg->height*iplImg->nChannels);
+  // note: this neat triple loop might be somewhat slower than a memcpy, but
+  // makes sure images are copied correctly irrespective of memory layout and
+  // line padding.
+  if(iplImg->depth == (int)IPL_DEPTH_8U || iplImg->depth == (int)IPL_DEPTH_8S)
+  {
+#ifndef FAST_DIRTY_CONVERSION
+    int x, y, c;
+    for(y = 0; y < iplImg->height; y++)
+      for(x = 0; x < iplImg->width; x++)
+        for(c = 0; c < 3; c++)
+          img.data[3*(y*img.width + x) + c] =
+            iplImg->imageData[y*iplImg->widthStep + 3*x + c];
+#else
+    memcpy(&img.data[0], iplImg->imageData, iplImg->height*iplImg->widthStep);
+#endif
+  }
+  else
+    throw runtime_error(exceptionMessage(__HERE__,
+      "can only handle 8 bit colour values"));
+}
+
+IplImage* convertImageToIplGray(const Video::Image & img)
+{
+  // colour channel scaling factors, taken from OpenCV cvcolor.cpp
+  const float cscGr_32f = 0.299;
+  const float cscGg_32f = 0.587;
+  const float cscGb_32f = 0.114;
+
+  IplImage *grayImg =
+    cvCreateImage(cvSize(img.width,img.height), IPL_DEPTH_8U, 1);
+  assert(grayImg != 0);
+  int x, y;
+  for(y = 0; y < img.height; y++)
+    for(x = 0; x < img.width; x++)
+    {
+      int i = 3*(y*img.width + x);
+      float rf = (float)img.data[i];
+      float gf = (float)img.data[i+1];
+      float bf = (float)img.data[i+2];
+      grayImg->imageData[y*grayImg->widthStep + x] = (unsigned char)
+        (cscGr_32f*rf + cscGg_32f*gf + cscGb_32f*bf);
+    }
+  return grayImg;
+}
+
+/*void swapRedBlueChannels(IplImage *iplImage)
+{
+  char *p = 
+}*/
+
+
+}
+
