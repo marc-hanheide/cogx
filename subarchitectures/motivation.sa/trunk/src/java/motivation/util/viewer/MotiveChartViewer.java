584529ba043566e31dd3cb54cc9e97668dd33a2b
Marc Hanheide <marc@hanheide.net>
2009-10-09 10:24:21 +0000

    fixed scheduler consistency bug

--- subarchitectures/motivation.sa/trunk/src/java/motivation/util/viewer/MotiveChartViewer.java
+++ subarchitectures/motivation.sa/trunk/src/java/motivation/util/viewer/MotiveChartViewer.java
@@ -1,12 +1,15 @@
 package motivation.util.viewer;
 
+import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
 import java.util.Map;
+import java.util.Vector;
 
 import javax.swing.JFrame;
 
+import motivation.slice.ExploreMotive;
 import motivation.slice.Motive;
 import motivation.slice.MotiveStatus;
 import motivation.util.WMMotiveSet;
@@ -72,108 +75,6 @@ public class MotiveChartViewer extends ManagedComponent implements
 		 */
 		private static final long serialVersionUID = -6559252766578349920L;
 
-		/**
-		 * Creates a dataset.
-		 * 
-		 * @return A dataset.
-		 */
-		public CategoryDataset createFakeDataset1() {
-
-			final DefaultCategoryDataset dataset1 = new DefaultCategoryDataset();
-
-			// row keys...
-			final String series1 = "First";
-			final String series2 = "Second";
-
-			// column keys...
-			final String type1 = "Type 1";
-			final String type2 = "Type 2";
-			final String type3 = "Type 3";
-			final String type4 = "Type 4";
-			final String type5 = "Type 5";
-			final String type6 = "Type 6";
-			final String type7 = "Type 7";
-			final String type8 = "Type 8";
-
-			dataset1.addValue(1.0, series1, type1);
-			dataset1.addValue(4.0, series1, type2);
-			dataset1.addValue(3.0, series1, type3);
-			dataset1.addValue(5.0, series1, type4);
-			dataset1.addValue(5.0, series1, type5);
-			dataset1.addValue(7.0, series1, type6);
-			dataset1.addValue(7.0, series1, type7);
-			dataset1.addValue(8.0, series1, type8);
-
-			dataset1.addValue(5.0, series2, type1);
-			dataset1.addValue(7.0, series2, type2);
-			dataset1.addValue(6.0, series2, type3);
-			dataset1.addValue(8.0, series2, type4);
-			dataset1.addValue(4.0, series2, type5);
-			dataset1.addValue(4.0, series2, type6);
-			dataset1.addValue(2.0, series2, type7);
-			dataset1.addValue(1.0, series2, type8);
-
-			return dataset1;
-
-		}
-
-		/**
-		 * Creates a dataset.
-		 * 
-		 * @return A dataset.
-		 */
-		public CategoryDataset createFakeDataset2() {
-
-			final DefaultCategoryDataset result = new DefaultCategoryDataset();
-
-			// row keys...
-			final String series1 = "Third";
-			final String series2 = "Fourth";
-
-			// column keys...
-			final String type1 = "Type 1";
-			final String type2 = "Type 2";
-			final String type3 = "Type 3";
-			final String type4 = "Type 4";
-			final String type5 = "Type 5";
-			final String type6 = "Type 6";
-			final String type7 = "Type 7";
-			final String type8 = "Type 8";
-
-			result.addValue(11.0, series1, type1);
-			result.addValue(14.0, series1, type2);
-			result.addValue(13.0, series1, type3);
-			result.addValue(15.0, series1, type4);
-			result.addValue(15.0, series1, type5);
-			result.addValue(17.0, series1, type6);
-			result.addValue(17.0, series1, type7);
-			result.addValue(18.0, series1, type8);
-
-			result.addValue(15.0, series2, type1);
-			result.addValue(17.0, series2, type2);
-			result.addValue(16.0, series2, type3);
-			result.addValue(18.0, series2, type4);
-			result.addValue(14.0, series2, type5);
-			result.addValue(14.0, series2, type6);
-			result.addValue(12.0, series2, type7);
-			result.addValue(11.0, series2, type8);
-
-			return result;
-
-		}
-
-		// ****************************************************************************
-		// * JFREECHART DEVELOPER GUIDE *
-		// * The JFreeChart Developer Guide, written by David Gilbert, is
-		// available *
-		// * to purchase from Object Refinery Limited: *
-		// * *
-		// * http://www.object-refinery.com/jfreechart/guide.html *
-		// * *
-		// * Sales are used to provide funding for the JFreeChart project -
-		// please *
-		// * support us so that we can continue developing free software. *
-		// ****************************************************************************
 
 		/**
 		 * Creates a chart.
@@ -222,6 +123,32 @@ public class MotiveChartViewer extends ManagedComponent implements
 					new Font("SansSerif", Font.BOLD, 10), plot, false);
 			result.addLegend(new LegendTitle(statusPlot));
 			return result;
+
+		}
+
+		synchronized void update(WorkingMemoryChange wmc, final Motive motive) {
+			switch (wmc.operation) {
+			case ADD:
+				dsStatus.addValue(motive.status.value() + 1, motive.getClass()
+						.getSimpleName(), wmc.address.id);
+				dsInformationGain.addValue(motive.informationGain, "",
+						wmc.address.id);
+				dsTries.addValue(motive.tries, "", wmc.address.id);
+				break;
+			case DELETE:
+				dsStatus.removeValue(motive.getClass().getSimpleName(),
+						wmc.address.id);
+				dsInformationGain.removeValue("", wmc.address.id);
+				dsTries.removeValue("", wmc.address.id);
+				break;
+			case OVERWRITE:
+				dsStatus.setValue(motive.status.value() + 1, motive.getClass()
+						.getSimpleName(), wmc.address.id);
+				dsInformationGain.setValue(motive.informationGain, "",
+						wmc.address.id);
+				dsTries.setValue(motive.tries, "", wmc.address.id);
+				break;
+			}
 
 		}
 
@@ -286,28 +213,7 @@ public class MotiveChartViewer extends ManagedComponent implements
 	public void motiveChanged(Map<WorkingMemoryAddress, ObjectImpl> map,
 			WorkingMemoryChange wmc, ObjectImpl newMotive, ObjectImpl oldMotive) {
 		Motive motive = (Motive) newMotive;
-		switch (wmc.operation) {
-		case ADD:
-			guiFrame.dsStatus.addValue(motive.status.value() + 1, motive
-					.getClass().getSimpleName(), wmc.address.id);
-			guiFrame.dsInformationGain.addValue(motive.informationGain, "",
-					wmc.address.id);
-			guiFrame.dsTries.addValue(motive.tries, "", wmc.address.id);
-			break;
-		case DELETE:
-			guiFrame.dsStatus.removeValue(motive.getClass().getSimpleName(),
-					wmc.address.id);
-			guiFrame.dsInformationGain.removeValue("", wmc.address.id);
-			guiFrame.dsTries.removeValue("", wmc.address.id);
-			break;
-		case OVERWRITE:
-			guiFrame.dsStatus.setValue(motive.status.value() + 1, motive
-					.getClass().getSimpleName(), wmc.address.id);
-			guiFrame.dsInformationGain.setValue(motive.informationGain, "",
-					wmc.address.id);
-			guiFrame.dsTries.setValue(motive.tries, "", wmc.address.id);
-			break;
-		}
+		guiFrame.update(wmc, motive);
 	}
 
 }
