bfb4c5848951d8cb761fba122d4144414a491767
grettonc <grettonc@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-14 14:20:14 +0000

    Few commenting fixes
     - Charles-G

--- code/planning/trunk/include/CAST_SCAT/stl__tuple_hash.hh
+++ code/planning/trunk/include/CAST_SCAT/stl__tuple_hash.hh
@@ -16,120 +16,12 @@
  *
  * ABOUT :: 
  *
- * NONSENSE :: 
- *
- * \begin{quote}
- * 
- * ...
- *
- * VYVYAN: I've been down the morgue!
- * 
- * NEIL: Oh, fine, yeah, great. Let's talk about death, I mean, don't
- * consider my feelings tonight, or anything, really.
- * 
- * VYVYAN: Cutting up bodies for my course, you know.
- * 
- * RICK: None of you ever give the slightest consideration to a word I've
- * said!
- * 
- * VYVYAN: That's because you're very boring!
- * 
- * RICK: Oh! Oh, and I suppose you think ideas like peace and freedom and
- * equality are boring too!
- * 
- * VYVYAN: Yes, they are!
- * 
- * RICK: Ha! Fallen into my trap! In that case, why isn't Cliff Richard
- * boring, clever-trousers? Tell me that!
- * 
- *       [VYVYAN responds by pushing RICK's face down into his food.]
- * 
- * NEIL: Okay, lads, umm... this is it, okay, I'm going, now, this is the
- * final moment, okay? Right.
- * 
- * VYVYAN: I've got a leg.
- * 
- * MIKE: Hey Vyvyan, that's not unusual.
- * 
- * VYVYAN: No, look. I'm supposed to write an essay on it, right,
- * but... I think I'm just gonna stick it on the bonnet of my car!
- * 
- * \end{quote}
- * 
- *  -- Ben Elton, Rik Mayall, and Lise Mayer. The Young Ones -- TV
- * Series Premiere "Demolition". Original Airdate 09/11/1982.
- * 
  */
 
 #ifndef STL__TUPLE_HASH_HH
 #define STL__TUPLE_HASH_HH
 
 #include "debug.hh"
-
-/* Copyright (C) 2009
- * Charles Gretton (charles.gretton@gmail.com)
- *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * ABOUT :: 
- *
- * C++-0X has tuples. The following header defines 
- *
- * \begin{quote}
- * 
- * ...
- *
- * VYVYAN: I've been down the morgue!
- * 
- * NEIL: Oh, fine, yeah, great. Let's talk about death, I mean, don't
- * consider my feelings tonight, or anything, really.
- * 
- * VYVYAN: Cutting up bodies for my course, you know.
- * 
- * RICK: None of you ever give the slightest consideration to a word I've
- * said!
- * 
- * VYVYAN: That's because you're very boring!
- * 
- * RICK: Oh! Oh, and I suppose you think ideas like peace and freedom and
- * equality are boring too!
- * 
- * VYVYAN: Yes, they are!
- * 
- * RICK: Ha! Fallen into my trap! In that case, why isn't Cliff Richard
- * boring, clever-trousers? Tell me that!
- * 
- *       [VYVYAN responds by pushing RICK's face down into his food.]
- * 
- * NEIL: Okay, lads, umm... this is it, okay, I'm going, now, this is the
- * final moment, okay? Right.
- * 
- * VYVYAN: I've got a leg.
- * 
- * MIKE: Hey Vyvyan, that's not unusual.
- * 
- * VYVYAN: No, look. I'm supposed to write an essay on it, right,
- * but... I think I'm just gonna stick it on the bonnet of my car!
- * 
- * \end{quote}
- * 
- *  -- Ben Elton, Rik Mayall, and Lise Mayer. The Young Ones -- TV
- * Series Premiere "Demolition". Original Airdate 09/11/1982.
- * 
- */
-
-
 
 /* At the time of writing, the most recent version of GCC did not
  * support \class{std::tuple} hashing. I know the functional folk have
@@ -219,3 +111,48 @@ class boost_combine_based_tuple_hasher : public tuple_hasher<hash_combine__boost
 
 
 #endif
+
+
+/*
+ * \begin{quote}
+ * 
+ * ...
+ *
+ * VYVYAN: I've been down the morgue!
+ * 
+ * NEIL: Oh, fine, yeah, great. Let's talk about death, I mean, don't
+ * consider my feelings tonight, or anything, really.
+ * 
+ * VYVYAN: Cutting up bodies for my course, you know.
+ * 
+ * RICK: None of you ever give the slightest consideration to a word I've
+ * said!
+ * 
+ * VYVYAN: That's because you're very boring!
+ * 
+ * RICK: Oh! Oh, and I suppose you think ideas like peace and freedom and
+ * equality are boring too!
+ * 
+ * VYVYAN: Yes, they are!
+ * 
+ * RICK: Ha! Fallen into my trap! In that case, why isn't Cliff Richard
+ * boring, clever-trousers? Tell me that!
+ * 
+ *       [VYVYAN responds by pushing RICK's face down into his food.]
+ * 
+ * NEIL: Okay, lads, umm... this is it, okay, I'm going, now, this is the
+ * final moment, okay? Right.
+ * 
+ * VYVYAN: I've got a leg.
+ * 
+ * MIKE: Hey Vyvyan, that's not unusual.
+ * 
+ * VYVYAN: No, look. I'm supposed to write an essay on it, right,
+ * but... I think I'm just gonna stick it on the bonnet of my car!
+ * 
+ * \end{quote}
+ * 
+ *  -- Ben Elton, Rik Mayall, and Lise Mayer. The Young Ones -- TV
+ * Series Premiere "Demolition". Original Airdate 09/11/1982.
+ *
+ * /
