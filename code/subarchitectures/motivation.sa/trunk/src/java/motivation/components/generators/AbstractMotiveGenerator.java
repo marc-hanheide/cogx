89c8bddcbfa6c90fbef40f25003c10fce6b30fbc
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-16 13:57:50 +0000

    - removed stupic stack trace

--- code/subarchitectures/motivation.sa/trunk/src/java/motivation/components/generators/AbstractMotiveGenerator.java
+++ code/subarchitectures/motivation.sa/trunk/src/java/motivation/components/generators/AbstractMotiveGenerator.java
@@ -91,8 +91,7 @@ public abstract class AbstractMotiveGenerator extends ManagedComponent {
 				try {
 					unlockEntry(motiveAddress);
 				} catch (CASTException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					log("caught a CASTException when unlocking entry: "+e.message+ ". This can be safely ignored...");
 				}
 			}
 		}
