72db5b34e08ee7a346db49fa32fc57cc47cc9596
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-19 15:22:28 +0000

    added first pass fake detector

---
+++ code/subarchitectures/motivation.sa/trunk/src/java/vision/components/ObjectDetectorFAKE.java
@@ -0,0 +1,70 @@
+package vision.components;
+
+import cogx.Math.Matrix33;
+import cogx.Math.Vector3;
+import VisionData.DetectionCommand;
+import VisionData.Face;
+import VisionData.GeometryModel;
+import VisionData.Vertex;
+import VisionData.VisualObject;
+import VisionData.VisualObjectView;
+import cast.CASTException;
+import cast.architecture.ChangeFilterFactory;
+import cast.architecture.ManagedComponent;
+import cast.architecture.WorkingMemoryChangeReceiver;
+import cast.cdl.WorkingMemoryChange;
+import cast.cdl.WorkingMemoryOperation;
+
+public class ObjectDetectorFAKE extends ManagedComponent implements
+		WorkingMemoryChangeReceiver {
+
+	public ObjectDetectorFAKE() {
+	}
+
+	@Override
+	protected void start() {
+		addChangeFilter(ChangeFilterFactory.createTypeFilter(
+				DetectionCommand.class, WorkingMemoryOperation.ADD), this);
+
+	}
+
+	@Override
+	public void workingMemoryChanged(WorkingMemoryChange _arg0)
+			throws CASTException {
+
+		// load command
+		DetectionCommand dc = getMemoryEntry(_arg0.address,
+				DetectionCommand.class);
+
+		// just 'cos
+		sleepComponent(500);
+
+		for (String label : dc.labels) {
+			// for the time being just fail
+			VisualObject obj = newVisualObject();
+			obj.detectionConfidence = 0d;
+			obj.label = label;
+			addToWorkingMemory(newDataID(), obj);
+		}
+
+	}
+
+	/**
+	 * @return
+	 */
+	private VisualObject newVisualObject() {
+		VisualObject obj = new VisualObject(new cogx.Math.Pose3(newVector3(), new Matrix33(0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d)),
+				new String[0], 0d, new cogx.Math.Sphere3(newVector3(), 0d), getCASTTime(),
+				new VisualObjectView[0], new GeometryModel(new Vertex[0], new Face[0]), "", 0d, 0d);
+		return obj;
+	}
+
+	/**
+	 * @return
+	 */
+	private Vector3 newVector3() {
+		return new Vector3(0d,
+				0d, 0d);
+	}
+
+}
