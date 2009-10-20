7887089e9788548b10e7c0ae3f51610f28e35f8a
mxz <mxz@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-20 21:56:10 +0000

    some changes, bugfixes maybe

---
+++ code/tools/hardware/stereo/branches/stable-1/src/c++/components/StereoClient.cpp
@@ -0,0 +1,85 @@
+/**
+ * @author Michael Zillich
+ * @date June 2009
+ */
+
+#include <sstream>
+#include <Ice/Ice.h>
+#include <cast/core/CASTUtils.hpp>
+#include "StereoClient.h"
+
+namespace cast
+{
+
+using namespace std;
+
+StereoClient::StereoClient()
+{
+  stereoServerHost = "localhost";
+  stereoServerName = "";
+  stereoServerPort = cdl::CPPSERVERPORT;
+}
+
+void StereoClient::startStereoCommunication(CASTComponent &owner)
+  throw(runtime_error)
+{
+  ostringstream stereoServerAddr;
+  Ice::Identity id;
+
+  id.name = stereoServerName;
+  id.category = "StereoServer";
+  stereoServerAddr << owner.getCommunicator()->identityToString(id)
+    << ":default -h " << stereoServerHost << " -p " << stereoServerPort;
+
+  Ice::ObjectPrx base = owner.getCommunicator()->stringToProxy(stereoServerAddr.str());
+  // doing a checkedCast here freezes the server
+  // stereoServer = Stereo::StereoInterfacePrx::checkedCast(base);
+  stereoServer = Stereo::StereoInterfacePrx::uncheckedCast(base);
+  if(!stereoServer)
+    throw runtime_error(exceptionMessage(__HERE__,
+          "failed to connect to stereo server: %s",
+          stereoServerAddr.str().c_str()));
+}
+
+void StereoClient::configureStereoCommunication(const map<string,string> & _config)
+  throw(runtime_error)
+{
+  map<string,string>::const_iterator it;
+
+  if((it = _config.find("--stereohost")) != _config.end())
+  {
+    stereoServerHost = it->second;
+  }
+  if((it = _config.find("--stereoname")) != _config.end())
+  {
+    stereoServerName = it->second;
+  }
+
+  // sanity checks: Have all important things be configured? Is the
+  // configuration consistent?
+  if(stereoServerHost.empty())
+    throw runtime_error(exceptionMessage(__HERE__, "no stereo server host given"));
+  if(stereoServerName.empty())
+    throw runtime_error(exceptionMessage(__HERE__, "no stereo server name given"));
+}
+
+void StereoClient::getPoints(bool transformToGlobal, VisionData::SurfacePointSeq& points)
+{
+  stereoServer->getPoints(transformToGlobal, points);
+}
+
+void StereoClient::getPointsInSOI(bool transformToGlobal, const VisionData::SOI &soi,
+    VisionData::SurfacePointSeq& points)
+{
+  VisionData::SOIPtr soiPtr = new VisionData::SOI;
+  *soiPtr = soi;
+  stereoServer->getPointsInSOI(transformToGlobal, soiPtr, points);
+}
+
+void StereoClient::getRectImage(int side, Video::Image& image)
+{
+  stereoServer->getRectImage(side, image);
+}
+
+}
+
---
+++ code/tools/hardware/stereo/branches/stable-1/src/c++/components/StereoServer.h
@@ -0,0 +1,151 @@
+/**
+ * @author Michael Zillich
+ * @date June 2009
+ */
+
+#ifndef STEREO_SERVER_H
+#define STEREO_SERVER_H
+
+#include <stdexcept>
+#include <vector>
+#include <map>
+#include <string>
+#include <cast/core/CASTComponent.hpp>
+#include <VideoClient.h>
+#include "StereoCamera.h"
+#include "Math.hpp"
+#include "Stereo.hpp"
+#include "gpustereo/CensusGPU.h"
+
+namespace cast
+{
+
+class StereoServer;
+
+/**
+ * Ice interface to a stereo server.
+ */
+class StereoServerI : public Stereo::StereoInterface
+{
+private:
+  StereoServer *stereoSrv;
+
+public:
+  StereoServerI(StereoServer *_stereo) : stereoSrv(_stereo) {}
+
+  /**
+   * Returns the 3D point cloud.
+   */
+  virtual void getPoints(bool transformToGlobal, VisionData::SurfacePointSeq& points, const Ice::Current&);
+
+  /**
+   * Returns part of the 3D point cloud inside given SOI.
+   */
+  virtual void getPointsInSOI(bool transformToGlobal, const VisionData::SOIPtr &soi,
+      VisionData::SurfacePointSeq& points, const Ice::Current&);
+
+  virtual void getRectImage(Ice::Int side, Video::Image& image, const Ice::Current&);
+};
+
+class StereoServer : public CASTComponent,
+                     public VideoClient
+{
+private:
+  /**
+   * Ice (servant) name for video interface
+   */
+  std::string iceStereoName;
+  /**
+   * Ice port for video interface
+   */
+  int iceStereoPort;
+
+  /**
+   * Camera IDs for getting left and right images
+   */
+  std::vector<int> camIds;
+
+  /**
+   * component ID of the video server to connect to
+   */
+  std::string videoServerName;
+
+  /**
+   * our ICE proxy to the video server
+   */
+  Video::VideoInterfacePrx videoServer;
+
+  /**
+   * Stereo parameters
+   */
+  StereoCamera stereoCam;
+
+  /**
+   * The GPU stereo matching code.
+   */
+  CensusGPU *census;
+
+  // stereo works better/faster with smaller images, so we might want to use a
+  // smaller resolution
+  int stereoWidth;
+  int stereoHeight;
+
+  /**
+   * Size of median filter for specle removeal in the disparity image.
+   * 0 = not median filtering
+   */
+  int medianSize;
+
+  /**
+   * maximum disparity range we want to search
+   */
+  int maxDisp;
+
+  IplImage *colorImg[2];
+  IplImage *rectColorImg[2];
+  IplImage *rectGreyImg[2];
+  IplImage *disparityImg;
+
+  bool doDisplay;
+  bool logImages;
+
+  /**
+   * Create Ice video interface.
+   */
+  void setupMyIceCommunication();
+
+  void configure(const std::map<std::string,std::string> & _config)
+    throw(std::runtime_error);
+
+  virtual void start();
+
+  virtual void runComponent();
+
+public:
+  StereoServer();
+  virtual ~StereoServer();
+
+  /**
+   * Returns the 3D point cloud.
+   */
+  void getPoints(bool transformToGlobal, std::vector<VisionData::SurfacePoint> &points);
+
+  /**
+   * Returns part of the 3D point cloud inside given SOI.
+   */
+  void getPointsInSOI(bool transformToGlobal, const VisionData::SOI &soi,
+      std::vector<VisionData::SurfacePoint> &points);
+
+  void getRectImage(int side, Video::Image& image);
+
+  /**
+   * The callback function for images pushed by the image server.
+   * To be overwritten by derived classes.
+   */
+  virtual void receiveImages(const std::vector<Video::Image>& images);
+};
+
+}
+
+#endif
+
