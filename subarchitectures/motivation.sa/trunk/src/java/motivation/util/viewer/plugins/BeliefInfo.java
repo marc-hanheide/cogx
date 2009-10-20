cd607f09922c6a0b98aab6f29b10cf84bba417c2
gj <gj@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-20 08:52:19 +0000

    BeliefInfo plugin

--- subarchitectures/motivation.sa/trunk/src/java/motivation/util/viewer/plugins/BeliefInfo.java
+++ subarchitectures/motivation.sa/trunk/src/java/motivation/util/viewer/plugins/BeliefInfo.java
@@ -20,22 +20,27 @@ public class  BeliefInfo implements Plugin {
 
 	@Override
 	public Vector<Object> toVector(Ice.ObjectImpl iceObject) {
-		Belief belief = (Belief) iceObject;
 		Vector<Object> extraInfo=new Vector<Object>();
-		String agentStatus = "{robot}";
-		if (belief.ags instanceof AttributedAgentStatus) {
-			agentStatus = "{robot[human]}";
+		Belief belief = (Belief) iceObject;
+		if (belief != null) { 
+			String agentStatus = "{robot}";
+			if (belief.ags instanceof AttributedAgentStatus) {
+				agentStatus = "{robot[human]}";
+			}
+			else if (belief.ags instanceof MutualAgentStatus) {
+				agentStatus = "{robot,human}";
+			} // end if..else for agent status
+			extraInfo.add("Status:"+agentStatus);
+			if (belief.sigma != null) { 
+				extraInfo.add("ST frame:"+belief.sigma.id);
+			} else { 
+				extraInfo.add("ST frame: [continue active]");
+			} 
+			if (belief.phi instanceof UncertainSuperFormula) { 
+				extraInfo.add("Phi: "+BeliefModelUtils.getFormulaPrettyPrint(((UncertainSuperFormula)belief.phi)));			
+				extraInfo.add("Prob: "+((UncertainSuperFormula)belief.phi).prob);
+			} 
 		}
-		else if (belief.ags instanceof MutualAgentStatus) {
-			agentStatus = "{robot,human}";
-		} // end if..else for agent status
-		extraInfo.add("Status:"+agentStatus);
-		extraInfo.add("ST frame:"+belief.sigma.id);
-
-		if (belief.phi instanceof UncertainSuperFormula) { 
-			extraInfo.add("Phi: "+BeliefModelUtils.getFormulaPrettyPrint(((UncertainSuperFormula)belief.phi)));			
-			extraInfo.add("Prob: "+((UncertainSuperFormula)belief.phi).prob);
-		} 
 		return extraInfo;
 	}
 
