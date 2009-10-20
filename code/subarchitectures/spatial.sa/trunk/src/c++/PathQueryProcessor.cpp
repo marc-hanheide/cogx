0a03b9af6d4f3c85ff0a5de84c938820b9f4c559
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-20 18:11:01 +0000

    fixed Path query for PLACEHOLDERS... this changed has to be approved by someone who is more familiar with the details

--- code/subarchitectures/spatial.sa/trunk/src/c++/PathQueryProcessor.cpp
+++ code/subarchitectures/spatial.sa/trunk/src/c++/PathQueryProcessor.cpp
@@ -421,7 +421,7 @@ PathQueryProcessor::newPathTransitionCostRequest(const cast::cdl::WorkingMemoryC
 {
   CASTData<SpatialData::PathTransitionCostRequest> oobj =
     getMemoryEntryWithData<SpatialData::PathTransitionCostRequest>(objID.address);
-
+  debug("entered newPathTransitionCostRequest");
   //if (oobj != 0) {
 
   SpatialData::PathTransitionCostRequestPtr ptr = oobj.getData();
@@ -436,6 +436,7 @@ PathQueryProcessor::newPathTransitionCostRequest(const cast::cdl::WorkingMemoryC
   }
 
   m_GraphMutex.lock();
+  debug("have lock");
 
   //Firstly, check if start place is a placeholder
   FrontierInterface::NodeHypothesisPtr startHyp =
@@ -456,6 +457,7 @@ PathQueryProcessor::newPathTransitionCostRequest(const cast::cdl::WorkingMemoryC
     else { //startNode != 0
       //Start place is an extant node
       //Secondly, check if goal place is a placeholder
+      NavData::FNodePtr goalNode;
       FrontierInterface::NodeHypothesisPtr goalHyp =
 	m_placeInterface->getHypFromPlaceID(ptr->goalPlaceID);
       if (goalHyp != 0) {
@@ -463,58 +465,68 @@ PathQueryProcessor::newPathTransitionCostRequest(const cast::cdl::WorkingMemoryC
 
 	//For unexplored edges, simply model cost as metric distance
 	//between the node and the frontier
-	bool reachable = goalHyp->originPlaceID == ptr->startPlaceID;
-	if (reachable) {
+	bool directlyReachable = goalHyp->originPlaceID == ptr->startPlaceID;
+	if (directlyReachable) {
 	  double distSq = (goalHyp->x - startNode->x)*(goalHyp->x - startNode->x) +
 	    (goalHyp->y - startNode->y)*(goalHyp->y - startNode->y);
 	  ptr->cost = sqrt(distSq);
+	  log("found direct connection... can break here");
+	  ptr->status = SpatialData::QUERYCOMPLETED;
 	}
-	else
-	{
-	  ptr->cost = 1e100;
+	else {
+	  goalNode =
+	    m_placeInterface->getNodeFromPlaceID(goalHyp->originPlaceID);
 	}
-	ptr->status = SpatialData::QUERYCOMPLETED;
+//	else
+//	{
+//	  log("frontier not reachable: %ld != %ld", goalHyp->originPlaceID, ptr->startPlaceID);
+//	  ptr->cost = 1e100;
+//	}
+//	ptr->status = SpatialData::QUERYCOMPLETED;
       }
       else { //goalHyp == 0
-	NavData::FNodePtr goalNode =
+	goalNode =
 	  m_placeInterface->getNodeFromPlaceID(ptr->goalPlaceID);
-	if (goalNode == 0) {
-	  log("Could not find goal Place!");
-	  ptr->status = SpatialData::QUERYPLACE2INVALID;
-	}
-	else { //goalNode != 0
-	  //Goal place is an extant node
-	  int startNodeID = startNode->nodeId;
-	  int goalNodeID = goalNode->nodeId;
+      } //goalHyp != 0
+      // if we are not yet done
+      if (ptr->status != SpatialData::QUERYCOMPLETED) {
+      if (goalNode == 0) {
+	log("Could not find goal Place!");
+	ptr->status = SpatialData::QUERYPLACE2INVALID;
+      }
+      else { //goalNode != 0
+	//Goal place is an extant node
+	int startNodeID = startNode->nodeId;
+	int goalNodeID = goalNode->nodeId;
 
-	  std::list<Cure::NavGraphNode> path;
-	  double cost = 1e100;
-	  debug("findPath(%ld, %ld,....)",startNodeID, goalNodeID);
-	  int ret = m_NavGraph.findPath(ptr->startPlaceID, ptr->goalPlaceID,
+	std::list<Cure::NavGraphNode> path;
+	double cost = 1e100;
+	debug("findPath(%ld, %ld,....)",startNodeID, goalNodeID);
+	int ret = m_NavGraph.findPath(startNodeID, goalNodeID,
 	      path, &cost);
 
-	  if (m_noIndirectPaths) {
-	    if (ret == 0 && path.size() < 3) {
-	      ptr->cost = cost;
-	    }
-	    else {
-	      ptr->cost = 1e100;
-	    }
+	if (m_noIndirectPaths) {
+	  if (ret == 0 && path.size() < 3) {
+	    ptr->cost = cost;
 	  }
-	  else { //m_noIndirectPaths
-	    if (ret == 0) {
-	      ptr->cost = cost;
-	    }
-	    else {
-	      ptr->cost = 1e100;
-	    }
+	  else {
+	    ptr->cost = 1e100;
 	  }
-	  ptr->status = SpatialData::QUERYCOMPLETED;
-	} //goalNode != 0
+	}
+	else { //m_noIndirectPaths
+	  if (ret == 0) {
+	    ptr->cost = cost;
+	  }
+	  else {
+	    ptr->cost = 1e100;
+	  }
+	}
+	ptr->status = SpatialData::QUERYCOMPLETED;
       } //goalHyp == 0
+      } //ptr->status != SpatialData::QUERYCOMPLETED
     } //startNode != 0
   } //startHyp == 0
-
+  debug("write PathTransitionCostRequest");
   overwriteWorkingMemory<SpatialData::PathTransitionCostRequest>(objID.address, ptr);
   m_GraphMutex.unlock();
   //}
