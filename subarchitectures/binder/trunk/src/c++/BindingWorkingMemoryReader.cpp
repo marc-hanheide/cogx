0a468989d8280dca31fb80baf6bc72865d4f018e
alen <alen@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-22 20:12:07 +0000

    Current unions are now a map instead of a vector (easier access) and some minors.

--- subarchitectures/binder/trunk/src/c++/BindingWorkingMemoryReader.cpp
+++ subarchitectures/binder/trunk/src/c++/BindingWorkingMemoryReader.cpp
@@ -3,6 +3,7 @@
 
 namespace binder {
 
+  using namespace std;
   using namespace autogen::core;
   using namespace cast;
   using namespace cast::cdl;
@@ -10,35 +11,50 @@ namespace binder {
   void
   BindingWorkingMemoryReader::start() {
     
+    m_haveAddr = false;
+
     // if the set of possible union configurations has been updated,
     // update the monitor accordingly
-
     
-    addChangeFilter(createGlobalTypeFilter<UnionConfiguration>(),
+    addChangeFilter(createGlobalTypeFilter<UnionConfiguration>(cdl::ADD),
 		    new MemberFunctionChangeReceiver<BindingWorkingMemoryReader>(this,
-										 &BindingWorkingMemoryReader::fetchThenExtract));
+			&BindingWorkingMemoryReader::fetchThenExtract));
+
+    addChangeFilter(createGlobalTypeFilter<UnionConfiguration>(cdl::OVERWRITE),
+		    new MemberFunctionChangeReceiver<BindingWorkingMemoryReader>(this,
+			&BindingWorkingMemoryReader::fetchThenExtract));
   }
   
   void 
   BindingWorkingMemoryReader::fetchThenExtract(const cast::cdl::WorkingMemoryChange & _wmc) {
+      addrFetchThenExtract(_wmc.address);
+      m_currentUnionsAddr = _wmc.address;
+      m_haveAddr = true;
+  }
+
+  void 
+  BindingWorkingMemoryReader::addrFetchThenExtract(const cast::cdl::WorkingMemoryAddress & _addr) {
     try {
       UnionConfigurationPtr config = 
-	getMemoryEntry<UnionConfiguration>(_wmc.address);
+	getMemoryEntry<UnionConfiguration>(_addr);
       extractUnionsFromConfig(config);
     }
     catch (CASTException & e) {
       println("EXCEPTION BindingWorkingMemoryReader::fetchThenExtract: %s", e.message.c_str());
     }
-  }
+  } 
 
   void BindingWorkingMemoryReader::extractUnionsFromConfig (UnionConfigurationPtr _config) {
+
     m_currentUnions.clear();
     for (UnionSequence::iterator i = _config->includedUnions.begin(); 
 	 i < _config->includedUnions.end() ; ++i) {
-      m_currentUnions.push_back(*i);
+	UnionPtr uni = *i;
+	m_currentUnions.insert(pair<string, UnionPtr>(uni->entityID, uni));
+	debug("Union ID %s is part of the current configuration", uni->entityID.c_str());
     }
 
-    println("Wow, I currently have %d unions", m_currentUnions.size());
+    log("Wow, I currently have %d unions", m_currentUnions.size());
   }
 
 }
