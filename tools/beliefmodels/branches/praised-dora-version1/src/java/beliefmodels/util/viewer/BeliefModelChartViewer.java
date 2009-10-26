f3dba0cd7d52ca93e517c0628b8bee73931bd739
Marc Hanheide <marc@hanheide.net>
2009-10-26 09:51:43 +0000

    branch for demo

---
+++ tools/beliefmodels/branches/praised-dora-version1/src/java/beliefmodels/util/viewer/BeliefModelChartViewer.java
@@ -0,0 +1,333 @@
+
+// =========================================================
+// PACKAGE 
+// =========================================================
+
+package beliefmodels.util.viewer;
+
+// =========================================================
+// IMPORTS
+// =========================================================
+
+// ---------------------------------------------------------
+// BINDER, BELIEFMODELS imports
+// ---------------------------------------------------------
+
+import beliefmodels.adl.Belief;
+import beliefmodels.adl.BeliefModel;
+import beliefmodels.adl.AttributedAgentStatus;
+import beliefmodels.adl.PrivateAgentStatus;
+import beliefmodels.adl.MutualAgentStatus;
+import beliefmodels.domainmodel.cogx.ContinualStatus;
+import beliefmodels.domainmodel.cogx.ContinualFormula;
+import beliefmodels.domainmodel.cogx.UncertainSuperFormula;
+
+
+
+// ---------------------------------------------------------
+// CAST imports
+// ---------------------------------------------------------
+
+import cast.architecture.ManagedComponent;
+import cast.architecture.ChangeFilterFactory;
+import cast.architecture.WorkingMemoryChangeReceiver;
+import cast.cdl.WorkingMemoryAddress;
+import cast.cdl.WorkingMemoryChange;
+import cast.cdl.WorkingMemoryOperation;
+
+// ---------------------------------------------------------
+// ICE imports
+// ---------------------------------------------------------
+
+import Ice.ObjectImpl;
+
+// ---------------------------------------------------------
+// JAVA imports
+// ---------------------------------------------------------
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.util.Map;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+
+// ---------------------------------------------------------
+// JFreeChart imports
+// ---------------------------------------------------------
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.annotations.CategoryAnnotation;
+import org.jfree.chart.annotations.CategoryTextAnnotation;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberAxis3D;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.labels.StandardCategoryToolTipGenerator;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.CombinedDomainCategoryPlot;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.BarRenderer3D;
+import org.jfree.chart.renderer.category.LayeredBarRenderer;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
+import org.jfree.chart.renderer.category.StackedBarRenderer;
+import org.jfree.chart.renderer.category.StackedBarRenderer3D;
+import org.jfree.chart.title.LegendTitle;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.RefineryUtilities;
+
+/** 
+The class <b>BeliefModelChartViewer</b> on-line charts the operations on a belief model: 
+ 
+ <ul>
+ <li> number of beliefs (robot), certainty (robot[human]), and truth ({robot,human}) beliefs. 
+ </ul>
+
+ 
+ @author Geert-Jan Kruijff (gj@dfki.de)
+ @author Marc Hanheide
+ @started 091013
+ @version 091013
+ 
+ */ 
+
+public class BeliefModelChartViewer 
+	extends ManagedComponent { 
+
+	protected class Frame extends JFrame {
+
+		// beliefs beliefs
+		DefaultCategoryDataset dsBeliefs;
+		// certainty beliefs
+		DefaultCategoryDataset dsCertainty;
+		// truth beliefs
+		DefaultCategoryDataset dsTruth;
+		
+		CategoryPlot beliefsPlot;
+		CategoryPlot certaintyPlot;
+		CategoryPlot truthPlot;
+
+
+		
+		/**
+		 * constructor initializes the GUI
+		 */
+
+		/**
+		 * Creates a new frame instance.
+		 * 
+		 * @param title
+		 *            the frame title.
+		 */
+		public Frame(final String title) {
+			super(title);
+			dsBeliefs = new DefaultCategoryDataset();
+			dsCertainty = new DefaultCategoryDataset();
+			dsTruth = new DefaultCategoryDataset();
+			final ChartPanel chartPanel = new ChartPanel(createChart());
+			chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+			setContentPane(chartPanel);
+		}
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -6559252766578349920L;
+
+
+		/**
+		 * Creates a chart. 
+		 * 
+		 * @return A chart.
+		 */
+		private JFreeChart createChart() {
+
+			// set up the beliefs beliefs
+			final NumberAxis beliefsAxis = new NumberAxis3D("Beliefs");
+			beliefsAxis.setLowerBound(0);
+			beliefsAxis.setUpperBound(5);
+			beliefsAxis
+					.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+			final StackedBarRenderer3D beliefsRender = new StackedBarRenderer3D();
+			beliefsRender
+					.setBaseToolTipGenerator(new StandardCategoryToolTipGenerator());
+			beliefsPlot = new CategoryPlot(dsBeliefs, null, beliefsAxis,
+					beliefsRender);
+			beliefsPlot.setDomainGridlinesVisible(true);
+
+			// set up the certainty beliefs
+			final NumberAxis certaintyAxis = new NumberAxis("Certainty");
+			certaintyAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+			final StackedBarRenderer3D certaintyRender = new StackedBarRenderer3D();
+			certaintyRender
+					.setBaseToolTipGenerator(new StandardCategoryToolTipGenerator());
+			certaintyPlot = new CategoryPlot(dsCertainty, null, certaintyAxis,
+					certaintyRender);
+			certaintyPlot.setDomainGridlinesVisible(true);
+
+			// set up the truth beliefs
+			final NumberAxis truthAxis = new NumberAxis("Status");
+			truthAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+			final BarRenderer3D truthRender = new BarRenderer3D();
+			truthRender
+					.setBaseToolTipGenerator(new StandardCategoryToolTipGenerator());
+			truthPlot = new CategoryPlot(dsTruth, null, truthAxis, truthRender);
+			truthPlot.setDomainGridlinesVisible(true);
+
+			final CategoryAxis domainAxis = new CategoryAxis("WM Id");
+			final CombinedDomainCategoryPlot plot = new CombinedDomainCategoryPlot(
+					domainAxis);
+			plot.add(beliefsPlot, 3);
+			plot.add(certaintyPlot, 2);
+			plot.add(truthPlot, 1);
+
+			final JFreeChart result = new JFreeChart("BeliefModel Monitor",
+					new Font("SansSerif", Font.BOLD, 10), plot, false);
+			result.addLegend(new LegendTitle(beliefsPlot));
+			result.addLegend(new LegendTitle(certaintyPlot));			
+			result.addLegend(new LegendTitle(truthPlot));
+			return result;
+
+		}
+
+		synchronized void update(WorkingMemoryChange wmc, final Belief belief) {
+			switch (wmc.operation) {
+			case ADD:
+					if (belief.ags instanceof PrivateAgentStatus) {
+						dsBeliefs.addValue(1, "private: {robot}", wmc.address.id);
+					} else if (belief.ags instanceof AttributedAgentStatus) { 
+						dsBeliefs.addValue(1, "attributed: {robot[human]}", wmc.address.id);				
+					} else if (belief.ags instanceof MutualAgentStatus) {
+						dsBeliefs.addValue(1, "shared: {robot,human}", wmc.address.id);						
+					}
+					dsCertainty.setValue(((UncertainSuperFormula)belief.phi).prob, "certainty", wmc.address.id);
+					int polarity = 1;
+					if (belief.phi instanceof ContinualFormula) { 
+						ContinualFormula phi = (ContinualFormula)belief.phi;
+						if (phi.polarity == false) { polarity = -1; }
+						if (phi.cstatus == ContinualStatus.proposition) { 
+							dsTruth.setValue(((UncertainSuperFormula)belief.phi).prob * polarity, "factual", wmc.address.id);
+						} else { 
+							dsTruth.setValue(((UncertainSuperFormula)belief.phi).prob * polarity, "unknown", wmc.address.id);
+						} 
+					} 
+					break;
+			case DELETE:
+				dsBeliefs.removeValue("beliefs", wmc.address.id);
+					if (belief.ags instanceof PrivateAgentStatus) {
+						dsBeliefs.removeValue("private: {robot}", wmc.address.id);
+					} else if (belief.ags instanceof AttributedAgentStatus) { 
+						dsBeliefs.removeValue("attributed: {robot[human]}", wmc.address.id);						
+					} else if (belief.ags instanceof MutualAgentStatus) {
+						dsBeliefs.removeValue("shared: {robot,human}", wmc.address.id);													   
+					}
+					dsTruth.removeColumn(wmc.address.id);
+				break;
+			case OVERWRITE:
+					if (belief.ags instanceof PrivateAgentStatus) {
+						dsBeliefs.addValue(1, "private: {robot}", wmc.address.id);
+					} else if (belief.ags instanceof AttributedAgentStatus) { 
+						dsBeliefs.addValue(1, "attributed: {robot[human]}", wmc.address.id);						
+					} else if (belief.ags instanceof MutualAgentStatus) {
+						dsBeliefs.addValue(1, "shared: {robot,human}", wmc.address.id);
+					}
+					dsCertainty.setValue(((UncertainSuperFormula)belief.phi).prob, "certainty", wmc.address.id);
+					break;
+			}
+
+		}
+
+			
+		
+	}
+
+		
+	// global variable for the gui
+	Frame guiFrame;		
+		
+	public BeliefModelChartViewer() {
+		super();
+		guiFrame = new Frame(this.getClass().getSimpleName());			
+	}
+		
+		
+		
+		
+	/*
+	 * start up the GUI, and register the change listeners for Belief and BeliefModel objects
+	 * 
+	 * @see cast.core.CASTComponent#start()
+	 */
+	@Override
+	protected void start() {
+
+		/* check if this is actually started in a cast system */
+		if (this.getSubarchitectureID() != null) {
+			super.start();
+		} else { // testmode
+
+		}
+
+		guiFrame.pack();
+		RefineryUtilities.centerFrameOnScreen(guiFrame);
+		guiFrame.setVisible(true);
+		// 
+		addChangeFilter(ChangeFilterFactory.createGlobalTypeFilter(Belief.class,
+																   WorkingMemoryOperation.WILDCARD), new WorkingMemoryChangeReceiver() {
+						
+					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+						try {
+							Belief belief = getMemoryEntry(_wmc.address, Belief.class);
+							guiFrame.update(_wmc, belief);
+						}
+						catch (Exception e) {
+							e.printStackTrace();
+						}
+					} 
+				});
+		
+		addChangeFilter(ChangeFilterFactory.createGlobalTypeFilter(BeliefModel.class,
+																   WorkingMemoryOperation.OVERWRITE), new WorkingMemoryChangeReceiver() {
+						
+					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+						try {
+							BeliefModel bmodel = getMemoryEntry(_wmc.address, BeliefModel.class);
+							
+						}
+						catch (Exception e) {
+							e.printStackTrace();
+						}
+					} 
+				});
+		
+		
+		
+
+	}
+
+	/**
+	 * Starting point for the demonstration application.
+	 * 
+	 * @param args
+	 *            ignored.
+	 * @throws InterruptedException
+	 */
+	public static void main(final String[] args) throws InterruptedException {
+
+		final BeliefModelChartViewer demo = new BeliefModelChartViewer();
+		demo.start();
+		Thread.sleep(2000);
+		// Thread.sleep(2000);
+		// demo.dataset.setValue(10, "First", "Type 7");
+		// Thread.sleep(2000);
+		// demo.dataset.setValue(20, "Second", "Type 10");
+
+	}
+
+
+
+}
---
+++ tools/beliefmodels/branches/praised-dora-version1/src/slice/DomainModel.ice
@@ -0,0 +1,174 @@
+#ifndef DOMAINMODEL_ICE 
+#define DOMAINMODEL_ICE
+  
+// ===================================================================
+// MODULE: beliefmodels.domainmodel.sample
+// 
+// Defines sample domain models for belief models
+// 
+// Authors:		Geert-Jan M. Kruijff	<gj@dfki.de>
+// 
+// For an edit log, see the bottom of this file. 
+//
+// ===================================================================
+
+#include <BeliefModels.ice>
+
+module beliefmodels { 
+module domainmodel { 
+module cogx { 
+	 
+			
+	// ===================================================================
+	// GROUNDED DOMAIN MODEL 
+	// A domain model is a collection of formula types, to define the 
+	// interfaces Formula and GoalFormula used in the belief models ADL 
+	// definition. We adopt a grounded domain model here: the "truth" value of 
+	// a belief is based in how it is connected with other structures. 
+	
+	// The class SuperFormula implements the Formula interface, acting as supertype class. 
+	// As an EpistemicObject, each SuperFormula has an identifier. 
+	
+	class SuperFormula extends beliefmodels::adl::Formula {
+	};
+	 
+	// The class UncertainSuperFormula extends the supertype with an uncertainty value
+				
+	class UncertainSuperFormula extends SuperFormula { 
+		float prob;
+	}; 
+		
+	// The class LogicalSuperFormula extends the supertype with a boolean value
+	
+	class LogicalSuperFormula extends SuperFormula { 
+		bool truth;
+	};  
+		
+	// A complex formula is a super formula of the form (LHS OP RHS). 
+	// Because ComplexFormula implements the SuperFormula class, and LHS and RHS are specified to be 
+	// of this class, we can create arbitrarily complex formulas. Furthermore, LHS and RHS can be 
+	// logical formulas, or uncertain formulas (this is not determined a priori).  
+				
+	enum LogicalOp { and, or , xor, none};				
+	 	
+					
+	sequence<SuperFormula> SuperFormulaSeq;
+	 	
+	class ComplexFormula extends UncertainSuperFormula { 
+		LogicalOp op;
+		SuperFormulaSeq formulae;
+	};
+	 
+
+	// GROUNDED BELIEFS
+	// We extend the ADL notion of Belief with structure to indicate how
+	// the belief is grounded in other structures. Grounding indicates several 
+	// aspects: What other structures the belief is related to (by reference; Id), 
+	// what the status of the belief is (by value; different "truth" values for
+	// assertions, propositions), and a formula explaining the reason for the grounding 
+	// status (empty, to indicate truth; or a complex formula, explaining a negative value). 
+
+	sequence<string>Ids; 
+
+	enum GroundStatus { assertionVerified, assertionFalsified, propositionTrue, propositionFalse, propositionAmbiguous}; 
+
+	class Ground { 
+		Ids indexSet;
+		GroundStatus gstatus;
+		string modality;  // source modality 
+		beliefmodels::adl::Formula reason;
+	}; 
+
+	class GroundedBelief extends beliefmodels::adl::Belief { 
+		Ground grounding;
+	}; 
+	
+	// A continual formula is a formula that is possibly uncertain, and which can be 
+	// either asserted, or be a proposition. 
+	
+	enum ContinualStatus { proposition, assertion };
+	
+	class ContinualFormula extends UncertainSuperFormula {
+		ContinualStatus cstatus;
+		bool polarity;
+	}; 
+					
+	// ===================================================================
+	// DOMAINMODEL: VISUAL OBJECTS 
+	
+	enum Shape { cylindrical, spherical, cubic, unknownShape };
+	
+	enum ObjectType { box, ball, cube, mug, unknownObjectType }; 
+	
+	enum Color { red, blue, yellow, green, unknownColor };
+	
+	enum Graspable { grasp, nograsp , unknownGrasp } ;
+	
+	// A property is always a ContinualFormula
+	class ObjectTypeProperty extends ContinualFormula { 
+		ObjectType typeValue;
+	}; 
+	
+	class LocationProperty extends ContinualFormula {
+		string location;
+	};
+	
+	 
+	class GraspableProperty extends ContinualFormula {
+		Graspable graspableValue;
+	};
+	
+	// A property is always a ContinualFormula
+	class ColorProperty extends ContinualFormula { 
+		Color colorValue;
+	};  
+	
+	sequence<ColorProperty> Colors;
+	
+	class ShapeProperty extends ContinualFormula { 
+		Shape shapeValue;
+	};  
+	
+	enum Saliency {low, high, unknownSaliency};
+	
+	class SaliencyProperty extends ContinualFormula {
+		Saliency sal;
+	}; 
+	
+	class LinguisticLabelProperty extends ContinualFormula { 
+		string label;
+	};  
+	 
+	class LinguisticAttributeProperty extends ContinualFormula {
+		string attribute;
+	};
+	
+	class UnionRefProperty extends ContinualFormula {
+		string unionRef;
+	};
+	
+	enum Proximity {proximal, distal, unknownProximity};
+	
+	class ProximityProperty extends ContinualFormula {
+		Proximity prox;
+	};
+	
+	// ==========================
+	// FOREGROUNDING STUFF
+	
+	dictionary<string,double> SaliencyAssignment;
+	
+	
+	
+}; // end cogx 
+}; // end domainmodel
+}; // end beliefmodels
+
+#endif
+
+// ===================================================================
+// EDIT LOG
+// 
+// GJ	090915	extended with grounded beliefs, continual formulas, complex formulas
+// GJ	090710	started module
+// 
