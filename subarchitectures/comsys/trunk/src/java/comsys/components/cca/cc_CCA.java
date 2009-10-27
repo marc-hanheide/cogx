ebbac1336a9b55cb7af777baeff51f471b6abe7e
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-27 19:55:26 +0000

    changing the handling of double human user's assertions

--- subarchitectures/comsys/trunk/src/java/comsys/components/cca/cc_CCA.java
+++ subarchitectures/comsys/trunk/src/java/comsys/components/cca/cc_CCA.java
@@ -654,6 +654,14 @@ public class cc_CCA extends BeliefModelInterface {
 				int idxValueNeed = ccaEngine.stack.findTopmostBlockByIntention("need_get_value");
 				int idxValueVerify = ccaEngine.stack.findTopmostBlockByIntention("need_verify_hypothesis");
 
+				if (idxValueNeed == ProofStack.NOT_FOUND && idxValueVerify == ProofStack.NOT_FOUND) {
+//					cu.beliefs = new Belief[0];
+//					cu.intention = PredicateFactory.predicate("not_understood", new Term[] {PredicateFactory.term("r")});
+//					return cu;
+					ccaEngine.stack.push(pi);
+					return cu;
+				}
+				
 				ProofBlock related = null;
 				if (idxValueNeed < idxValueVerify) {
 					
