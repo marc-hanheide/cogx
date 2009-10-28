5aaedba9217e6fc541f8b917973dec8ededf4840
matejk <matejk@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-28 11:10:39 +0000

    aded functionality for 3d points visualization and colorization.

--- subarchitectures/vision.sa/trunk/src/matlab/vis/showSurfaceFromPoints.m
+++ subarchitectures/vision.sa/trunk/src/matlab/vis/showSurfaceFromPoints.m
@@ -3,13 +3,23 @@
 % Author: Matej Kristan, 2009 (matej.kristan@fri.uni-lj.si; http://vicos.fri.uni-lj.si/matejk/)
 % Last revised: 2009
 %%
-function showSurfaceFromPoints( x )
+function showSurfaceFromPoints( x, rgb3d )
+
+if nargin < 2
+    rgb3d = [] ;
+end
 
 x(:,3) = -x(:,3) ;
 
-TRI = delaunay(x(:,1),x(:,2)); 
-trisurf(TRI,x(:,1),x(:,2),x(:,3), x(:,3)) ;
-colormap bone ;
+TRI = delaunay(x(:,1),x(:,2));
+if isempty(rgb3d)
+    trisurf(TRI,x(:,1),x(:,2),x(:,3)) ;
+    colormap bone ;
+else
+    trisurf(TRI,x(:,1),x(:,2),x(:,3),[1:size(x(:,3),1)]') ;
+    colormap(rgb3d/255) ;
+    shading interp ;
+end
 hold on ;
 plot3(x(:,1),x(:,2),x(:,3),'r.') ;
 axis equal ; axis tight ; grid off ;
