a37a724cf05870fac1961d983cbb301575cc0d8f
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-29 13:24:56 +0000

    TestLearner: TestCases separated from the main component

--- subarchitectures/vision.sa/trunk/src/c++/vision/components/VisualLearner/test/TestLearner.cpp
+++ subarchitectures/vision.sa/trunk/src/c++/vision/components/VisualLearner/test/TestLearner.cpp
@@ -11,9 +11,10 @@
 #include <cast/architecture/ChangeFilterFactory.hpp>
 #include <VideoUtils.h>
 #include <Video.hpp>
+#include "../../../VisionUtils.h"
 
 #include "TestLearner.h"
-#include "../../../VisionUtils.h"
+#include "TestCases.h"
 
 using namespace std;
 using namespace cast;
@@ -35,17 +36,18 @@ static string descAddr(const cdl::WorkingMemoryAddress &addr)
 
 CTestRecognizer::CTestRecognizer()
 {
-   testmode = "standalone";
-   nRequests = 0;
-   learningStepsComplete = 999;
+   m_pTestCase = NULL;
+   testmode = "*** unknown-test ***";
+   m_RecogTasks = 0;
+   m_LearnTasks = 0;
+   dumpChanged_RecogTask = false;
+   dumpNew_LearningTask = false;
 }
 
 void CTestRecognizer::start()
 {
    log("Recognizer TEST starting");
 
-   // Global change filter expecting message from vision.sa
-   // (ID set in CAST file, subarchitecture entry)
    addChangeFilter(
       createLocalTypeFilter<VisualLearnerRecognitionTask>(cdl::ADD),
       new MemberFunctionChangeReceiver<CTestRecognizer>(
@@ -59,10 +61,10 @@ void CTestRecognizer::start()
       );
 
    addChangeFilter(
-      createLocalTypeFilter<VisualLearnerRecognitionTask>(cdl::OVERWRITE),
-      new MemberFunctionChangeReceiver<CTestRecognizer>(
-         this, &CTestRecognizer::onChange_RecognitionTask)
-      );
+     createLocalTypeFilter<VisualLearnerRecognitionTask>(cdl::OVERWRITE),
+     new MemberFunctionChangeReceiver<CTestRecognizer>(
+        this, &CTestRecognizer::onChange_RecognitionTask)
+     );
 
    addChangeFilter(
       createLocalTypeFilter<VisualLearnerLearningTask>(cdl::ADD),
@@ -77,17 +79,26 @@ void CTestRecognizer::start()
       );
 
    addChangeFilter(
-      createLocalTypeFilter<VisualLearnerLearningTask>(cdl::OVERWRITE),
-      new MemberFunctionChangeReceiver<CTestRecognizer>(
-         this, &CTestRecognizer::onChange_LearningTask)
-      );
+     createLocalTypeFilter<VisualLearnerLearningTask>(cdl::OVERWRITE),
+     new MemberFunctionChangeReceiver<CTestRecognizer>(
+        this, &CTestRecognizer::onChange_LearningTask)
+     );
 
    addChangeFilter(
       createLocalTypeFilter<ProtoObject>(cdl::ADD),
       new MemberFunctionChangeReceiver<CTestRecognizer>(
-         this, &CTestRecognizer::onAddProtoObject)
+         this, &CTestRecognizer::onAdd_ProtoObject)
       );
 
+   addChangeFilter(
+      createLocalTypeFilter<ProtoObject>(cdl::DELETE),
+      new MemberFunctionChangeReceiver<CTestRecognizer>(
+         this, &CTestRecognizer::onDelete_ProtoObject)
+      );
+
+   if (m_pTestCase) {
+      m_pTestCase->onStart();
+   }
 }
 
 void CTestRecognizer::configure(const std::map<std::string,std::string> & _config)
@@ -99,10 +110,22 @@ void CTestRecognizer::configure(const std::map<std::string,std::string> & _confi
       string mode;
       istringstream istr(it->second);
       istr >> mode;
-      if (mode == "standalone") testmode = mode;
-      else if (mode == "fake-proto") testmode = mode;
-      else if (mode == "learning") testmode = mode;
-      else testmode = "***unknown***";
+      if (mode == "standalone") {
+         testmode = mode;
+         m_pTestCase = new CTestCase_Standalone(mode, this);
+      }
+      else if (mode == "fake-proto") {
+         testmode = mode;
+         m_pTestCase = new CTestCase_FakeProto(mode, this);
+      }
+      else if (mode == "learning") {
+         testmode = mode;
+         m_pTestCase = new CTestCase_Learning(mode, this);
+      }
+      else if (mode == "saved-example") {
+         testmode = mode;
+         m_pTestCase = new CTestCase_SavedExamples(mode, this);
+      }
       log("TEST MODE: %s", testmode.c_str());
    }
    else if((it = _config.find("--delay")) != _config.end()) {
@@ -115,14 +138,14 @@ void CTestRecognizer::configure(const std::map<std::string,std::string> & _confi
 
 void CTestRecognizer::onAdd_RecognitionTask(const cast::cdl::WorkingMemoryChange & _wmc)
 {
-   nRequests++;
-   log("Recognition task %s added. %d requests in WM.", descAddr(_wmc.address).c_str(), nRequests);
+   m_RecogTasks++;
+   log("Recognition task %s added. %d requests in WM.", descAddr(_wmc.address).c_str(), m_RecogTasks);
 }
 
 void CTestRecognizer::onDelete_RecognitionTask(const cast::cdl::WorkingMemoryChange & _wmc)
 {
-   nRequests--;
-   log("Recognition task %s removed. %d requests in WM.", descAddr(_wmc.address).c_str(), nRequests);
+   m_RecogTasks--;
+   log("Recognition task %s removed. %d requests in WM.", descAddr(_wmc.address).c_str(), m_RecogTasks);
 }
 
 static
@@ -133,77 +156,66 @@ bool intLower (int i, int j)
 
 void CTestRecognizer::onChange_RecognitionTask(const cast::cdl::WorkingMemoryChange & _wmc)
 {
-   log("Recognition task %s modified. %d requests in WM.", descAddr(_wmc.address).c_str(), nRequests);
-   VisualLearnerRecognitionTaskPtr pTask = getMemoryEntry<VisualLearnerRecognitionTask>(_wmc.address);
-   std::vector<int>::iterator plabel;
-   std::vector<double>::iterator pdbl;
+   log("Recognition task %s modified. %d requests in WM.", descAddr(_wmc.address).c_str(), m_RecogTasks);
+   if (dumpChanged_RecogTask) {
+      VisualLearnerRecognitionTaskPtr pTask = getMemoryEntry<VisualLearnerRecognitionTask>(_wmc.address);
+      std::vector<int>::iterator plabel;
+      std::vector<double>::iterator pdbl;
 
-   pdbl = pTask->distribution.begin();
-   for( plabel = pTask->labels.begin(); plabel != pTask->labels.end(); plabel++) {
-      log("Label '%d' Distr '%f'", *plabel, pdbl != pTask->distribution.end() ? *pdbl : -1);
-      if (pdbl != pTask->distribution.end()) pdbl++;
-   }
-
-   sleepComponent(100);
-   deleteFromWorkingMemory(_wmc.address);
-   
-   if (testmode == "learning") {
-      learningStepsComplete++;
-      if (learningStepsComplete == 1) {
-         labels.clear();
-         for( plabel = pTask->labels.begin(); plabel != pTask->labels.end(); plabel++) {
-            labels.push_back(*plabel);
-         std::sort(labels.begin(), labels.end(), intLower);
-         }
+      pdbl = pTask->distribution.begin();
+      for( plabel = pTask->labels.begin(); plabel != pTask->labels.end(); plabel++) {
+         log("Label '%d' Distr '%f'", *plabel, pdbl != pTask->distribution.end() ? *pdbl : -1);
+         if (pdbl != pTask->distribution.end()) pdbl++;
       }
    }
 }
 
 void CTestRecognizer::onAdd_LearningTask(const cast::cdl::WorkingMemoryChange & _wmc)
 {
-   nRequests++;
-   log("Learning task %s added. %d requests in WM.", descAddr(_wmc.address).c_str(), nRequests);
-   VisualLearnerLearningTaskPtr pTask = getMemoryEntry<VisualLearnerLearningTask>(_wmc.address);
+   m_LearnTasks++;
+   log("Learning task %s added. %d requests in WM.", descAddr(_wmc.address).c_str(), m_LearnTasks);
+   if (dumpNew_LearningTask) {
+      VisualLearnerLearningTaskPtr pTask = getMemoryEntry<VisualLearnerLearningTask>(_wmc.address);
 
-   std::vector<int>::iterator plabel;
-   std::vector<double>::iterator pdbl;
-   ostringstream ostr;
-   ostr << "Labels: ";
-   for( plabel = pTask->labels.begin(); plabel != pTask->labels.end(); plabel++) {
-      ostr << "'" << *plabel << "' ";
-   }
-   log(ostr.str());
+      std::vector<int>::iterator plabel;
+      std::vector<double>::iterator pdbl;
+      ostringstream ostr;
+      ostr << "Labels: ";
+      for( plabel = pTask->labels.begin(); plabel != pTask->labels.end(); plabel++) {
+         ostr << "'" << *plabel << "' ";
+      }
+      log(ostr.str());
 
-   ostringstream ostr2;
-   ostr2 << "Distr: ";
-   for( pdbl = pTask->distribution.begin(); pdbl != pTask->distribution.end(); pdbl++) {
-      ostr2 << *pdbl << " ";
+      ostringstream ostr2;
+      ostr2 << "Distr: ";
+      for( pdbl = pTask->distribution.begin(); pdbl != pTask->distribution.end(); pdbl++) {
+         ostr2 << *pdbl << " ";
+      }
+      log(ostr2.str());
    }
-   log(ostr2.str());
 }
 
 void CTestRecognizer::onDelete_LearningTask(const cast::cdl::WorkingMemoryChange & _wmc)
 {
-   nRequests--;
-   log("Learning task %s removed. %d requests in WM.", descAddr(_wmc.address).c_str(), nRequests);
+   m_LearnTasks--;
+   log("Learning task %s removed. %d requests in WM.", descAddr(_wmc.address).c_str(), m_LearnTasks);
 }
 
 void CTestRecognizer::onChange_LearningTask(const cast::cdl::WorkingMemoryChange & _wmc)
 {
-   log("Learning task %s modified. %d requests in WM.", descAddr(_wmc.address).c_str(), nRequests);
-   VisualLearnerLearningTaskPtr pTask = getMemoryEntry<VisualLearnerLearningTask>(_wmc.address);
-   std::vector<int>::iterator plabel;
-   std::vector<double>::iterator pdbl;
-
-   sleepComponent(100);
-   deleteFromWorkingMemory(_wmc.address);
-   
-   if (testmode == "learning") learningStepsComplete++;
+   log("Learning task %s modified. %d requests in WM.", descAddr(_wmc.address).c_str(), m_LearnTasks);
 }
 
-void CTestRecognizer::onAddProtoObject(const cast::cdl::WorkingMemoryChange & _wmc)
+void CTestRecognizer::onAdd_ProtoObject(const cast::cdl::WorkingMemoryChange & _wmc)
 {
-   log("Detected: new proto object: %s", descAddr(_wmc.address).c_str());
+   m_ProtoObjects++;
+   log("New proto object: %s. %d in WM", descAddr(_wmc.address).c_str(), m_ProtoObjects);
+}
+
+void CTestRecognizer::onDelete_ProtoObject(const cast::cdl::WorkingMemoryChange & _wmc)
+{
+   m_ProtoObjects--;
+   log("Deleted proto object: %s. %d in WM", descAddr(_wmc.address).c_str(), m_ProtoObjects);
 }
 
 ProtoObjectPtr CTestRecognizer::loadFakeProtoObject()
@@ -230,135 +242,21 @@ ProtoObjectPtr CTestRecognizer::loadFakeProtoObject()
    return pproto;
 }
 
-// Add a new recognition task with an artificial protoobject.
-// The resutls are parsed in onChangeRecognitionTask
-void CTestRecognizer::_test_addRecognitionTask()
-{
-   println("Adding new ProtoObject");
-
-   if (testmode == "standalone") {
-      ProtoObjectPtr pproto = loadFakeProtoObject();
-      string protoId = newDataID();
-      addToWorkingMemory(protoId, pproto);
-      m_protoIDs.push_back(protoId);
-
-      println("Adding new VisualLearnerRecognitionTask");
-      VisualLearnerRecognitionTaskPtr ptask = new VisualLearnerRecognitionTask();
-      ptask->protoObjectId = protoId;
-
-      string reqId(newDataID());
-      addToWorkingMemory(reqId, ptask);
-   }
-   else if (testmode == "fake-proto") {
-      ProtoObjectPtr pproto = loadFakeProtoObject();
-      string protoId = newDataID();
-      addToWorkingMemory(protoId, pproto);
-      m_protoIDs.push_back(protoId);
-   }
-}
-
-void CTestRecognizer::_test_performLearningStep(int issued, string protoId)
-{
-   // 1. Recognize
-   // 2. Update: learn
-   // 3. Recognize
-   // 4. Update: unlearn
-   // 5. Recognize
-   std::vector<int>::iterator plabel;
-   int n;
-   if (issued == 1 || issued == 3 || issued == 5) {
-      log("STEP %d: RECOGNITION", issued);
-      VisualLearnerRecognitionTaskPtr ptask = new VisualLearnerRecognitionTask();
-      ptask->protoObjectId = protoId;
-
-      string reqId(newDataID());
-      addToWorkingMemory(reqId, ptask);
-   }
-   else if (issued == 2) {
-      log("STEP %d: LEARNING", issued);
-      VisualLearnerLearningTaskPtr ptask = new VisualLearnerLearningTask();
-      ptask->protoObjectId = protoId;
-      std::vector<double> &distrib = ptask->distribution;
-
-      double sum = 0;
-      for( plabel = labels.begin(); plabel != labels.end(); plabel++) {
-         ptask->labels.push_back(*plabel);
-         n = rand() % 100;
-         distrib.push_back(n);
-         sum += n;
-      }
-      for (n = 0; n < distrib.size(); n++) distrib[n] = distrib[n] / sum;
-
-      string reqId(newDataID());
-      addToWorkingMemory(reqId, ptask);
-   }
-   else if (issued == 4) {
-      log("STEP %d: UNLEARNING", issued);
-      VisualLearnerLearningTaskPtr ptask = new VisualLearnerLearningTask();
-      ptask->protoObjectId = protoId;
-      std::vector<double> &distrib = ptask->distribution;
-
-      double sum = 0;
-      for( plabel = labels.begin(); plabel != labels.end(); plabel++) {
-         ptask->labels.push_back(*plabel);
-         n = rand() % 100;
-         distrib.push_back(n);
-         sum += n;
-      }
-      for (n = 0; n < distrib.size(); n++) {
-         distrib[n] = distrib[n] / sum;
-         if (n % 2 == 0) distrib[n] = -distrib[n];
-      }
-
-      string reqId(newDataID());
-      addToWorkingMemory(reqId, ptask);
-   }
-}
-
 void CTestRecognizer::runComponent()
 {
    sleepComponent(2000);
 
-   if (testmode == "learning") {
-      ProtoObjectPtr pproto = loadFakeProtoObject();
-      string protoId = newDataID();
-      addToWorkingMemory(protoId, pproto);
-      learningStepsComplete = 0;
-      int issued = 0;
+   if (m_pTestCase != NULL) {
       while(isRunning()) {
-         if (issued <= learningStepsComplete && issued < 6) {
-            issued++;
-            log("Steps issued: %d, complete: %d", issued, learningStepsComplete);
-            _test_performLearningStep(issued, protoId);
-         }
-         sleepComponent(500);
+         m_pTestCase->runOneStep();
+         sleepComponent(10);
       }
    }
-   else {
-      int nCalls = 3;
-      while(isRunning()) {
-         // TODO: if request queue not empty and not processing
-         // If deleteFromWorkingMemory is disabled in onChangeRecognitionTask
-         // then stop after 3 requests, otherwise create new tasks indefinitely.
-         if (nRequests < 1) {
-            if (nCalls) {
-               _test_addRecognitionTask();
-               nCalls --;
-            }
-            sleepComponent(500);
-         }
-         
-         if(nCalls == 0 && m_protoIDs.size() > 1 && testmode == "fake-proto")
-         {
-            sleep(2);
-            deleteFromWorkingMemory(m_protoIDs.back());
-            m_protoIDs.pop_back();
-         }
-         
-            
-
-         sleepComponent(300);
-      }
+   if (m_pTestCase) {
+      log("runComponent: Test Case Cleanup.");
+      m_pTestCase->onExitComponent();
+      delete m_pTestCase;
+      m_pTestCase = NULL;
    }
    log("runComponent Done.");
 }
