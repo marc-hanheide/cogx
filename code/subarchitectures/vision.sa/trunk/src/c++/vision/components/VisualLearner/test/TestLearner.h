7d8396e2de31981876cff05135af787b17a52207
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-10-29 13:24:56 +0000

    TestLearner: TestCases separated from the main component

--- code/subarchitectures/vision.sa/trunk/src/c++/vision/components/VisualLearner/test/TestLearner.h
+++ code/subarchitectures/vision.sa/trunk/src/c++/vision/components/VisualLearner/test/TestLearner.h
@@ -9,15 +9,40 @@
 #include <VideoClient.h>
 #include <VisionData.hpp>
 
+class CTestRecognizer; // forward
+class CTestCase
+{
+public:
+   std::string m_name;
+   CTestRecognizer *m_pOwner;
+   CTestCase(std::string name, CTestRecognizer *pOwner) {
+      m_pOwner = pOwner;
+   }
+   virtual void runOneStep() { }
+   virtual void onStart() { }
+   virtual void onExitComponent() { }
+};
+
 class CTestRecognizer: public cast::ManagedComponent
 {
 private:
+   CTestCase *m_pTestCase;
    std::string testmode;
-   int nRequests;
    int learningStepsComplete;
    std::vector<int> labels;
    
    std::vector<std::string> m_protoIDs;
+
+public:
+   // TODO Couters, should be RO for public!
+   int m_ProtoObjects;
+   int m_RecogTasks;
+   int m_LearnTasks;
+
+   bool dumpChanged_RecogTask;
+   bool dumpNew_LearningTask;
+
+private:
 
    // Proof of concept
    class CMyVideoClient: public cast::VideoClient {
@@ -48,12 +73,15 @@ protected:
    void onChange_LearningTask(const cast::cdl::WorkingMemoryChange & _wmc);
 
    // not really used, just for logging
-   void onAddProtoObject(const cast::cdl::WorkingMemoryChange & _wmc);
+   void onAdd_ProtoObject(const cast::cdl::WorkingMemoryChange & _wmc);
+   void onDelete_ProtoObject(const cast::cdl::WorkingMemoryChange & _wmc);
 
    // Some helpers for testing
    void _test_addRecognitionTask();
-   VisionData::ProtoObjectPtr loadFakeProtoObject();
    void _test_performLearningStep(int issued, std::string protoId);
+public:
+   VisionData::ProtoObjectPtr loadFakeProtoObject();
+   using cast::ManagedComponent::sleepComponent;
 };
 
 //class CommandListener: public cast::WorkingMemoryChangeReceiver 
