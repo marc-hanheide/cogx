576626cc34481bb4489228f9617ec7520464d4f4
Raveesh <Raveesh@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-11-05 15:31:08 +0000

    arguments

--- systems/dfki/instantiations/prosodic_dia/testprosodicutterances.sh
+++ systems/dfki/instantiations/prosodic_dia/testprosodicutterances.sh
@@ -3,5 +3,9 @@ if [ $# -ne 1 ]; then
         echo 1>&2 Usage: $0 InputFile [plain txt or prosodic]
         exit 127
 fi
-
-java -classpath $CLASSPATH:../../output/classes/ comsys.components.tts.ProsodicTextToRawMARYXml $1 ../../subarchitectures/comsys/src/java/comsys/components/tts/RAWMARYXMLhead.xml ./maryXMLs/ 
+#echo "ProsodicTextToRawMARYXml takes as input: "
+#echo "1) file containing prosodic strings."
+#echo "2) MaryXMLheader."
+#echo "3) location to save RawMaryXML and wav files ."
+#echo "4) a flag indicating whether to save wav and xml files."
+java -classpath $CLASSPATH:../../output/classes/ comsys.components.tts.ProsodicTextToRawMARYXml $1 ../../subarchitectures/comsys/src/java/comsys/components/tts/RAWMARYXMLhead.xml ./maryXMLs/  True
