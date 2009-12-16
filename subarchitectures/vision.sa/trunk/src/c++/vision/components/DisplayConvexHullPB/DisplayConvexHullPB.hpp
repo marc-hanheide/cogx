0b819afe19e3fd58357bc34db9d92d9eafb7edd9
patric <patric@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2009-12-16 15:26:04 +0000

    Added transforms to DisplayConvexHullPB

--- subarchitectures/vision.sa/trunk/src/c++/vision/components/DisplayConvexHullPB/DisplayConvexHullPB.hpp
+++ subarchitectures/vision.sa/trunk/src/c++/vision/components/DisplayConvexHullPB/DisplayConvexHullPB.hpp
@@ -7,6 +7,10 @@
 #include <peekabot.hh>
 #include <peekabot/Types.hh>
 #include <VisionData.hpp>
+#include <NavData.hpp>
+#include <SensorData/SensorPose.hh>
+#include <Transformation/Transformation3D.hh>
+#include <PTZ.hpp>
 
 class DisplayConvexHullPB : public cast::ManagedComponent 
 {
@@ -24,19 +28,27 @@ protected:
 private:
   std::vector<double> previouscenter;
   void newConvexHull(const cast::cdl::WorkingMemoryChange &objID);
+  void robotPoseChanged(const cast::cdl::WorkingMemoryChange &objID);
+  Cure::Transformation3D getCameraToWorldTransform();
   void connectPeekabot();
   int m_RetryDelay; // Seconds to retry if cannot connect. -1 means dont retry
 
   double m_FovH; // horisontal fov in degs
   double m_FovV; // vertical fov in degs
 
+  NavData::RobotPose2dPtr lastRobotPose;
+
   IceUtil::Mutex m_Mutex;
+  ptz::PTZInterfacePrx m_ptzInterface;
 
   std::string m_PbHost;
   int m_PbPort;
+  std::string m_PTZServer;
 
   peekabot::PeekabotClient m_PeekabotClient;
   peekabot::ObjectProxy m_ProxyRoot;
+
+  Cure::SensorPose m_CameraPoseR;
 };
 
 #endif // DisplayConvexHullPB_hpp
