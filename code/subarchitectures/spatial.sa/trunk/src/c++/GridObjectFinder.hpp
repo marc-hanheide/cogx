43d6a53e23949cc52c54dd266f2b17f9afb889bb
krsj <krsj@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-01-11 12:55:50 +0000

    Added missing CPP file for LocalGridMap

---
+++ code/subarchitectures/spatial.sa/trunk/src/c++/GridObjectFinder.hpp
@@ -0,0 +1,47 @@
+//
+// = FILENAME
+//    GridObjectFinder.hpp
+//
+// = FUNCTION
+//    Helper class used to extract candidate objects
+//    from a discrete grid map, given bitmaps
+//    for the objects' shapes
+//    Uses naïve brute-force search
+//
+// = AUTHOR(S)
+//    Kristoffer Sjöö
+//
+// = COPYRIGHT
+//    Copyright (c) 2010 Kristoffer Sjöö
+//
+/*----------------------------------------------------------------------*/
+
+#ifndef GridObjectFinder_hpp
+#define GridObjectFinder_hpp
+#include <cv.h>
+#include <vector>
+//#include <highgui.h>
+
+struct PlaneData;
+
+namespace spatial {
+
+  enum ObjectFinderSymmetry { NO_SYMMETRY, HALF_SYMMETRY, QUARTER_SYMMETRY, 
+    CIRCULAR_SYMMETRY };
+  
+  class GridObjectFinder
+  {
+    public:
+      GridObjectFinder(IplImage *objectGrid, int objectXCenter,
+	  int objectYCenter, ObjectFinderSymmetry symmetry);
+      ~GridObjectFinder();
+
+      void findObject(IplImage *image, double *outX, double *outY, double *outAngle,
+	  double *outConfidence = 0);
+
+
+    private:
+      std::vector<IplImage *> m_aspectImages;
+  };
+};
+#endif //GridObjectFinder_hpp
