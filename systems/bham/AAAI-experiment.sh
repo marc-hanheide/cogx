d417fa94ccec733645ccbaa92ac4bd50bbf96131
Marc Hanheide <marc@hanheide.net>
2010-01-17 14:11:29 +0000

    tweaks for AAAI study

--- systems/bham/AAAI-experiment.sh
+++ systems/bham/AAAI-experiment.sh
@@ -27,7 +27,8 @@ PIDS="$PIDS $!"
 
 sleep 20
 
-BUILD/tools/hardware/robotbase/src/c++/components/TourGuide 0 0  1 0 6.3 0
+BUILD/tools/hardware/robotbase/src/c++/components/TourGuide 0 0  1 0 5.8 0 5.8 4
+#BUILD/tools/hardware/robotbase/src/c++/components/TourGuide 0 0  1 0 6.3 0
 output/bin/universalIceClient MotiveFilterManager localhost motivation.slice.RemoteFilterServer setPriority ExploreMotive NORMAL
 output/bin/universalIceClient MotiveFilterManager localhost motivation.slice.RemoteFilterServer setPriority CategorizeRoomMotive NORMAL
 
