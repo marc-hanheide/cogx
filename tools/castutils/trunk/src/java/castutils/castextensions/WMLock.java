769a4c2a481aad62bc4e72494286a1369a35b155
goebelbe <goebelbe@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-03-26 12:09:01 +0000

    Fix import

--- tools/castutils/trunk/src/java/castutils/castextensions/WMLock.java
+++ tools/castutils/trunk/src/java/castutils/castextensions/WMLock.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
-import motivation.slice.WMMutex;
+import castutils.slice.WMMutex;
 
 import Ice.Object;
 
