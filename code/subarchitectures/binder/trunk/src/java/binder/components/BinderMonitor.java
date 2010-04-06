47655e7f52748df37a31e0c7a5f7f0ee6029b4c3
plison <plison@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-06 15:35:08 +0000

    monitor

---
+++ code/subarchitectures/binder/trunk/src/java/binder/components/BinderMonitor.java
@@ -0,0 +1,97 @@
+package binder.components;
+
+import binder.autogen.beliefs.PerceptBelief;
+import binder.autogen.beliefs.PerceptUnionBelief;
+import cast.DoesNotExistOnWMException;
+import cast.UnknownSubarchitectureException;
+import cast.architecture.ChangeFilterFactory;
+import cast.architecture.ManagedComponent;
+import cast.architecture.WorkingMemoryChangeReceiver;
+import cast.cdl.WorkingMemoryChange;
+import cast.cdl.WorkingMemoryOperation;
+import cast.core.CASTData;
+
+
+/**
+ * Simple class to demonstrate how to monitor the working memory for new changes 
+ * 
+ * @author plison
+ *
+ */
+public class BinderMonitor extends ManagedComponent {
+
+
+	public void start() {
+		addChangeFilter(
+				ChangeFilterFactory.createLocalTypeFilter(PerceptBelief.class,
+						WorkingMemoryOperation.ADD), new WorkingMemoryChangeReceiver() {
+					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+						newPerceptBeliefAdded(_wmc);
+					}
+				}
+		);
+		
+		addChangeFilter(
+				ChangeFilterFactory.createLocalTypeFilter(PerceptBelief.class,
+						WorkingMemoryOperation.OVERWRITE), new WorkingMemoryChangeReceiver() {
+					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+						perceptBeliefUpdated(_wmc);
+					}
+				}
+		);
+		
+		addChangeFilter(
+				ChangeFilterFactory.createLocalTypeFilter(PerceptBelief.class,
+						WorkingMemoryOperation.DELETE), new WorkingMemoryChangeReceiver() {
+					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+						perceptBeliefDeleted(_wmc);
+					}
+				}
+		);
+		
+		addChangeFilter(
+				ChangeFilterFactory.createLocalTypeFilter(PerceptUnionBelief.class,
+						WorkingMemoryOperation.ADD), new WorkingMemoryChangeReceiver() {
+					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+						unionBeliefAdded(_wmc);
+					}
+				}
+		);
+	}
+	
+	
+	protected void unionBeliefAdded(WorkingMemoryChange _wmc) {
+		try {
+			CASTData<PerceptUnionBelief> beliefData = getMemoryEntryWithData(_wmc.address,
+					PerceptUnionBelief.class);
+			PerceptUnionBelief newBelief = beliefData.getData();
+			
+			// here you can do what you want with the belief
+			
+		}
+		 catch (DoesNotExistOnWMException e) {
+				e.printStackTrace();
+			}
+		 catch (UnknownSubarchitectureException e) {	
+			e.printStackTrace();
+		} 
+		
+	}
+
+
+	protected void perceptBeliefDeleted(WorkingMemoryChange _wmc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	protected void perceptBeliefUpdated(WorkingMemoryChange _wmc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void newPerceptBeliefAdded(WorkingMemoryChange _wmc) {
+		// TODO Auto-generated method stub
+	}
+}
