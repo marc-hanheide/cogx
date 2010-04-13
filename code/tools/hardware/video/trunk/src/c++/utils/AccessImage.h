2207ca86f15eb2630678e7fc89b1f1dd24906fcb
michaelz <michaelz@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-13 08:05:24 +0000

    can now handle float images

--- code/tools/hardware/video/trunk/src/c++/utils/AccessImage.h
+++ code/tools/hardware/video/trunk/src/c++/utils/AccessImage.h
@@ -13,6 +13,59 @@ namespace Video
 {
 
 /**
+ * access an image as img[y][x]
+ *
+ * (by Gady Agam, http://www.cs.iit.edu/~agam/cs512/lect-notes/opencv-intro/index.html)
+
+  # For a single-channel byte image:
+
+  IplImage* img=cvCreateImage(cvSize(640,480),IPL_DEPTH_8U,1);
+  BwImage imgA(img);
+  imgA[i][j] = 111;
+
+  # For a multi-channel byte image:
+
+  IplImage* img=cvCreateImage(cvSize(640,480),IPL_DEPTH_8U,3);
+  RgbImage  imgA(img);
+  imgA[i][j].b = 111;
+  imgA[i][j].g = 111;
+  imgA[i][j].r = 111;
+
+  # For a multi-channel float image:
+
+  IplImage* img=cvCreateImage(cvSize(640,480),IPL_DEPTH_32F,3);
+  RgbImageFloat imgA(img);
+  imgA[i][j].b = 111;
+  imgA[i][j].g = 111;
+  imgA[i][j].r = 111;
+
+ */
+template<class T> class AccessImage
+{
+private:
+  const IplImage* imgp;
+public:
+  AccessImage(const IplImage* img=0) {imgp=img;}
+  ~AccessImage(){imgp=0;}
+  void operator=(IplImage* img) {imgp=img;}
+  const T* operator[](const int rowIndx) const {
+    return ((T *)(imgp->imageData + rowIndx*imgp->widthStep));}
+};
+
+typedef struct{
+  unsigned char b,g,r;
+} AccessRgbPixel;
+
+typedef struct{
+  float b,g,r;
+} AccessRgbPixelFloat;
+
+typedef AccessImage<AccessRgbPixel>       AccessRgbImage;
+typedef AccessImage<AccessRgbPixelFloat>  AccessRgbImageFloat;
+typedef AccessImage<unsigned char>  AccessBwImage;
+typedef AccessImage<float>          AccessBwImageFloat;
+
+/**
  * Access raw image data at position (x,y), where x and y are not checked for
  * valid range.
  * const version.
