eb269c77b0176d3e08a69137bb16f13fe44b68cf
Marc Hanheide <marc@hanheide.net>
2010-04-13 16:23:52 +0000

    added rotation test

--- subarchitectures/binder/trunk/src/java/binder/components/perceptmediator/PerceptMediatorTest.java
+++ subarchitectures/binder/trunk/src/java/binder/components/perceptmediator/PerceptMediatorTest.java
@@ -3,8 +3,13 @@
  */
 package binder.components.perceptmediator;
 
+import SpatialData.CommandType;
+import SpatialData.Completion;
+import SpatialData.NavCommand;
 import SpatialData.Place;
 import SpatialData.PlaceStatus;
+import SpatialData.Priority;
+import SpatialData.StatusError;
 import SpatialProperties.ConnectivityPathProperty;
 import SpatialProperties.GatewayPlaceProperty;
 import cast.CASTException;
@@ -16,36 +21,50 @@ import cast.architecture.ManagedComponent;
  */
 public class PerceptMediatorTest extends ManagedComponent {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see cast.core.CASTComponent#runComponent()
 	 */
 	@Override
 	protected void runComponent() {
 		try {
-			String id=newDataID();
+			String id = newDataID();
 			addToWorkingMemory(id, new Place(0, PlaceStatus.PLACEHOLDER));
 			Thread.sleep(100);
-			addToWorkingMemory(newDataID(), new Place(1, PlaceStatus.PLACEHOLDER));
+			addToWorkingMemory(newDataID(), new Place(1,
+					PlaceStatus.PLACEHOLDER));
 			Thread.sleep(100);
 			addToWorkingMemory(newDataID(), new Place(2, PlaceStatus.TRUEPLACE));
-			Thread.sleep(200);			
+			Thread.sleep(200);
 			overwriteWorkingMemory(id, new Place(0, PlaceStatus.TRUEPLACE));
-			Thread.sleep(200);			
+			Thread.sleep(200);
 			deleteFromWorkingMemory(id);
-			
-			Thread.sleep(200);				
-			String connid=newDataID();
-			addToWorkingMemory(connid, new ConnectivityPathProperty(3, 2, null, null, true));
 
-			Thread.sleep(200);				
-			addToWorkingMemory(newDataID(), new GatewayPlaceProperty(3, null, null, true));
+			Thread.sleep(200);
+			String connid = newDataID();
+			addToWorkingMemory(connid, new ConnectivityPathProperty(3, 2, null,
+					null, true));
 
-			
+			Thread.sleep(200);
+			addToWorkingMemory(newDataID(), new GatewayPlaceProperty(3, null,
+					null, true));
+
 			Thread.sleep(500);
 			addToWorkingMemory(newDataID(), new Place(3, PlaceStatus.TRUEPLACE));
 
-			
-			
+			double[] angle = new double[1];
+			angle[0] = 30.0;
+			double[] precision = new double[1];
+			precision[0] = 0.1;
+
+			NavCommand nc = newNavCommand();
+			nc.cmd=CommandType.TURN;
+			nc.angle=angle;
+			nc.tolerance=precision;
+			log("MOVE!");
+			addToWorkingMemory(newDataID(), nc);
+
 		} catch (CASTException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -53,7 +72,15 @@ public class PerceptMediatorTest extends ManagedComponent {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
+	}
+	/**
+	 * Sets all values necessary to prevent exceptions later on
+	 */
+	private NavCommand newNavCommand() {
+		return new NavCommand(CommandType.STOP, Priority.NORMAL, null,
+				null, null, null, null, StatusError.NONE,
+				Completion.COMMANDPENDING);
 	}
 
 }
