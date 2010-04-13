a85ba0c565c8d136bb93563f3c5b5f82ff69aef8
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-13 06:32:33 +0000

    pymodulepaths - relative paths fixed

--- subarchitectures/vision.sa/trunk/src/python/ObjectRecognizer/apps/mbuilder/pymodulepaths.py
+++ subarchitectures/vision.sa/trunk/src/python/ObjectRecognizer/apps/mbuilder/pymodulepaths.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # vim:set fileencoding=utf-8 sw=4 ts=8 et:vim
 import os, sys
-sys.path.insert(0, os.path.abspath("../" * 6 + "output/python"))
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../" * 7 + "output/python"))
+sys.path.insert(0, os.path.abspath("../../dist-packages"))
 
