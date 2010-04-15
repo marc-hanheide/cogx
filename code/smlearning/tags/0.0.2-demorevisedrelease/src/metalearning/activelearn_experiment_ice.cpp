900ca4ed056d0a5bcee671859e4ff8554c149f2b
sergio <sergio@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-15 21:47:49 +0000

    updating CMakeFiles in trunk and commiting tag based on deleted armcoord branch

---
+++ code/smlearning/tags/0.0.2-demorevisedrelease/src/metalearning/activelearn_experiment_ice.cpp
@@ -0,0 +1,47 @@
+/** @file offline_experiment_ice.cpp
+ *
+ * vHanz02
+ *
+ * Program which moves the arm along a straight line
+ * using reactive trajectory planner and collision detection
+ * simulating a pushing action on a polyflap
+ * (also see DemoReacPlannerPhys and DemoRobotFinger).
+ * 
+ * Program can be run in two modes:
+ * - the first uses real Katana arm
+ * - the second runs the arm simulators
+ *
+ * data are stored that can be used for offline training RNNs
+ *
+ * @author	Marek Kopicki (see copyright.txt),
+ * 			<A HREF="http://www.cs.bham.ac.uk/~msk">The University Of Birmingham</A>
+ * @author      Sergio Roa - DFKI
+ * @author      Jan Hanzelka - DFKI
+ * @version 1.0
+ *
+ */
+
+#include <metalearning/ActiveLearnScenarioIce.h>
+
+
+using namespace smlearning;
+
+int main(int argc, char *argv[]) {
+	
+//  	QApplication a(argc, argv);
+
+// 	vector<double> data;
+// 	int max_size = 40;
+// 	MainWindow mainWindow (max_size, data);
+
+// 	mainWindow.resize(600,400);
+	//mainWindow.start();
+	//mainWindow.show();
+
+	//ActiveLearnScenarioIce().setMainWindow (&mainWindow);
+	return ActiveLearnScenarioIce().main (argc, argv);
+// 	//ActiveLearnScenarioIce scenario;
+
+// 	a.exec ();
+
+}
