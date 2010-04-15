1b99ae3979cc8bf56422d1dfa24abbcc468a46bb
cehrler <cehrler@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-15 10:37:59 +0000

    modfified the MLNGenerator to fix issues with missing predicates

---
+++ code/subarchitectures/binder/trunk/src/java/binder/utils/MLNPreferences.java
@@ -0,0 +1,62 @@
+package binder.utils;
+
+public class MLNPreferences {
+	
+	public static String markovlogicDir = "subarchitectures/binder/markovlogic/";
+	
+	public float getGreediness() {
+		return greediness;
+	}
+
+	public void setGreediness(float greediness) {
+		this.greediness = greediness;
+	}
+
+	public float getOutcome() {
+		return outcome;
+	}
+
+	public void setOutcome(float outcome) {
+		this.outcome = outcome;
+	}
+
+	public String getFile_correlations() {
+		return file_correlations;
+	}
+
+	public void setFile_correlations(String fileCorrelations) {
+		file_correlations = fileCorrelations;
+	}
+
+	public String getFile_predicates() {
+		return file_predicates;
+	}
+
+	public void setFile_predicates(String filePredicates) {
+		file_predicates = filePredicates;
+	}
+
+	private float greediness;
+	
+	private float outcome;
+	
+	private String file_correlations;
+	
+	private String file_predicates;
+	
+	public MLNPreferences() {
+		greediness = -1.5f;
+		outcome = -1f;
+		file_correlations = markovlogicDir + "grouping/correlations.mln"; 
+		file_predicates = markovlogicDir + "grouping/correlations_predicates.mln";
+	}
+
+	public MLNPreferences(float greediness, float outcome,
+			String fileCorrelations, String filePredicates) {
+		super();
+		this.greediness = greediness;
+		this.outcome = outcome;
+		file_correlations = fileCorrelations;
+		file_predicates = filePredicates;
+	}
+}
