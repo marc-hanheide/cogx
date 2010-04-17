088e79ab3e2f85bd8be8ce788e90666646ccfb5f
Marc Hanheide <marc@hanheide.net>
2010-04-17 15:10:17 +0000

    - new component that continuously trigger object detection - bugfixed VisionUtils to create VisualObject

--- subarchitectures/motivation.sa/branches/spring-school-2010/src/java/vision/VisionUtils.java
+++ subarchitectures/motivation.sa/branches/spring-school-2010/src/java/vision/VisionUtils.java
@@ -1,8 +1,13 @@
 package vision;
 
+import VisionData.Face;
+import VisionData.GeometryModel;
+import VisionData.Vertex;
 import VisionData.VisualObject;
+import VisionData.VisualObjectView;
 import blobfinder.ColorRGB;
 import cast.core.CASTUtils;
+import cogx.Math.Matrix33;
 import cogx.Math.Vector3;
 
 public class VisionUtils {
@@ -18,14 +23,13 @@ public class VisionUtils {
 	 * @return
 	 */
 	public static VisualObject newVisualObject() {
-		/*VisualObject obj = new VisualObject(new cogx.Math.Pose3(newVector3(),
+		VisualObject obj = new VisualObject(new cogx.Math.Pose3(newVector3(),
 				new Matrix33(0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d)),
 				new String[0], 0d, new cogx.Math.Sphere3(newVector3(), 0d),
 				CASTUtils.getTimeServer().getCASTTime(),
 				new VisualObjectView[0], new GeometryModel(new Vertex[0],
 						new Face[0]), "", 0d);
-		return obj;*/
-		return null;
+		return obj;
 	}
 
 
