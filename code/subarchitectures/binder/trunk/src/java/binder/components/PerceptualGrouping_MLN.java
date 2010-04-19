3da6746ca6570b4df5c3f17dc584018381ce653e
cehrler <cehrler@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-19 09:36:30 +0000

    refactroign changes to perceptuialgrouping & tracking

--- code/subarchitectures/binder/trunk/src/java/binder/components/PerceptualGrouping_MLN.java
+++ code/subarchitectures/binder/trunk/src/java/binder/components/PerceptualGrouping_MLN.java
@@ -1,15 +1,9 @@
 package binder.components;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Vector;
-import java.util.Map.Entry;
 
 import beliefmodels.arch.BeliefException;
 import beliefmodels.autogen.beliefs.Belief;
@@ -25,11 +19,7 @@ import binder.ml.MLException;
 import binder.utils.MLNGenerator;
 import cast.SubarchitectureComponentException;
 import cast.UnknownSubarchitectureException;
-import cast.architecture.ChangeFilterFactory;
-import cast.architecture.WorkingMemoryChangeReceiver;
 import cast.cdl.WorkingMemoryAddress;
-import cast.cdl.WorkingMemoryChange;
-import cast.cdl.WorkingMemoryOperation;
 import cast.core.CASTData;
 
 
@@ -63,90 +53,15 @@ import cast.core.CASTData;
  * @author plison
  *
  */ 
-public class PerceptualGrouping_MLN extends MarkovLogicComponent {
- 
-	
-	String MLNFile = markovlogicDir + "grouping.mln";
-	
-	String resultsFile = markovlogicDir + "unions.results";
-	
-	// lowest probability threshold for the existence probability of a percept union
-	public float lowestProbThreshold = 0.08f;
-	
-	// maximum number of alternatives to consider for perceptual grouping.  If more are available, take the highest-probability ones
-	public int maxAlternatives = 2;
-	
-	// minimum difference in probability in order to trigger a working memory update on an existing union
-	public float minProbDifferenceForUpdate = 0.1f;
-	
-	
-	/**
-	 * Add a change filter on the insertion of new percept beliefs on the binder working memory
-	 */
-	@Override
-	public void start() {
-			
-		// Insertion
-		addChangeFilter(
-				ChangeFilterFactory.createLocalTypeFilter(PerceptBelief.class,
-						WorkingMemoryOperation.ADD), new WorkingMemoryChangeReceiver() {
-					public void workingMemoryChanged(WorkingMemoryChange _wmc) {	
-						try {
-							CASTData<PerceptBelief> beliefData = getMemoryEntryWithData(_wmc.address, PerceptBelief.class);	
-							
-							log("received a new percept: " + beliefData.getID());
-							performPerceptualGrouping (beliefData.getData(), _wmc.address);
-							log("perceptual grouping operation on percept " + beliefData.getID() + " now finished");
-						}	
-			
-						 catch (Exception e) {
-								e.printStackTrace();
-							} 
-					}
-				}
-		);
-		
-		// Deletion
-		addChangeFilter(
-				ChangeFilterFactory.createLocalTypeFilter(PerceptBelief.class,
-						WorkingMemoryOperation.DELETE), new WorkingMemoryChangeReceiver() {
-					public void workingMemoryChanged(WorkingMemoryChange _wmc) {	
-						try {
-							deleteAllMultiModalBeliefAttachedToUnion(_wmc.address);
-						}	
-			
-						 catch (Exception e) {
-								e.printStackTrace();
-							} 
-					}
-				}
-		);
-		
-		// Update
-		addChangeFilter(
-				ChangeFilterFactory.createLocalTypeFilter(PerceptBelief.class,
-						WorkingMemoryOperation.OVERWRITE), new WorkingMemoryChangeReceiver() {
-					
-					public void workingMemoryChanged(WorkingMemoryChange _wmc) {	
-						try {
-							deleteAllMultiModalBeliefAttachedToUnion(_wmc.address);
-							CASTData<PerceptBelief> beliefData = getMemoryEntryWithData(_wmc.address, PerceptBelief.class);	
-							
-							log("received a new percept: " + beliefData.getID());
-							performPerceptualGrouping (beliefData.getData(), _wmc.address);
-							log("perceptual grouping operation on percept " + beliefData.getID() + " now finished");
-						}	
-			
-						 catch (Exception e) {
-								e.printStackTrace();
-							} 
-					}
-				}
-		);
+public class PerceptualGrouping_MLN extends MarkovLogicComponent<PerceptBelief> {
+
+
+	public PerceptualGrouping_MLN() {
+		super(new PerceptBelief());
+		MLNFile = markovlogicDir + "grouping.mln";
+		resultsFile = markovlogicDir + "unions.results";
 	}
-  
-	
-	
+
 	/**
 	 * Perform the perceptual grouping operation for the given belief, and subsequently update
 	 * the working memory with new percept unions (and possibly also with updates on existing ones)
@@ -154,84 +69,84 @@ public class PerceptualGrouping_MLN extends MarkovLogicComponent {
 	 * @param percept the new percept which was inserted
 	 */
 	public void performPerceptualGrouping(PerceptBelief percept, WorkingMemoryAddress perceptWMAddress) throws BeliefException {
-	
+
 		log("now starting perceptual grouping...");
 
 		// extract the unions already existing in the binder WM
 		HashMap<String, Belief> existingUnions = extractExistingUnions();
-		
+
 		HashMap<String, Belief> relevantUnions = selectRelevantUnions(existingUnions, percept);
-		
+
 		if (relevantUnions.size() > 0) {
-		// Create identifiers for each possible new union		
-		HashMap<String,String> unionsMapping = new HashMap<String,String>();
-		for (String existingUnionId : relevantUnions.keySet()) {
-			String newUnionId = newDataID();
-			unionsMapping.put(newUnionId, existingUnionId);
-		}
-		log("newly created union ids: " + unionsMapping.keySet().toString());
-			
-		String newSingleUnionId = newDataID();
-	//	unionsMapping.put("P", newSingleUnionId);
-		
-		// Write the markov logic network to a file
-		try {
-			(new MLNGenerator()).writeMLNFile(percept, relevantUnions.values(), unionsMapping, newSingleUnionId, MLNFile);
-		} catch (MLException e1) {
-			e1.printStackTrace();
-		}
-		
-		// run the alchemy inference
-		try { 
-		HashMap<String,Float> inferenceResults = runAlchemyInference(MLNFile, resultsFile);
-		
-		log("filtering inference results to keep only the " + maxAlternatives + " best alternatives");
-		HashMap<String,Float> filteredInferenceResults = filterInferenceResults(inferenceResults, maxAlternatives);
-		
-		// create the new unions given the inference results
-		Vector<Belief> newUnions = createNewUnions(percept, perceptWMAddress, relevantUnions,
-				unionsMapping, newSingleUnionId, filteredInferenceResults);
-
-		// and add them to the working memory
-		for (Belief newUnion : newUnions) {
-			if (DistributionUtils.getExistenceProbability(newUnion) > lowestProbThreshold)  {
-				insertBeliefInWM(newUnion);
-				log("inserting belief " + newUnion.id + " on WM");
+			// Create identifiers for each possible new union		
+			HashMap<String,String> unionsMapping = new HashMap<String,String>();
+			for (String existingUnionId : relevantUnions.keySet()) {
+				String newUnionId = newDataID();
+				unionsMapping.put(newUnionId, existingUnionId);
 			}
-			else {
-				log ("Belief " + newUnion.id + " has probability " + DistributionUtils.getExistenceProbability(newUnion) +
+			log("newly created union ids: " + unionsMapping.keySet().toString());
+
+			String newSingleUnionId = newDataID();
+			//	unionsMapping.put("P", newSingleUnionId);
+
+			// Write the markov logic network to a file
+			try {
+				(new MLNGenerator()).writeMLNFile(percept, relevantUnions.values(), unionsMapping, newSingleUnionId, MLNFile);
+			} catch (MLException e1) {
+				e1.printStackTrace();
+			}
+
+			// run the alchemy inference
+			try { 
+				HashMap<String,Float> inferenceResults = runAlchemyInference(MLNFile, resultsFile);
+
+				log("filtering inference results to keep only the " + maxAlternatives + " best alternatives");
+				HashMap<String,Float> filteredInferenceResults = filterInferenceResults(inferenceResults, maxAlternatives);
+
+				// create the new unions given the inference results
+				Vector<Belief> newUnions = createNewUnions(percept, perceptWMAddress, relevantUnions,
+						unionsMapping, newSingleUnionId, filteredInferenceResults);
+
+				// and add them to the working memory
+				for (Belief newUnion : newUnions) {
+					if (DistributionUtils.getExistenceProbability(newUnion) > lowestProbThreshold)  {
+						insertBeliefInWM(newUnion);
+						log("inserting belief " + newUnion.id + " on WM");
+					}
+					else {
+						log ("Belief " + newUnion.id + " has probability " + DistributionUtils.getExistenceProbability(newUnion) +
 						", which is lower than the minimum threshold.  Not inserting");
-			}
-		}
+					}
+				}
 
-		// modify the existence probabilities of the existing unions
-		List<Belief> unionsToUpdate = getExistingUnionsToUpdate(percept, relevantUnions, unionsMapping, filteredInferenceResults);
-		
-		// and update them on the working memory
-		for (Belief unionToUpdate : unionsToUpdate) {
-			if (DistributionUtils.getExistenceProbability(unionToUpdate) > lowestProbThreshold)  {
-				log("updating belief " + unionToUpdate.id + " on WM");
-				updateBeliefOnWM(unionToUpdate);
+				// modify the existence probabilities of the existing unions
+				List<Belief> unionsToUpdate = getExistingUnionsToUpdate(percept, relevantUnions, unionsMapping, filteredInferenceResults);
+
+				// and update them on the working memory
+				for (Belief unionToUpdate : unionsToUpdate) {
+					if (DistributionUtils.getExistenceProbability(unionToUpdate) > lowestProbThreshold)  {
+						log("updating belief " + unionToUpdate.id + " on WM");
+						updateBeliefOnWM(unionToUpdate);
+					}
+					else  {
+						log("deleting belief: " + unionToUpdate.id + " on WM");
+						deleteBeliefOnWM(unionToUpdate.id);
+					}
+				}
+
 			}
-			else  {
-				log("deleting belief: " + unionToUpdate.id + " on WM");
-				deleteBeliefOnWM(unionToUpdate.id);
+			catch (Exception e) {
+				e.printStackTrace();
 			}
-		}
-		
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
 		}
 		else {
 			try {
-			log("no relevant union to group with percept " + percept.id + " has been found");
-			PerceptUnionBelief union = PerceptUnionBuilder.createNewSingleUnionBelief(percept, perceptWMAddress, newDataID());
-			if (DistributionUtils.getExistenceProbability(union) > lowestProbThreshold)  {
-				insertBeliefInWM(union);
-				log("inserting belief " + union.id + " on WM");
-			}
+				log("no relevant union to group with percept " + percept.id + " has been found");
+				PerceptUnionBelief union = PerceptUnionBuilder.createNewSingleUnionBelief(percept, perceptWMAddress, newDataID());
+				if (DistributionUtils.getExistenceProbability(union) > lowestProbThreshold)  {
+					insertBeliefInWM(union);
+					log("inserting belief " + union.id + " on WM");
+				}
 			}
 			catch (Exception e) {
 				e.printStackTrace();
@@ -240,42 +155,6 @@ public class PerceptualGrouping_MLN extends MarkovLogicComponent {
 	}
 
 
-	/**
-	 * Filter the inference results to retain only the maxSize-best results
-	 * 
-	 * @param results the results of Alchemy inference
-	 * @param maxSize the number of results to retain
-	 * @return the filtered results
-	 */
-	private HashMap<String, Float> filterInferenceResults (HashMap<String,Float> results, int maxSize) {
-		
-		     List<Entry<String,Float>> list = new LinkedList<Entry<String,Float>>(results.entrySet());
-		     Collections.sort(list, new Comparator<Entry<String,Float>>() {
-		          public int compare(Entry<String,Float> o1, Entry<String,Float> o2) {
-		               if (o1.getValue().floatValue() >= o2.getValue().floatValue()) {
-		            	   return -1;
-		               }
-		              return 1;
-		          }
-		     });
-		
-		int increment = 0;
-		HashMap<String,Float> newResults = new HashMap<String,Float>();
-		for (Iterator<Entry<String,Float>> it = list.iterator(); it.hasNext();) {
-		     Map.Entry<String,Float> entry = (Map.Entry<String,Float>)it.next();
-		     if (increment < maxAlternatives) {
-		     newResults.put(entry.getKey(), entry.getValue());
-		     increment++;
-		     }
-		     else {
-		    	 newResults.put(entry.getKey(), new Float(0.0f)); 
-		     }
-		     }
-		return newResults;
-	}
-	
-	
-	
 	/**
 	 * Create a set of new percept union beliefs from the inference results, associated with the 
 	 * original percept
@@ -297,43 +176,43 @@ public class PerceptualGrouping_MLN extends MarkovLogicComponent {
 
 		Vector<Belief> newUnions = new Vector<Belief>();
 		for (String id : unionsMapping.keySet()) {
-			
+
 			if (!inferenceResults.containsKey(id)) {
 				throw new BeliefException("ERROR, id " + id + " is not in inferenceResults.  inferenceResults = " + inferenceResults.keySet().toString());
 			}
-			
+
 			else if (!existingUnions.containsKey(unionsMapping.get(id))) {
 				throw new BeliefException("ERROR, existing union id " + unionsMapping.get(id) + " is not in existingUnions");
 			}
-			
+
 			float prob = perceptExistProb * inferenceResults.get(id);
 			log("computed probability for new percept union " + id + ": " + prob);
-			
+
 			Belief existingUnion = existingUnions.get(unionsMapping.get(id)); 
 			try {
-			List<WorkingMemoryAddress> addresses = new LinkedList<WorkingMemoryAddress>();
-			addresses.add(perceptWMAddress);
-			addresses.add(new WorkingMemoryAddress(existingUnion.id, BindingWorkingMemory.BINDER_SA));
-			Belief newUnion = PerceptUnionBuilder.createNewDoubleUnionBelief(percept, addresses, existingUnion, prob, id);
-			newUnions.add(newUnion);
+				List<WorkingMemoryAddress> addresses = new LinkedList<WorkingMemoryAddress>();
+				addresses.add(perceptWMAddress);
+				addresses.add(new WorkingMemoryAddress(existingUnion.id, BindingWorkingMemory.BINDER_SA));
+				Belief newUnion = PerceptUnionBuilder.createNewDoubleUnionBelief(percept, addresses, existingUnion, prob, id);
+				newUnions.add(newUnion);
 			}
 			catch (BeliefException e) {
 				e.printStackTrace();
 			}
 		} 
-		
+
 		if (!inferenceResults.containsKey(newSingleUnionId)) {
 			throw new BeliefException("ERROR, id " + newSingleUnionId + " is not in inferenceResults");
 		}
 		PerceptUnionBelief newSingleUnion = 
 			PerceptUnionBuilder.createNewSingleUnionBelief(percept, perceptWMAddress, inferenceResults.get(newSingleUnionId), newSingleUnionId);
-		
+
 		newUnions.add(newSingleUnion);
-		
+
 		return newUnions;
 	}
-	 
-	 
+
+
 	/**
 	 * Modify the existence probability of existing unions, based on the inference results, 
 	 * the newly inserted percept, the set of existing unions, and the mapping between the new 
@@ -349,21 +228,21 @@ public class PerceptualGrouping_MLN extends MarkovLogicComponent {
 			HashMap<String,Belief> existingUnions, 
 			HashMap<String,String> linkToExistingUnions,
 			HashMap<String,Float> inferenceResults) {
-		
+
 		List<Belief> unionsToUpdate = new LinkedList<Belief>();
-		
+
 		// extract the existence probability of the percept
 		float perceptExistProb = DistributionUtils.getExistenceProbability(percept);
-		
+
 		for (String newUnionId: linkToExistingUnions.keySet()) {
 			Belief existingUnion = existingUnions.get(linkToExistingUnions.get(newUnionId));
 			float unionCurrentExistProb = DistributionUtils.getExistenceProbability(existingUnion);
 			float unionNewExistProb = (unionCurrentExistProb * (1-perceptExistProb)) + 
-				(perceptExistProb * (1 - inferenceResults.get(newUnionId)) * unionCurrentExistProb);
+			(perceptExistProb * (1 - inferenceResults.get(newUnionId)) * unionCurrentExistProb);
 			log("new prob for " +  linkToExistingUnions.get(newUnionId) + ": " + unionNewExistProb);
-			
+
 			try {
-			DistributionUtils.setExistenceProbability(existingUnion, unionNewExistProb);
+				DistributionUtils.setExistenceProbability(existingUnion, unionNewExistProb);
 			}
 			catch (Exception e) {
 				e.printStackTrace();
@@ -376,96 +255,39 @@ public class PerceptualGrouping_MLN extends MarkovLogicComponent {
 		}
 		return unionsToUpdate;
 	}  
-	 
-	
-	private List<String> getOriginSubarchitectures(Belief b) throws BeliefException {
-		
-		List<String> subarchitectures = new ArrayList<String>();
-		
-		if (!(b.hist instanceof CASTBeliefHistory)) {
-			throw new BeliefException ("ERROR: history not specified as cast pointer");
-		}
-		else if (((CASTBeliefHistory)b.hist).ancestors == null) {
-			throw new BeliefException ("ERROR: percept history is null");
-		}
-	
-		for (WorkingMemoryAddress ancestorAddress : ((CASTBeliefHistory)b.hist).ancestors) {
-			if (!(ancestorAddress.subarchitecture.equals(BindingWorkingMemory.BINDER_SA))) {
-				subarchitectures.add(ancestorAddress.subarchitecture);
-			} else
-				try {
-					if (existsOnWorkingMemory(ancestorAddress)) {
-						Belief subB = getMemoryEntry(ancestorAddress, Belief.class);
-						subarchitectures.addAll(getOriginSubarchitectures(subB));
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-		}
-		return subarchitectures;
-	}
-	
-	
+
+
 	private HashMap<String,Belief> selectRelevantUnions (HashMap<String,Belief> existingUnions, 
 			PerceptBelief percept) throws BeliefException {
-		
+
 		HashMap<String,Belief> relevantUnions = new HashMap<String,Belief>();
-	
+
 		if (((CASTBeliefHistory)percept.hist).ancestors.size() == 0) {
 			throw new BeliefException ("ERROR: percept history contains 0 element");
 		}
 		else if (((CASTBeliefHistory)percept.hist).ancestors.size() > 1) {
 			throw new BeliefException ("ERROR: percept history contains more than 1 element");
 		}
-		
+
 		String perceptOrigin = getOriginSubarchitectures(percept).get(0);
-		
+
 		for (String existingUnionId : existingUnions.keySet()) {
-			
+
 			Belief existingUnion = existingUnions.get(existingUnionId);
 			List<String> existinUnionOrigins = getOriginSubarchitectures(existingUnion);
-			
+
 			if (!existinUnionOrigins.contains(perceptOrigin)) {
 				relevantUnions.put(existingUnionId, existingUnion);
 			}
-			
+
 		}
-		
-		
+
+
 		return relevantUnions;
 	}
-	
-	/**
-	 * Exact the set of existing unions from the binder working memory
-	 * 
-	 * @return the set of existing unions (as a mapping from identifier to objects)
-	 */
-	private HashMap<String, Belief> extractExistingUnions() {
 
-		HashMap<String, Belief> existingunions = new HashMap<String, Belief>();
-
-		try {
-			CASTData<PerceptUnionBelief>[] unions;
-
-			unions = getWorkingMemoryEntries(BindingWorkingMemory.BINDER_SA, PerceptUnionBelief.class);
-
-			for (int i = (unions.length - 1) ; i >= 0 ; i--) {
-				existingunions.put(unions[i].getData().id, unions[i].getData());
-			}
-		}
-		catch (UnknownSubarchitectureException e) {
-			log("Problem with architecture name!");
-		}
-		catch (SubarchitectureComponentException e) {
-			e.printStackTrace();
-		}
-		return existingunions;
-	}
-	 
-	
-	
 	private void deleteAllMultiModalBeliefAttachedToUnion (WorkingMemoryAddress unionAddress) {
-		
+
 		try {
 			CASTData<MultiModalBelief>[] mmBeliefs;
 
@@ -488,5 +310,40 @@ public class PerceptualGrouping_MLN extends MarkovLogicComponent {
 		}
 	}
 	
+	@Override
+	public void workingMemoryChangeDelete(WorkingMemoryAddress perceptWMAddress) {
+		deleteAllMultiModalBeliefAttachedToUnion(perceptWMAddress);
+	}
 	
+	@Override
+	public void workingMemoryChangeInsert(Belief percept, WorkingMemoryAddress perceptWMAddress) throws BeliefException {
+		performPerceptualGrouping((PerceptBelief)percept, perceptWMAddress);
+	}
+
+	/**
+	 * Exact the set of existing unions from the binder working memory
+	 * 
+	 * @return the set of existing unions (as a mapping from identifier to objects)
+	 */
+	protected HashMap<String, Belief> extractExistingUnions() {
+	
+		HashMap<String, Belief> existingunions = new HashMap<String, Belief>();
+	
+		try {
+			CASTData<PerceptUnionBelief>[] unions;
+	
+			unions = getWorkingMemoryEntries(BindingWorkingMemory.BINDER_SA, PerceptUnionBelief.class);
+	
+			for (int i = (unions.length - 1) ; i >= 0 ; i--) {
+				existingunions.put(unions[i].getData().id, unions[i].getData());
+			}
+		}
+		catch (UnknownSubarchitectureException e) {
+			log("Problem with architecture name!");
+		}
+		catch (SubarchitectureComponentException e) {
+			e.printStackTrace();
+		}
+		return existingunions;
+	}
 }
