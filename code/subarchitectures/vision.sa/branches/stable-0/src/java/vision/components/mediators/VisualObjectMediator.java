663788a0acef79e04c5275157ba96b91a08bfee0
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-19 09:22:23 +0000

    - new mediator framework though it is not used currently

--- code/subarchitectures/vision.sa/branches/stable-0/src/java/vision/components/mediators/VisualObjectMediator.java
+++ code/subarchitectures/vision.sa/branches/stable-0/src/java/vision/components/mediators/VisualObjectMediator.java
@@ -3,39 +3,50 @@
  */
 package vision.components.mediators;
 
+import java.util.EnumSet;
+import java.util.Vector;
+
+import vision.components.mediators.abstr.AbstractLocalizedPerceptionMediator;
+
+import SpatialProperties.DiscreteProbabilityDistribution;
+import SpatialProperties.IntegerValue;
 import SpatialProperties.PlaceContainmentObjectProperty;
+import SpatialProperties.ValueProbabilityPair;
 import VisionData.VisualObject;
+import cast.architecture.ManagedComponent;
+import cast.cdl.WorkingMemoryAddress;
 import cast.cdl.WorkingMemoryChange;
+import cast.cdl.WorkingMemoryOperation;
 
 /**
  * @author marc
  * 
  */
 public class VisualObjectMediator extends
-		AbstractDetectedObjectMediator<VisualObject> {
+		AbstractLocalizedPerceptionMediator<VisualObject, PlaceContainmentObjectProperty> {
 
-	/**
-	 * this is a threshold to indentify which confidence is required to assume
-	 * an object to be detected
-	 */
-	private static final double CONFIDENCE_THRESHOLD = 0.5;
-
-	public VisualObjectMediator() {
-		super(VisualObject.class, false);
+	public VisualObjectMediator(ManagedComponent c) {
+		super(c, VisualObject.class, PlaceContainmentObjectProperty.class, EnumSet.of(
+				WorkingMemoryOperation.ADD, WorkingMemoryOperation.OVERWRITE));
+		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	protected boolean transform(PlaceContainmentObjectProperty pcop,
-			VisualObject object, WorkingMemoryChange event) {
-		println("confidence=" + object.detectionConfidence);
-		if (object.detectionConfidence < CONFIDENCE_THRESHOLD)
-			return false;
-		pcop.label = object.label;
-
-		// TODO: using the hashcode assume that each object only exists once!
-		pcop.objectId = pcop.label.hashCode();
-
+	protected boolean convert(WorkingMemoryChange wmc, VisualObject from,
+			PlaceContainmentObjectProperty to, long placeId) {
+		Vector<ValueProbabilityPair> data = new Vector<ValueProbabilityPair>(1);
+		to.mapValue = new IntegerValue(placeId);
+		data.add(new ValueProbabilityPair(to.mapValue, 1.0));
+		to.distribution = new DiscreteProbabilityDistribution(data);
+		to.mapValueReliable = true;
 		return true;
+	}
+
+	@Override
+	public PlaceContainmentObjectProperty create(
+			WorkingMemoryAddress idToCreate, WorkingMemoryChange wmc,
+			VisualObject from) {
+		return new PlaceContainmentObjectProperty(from.label.hashCode(), null, null, false, from.label);
 	}
 
 }
