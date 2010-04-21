b3701daed08b858a7e50d31a44a814889963b089
plison <plison@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-21 10:23:49 +0000

    new methods

--- tools/beliefs/src/java/beliefmodels/utils/FeatureContentUtils.java
+++ tools/beliefs/src/java/beliefmodels/utils/FeatureContentUtils.java
@@ -12,6 +12,7 @@ import beliefmodels.autogen.distribs.DistributionWithExistDep;
 import beliefmodels.autogen.distribs.FeatureValueProbPair;
 import beliefmodels.autogen.distribs.FeatureValues;
 import beliefmodels.autogen.distribs.ProbDistribution;
+import beliefmodels.autogen.featurecontent.FeatureValue;
 import beliefmodels.autogen.featurecontent.PointerValue;
 
 
@@ -59,6 +60,52 @@ public class FeatureContentUtils {
 	
 	
 	
+	
+	public static BasicProbDistribution getFeatureForFeatureValueInBelief (Belief b, FeatureValue featval) throws BeliefException {
+		
+		if (b == null || b.content == null) {
+			throw new BeliefException("ERROR, null value");
+		}
+		else {
+			return getFeatureForFeatureValueInDistribution (b.content, featval);
+		}
+	}
+	
+	
+	public static BasicProbDistribution getFeatureForFeatureValueInDistribution (ProbDistribution distrib, 
+			FeatureValue featval) throws BeliefException {
+		
+	if (distrib instanceof DistributionWithExistDep) {
+		return getFeatureForFeatureValueInDistribution (((DistributionWithExistDep)distrib).Pc, featval);
+	}
+	
+	if (distrib instanceof CondIndependentDistribs) {
+		for (String feat : ((CondIndependentDistribs)distrib).distribs.keySet()) {
+			BasicProbDistribution result = getFeatureForFeatureValueInDistribution(((CondIndependentDistribs)distrib).distribs.get(feat),featval);
+			if (result!= null) {
+				return result;
+			}
+		}
+	}
+	
+	if (distrib instanceof BasicProbDistribution) {
+		if (((BasicProbDistribution)distrib).values.getClass().equals(FeatureValues.class)) {
+			for (FeatureValueProbPair pair : ((FeatureValues)((BasicProbDistribution)distrib).values).values) {
+				if (pair.val.equals(featval)) {
+					return ((BasicProbDistribution)distrib);
+				}
+			}
+		}
+	}
+	
+		return null;
+	}
+	
+	
+	
+	
+	
+	
 	public static List<FeatureValueProbPair> getAllPointerValuesInBelief (Belief b) throws BeliefException {
 		if (b != null && b.content != null) {
 			return getAllPointerValuesInDistribution(b.content);
@@ -69,7 +116,9 @@ public class FeatureContentUtils {
 	}
 	
 	
-	public static List<FeatureValueProbPair> getAllPointerValuesInDistribution (ProbDistribution distrib) throws BeliefException {
+	
+	public static List<FeatureValueProbPair> getAllPointerValuesInDistribution (ProbDistribution distrib) 
+		throws BeliefException {
 		
 		List<FeatureValueProbPair> all = new LinkedList<FeatureValueProbPair>();
 
@@ -103,5 +152,48 @@ public class FeatureContentUtils {
 	}
 	
 	
+	public static void addAnotherValueInBelief (Belief b, String featlabel, 
+			FeatureValueProbPair newPair) throws BeliefException {
+		
+		System.out.println("adding another value for featlabel: " + featlabel);
+		
+		if (b == null || b.content == null) {
+			throw new BeliefException("ERROR, null value");
+		}
+		
+		else {
+			addAnotherValueInDistribution (b.content, featlabel, newPair);
+		}
+	}
+	
+	
+	public static void addAnotherValueInBasicProbDistribution(BasicProbDistribution distrib, FeatureValueProbPair newPair) {
+		if (distrib.values.getClass().equals(FeatureValues.class)) {
+			FeatureValues featvals = (FeatureValues)distrib.values;
+			
+			// here, we should verify that the prob distribution is still coherent!!
+			featvals.values.add(newPair);
+		}
+	}
+	
+	
+	public static void addAnotherValueInDistribution (ProbDistribution distrib, String featlabel, 
+			FeatureValueProbPair newPair) throws BeliefException {
+		
+		if (distrib instanceof DistributionWithExistDep) {
+			addAnotherValueInDistribution (((DistributionWithExistDep)distrib).Pc, featlabel, newPair);
+		}
+		
+		if (distrib instanceof CondIndependentDistribs) {
+						
+			if (((CondIndependentDistribs)distrib).distribs.containsKey(featlabel)) {
+							
+				ProbDistribution featDistrib = ((CondIndependentDistribs)distrib).distribs.get(featlabel);
+				
+				addAnotherValueInBasicProbDistribution((BasicProbDistribution)featDistrib, newPair);
+					
+			}
+		}
+	}
 }
 
