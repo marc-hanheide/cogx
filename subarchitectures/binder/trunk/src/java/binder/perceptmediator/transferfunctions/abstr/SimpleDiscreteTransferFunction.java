1cf49e57dad4b2a9338232ab08638ce5d6b8210c
Marc Hanheide <marc@hanheide.net>
2010-04-25 12:08:20 +0000

    - ComaRooms now referenced from the Places  - types are now without "::"

--- subarchitectures/binder/trunk/src/java/binder/perceptmediator/transferfunctions/abstr/SimpleDiscreteTransferFunction.java
+++ subarchitectures/binder/trunk/src/java/binder/perceptmediator/transferfunctions/abstr/SimpleDiscreteTransferFunction.java
@@ -6,10 +6,12 @@ package binder.perceptmediator.transferfunctions.abstr;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 
+import SpatialData.Place;
 import beliefmodels.arch.BeliefException;
 import beliefmodels.autogen.beliefs.PerceptBelief;
 import beliefmodels.autogen.distribs.BasicProbDistribution;
@@ -66,11 +68,12 @@ public abstract class SimpleDiscreteTransferFunction<From extends Ice.ObjectImpl
 		assert (perceptBelief.content != null);
 		assert (perceptBelief.content instanceof CondIndependentDistribs);
 		{
-			String offsprings="";
+			String offsprings = "";
 			for (WorkingMemoryAddress offspringWMA : ((CASTBeliefHistory) perceptBelief.hist).offspring) {
 				offsprings += " " + offspringWMA.id;
 			}
-			component.println("offspring BEFORE transformation occurs:"+offsprings);
+			component.println("offspring BEFORE transformation occurs:"
+					+ offsprings);
 		}
 
 		CondIndependentDistribs features = (CondIndependentDistribs) perceptBelief.content;
@@ -86,11 +89,12 @@ public abstract class SimpleDiscreteTransferFunction<From extends Ice.ObjectImpl
 			component.logException(e);
 		}
 		{
-			String offsprings="";
+			String offsprings = "";
 			for (WorkingMemoryAddress offspringWMA : ((CASTBeliefHistory) perceptBelief.hist).offspring) {
 				offsprings += " " + offspringWMA.id;
 			}
-			component.println("offspring AFTER transformation occured:" + offsprings);
+			component.println("offspring AFTER transformation occured:"
+					+ offsprings);
 		}
 		return true;
 	}
@@ -159,9 +163,10 @@ public abstract class SimpleDiscreteTransferFunction<From extends Ice.ObjectImpl
 					.createNewCondIndependentDistribs();
 
 			PerceptBelief pb;
-			pb = PerceptBuilder.createNewPerceptBelief(idToCreate.id, wmc.type,
-					"here", CASTUtils.getTimeServer().getCASTTime(), features,
-					hist);
+
+			pb = PerceptBuilder.createNewPerceptBelief(idToCreate.id,
+					getBeliefTypeFromCastType(wmc.type), "here", CASTUtils
+							.getTimeServer().getCASTTime(), features, hist);
 			return pb;
 		} catch (BeliefException e) {
 			component.logException(e);
@@ -170,4 +175,15 @@ public abstract class SimpleDiscreteTransferFunction<From extends Ice.ObjectImpl
 
 	}
 
+	public static String getBeliefTypeFromCastType(String casttype) {
+		StringTokenizer st = new StringTokenizer(casttype, ":");
+		String type = casttype;
+		while (st.hasMoreTokens())
+			type = st.nextToken();
+		return type;
+	}
+
+	public static Object getBeliefTypeFromCastType(Class<? extends Ice.Object> class1) {
+		return getBeliefTypeFromCastType(CASTUtils.typeName(class1));
+	}
 }
