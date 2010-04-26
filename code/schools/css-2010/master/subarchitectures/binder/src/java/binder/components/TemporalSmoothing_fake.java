69d7d63fc677e51f924cb10d6d9b4833e5f30ea3
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-26 11:30:38 +0000

    MERGE: binder bugfixes

--- code/schools/css-2010/master/subarchitectures/binder/src/java/binder/components/TemporalSmoothing_fake.java
+++ code/schools/css-2010/master/subarchitectures/binder/src/java/binder/components/TemporalSmoothing_fake.java
@@ -1,26 +1,33 @@
+
+//=================================================================                                                        
+//Copyright (C) 2009-2011 Pierre Lison (pierre.lison@dfki.de)                                                                
+//                                                                                                                       
+//This library is free software; you can redistribute it and/or                                                            
+//modify it under the terms of the GNU Lesser General Public License                                                       
+//as published by the Free Software Foundation; either version 2.1 of                                                      
+//the License, or (at your option) any later version.                                                                      
+//                                                                                                                       
+//This library is distributed in the hope that it will be useful, but                                                      
+//WITHOUT ANY WARRANTY; without even the implied warranty of                                                               
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU                                                         
+//Lesser General Public License for more details.                                                                          
+//                                                                                                                       
+//You should have received a copy of the GNU Lesser General Public                                                         
+//License along with this program; if not, write to the Free Software                                                      
+//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA                                                                
+//02111-1307, USA.                                                                                                         
+//=================================================================                                                        
+
 package binder.components;
 
-import java.util.LinkedList;
 import java.util.List;
 
-import beliefmodels.arch.BeliefException;
-import beliefmodels.autogen.beliefs.Belief;
-import beliefmodels.autogen.beliefs.MultiModalBelief;
-import beliefmodels.autogen.beliefs.PerceptUnionBelief;
 import beliefmodels.autogen.beliefs.StableBelief;
 import beliefmodels.autogen.beliefs.TemporalUnionBelief;
-import beliefmodels.autogen.distribs.FeatureValueProbPair;
-import beliefmodels.autogen.featurecontent.PointerValue;
 import beliefmodels.autogen.history.CASTBeliefHistory;
 import beliefmodels.builders.StableBeliefBuilder;
-import beliefmodels.builders.TemporalUnionBuilder;
-import beliefmodels.utils.FeatureContentUtils;
-import binder.abstr.BeliefWriter;
 import binder.abstr.FakeComponent;
 import binder.arch.BindingWorkingMemory;
-import cast.AlreadyExistsOnWMException;
-import cast.DoesNotExistOnWMException;
-import cast.UnknownSubarchitectureException;
 import cast.architecture.ChangeFilterFactory;
 import cast.architecture.WorkingMemoryChangeReceiver;
 import cast.cdl.WorkingMemoryAddress;
@@ -28,101 +35,146 @@ import cast.cdl.WorkingMemoryChange;
 import cast.cdl.WorkingMemoryOperation;
 import cast.core.CASTData;
 
+
+/**
+ * Dummy component for temporal smoothing (currently directly converts a temporal union belief
+ * belief into a stable belief)
+ * 
+ * @author Pierre Lison (plison@dfki.de)
+ *
+ */
+
 public class TemporalSmoothing_fake extends FakeComponent {
 
-	
-	String beliefUpdateToIgnore = "";
 
-	
+	/**
+	 * Adding change filters on working memory changes (insertion, overwrite, deletion)
+	 * pertaining to temporal union beliefs
+	 * 
+	 */
 	@Override
 	public void start() {
+		
+		// Insertion
 		addChangeFilter(
 				ChangeFilterFactory.createLocalTypeFilter(TemporalUnionBelief.class,
 						WorkingMemoryOperation.ADD), new WorkingMemoryChangeReceiver() {
 					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
-						
-						try {
-							CASTData<TemporalUnionBelief> beliefData = getMemoryEntryWithData(_wmc.address, TemporalUnionBelief.class);
-							
-							StableBelief stableBelief = StableBeliefBuilder.createnewStableBelief(beliefData.getData(), _wmc.address, newDataID());
-							
-							updatePointers(stableBelief, StableBelief.class);
-							
-							insertBeliefInWM(stableBelief);
-
-							addOffspring(beliefData.getData(), stableBelief.id);	
-							beliefUpdateToIgnore = beliefData.getID();
-							updateBeliefOnWM(beliefData.getData());
-								
-						}	
-			
-						 catch (Exception e) {
-								e.printStackTrace();
-							}
+						tunionBeliefAdded(_wmc);
 					}
 				}
 		);
 		
+		// Update
 		addChangeFilter(
 				ChangeFilterFactory.createLocalTypeFilter(TemporalUnionBelief.class,
 						WorkingMemoryOperation.OVERWRITE), new WorkingMemoryChangeReceiver() {
 					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
-						
-						try {
-							CASTData<TemporalUnionBelief> beliefData = getMemoryEntryWithData(_wmc.address, TemporalUnionBelief.class);
-
-							if (!beliefData.getID().equals(beliefUpdateToIgnore)) {
-
-							List<WorkingMemoryAddress> offspring = ((CASTBeliefHistory)beliefData.getData().hist).offspring;
-							log("number of offspring for : " + beliefData.getData().id + ": "+ offspring.size());
-
-							for (WorkingMemoryAddress child : offspring) {
-								if (existsOnWorkingMemory(child)) {
-									StableBelief childBelief = getMemoryEntry(child, StableBelief.class);
-									childBelief =StableBeliefBuilder.createnewStableBelief(beliefData.getData(), _wmc.address, childBelief.id);
-									updateBeliefOnWM(childBelief);
-									updatePointers(childBelief, PerceptUnionBelief.class);
-								}
-							}
-							}
-							else {
-								log("ignore update, simple addition of offspring");
-								beliefUpdateToIgnore = "";
-							}
-							
-						}	
-
-						catch (Exception e) {
-							e.printStackTrace();
-						} 
+						tunionBeliefUpdated(_wmc);
 					}
 				}
 		);
 		
+		// Deletion
 		addChangeFilter(
 				ChangeFilterFactory.createLocalTypeFilter(TemporalUnionBelief.class,
 						WorkingMemoryOperation.DELETE), new WorkingMemoryChangeReceiver() {
 					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
-						
-						try {
-							CASTData<TemporalUnionBelief> beliefData = getMemoryEntryWithData(_wmc.address, TemporalUnionBelief.class);
-
-							List<WorkingMemoryAddress> offspring = ((CASTBeliefHistory)beliefData.getData().hist).offspring;
-
-							for (WorkingMemoryAddress child : offspring) {
-								if (existsOnWorkingMemory(child)) {
-									deleteBeliefOnWM(child.id);
-								}
-							}
-							
-						}	
-
-						catch (Exception e) {
-							e.printStackTrace();
-						} 
+						tunionBeliefDeleted(_wmc);
 					}
 				}
 		);
+	}
+	
+
+
+	/**
+	 * Upon the insertion of a new temporal union belief, create one single offspring, add it to the
+	 * belief offspring field, and update it
+	 * 
+	 * @param wmc the working memory change
+	 */
+	private void tunionBeliefAdded (WorkingMemoryChange wmc) {
+		 
+		try {
+			CASTData<TemporalUnionBelief> beliefData = getMemoryEntryWithData(wmc.address, TemporalUnionBelief.class);
+			
+			addOffspring(beliefData.getData(), newDataID());	
+			updateBeliefOnWM(beliefData.getData());
+				
+		}	
+
+		 catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
+	
+	
+	/**
+	 * Upon the update of a given temporal union belief, loop on its offspring.  If an offspring doesn't
+	 * exist yet on the WM, create it, else update the existing one
+	 * 
+	 * @param wmc the working memory change
+	 */
+	private void tunionBeliefUpdated (WorkingMemoryChange wmc) {
+	
+		try {
+			CASTData<TemporalUnionBelief> beliefData = getMemoryEntryWithData(wmc.address, TemporalUnionBelief.class);
+
+			List<WorkingMemoryAddress> offspring = ((CASTBeliefHistory)beliefData.getData().hist).offspring;
+			log("number of offspring for : " + beliefData.getData().id + ": "+ offspring.size());
+
+			for (WorkingMemoryAddress child : offspring) {
+				if (existsOnWorkingMemory(child)) {
+					log("belief " + child.id + " exists on WM, merging content");
+					StableBelief childBelief =StableBeliefBuilder.createnewStableBelief(beliefData.getData(), wmc.address, child.id);
+
+					updatePointers(childBelief, StableBelief.class);
+
+					StableBelief existingBelief = getMemoryEntry(new WorkingMemoryAddress(child.id, BindingWorkingMemory.BINDER_SA), StableBelief.class);
+					childBelief.content = mergeBeliefContent(existingBelief.content, childBelief.content);
+					
+					updateBeliefOnWM(childBelief);
+				}
+				else {
+					log("belief " + child.id + " does not exist on WM, creating it");
+					StableBelief childBelief =StableBeliefBuilder.createnewStableBelief(beliefData.getData(), wmc.address, child.id);
+					updatePointers(childBelief, StableBelief.class);
+					insertBeliefInWM(childBelief);
+				}
+			}
+			
+		}	
+
+		catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	
+	/**
+	 * Upon the deletion of a given temporal union belief, delete all the offspring of the belief
+	 * 
+	 * @param wmc the working memory change
+	 */
+	private void tunionBeliefDeleted (WorkingMemoryChange wmc) {
+		
+		try {
+			CASTData<TemporalUnionBelief> beliefData = getMemoryEntryWithData(wmc.address, TemporalUnionBelief.class);
+
+			List<WorkingMemoryAddress> offspring = ((CASTBeliefHistory)beliefData.getData().hist).offspring;
+
+			for (WorkingMemoryAddress child : offspring) {
+				if (existsOnWorkingMemory(child)) {
+					deleteBeliefOnWM(child.id);
+				}
+			}
+			
+		}	
+
+		catch (Exception e) {
+			e.printStackTrace();
+		} 
 	}
 	
 }
---
+++ code/schools/css-2010/master/subarchitectures/binder/src/java/binder/testing/SpringSchoolTest.java
@@ -0,0 +1,208 @@
+package binder.testing;
+
+import beliefmodels.autogen.beliefs.PerceptBelief;
+import beliefmodels.autogen.beliefs.TemporalUnionBelief;
+import beliefmodels.autogen.distribs.BasicProbDistribution;
+import beliefmodels.autogen.distribs.CondIndependentDistribs;
+import beliefmodels.autogen.distribs.FeatureValueProbPair;
+import beliefmodels.autogen.distribs.ProbDistribution;
+import beliefmodels.autogen.history.CASTBeliefHistory;
+import beliefmodels.builders.BeliefContentBuilder;
+import beliefmodels.builders.FeatureValueBuilder;
+import beliefmodels.builders.PerceptBuilder;
+import binder.arch.BindingWorkingMemory;
+import cast.SubarchitectureComponentException;
+import cast.architecture.ChangeFilterFactory;
+import cast.architecture.WorkingMemoryChangeReceiver;
+import cast.cdl.CASTTime;
+import cast.cdl.WorkingMemoryAddress;
+import cast.cdl.WorkingMemoryChange;
+import cast.cdl.WorkingMemoryOperation;
+import cast.core.CASTData;
+
+public class SpringSchoolTest extends AbstractBinderTest {
+
+	
+	
+	@Override
+	public void start() {
+		addChangeFilter(
+				ChangeFilterFactory.createLocalTypeFilter(TemporalUnionBelief.class,
+						WorkingMemoryOperation.ADD), new WorkingMemoryChangeReceiver() {
+					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+						try {
+							
+							CASTData<TemporalUnionBelief>[] unions = getWorkingMemoryEntries(TemporalUnionBelief.class);
+							log("Current number of unions in WM: " + unions.length);
+						} 
+						catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+					}
+				}
+		);
+		
+		addChangeFilter(
+				ChangeFilterFactory.createLocalTypeFilter(TemporalUnionBelief.class,
+						WorkingMemoryOperation.OVERWRITE), new WorkingMemoryChangeReceiver() {
+					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+						try {
+							
+							CASTData<TemporalUnionBelief>[] unions = getWorkingMemoryEntries(TemporalUnionBelief.class);
+							log("Current number of unions in WM: " + unions.length);
+						} 
+						catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+					}
+				}
+		);
+	}
+	
+	@Override
+	public String getReasonForFailure() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTestDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTestName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isTestFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isTestSuccessful() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void startTest() {
+
+
+		try {
+
+			CASTBeliefHistory hist_b1 = PerceptBuilder.createNewPerceptHistory(new WorkingMemoryAddress("ddfsadsf","spatial"));
+
+			CondIndependentDistribs contentDistrib_b1 = BeliefContentBuilder.createNewCondIndependentDistribs();
+
+			BasicProbDistribution typeDistrib_b1 = BeliefContentBuilder.createNewFeatureDistributionWithSinglePair("type", 
+					new FeatureValueProbPair(FeatureValueBuilder.createNewStringValue("Office"), 0.85f));
+
+			BeliefContentBuilder.putNewCondIndependentDistrib(contentDistrib_b1, typeDistrib_b1);
+
+			ProbDistribution content_b1 = BeliefContentBuilder.createNewDistributionWithExistDep(0.9f, contentDistrib_b1);
+
+			PerceptBelief b1 = PerceptBuilder.createNewPerceptBelief(newDataID(), "place", "here", getCASTTime(), content_b1, hist_b1);
+
+			insertBeliefInWM(b1);
+
+
+
+			CASTBeliefHistory hist_b2 = PerceptBuilder.createNewPerceptHistory(new WorkingMemoryAddress("ddfsadsf","vision"));
+
+			CondIndependentDistribs contentDistrib_b2 = BeliefContentBuilder.createNewCondIndependentDistribs();
+
+			BasicProbDistribution typeDistrib_b2 = BeliefContentBuilder.createNewFeatureDistributionWithSinglePair("type", 
+					new FeatureValueProbPair(FeatureValueBuilder.createNewStringValue("3:2"), 0.89f));
+
+			BeliefContentBuilder.putNewCondIndependentDistrib(contentDistrib_b2, typeDistrib_b2);
+
+			BasicProbDistribution locationDistrib_b2 = BeliefContentBuilder.createNewFeatureDistributionWithSinglePair("is-in", 
+					new FeatureValueProbPair(FeatureValueBuilder.createNewPointerValue(new WorkingMemoryAddress(b1.id, BindingWorkingMemory.BINDER_SA)), 0.93f));
+
+			BeliefContentBuilder.putNewCondIndependentDistrib(contentDistrib_b2, locationDistrib_b2);
+
+			ProbDistribution content_b2 = BeliefContentBuilder.createNewDistributionWithExistDep(1.0f, contentDistrib_b2);
+
+			PerceptBelief b2 = PerceptBuilder.createNewPerceptBelief(newDataID(), "Object", "here", getCASTTime(), content_b2, hist_b2);
+
+			insertBeliefInWM(b2);
+
+
+
+			CASTBeliefHistory hist_b3 = PerceptBuilder.createNewPerceptHistory(new WorkingMemoryAddress("aa","vision"));
+
+			CondIndependentDistribs contentDistrib_b3 = BeliefContentBuilder.createNewCondIndependentDistribs();
+
+			BasicProbDistribution typeDistrib_b3 = BeliefContentBuilder.createNewFeatureDistributionWithSinglePair("type", 
+					new FeatureValueProbPair(FeatureValueBuilder.createNewStringValue("3:2"), 0.95f));
+
+			BeliefContentBuilder.putNewCondIndependentDistrib(contentDistrib_b3, typeDistrib_b3);
+
+			BasicProbDistribution locationDistrib_b3 = BeliefContentBuilder.createNewFeatureDistributionWithSinglePair("is-in", 
+					new FeatureValueProbPair(FeatureValueBuilder.createNewPointerValue(new WorkingMemoryAddress(b1.id, BindingWorkingMemory.BINDER_SA)), 0.95f));
+
+			BeliefContentBuilder.putNewCondIndependentDistrib(contentDistrib_b3, locationDistrib_b3);
+
+			ProbDistribution content_b3 = BeliefContentBuilder.createNewDistributionWithExistDep(1.0f, contentDistrib_b3);
+
+			PerceptBelief b3 = PerceptBuilder.createNewPerceptBelief(newDataID(), "Object", "here", getCASTTime(), content_b3, hist_b3);
+
+			insertBeliefInWM(b3);
+			
+			 
+
+			CASTBeliefHistory hist_b4 = PerceptBuilder.createNewPerceptHistory(new WorkingMemoryAddress("ddfsadsf","vision"));
+
+			CondIndependentDistribs contentDistrib_b4 = BeliefContentBuilder.createNewCondIndependentDistribs();
+
+			BasicProbDistribution locationDistrib_b4 = BeliefContentBuilder.createNewFeatureDistributionWithSinglePair("is-in", 
+					new FeatureValueProbPair(FeatureValueBuilder.createNewPointerValue(new WorkingMemoryAddress(b1.id, BindingWorkingMemory.BINDER_SA)), 0.93f));
+
+			BeliefContentBuilder.putNewCondIndependentDistrib(contentDistrib_b4, locationDistrib_b4);
+
+		//	ProbDistribution content_b4 = BeliefContentBuilder.createNewDistributionWithExistDep(0.9f, contentDistrib_b2);
+
+			PerceptBelief b4 = PerceptBuilder.createNewPerceptBelief(newDataID(), "Person", "here", getCASTTime(), contentDistrib_b4, hist_b4);
+
+			insertBeliefInWM(b4);
+
+
+
+			CASTBeliefHistory hist_b5 = PerceptBuilder.createNewPerceptHistory(new WorkingMemoryAddress("aa","vision"));
+
+			CondIndependentDistribs contentDistrib_b5 = BeliefContentBuilder.createNewCondIndependentDistribs();
+
+			BasicProbDistribution locationDistrib_b5 = BeliefContentBuilder.createNewFeatureDistributionWithSinglePair("is-in", 
+					new FeatureValueProbPair(FeatureValueBuilder.createNewPointerValue(new WorkingMemoryAddress(b1.id, BindingWorkingMemory.BINDER_SA)), 0.95f));
+
+			BeliefContentBuilder.putNewCondIndependentDistrib(contentDistrib_b5, locationDistrib_b5);
+
+	//		ProbDistribution content_b5 = BeliefContentBuilder.createNewDistributionWithExistDep(0.9f, contentDistrib_b5);
+
+			BasicProbDistribution distanceDistrib_b5 = BeliefContentBuilder.createNewFeatureDistributionWithSinglePair("distance", 
+					new FeatureValueProbPair(FeatureValueBuilder.createNewFloatValue(0.67f), 0.95f));
+
+			BeliefContentBuilder.putNewCondIndependentDistrib(contentDistrib_b5, distanceDistrib_b5); 
+	
+			PerceptBelief b5 = PerceptBuilder.createNewPerceptBelief(newDataID(), "Person", "here", getCASTTime(), contentDistrib_b5, hist_b5);
+
+			insertBeliefInWM(b5);
+					
+			updateBeliefOnWM(b5);
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
