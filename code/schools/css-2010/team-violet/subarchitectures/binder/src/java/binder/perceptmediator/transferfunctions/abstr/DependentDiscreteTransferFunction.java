1c5665877b452f96cbbef16d7bbd9ce5a129298f
brenner <brenner@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-27 10:37:09 +0000

    Objects in binding

--- code/schools/css-2010/team-violet/subarchitectures/binder/src/java/binder/perceptmediator/transferfunctions/abstr/DependentDiscreteTransferFunction.java
+++ code/schools/css-2010/team-violet/subarchitectures/binder/src/java/binder/perceptmediator/transferfunctions/abstr/DependentDiscreteTransferFunction.java
@@ -5,12 +5,15 @@ import java.util.Map.Entry;
 import org.apache.log4j.Logger;
 
 import beliefmodels.autogen.beliefs.PerceptBelief;
+import cast.UnknownSubarchitectureException;
 import cast.architecture.ManagedComponent;
 import cast.cdl.WorkingMemoryAddress;
 import castutils.castextensions.WMContentWaiter;
 import castutils.castextensions.WMView;
 import castutils.castextensions.WMContentWaiter.ContentMatchingFunction;
 import castutils.castextensions.WMEntrySynchronizer.TransferFunction;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * a more complex abstract {@link TransferFunction} required to realize
@@ -24,45 +27,73 @@ import castutils.castextensions.WMEntrySynchronizer.TransferFunction;
  *            the type to transfer from
  */
 public abstract class DependentDiscreteTransferFunction<From extends Ice.ObjectImpl>
-		extends SimpleDiscreteTransferFunction<From> {
+        extends SimpleDiscreteTransferFunction<From> {
 
-	protected WMView<PerceptBelief> allBeliefs;
-	WMContentWaiter<PerceptBelief> waitingBeliefReader;
+    protected WMView<PerceptBelief> allBeliefs;
+    WMContentWaiter<PerceptBelief> waitingBeliefReader;
 
-	/**
-	 * constructor. Requires a {@link WMView} of all {@link PerceptBelief} to
-	 * look for the referred Beliefs.
-	 * 
-	 * @see SimpleDiscreteTransferFunction
-	 * @param component
-	 * @param allBeliefs
-	 * @param logger
-	 */
-	public DependentDiscreteTransferFunction(ManagedComponent component,
-			WMView<PerceptBelief> allBeliefs, Logger logger) {
-		super(component, logger);
-		this.allBeliefs = allBeliefs;
-		waitingBeliefReader = new WMContentWaiter<PerceptBelief>(
-				this.allBeliefs);
-	}
+    /**
+     * constructor. Requires a {@link WMView} of all {@link PerceptBelief} to
+     * look for the referred Beliefs.
+     *
+     * @see SimpleDiscreteTransferFunction
+     * @param component
+     * @param allBeliefs
+     * @param logger
+     */
+    public DependentDiscreteTransferFunction(ManagedComponent component,
+            WMView<PerceptBelief> allBeliefs, Logger logger) {
+        super(component, logger);
+        this.allBeliefs = allBeliefs;
+        waitingBeliefReader = new WMContentWaiter<PerceptBelief>(
+                this.allBeliefs);
+    }
 
-	/**
-	 * this method tries to dereference a referred belief. It waits for the
-	 * referred object to appear and blocks until then.
-	 * 
-	 * @param contentMatchingFunction the matching function to be used
-	 * @see WMContentWaiter
-	 * @return the working memory address that corresponds
-	 * @throws InterruptedException
-	 */
-	protected WorkingMemoryAddress getReferredBelief(
-			ContentMatchingFunction<PerceptBelief> contentMatchingFunction)
-			throws InterruptedException {
-		logger.debug("trying to find referred belief");
-		Entry<WorkingMemoryAddress, PerceptBelief> entry = waitingBeliefReader
-				.read(contentMatchingFunction);
-		logger.debug("got it: " + entry.getKey().id);
-		return entry.getKey();
-	}
+    /**
+     * this method tries to dereference a referred belief. It waits for the
+     * referred object to appear and blocks until then.
+     *
+     * @param contentMatchingFunction the matching function to be used
+     * @see WMContentWaiter
+     * @return the working memory address that corresponds
+     * @throws InterruptedException
+     */
+    protected WorkingMemoryAddress getReferredBelief(
+            ContentMatchingFunction<PerceptBelief> contentMatchingFunction)
+            throws InterruptedException {
+        logger.debug("trying to find referred belief");
+        Entry<WorkingMemoryAddress, PerceptBelief> entry = waitingBeliefReader.read(contentMatchingFunction);
+        logger.debug("got it: " + entry.getKey().id);
+        return entry.getKey();
+    }
 
+    /**
+     * this method tries to dereference a referred belief. It returns null
+     * if no match can currently be found
+     *
+     * @param contentMatchingFunction the matching function to be used
+     * @see WMContentWaiter
+     * @return the first working memory address that corresponds
+     * @throws InterruptedException
+     */
+    protected PerceptBelief tryGetReferredBelief(
+            ContentMatchingFunction<PerceptBelief> contentMatchingFunction) {
+        logger.debug("trying to find referred belief");
+        List<PerceptBelief> entries = new ArrayList<PerceptBelief>();
+        try {
+            component.getMemoryEntries(PerceptBelief.class, entries, "binder");
+
+            for (PerceptBelief pb : entries) {
+                if (contentMatchingFunction.matches(pb)) {
+                    logger.debug("got it: " + entries.get(0).id);
+                    return entries.get(0);
+                }
+            }
+            return null;
+        } catch (UnknownSubarchitectureException e) {
+            logger.debug("Unknown subarch error");
+            return null;
+        }
+
+    }
 }
