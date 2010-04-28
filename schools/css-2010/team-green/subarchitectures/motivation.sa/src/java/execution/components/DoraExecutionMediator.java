b7a78bd6e732219fde0331e7552c0983691958a3
alen <alen@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-28 12:06:57 +0000

    running execution monitor

--- schools/css-2010/team-green/subarchitectures/motivation.sa/src/java/execution/components/DoraExecutionMediator.java
+++ schools/css-2010/team-green/subarchitectures/motivation.sa/src/java/execution/components/DoraExecutionMediator.java
@@ -16,7 +16,14 @@ import execution.slice.actions.DetectObjects;
 import execution.slice.actions.DetectPeople;
 import execution.slice.actions.ExplorePlace;
 import execution.slice.actions.GoToPlace;
+import execution.slice.actions.LookForObjects;
+import execution.slice.actions.LookForPeople;
+import execution.slice.actions.PrintMessage;
+import execution.slice.actions.Start;
 import execution.util.ActionConverter;
+
+import execution.slice.TriBool;
+import execution.slice.ActionStatus;
 
 /**
  * Execution mediator specifically for Dora/Spring School.
@@ -50,67 +57,213 @@ public class DoraExecutionMediator extends PlanExecutionMediator implements
 	 */
 	public execution.slice.Action toSystemAction(Action _plannedAction)
 			throws CASTException {
-		if (_plannedAction.name.equals("move")) {
-			assert _plannedAction.arguments.length == 2 : "move action arity is expected to be 1";
+	    if (_plannedAction.name.equals("move") || _plannedAction.name.equals("move_to_report_pos")) {
+			assert _plannedAction.arguments.length == 2 : "move action arity is expected to be 2";
 
+			// create a new instance of the action, look in the
+			// execution.slice.actions package to see all available actions:
+			// http://www.cs.bham.ac.uk/~hanheidm/spring-school-javadoc/execution/slice/actions/package-summary.html
 			GoToPlace act = newActionInstance(GoToPlace.class);
-			String placeUnionID = ((PointerValue) _plannedAction.arguments[1]).beliefId.id;
-			Belief placeUnion = m_binderFacade.getBelief(placeUnionID);
 
-			if (placeUnion == null) {
+			// read the stable belief of the place that has been passed as an
+			// argument of this action
+			String stableBeliefID = ((PointerValue) _plannedAction.arguments[1]).beliefId.id;
+
+			// read the belief from working memory using the binder facade
+			// helper class
+			Belief stableBelief = m_binderFacade.getBelief(stableBeliefID);
+
+			if (stableBelief == null) {
 				throw new ActionExecutionException(
-						"No union for place union id: " + placeUnionID);
+						"No union for place union id: " + stableBeliefID);
 			}
 
+			// read all features PlaceId from the StableBelief by using the
+			// binder facade again. should be exactly one
 			List<FeatureValue> placeIDFeatures = m_binderFacade
-					.getFeatureValue(placeUnion, "PlaceId");
+					.getFeatureValue(stableBelief, "PlaceId");
 			if (placeIDFeatures.isEmpty()) {
 				throw new ActionExecutionException(
-						"No PlaceId features for union id: " + placeUnionID);
+						"No PlaceId features for belief id: " + stableBeliefID);
 
 			}
+
+			// read the one placeID from the StableBelief
 			IntegerValue placeID = (IntegerValue) placeIDFeatures.get(0);
 			act.placeID = placeID.val;
+			// we have now created the action object, so we create it and let
+			// the execution framework to the rest of the work
 			return act;
-		} else if (_plannedAction.name.equals("look-for-object")) {
-			assert _plannedAction.arguments.length == 1 : "look-for-object action arity is expected to be 1";
-
+		} else if (_plannedAction.name.equals("explore_place")) {
+			// this is an empty action that actually doesn't do anything
+			return new ExplorePlace();
+		} else if (_plannedAction.name.equals("detect-objects")) {
+			// this is the action that just triggers the object detector once
+			assert _plannedAction.arguments.length == 1 : "detect-objects action arity is expected to be 1";
+			// create the action instance and initialize it with the default
+			// labels. This will trigger the object detector for objects with
+			// this label. check
+			// instantiations/includes/vision.sa/vision-objects.cast or
+			// instantiations/includes/vision.sa/vision-blobs.cast to setup the
+			// corresponding components to accept these labels.
 			DetectObjects act = newActionInstance(DetectObjects.class);
 			act.labels = DEFAULT_LABELS;
 			return act;
-		} else if (_plannedAction.name.equals("look-for-people")) {
-			assert _plannedAction.arguments.length == 1 : "look-for-people action arity is expected to be 1";
-
+		} else if (_plannedAction.name.equals("detect-people")) {
+			// this is the action that just triggers the people detector once
+			assert _plannedAction.arguments.length == 1 : "detect-people action arity is expected to be 1";
+			// create the action instance and return it
 			DetectPeople act = newActionInstance(DetectPeople.class);
+			return act;
+		} else if (_plannedAction.name.equals("look-for-people")) {
+			// this is the action that make the robot turn and triggers the
+			// people detector several times
+			assert _plannedAction.arguments.length == 1 : "look-for-people action arity is expected to be 1";
+			// create the action instance and return it
+			LookForPeople act = newActionInstance(LookForPeople.class);
+			return act;
+		} else if (_plannedAction.name.equals("look-for-objects")) {
+			// this is the action that make the robot turn and triggers the
+			// object detector several times
+			assert _plannedAction.arguments.length == 1 : "look-for-objects action arity is expected to be 1";
+			// create the action instance and return it
+			LookForObjects act = newActionInstance(LookForObjects.class);
+			act.labels = DEFAULT_LABELS;
+			return act;
+		} else if (_plannedAction.name.equals("ask-for-your-name")) {
+			// this is the action to ask for a person's name
+			assert _plannedAction.arguments.length == 2 : "ask-for-your-name action arity is expected to be 2";
+			// create the action instance: This action will trigger a GUI
+			// dialog. The feature that is being asked for (in this case "name")
+			// will be written to the belief that is given as argument if the
+			// user confirms that he knows it.
+			ComsysQueryFeature act = newActionInstance(ComsysQueryFeature.class);
+			// the action requires the id of the StableBelief in which the
+			// feature should be put
+			act.beliefID = ((PointerValue) _plannedAction.arguments[1]).beliefId.id;
+			// this is the feature that shall be written
+			act.featureID = "name";
+			// optionally we can define an explicit question here to make life
+			// simpler
+			act.question = "What is your name?";
 			return act;
 		} else if (_plannedAction.name.equals("ask-for-placename")) {
 			assert _plannedAction.arguments.length == 2 : "ask-for-feature action arity is expected to be 2";
-			String beliefID =  ((PointerValue) _plannedAction.arguments[1]).beliefId.id;
+			String beliefID = ((PointerValue) _plannedAction.arguments[1]).beliefId.id;
 			String featureID = "name";
 			ComsysQueryFeature act = newActionInstance(ComsysQueryFeature.class);
-            act.beliefID = beliefID;
-            act.featureID = featureID;
+			act.question = "";
+			act.beliefID = beliefID;
+			act.featureID = featureID;
 			return act;
 		} else if (_plannedAction.name.equals("verify-placename")) {
 			assert _plannedAction.arguments.length == 3 : "ask-for-feature action arity is expected to be 2";
-			String beliefID =  ((PointerValue) _plannedAction.arguments[1]).beliefId.id;
+			String beliefID = ((PointerValue) _plannedAction.arguments[1]).beliefId.id;
 			String featureID = "name";
 			ComsysTestFeatureValue act = newActionInstance(ComsysTestFeatureValue.class);
-            act.beliefID = beliefID;
-            act.featureType = featureID;
-            act.featureValue = _plannedAction.arguments[2];
+			act.question = "";
+			act.beliefID = beliefID;
+			act.featureType = featureID;
+			act.featureValue = _plannedAction.arguments[2];
+			return act;
+		} else if (_plannedAction.name.equals("commit-name")) {
+			PrintMessage act = newActionInstance(PrintMessage.class);
+			act.status=ActionStatus.COMPLETE;
+			act.success=TriBool.TRITRUE;
+			act.message=_plannedAction.fullName;
 			return act;
 		} else if(_plannedAction.name.equals("start")) {
 		    assert _plannedAction.arguments.length == 1 : "start action arity is expected to be 1";
-		} else if(_plannedAction.name.equals("move_to_report_pos")) {
-		     assert _plannedAction.arguments.length == 1 : "move_to_report_pos action arity is expected to be 1";
+		    //execution.slice.Action act = newActionInstance(execution.slice.Action.class);
+		    Start act = newActionInstance(Start.class);
+		    act.status = ActionStatus.COMPLETE;
+		    act.success = TriBool.TRITRUE;
+		    System.out.println("Creating Start Action!!!");
+		    return act;
 		} else if(_plannedAction.name.equals("report")) {
-		     assert _plannedAction.arguments.length == 0 : "report action arity is expected to be 0";
+		    assert _plannedAction.arguments.length == 1 : "report action arity is expected to be 1";
+		    execution.slice.Action act = newActionInstance(execution.slice.Action.class);
+		    act.status = ActionStatus.COMPLETE;
+		    act.success = TriBool.TRITRUE;
+		    return act;
 		}
-
+		// in case we do not find the action we have an exception
 		throw new ActionExecutionException("No conversion available for: "
-				+ _plannedAction.fullName);
+				+ _plannedAction.name);
 	}
+
+
+// 	public execution.slice.Action toSystemAction(Action _plannedAction)
+// 			throws CASTException {
+// 		if (_plannedAction.name.equals("move") || _plannedAction.name.equals("move_to_report_pos")) {
+// 			assert _plannedAction.arguments.length == 2 : "move action arity is expected to be 1";
+
+// 			GoToPlace act = newActionInstance(GoToPlace.class);
+// 			String placeUnionID = ((PointerValue) _plannedAction.arguments[1]).beliefId.id;
+// 			Belief placeUnion = m_binderFacade.getBelief(placeUnionID);
+
+// 			if (placeUnion == null) {
+// 				throw new ActionExecutionException(
+// 						"No union for place union id: " + placeUnionID);
+// 			}
+
+// 			List<FeatureValue> placeIDFeatures = m_binderFacade
+// 					.getFeatureValue(placeUnion, "PlaceId");
+// 			if (placeIDFeatures.isEmpty()) {
+// 				throw new ActionExecutionException(
+// 						"No PlaceId features for union id: " + placeUnionID);
+
+// 			}
+// 			IntegerValue placeID = (IntegerValue) placeIDFeatures.get(0);
+// 			act.placeID = placeID.val;
+// 			return act;
+// 		} else if (_plannedAction.name.equals("look-for-object")) {
+// 			assert _plannedAction.arguments.length == 1 : "look-for-object action arity is expected to be 1";
+
+// 			DetectObjects act = newActionInstance(DetectObjects.class);
+// 			act.labels = DEFAULT_LABELS;
+// 			return act;
+// 		} else if (_plannedAction.name.equals("look-for-people")) {
+// 			assert _plannedAction.arguments.length == 1 : "look-for-people action arity is expected to be 1";
+
+// 			DetectPeople act = newActionInstance(DetectPeople.class);
+// 			return act;
+// 		} else if (_plannedAction.name.equals("ask-for-placename")) {
+// 			assert _plannedAction.arguments.length == 2 : "ask-for-feature action arity is expected to be 2";
+// 			String beliefID =  ((PointerValue) _plannedAction.arguments[1]).beliefId.id;
+// 			String featureID = "name";
+// 			ComsysQueryFeature act = newActionInstance(ComsysQueryFeature.class);
+//             act.beliefID = beliefID;
+//             act.featureID = featureID;
+// 			return act;
+// 		} else if (_plannedAction.name.equals("verify-placename")) {
+// 			assert _plannedAction.arguments.length == 3 : "ask-for-feature action arity is expected to be 2";
+// 			String beliefID =  ((PointerValue) _plannedAction.arguments[1]).beliefId.id;
+// 			String featureID = "name";
+// 			ComsysTestFeatureValue act = newActionInstance(ComsysTestFeatureValue.class);
+//             act.beliefID = beliefID;
+//             act.featureType = featureID;
+//             act.featureValue = _plannedAction.arguments[2];
+// 			return act;
+// 		} else if(_plannedAction.name.equals("start")) {
+// 		    assert _plannedAction.arguments.length == 1 : "start action arity is expected to be 1";
+// 		    //execution.slice.Action act = newActionInstance(execution.slice.Action.class);
+// 		    Start act = newActionInstance(Start.class);
+// 		    act.status = ActionStatus.COMPLETE;
+// 		    act.success = TriBool.TRITRUE;
+// 		    System.out.println("Creating Start Action!!!");
+// 		    return act;
+// 		} else if(_plannedAction.name.equals("report")) {
+// 		    assert _plannedAction.arguments.length == 1 : "report action arity is expected to be 1";
+// 		    execution.slice.Action act = newActionInstance(execution.slice.Action.class);
+// 		    act.status = ActionStatus.COMPLETE;
+// 		    act.success = TriBool.TRITRUE;
+// 		    return act;
+// 		}
+
+// 		throw new ActionExecutionException("No conversion available for: "
+// 				+ _plannedAction.fullName);
+// 	}
 
 	@Override
 	public ActionConverter getActionConverter() {
