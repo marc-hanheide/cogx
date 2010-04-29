dd7337f0968e7258e251640658cf15b69e061534
matejk <matejk@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-29 08:08:35 +0000

    commented out some stuff

--- code/schools/css-2010/team-green/subarchitectures/binder/src/java/binder/perceptmediator/transferfunctions/LocalizedVisualObjectTransferFunction.java
+++ code/schools/css-2010/team-green/subarchitectures/binder/src/java/binder/perceptmediator/transferfunctions/LocalizedVisualObjectTransferFunction.java
@@ -23,15 +23,22 @@ import cast.cdl.WorkingMemoryAddress;
 import cast.cdl.WorkingMemoryChange;
 import castutils.castextensions.WMView;
 import castutils.facades.SpatialFacade;
-/*
+
 import java.util.List ;
 import java.util.ArrayList;
 
 import beliefmodels.autogen.beliefs.StableBelief ; 
-//import java.util.Iterator;
-//import beliefmodels.autogen.distribs.*;
-//import beliefmodels.autogen.distribs.FeatureContentUtils;*/
+import beliefmodels.autogen.beliefs.Belief ;
+import java.util.Iterator;
+import beliefmodels.autogen.distribs.*;
+import beliefmodels.utils.FeatureContentUtils;
 
+import java.lang.String ; 
+import beliefmodels.autogen.featurecontent.StringValue ;
+import beliefmodels.autogen.featurecontent.FeatureValue ;
+import beliefmodels.autogen.featurecontent.PointerValue ;
+import beliefmodels.autogen.featurecontent.IntegerValue;
+import beliefmodels.autogen.beliefs.PerceptBelief;
 /**
  * @author marc
  * 
@@ -53,52 +60,55 @@ public class LocalizedVisualObjectTransferFunction extends
 		Map<String, FeatureValue> result = new HashMap<String, FeatureValue>();
 		// TODO: we should use a DoubleValue here!
 		try {
-	 	/*	// scan thorugh all working memory stable entries...
-			List<StableBelief> ls = new ArrayList<StableBelief>(); //Ice.Object
-			String str_tmp ;
-			//StableBelief tmp_stbel ;
-			//StableBelief [] tmp_stbel2 ;
+	  		// scan thorugh all working memory stable entries...
+		/*	List<StableBelief> ls = new ArrayList<StableBelief>(); //Ice.Object
+			String str_tmp ; 
 
 			List<FeatureValueProbPair>  ls_pair = new ArrayList<FeatureValueProbPair>(); 
 			FeatureContentUtils tmp_util ;
 
 			component.getMemoryEntries(StableBelief.class, ls) ;
-			//component.getMemoryEntriesWithData(StableBelief.class, ls) ;
- 
-//wmc.address.id
-// from.label
 
-Iterator it=ls.iterator();
+			Iterator it=ls.iterator();
+			Belief stb ; 
+				
+			while( it.hasNext() ) {
+				stb = (Belief)it.next() ;
+				ls_pair = FeatureContentUtils.getValuesInBelief(stb, "name" ) ;
+				if ( ls_pair.size() > 0 ) {
+					Iterator it2=ls_pair.iterator();
+					if( it2.hasNext()  ) {
+						FeatureValueProbPair fvp_tmp ;
+						fvp_tmp = (FeatureValueProbPair)it2.next() ;
+						StringValue val  = (StringValue)fvp_tmp.val ;
+						String vv  = val.val ;
+						if ( vv.equals("record_1") || vv.equals("record_2") || vv.equals("record_3") || vv.equals("record_4")) {
+							List<FeatureValueProbPair>  ls_pair2 = new ArrayList<FeatureValueProbPair>();
+							ls_pair2 = FeatureContentUtils.getValuesInBelief(stb, "is-in" ) ;
+							Iterator it3 =ls_pair2.iterator();
+							fvp_tmp = (FeatureValueProbPair)it3.next() ;
+							 
+							PointerValue  valxs  = (PointerValue)fvp_tmp.val   ;
+ 							WorkingMemoryAddress wma = valxs.beliefId ;
 
-			StableBelief stb ; 
-			stb = 	(StableBelief)it.next() ;
+							StableBelief beliefData = component.getMemoryEntry(wma,StableBelief.class );
 
-		String featlabel = "label" ;
-		ls_pair = tmp_util.FeatureContentUtils.getValuesInBelief(stb, featlabel) ; 
+							List<FeatureValueProbPair>  ls_pair_plc = new ArrayList<FeatureValueProbPair>(); 
+							ls_pair_plc = FeatureContentUtils.getValuesInBelief(beliefData, "PlaceId" ) ;
+							Iterator it4 =ls_pair_plc.iterator();							
+							FeatureValueProbPair fvp_tmp2 = (FeatureValueProbPair)it4.next() ;
 
+							IntegerValue val_plc  = (IntegerValue)fvp_tmp2.val ;
+							int valofplc =  val_plc.val ;
 
- 
- 
-		//	str_tmp =  stb.id ; //FeatureValueBuilder.createNewStringValue( ls[0].label ) ;
-		//	System.out.print("--------------------------------------------------->" + str_tmp ); 
-			
+							System.out.print("----------------------->" + vv + " is at place " + valofplc  );
+						}				
+					}
+				}
+			}
+			 
 
-
-		/*	for ( int i = 0 ; i < ls.size() ; i++) {
-				str_tmp =  FeatureValueBuilder.createNewStringValue( ls[i].label )) ;
-				//System.out.print("An error occured in LocalizedVisualObjectTransferFunction!!"); 
-
-			}*/
-			
-
-                      // component.getWorkingMemoryEntries(tmp_stbel) ;
-
-
-		//	component.getMemoryEntries(ls) ;
- 
-
-
-			//
+ */
 			
 
 
