ca80bc2d0c41663b4d85a63e9950eed6882236ed
matejk <matejk@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-04-30 09:31:49 +0000

    modified presentation

--- code/schools/css-2010/team-green/subarchitectures/binder/src/java/binder/perceptmediator/transferfunctions/PersonTransferFunction.java
+++ code/schools/css-2010/team-green/subarchitectures/binder/src/java/binder/perceptmediator/transferfunctions/PersonTransferFunction.java
@@ -35,7 +35,7 @@ public class PersonTransferFunction extends SimpleDiscreteTransferFunction<Perso
 		// TODO: we should use a DoubleValue here!
 		result.put("PersonId", FeatureValueBuilder.createNewStringValue(wmc.address.id));
 		result.put("distance", FeatureValueBuilder.createNewFloatValue(from.distance));
-		result.put("record", FeatureValueBuilder.createNewStringValue("no_record"));
+		result.put("record", FeatureValueBuilder.createNewStringValue("NO_RECORD"));
 		result.put("name", FeatureValueBuilder.createNewStringValue("none"));
 
 boolean detected ; 
