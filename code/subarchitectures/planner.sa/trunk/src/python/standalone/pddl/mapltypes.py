f36195dade7640d8e99c3bdcc386cbb272a73d87
goebelbe <goebelbe@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-05-04 13:46:00 +0000

    Add docstrings

--- code/subarchitectures/planner.sa/trunk/src/python/standalone/pddl/mapltypes.py
+++ code/subarchitectures/planner.sa/trunk/src/python/standalone/pddl/mapltypes.py
@@ -5,7 +5,18 @@ import parser
 from parser import ParseError, UnexpectedTokenError
 
 class Type(object):
+    """This class represents (normal) PDDL types. Each type can have a
+    unlimited number of supertypes, the default supertype is t_object.
+    """
+    
     def __init__(self, typename, supertypes=None):
+        """Create a new Type object.
+
+        Arguments:
+        typename -- the name of the type
+        supertypes -- list of Type objects that are supertypes of this
+        Type. Defaults to [t_object].
+        """
         self.name = typename
         
         if supertypes is None:
@@ -13,9 +24,18 @@ class Type(object):
         self.supertypes = set(supertypes)
 
     def get_supertypes(self):
+        """Returns a list of all supertypes of this type. This will also return
+        supertypes of supertypes and so on.
+        """
         return self.supertypes | reduce(lambda a,b: a|b, (t.get_supertypes() for t in self.supertypes), set())
 
     def is_subtype_of(self, other):
+        """Returns True if self is a proper subtype of other.
+
+        Arguments:
+        other -- Type object.
+        """
+        
         if self.__class__ != other.__class__:
             return other.is_supertype_of(self)
         
@@ -24,6 +44,12 @@ class Type(object):
         return any(sup.is_subtype_of(other) for sup in self.supertypes)
 
     def equal_or_subtype_of(self, other):
+        """Returns True if self is equal to or a a subtype of other.
+
+        Arguments:
+        other -- Type object.
+        """
+        
         if self.__class__ != other.__class__:
             return other.equal_or_supertype_of(self)
         
@@ -32,9 +58,19 @@ class Type(object):
         return any(sup.is_subtype_of(other) for sup in self.supertypes)
     
     def is_supertype_of(self, other):
+        """Returns True if self is a proper supertype of other.
+
+        Arguments:
+        other -- Type object.
+        """
         return other.is_subtype_of(self)
 
     def equal_or_supertype_of(self, other):
+        """Returns True if self is equal to or a a supertype of other.
+
+        Arguments:
+        other -- Type object.
+        """
         return other.equal_or_subtype_of(self)
     
     def __str__(self):
@@ -85,11 +121,27 @@ class Type(object):
             
 
 class CompositeType(Type):
+    """This class represents a PDDL type that is composed of other
+    types using the "(either a b c)" syntax.
+    """
+    
     def __init__(self, types):
+        """Create a new CompositeType object.
+
+        Arguments:
+        types -- a list of Type objects that this CompositeType is composed of
+        """
+        
         self.name = "-".join(t.name for t in types)
         self.types = types
 
     def is_subtype_of(self, other):
+        """Returns True if self is a proper subtype of other.
+
+        Arguments:
+        other -- Type object.
+        """
+        
         if isinstance(other, CompositeType):
             return other.is_supertype_of(self)
 
@@ -97,9 +149,20 @@ class CompositeType(Type):
         return all(t.equal_or_subtype_of(other) for t in self.types)
 
     def equal_or_subtype_of(self, other):
+        """Returns True if self is equal to or a a subtype of other.
+
+        Arguments:
+        other -- Type object.
+        """
         return self == other or self.is_subtype_of(other)
     
     def is_supertype_of(self, other):
+        """Returns True if self is a proper supertype of other.
+
+        Arguments:
+        other -- Type object.
+        """
+        
         if isinstance(other, CompositeType):
             strictSupertype = any(any(t.is_supertype_of(t2) for t2 in other.types) for t in self.types)
             return all(self.equal_or_supertype_of(t) for t in other.types) and strictSupertype
@@ -107,6 +170,11 @@ class CompositeType(Type):
         return any(t.equal_or_supertype_of(other) for t in self.types)
 
     def equal_or_supertype_of(self, other):
+        """Returns True if self is equal to or a a supertype of other.
+
+        Arguments:
+        other -- Type object.
+        """
         return self == other or self.is_supertype_of(other)
         
     def __str__(self):
@@ -122,26 +190,60 @@ class CompositeType(Type):
             return False
 
 class FunctionType(Type):
+    """This class represents a function type as specified by the
+    "(function x)" syntax. Function types are used to model modal
+    predicates/actions/axioms in MAPL.
+    """
+    
     def __init__(self, type):
+        """Create a new FunctionType object.
+
+        Arguments:
+        type -- a Type object that specifies the type of function this
+        FunctionType represents.
+        """
+        
         self.name = "function(%s)" % str(type)
         self.type = type
 
     def is_subtype_of(self, other):
+        """Returns True if self is a proper subtype of other.
+
+        Arguments:
+        other -- Type object.
+        """
+
         if isinstance(other, FunctionType):
             return self.type.is_subtype_of(other.type)
         return self.type.equal_or_subtype_of(other)
 
     def equal_or_subtype_of(self, other):
+        """Returns True if self is equal to or a a subtype of other.
+
+        Arguments:
+        other -- Type object.
+        """
         if isinstance(other, FunctionType):
             return self.type.equal_or_subtype_of(other.type)
         return self.type.equal_or_subtype_of(other)
     
     def is_supertype_of(self, other):
+        """Returns True if self is a proper supertype of other.
+
+        Arguments:
+        other -- Type object.
+        """
+        
         if isinstance(other, FunctionType):
             return self.type.is_supertype_of(other.type)
         return False
 
     def equal_or_supertype_of(self, other):
+        """Returns True if self is equal to or a a supertype of other.
+
+        Arguments:
+        other -- Type object.
+        """
         if isinstance(other, FunctionType):
             return self.type.equal_or_supertype_of(other.type)
         return False
@@ -156,12 +258,30 @@ class FunctionType(Type):
         return self.__class__ == other.__class__ and self.type == other.type
 
 class ProxyType(Type):
+    """This class represents a proxy type as specified by the "(typeof
+    ?x)" syntax. It is used to refer to types of function parameters
+    which might not be known at the time of parsing.
+
+    A ProxyType created with ProxyType(param) (with param.type being a
+    FunctionType) will behave like the base type of param when param is not
+    instantiated. When param is instantiated with a FunctionTerm, it
+    will behave like the type of this FunctionTerm's function.
+    """
+    
     def __init__(self, param):
+        """Create a new ProxyType object.
+
+        Arguments:
+        param -- the Parameter object that this ProxyType refers to.
+        """
         assert isinstance(param.type, FunctionType)
         self.name = "typeof(%s)" % str(param.name)
         self.parameter = param
 
     def effective_type(self):
+        """Return the base type of param if param is not instantiated.
+        Return the type of the function of param's instance if it is instantiated"""
+        
         if self.parameter.is_instantiated():
             return self.parameter.get_instance().function.type
         return self.parameter.type.type
@@ -188,7 +308,16 @@ class ProxyType(Type):
         return self.__class__ == other.__class__ and self.parameter == other.parameter
 
 class AnyType(Type):
+    """This class is used to represent types that match all other
+    types. Used to model the "unknown" constant.
+    """
+    
     def __init__(self, name="any"):
+        """Create a new AnyType object.
+
+        Arguments:
+        name -- the name of the type
+        """
         self.name = name
 
     def is_subtype_of(self, other):
@@ -207,7 +336,17 @@ class AnyType(Type):
         return isinstance(other, Type)
     
 class TypedObject(object):
+    """This class represents all constants or objects in a PDDL
+    domain.
+    """
+    
     def __init__(self, name, _type):
+        """Create a new TypedObject.
+
+        Arguments:
+        name -- name of the object
+        _type -- Type object that represents the type of the object.
+        """
         if type(name) in (int, float, long):
             self.__class__ = TypedNumber
             TypedNumber.__init__(self, name)
@@ -217,9 +356,12 @@ class TypedObject(object):
         self.hash = hash((self.__class__, self.name, self.type))
 
     def is_instance_of(self, type):
+        """Returns true if this TypedObject's type is equal to "type"
+        or a subtype."""
         return self.type.equal_or_subtype_of(type)
 
     def copy(self):
+        """Create a copy of the object."""
         return self.__class__(self.name, self.type)
 
     def __str__(self):
@@ -238,7 +380,15 @@ class TypedObject(object):
         return not self.__eq__(other)
 
 class TypedNumber(TypedObject):
+    """Specialized subclass of TypedObject to represent numeric
+    values. It will automatically have a type of t_number.
+    """
     def __init__(self, number):
+        """Create a new numberic object.
+
+        Arguments:
+        value -- number represented by the new object.
+        """
         self.value = number
         self.type = t_number
         self.hash = hash((self.__class__, self.name, self.type))
@@ -258,7 +408,20 @@ class TypedNumber(TypedObject):
             return self.value == other
         
 class Parameter(TypedObject):
+    """Subclass of TypedObject that represents parameters of actions or
+    quantified conditions/effects.
+
+    A Parameter object can be instantiated with a object whose type is
+    compatible with the type of the Parameter.
+    """
+    
     def __init__(self, name, type):
+        """Create a new Parameter.
+
+        Arguments:
+        name -- name of the parameter (needs to start with a "?")
+        type -- Type object of the Parameter's type
+        """
         assert name[0] == "?"
         self.name = name
         self.type = type
@@ -266,14 +429,25 @@ class Parameter(TypedObject):
         self.hash = hash((self.__class__, self.name, self.type))
 
     def instantiate(self, value):
+        """Instantiate this Parameter with the object value. Throws an
+        exception if the type of value is not compatible with the
+        Parameter.
+
+        Arguments:
+        value -- TypedObject the Parameter should be instantiated with.
+        """
+        
         if value is not None:
             assert value.is_instance_of(self.type)
         self.instantiated = value
 
     def is_instantiated(self):
+        """Returns True if this Parameter is instantiated"""
         return self.instantiated is not None
 
     def get_instance(self):
+        """Returns the TypedObject this Parameter is instantiated with
+        (None if it is not instantiated)."""
         return self.instantiated
     
     # def __hash__(self):
