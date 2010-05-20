58638555ad11c138ddd7120b5222fa7aafa57dc0
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-05-20 07:19:40 +0000

    mbuilder paths set

--- code/subarchitectures/vision.sa/trunk/src/python/ObjectRecognizer/apps/mbuilder/pymodulepaths.py
+++ code/subarchitectures/vision.sa/trunk/src/python/ObjectRecognizer/apps/mbuilder/pymodulepaths.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # vim:set fileencoding=utf-8 sw=4 ts=8 et:vim
 import os, sys
-output = os.path.abspath("../" * 7 + "output")
+here = os.path.abspath(os.path.dirname(__file__))
+output = os.path.abspath(here + "/" + ("../" * 7) + "output")
 sys.path.insert(0, output + "/python")
-sys.path.insert(0, os.path.abspath("../../dist-packages"))
+sys.path.insert(0, os.path.abspath(here + "/../../dist-packages"))
