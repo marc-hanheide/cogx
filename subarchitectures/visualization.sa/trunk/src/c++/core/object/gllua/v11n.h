61aa67cde616df48d9f11d0af60476d85a010fec
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-06-08 19:19:08 +0000

    v11n: display list management in lua for LuaGl

---
+++ subarchitectures/visualization.sa/trunk/src/c++/core/object/gllua/v11n.h
@@ -0,0 +1,11 @@
+/*
+ * @author:  Marko Mahnič
+ * @created: jun 2010 
+ */
+   
+#ifndef V11N_WWMWW49Q
+#define V11N_WWMWW49Q
+
+void* v11nGetOpenGlContext();
+
+#endif /* end of include guard: V11N_WWMWW49Q */
