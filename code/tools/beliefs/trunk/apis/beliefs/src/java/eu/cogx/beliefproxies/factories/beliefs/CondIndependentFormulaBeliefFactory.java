7d85687e877e719ced93de306257dd37c4bf3bf9
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-06-10 16:30:49 +0000

    - more useful constructor

--- code/tools/beliefs/trunk/apis/beliefs/src/java/eu/cogx/beliefproxies/factories/beliefs/CondIndependentFormulaBeliefFactory.java
+++ code/tools/beliefs/trunk/apis/beliefs/src/java/eu/cogx/beliefproxies/factories/beliefs/CondIndependentFormulaBeliefFactory.java
@@ -5,19 +5,17 @@ package eu.cogx.beliefproxies.factories.beliefs;
 
 import java.util.HashMap;
 
-import de.dfki.lt.tr.beliefs.slice.distribs.CondIndependentDistribs;
-import de.dfki.lt.tr.beliefs.slice.distribs.DistributionValues;
-import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
 import Ice.Object;
-import eu.cogx.beliefproxies.factories.ProxyFactory;
-import eu.cogx.beliefproxies.proxies.Proxy;
+import de.dfki.lt.tr.beliefs.slice.distribs.CondIndependentDistribs;
 import de.dfki.lt.tr.beliefs.slice.distribs.ProbDistribution;
 import de.dfki.lt.tr.beliefs.slice.epstatus.PrivateEpistemicStatus;
-import de.dfki.lt.tr.beliefs.slice.framing.AbstractFrame;
+import de.dfki.lt.tr.beliefs.slice.framing.SpatioTemporalFrame;
+import de.dfki.lt.tr.beliefs.slice.framing.TemporalInterval;
 import de.dfki.lt.tr.beliefs.slice.history.AbstractBeliefHistory;
+import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
+import eu.cogx.beliefproxies.factories.ProxyFactory;
+import eu.cogx.beliefproxies.proxies.Proxy;
 import eu.cogx.beliefproxies.proxies.beliefs.CondIndepedentFormulaBeliefProxy;
-import eu.cogx.beliefproxies.proxies.distributions.BasicProbDistributionProxy;
-import eu.cogx.beliefproxies.proxies.distributions.CondIndependentFormulaDistributionsProxy;
 
 /**
  * @author marc
@@ -42,7 +40,7 @@ public class CondIndependentFormulaBeliefFactory<T extends dBelief> implements
 		belief.id=id;
 		belief.content = new CondIndependentDistribs(new HashMap<String, ProbDistribution>());
 		belief.estatus = new PrivateEpistemicStatus("robot");
-		belief.frame = new AbstractFrame();
+		belief.frame = new SpatioTemporalFrame("here", new TemporalInterval());
 		belief.hist = new AbstractBeliefHistory();
 		belief.type = beliefType;
 		return fact.create(belief);
