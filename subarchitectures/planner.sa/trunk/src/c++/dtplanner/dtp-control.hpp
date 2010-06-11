dcb19ee40cfe6d6a77b015489705e40b89547386
grettonc <grettonc@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-06-11 16:50:58 +0000

    PLanning interface stalled because I am not sure how to call the python methods Moritz wants me to called. Email has been sent.
     - Charles

--- subarchitectures/planner.sa/trunk/src/c++/dtplanner/dtp-control.hpp
+++ subarchitectures/planner.sa/trunk/src/c++/dtplanner/dtp-control.hpp
@@ -11,32 +11,49 @@
 #include "../Planner.hpp"
 
 #include <map>
+#include <memory>
 
+#include "planning_symbols.hh"
 
 using namespace autogen::Planner;
 
+#include <pthread.h>
+
 
 class DTPCONTROL : 
-  public autogen::Planner::DTPServer,
-  public cast::CASTComponent 
+    public autogen::Planner::DTPServer,
+    public cast::CASTComponent 
 {
 public:
-  DTPCONTROL(){};
-  ~DTPCONTROL(){};
+    DTPCONTROL();
+    ~DTPCONTROL(){};
 
-  //~DTPCONTROL();
-  void deliverObservation(Ice::Int, const autogen::Planner::ObservationSeq& observationSeq, const Ice::Current&);
-  void newTask(Ice::Int, const std::string& probleFile, const std::string& domainFile, const Ice::Current&);  
-  void cancelTask(Ice::Int, const Ice::Current&);
+    //~DTPCONTROL();
+    void deliverObservation(Ice::Int, const autogen::Planner::ObservationSeq& observationSeq, const Ice::Current&);
+    void newTask(Ice::Int, const std::string& probleFile, const std::string& domainFile, const Ice::Current&);  
+    void cancelTask(Ice::Int, const Ice::Current&);
 
+    /* Running in a thread. Is woken up every time an observation comes
+     * in on the relevant task.*/
+    void post_action(Ice::Int id);
+    
 protected:
-  void configure(const cast::cdl::StringMap& _config, const Ice::Current& _current){}
-   void start(){}
-   void runComponent(){}
-   void stop(){};
+    void configure(const cast::cdl::StringMap& _config, const Ice::Current& _current){}
+    void start(){}
+    void runComponent(){}
+    void stop(){};
   
-// private:
-//     std::map<int, > ;
+private:
+    typedef std::tr1::shared_ptr<pthread_mutex_t> Mutex;
+    
+    std::map<Ice::Int, Planning::Domain_Name> thread_to_domain;
+    std::map<Ice::Int, Planning::Problem_Name> thread_to_problem;
+
+    std::map<Ice::Int, bool> thread_statuus;
+    std::map<Ice::Int, Mutex> thread_mutex;
+    std::map<Ice::Int, pthread_t> threads;
+    std::map<Ice::Int, pthread_attr_t> thread_attributes;
+
 };
 
 
