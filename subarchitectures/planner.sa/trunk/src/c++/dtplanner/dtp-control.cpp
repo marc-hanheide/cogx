2e66d2480839ea64b278187d9d37019b2e280257
grettonc <grettonc@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-06-11 14:44:49 +0000

    Parsing interface seems to be there.
     - Charles

--- subarchitectures/planner.sa/trunk/src/c++/dtplanner/dtp-control.cpp
+++ subarchitectures/planner.sa/trunk/src/c++/dtplanner/dtp-control.cpp
@@ -7,9 +7,25 @@ extern "C" {
     }
 }
 
+#include "dtp_pddl_parsing_interface.hh"
+#include "dtp_pddl_parsing_data.hh"
 
-void DTPCONTROL::deliverObservation(Ice::Int id, const autogen::Planner::ObservationSeq& observationSeq, const Ice::Current&){}
+void DTPCONTROL::deliverObservation(Ice::Int id,
+                                    const autogen::Planner::ObservationSeq& observationSeq,
+                                    const Ice::Current&){
+    
+}
 
-void DTPCONTROL::newTask(Ice::Int id, const std::string& probleFile, const std::string& domainFile, const Ice::Current&) {}
+void DTPCONTROL::newTask(Ice::Int id,
+                         const std::string& probleFile,
+                         const std::string& domainFile, const Ice::Current&)
+{
+    
+    Planning::Parsing::parse_domain(domainFile);
+    Planning::Parsing::parse_problem(probleFile);
+}
 
-void DTPCONTROL::cancelTask(Ice::Int id, const Ice::Current&){}
+void DTPCONTROL::cancelTask(Ice::Int id, const Ice::Current&)
+{
+    
+}
