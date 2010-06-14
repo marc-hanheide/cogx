5f3673906c73a04810313a993eddb67617386739
grettonc <grettonc@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-06-14 14:53:05 +0000

    First cut of interface.
     - Charles

--- code/subarchitectures/planner.sa/trunk/src/c++/dtplanner/dtp-control.hpp
+++ code/subarchitectures/planner.sa/trunk/src/c++/dtplanner/dtp-control.hpp
@@ -26,7 +26,7 @@ class DTPCONTROL :
 {
 public:
     DTPCONTROL();
-    ~DTPCONTROL(){};
+    ~DTPCONTROL();
 
     //~DTPCONTROL();
     void deliverObservation(Ice::Int, const autogen::Planner::ObservationSeq& observationSeq, const Ice::Current&);
@@ -36,23 +36,62 @@ public:
     /* Running in a thread. Is woken up every time an observation comes
      * in on the relevant task.*/
     void post_action(Ice::Int id);
-    
 protected:
-    void configure(const cast::cdl::StringMap& _config, const Ice::Current& _current){}
-    void start(){}
-    void runComponent(){}
-    void stop(){};
+
+    
+    void configure(const cast::cdl::StringMap& _config, const Ice::Current& _current);
+    
+    void start();
+    void stop();
+
+    /*Does nothing.*/
+    void runComponent();
+
+    
   
 private:
-    typedef std::tr1::shared_ptr<pthread_mutex_t> Mutex;
+    /* Freiburg python planning server.*/
+    PythonServerPrx pyServer;
     
+    /* Joins the thread associated with DTP planning task indexed by
+     * \argument{id}. (see \member{cancelTask} and \member{stop})*/
+    void _cancelTask(Ice::Int);
+    
+    /* Called by \member{newTask}. Creates a thread that executes
+     * \member{post_action(id)}. \members{thread_statuus,
+     * thread_mutex, threads, thread_attributes} are configured during
+     * this call.*/ 
+    void spawn__post_action__thread(Ice::Int id);
+
+    /* Name of the CAST planning component (see \module{WMControl} and
+     * \method{configure})*/
+    std::string m_python_server;
+    
+    typedef std::tr1::shared_ptr<pthread_t> Thread;
+    typedef std::tr1::shared_ptr<pthread_attr_t> Thread_Attributes;
+    typedef std::tr1::shared_ptr<pthread_mutex_t> Mutex;
+
+    /* Domain associated with task \index{Ice::Int}*/
     std::map<Ice::Int, Planning::Domain_Name> thread_to_domain;
+    
+    /* Problem associated with task \index{Ice::Int}*/
     std::map<Ice::Int, Planning::Problem_Name> thread_to_problem;
 
+    /* Thread status (false == being killed, true == alive) associated
+     * with task \index{Ice::Int}*/
     std::map<Ice::Int, bool> thread_statuus;
+    
+    /* Mutex (action executes, then observation is received, then
+     * action executes, etc...) associated with task
+     * \index{Ice::Int}*/
     std::map<Ice::Int, Mutex> thread_mutex;
-    std::map<Ice::Int, pthread_t> threads;
-    std::map<Ice::Int, pthread_attr_t> thread_attributes;
+    
+    /* Thread associated with task \index{Ice::Int}*/
+    std::map<Ice::Int, Thread> threads;
+    
+    /* Thread attributes (i.e., joinable) associated with task
+     * \index{Ice::Int}*/
+    std::map<Ice::Int, Thread_Attributes> thread_attributes;
 
 };
 
