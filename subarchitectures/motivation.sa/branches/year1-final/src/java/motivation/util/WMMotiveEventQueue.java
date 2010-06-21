8d97e0c6552680f718c0aa1e805ae450b74c1159
Marc Hanheide <marc@hanheide.net>
2010-06-21 12:12:18 +0000

    branched based on yr1 system

---
+++ subarchitectures/motivation.sa/branches/year1-final/src/java/motivation/util/WMMotiveEventQueue.java
@@ -0,0 +1,42 @@
+package motivation.util;
+
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import motivation.util.castextensions.WMEntrySet;
+import Ice.ObjectImpl;
+import cast.cdl.WorkingMemoryAddress;
+import cast.cdl.WorkingMemoryChange;
+
+public class WMMotiveEventQueue extends LinkedBlockingQueue<WMMotiveEventQueue.MotiveEvent> implements WMEntrySet.ChangeHandler{
+
+	public class MotiveEvent {
+		public Map<WorkingMemoryAddress, ObjectImpl> map;
+		public WorkingMemoryChange wmc; 
+		public ObjectImpl newMotive; 
+		public ObjectImpl oldMotive;
+	}
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7628018827365709394L;
+
+
+	@Override
+	public void entryChanged(Map<WorkingMemoryAddress, ObjectImpl> map,
+			WorkingMemoryChange wmc, ObjectImpl newMotive, ObjectImpl oldMotive) {
+		MotiveEvent m = new MotiveEvent(); 
+		try {
+			m.map=map;
+			m.wmc=wmc;
+			m.newMotive=newMotive;
+			m.oldMotive=oldMotive;
+			this.put(m);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+}
