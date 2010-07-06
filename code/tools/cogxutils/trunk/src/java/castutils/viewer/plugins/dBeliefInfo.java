6c25bea627ee202afb1253c1d83ff968fa827916
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-07-06 17:42:09 +0000

    - new Belief viewer - initial support for Percept Tracking

---
+++ code/tools/cogxutils/trunk/src/java/castutils/viewer/plugins/dBeliefInfo.java
@@ -0,0 +1,95 @@
+package castutils.viewer.plugins;
+
+import java.util.Vector;
+import java.util.Map.Entry;
+
+import de.dfki.lt.tr.beliefs.data.Belief;
+import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
+import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
+import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
+import de.dfki.lt.tr.beliefs.util.ProbFormula;
+
+/**
+ * @author Marc Hanheide (marc@hanheide.de)
+ *
+ */
+public class dBeliefInfo implements Plugin {
+
+	@Override
+	public Vector<Object> toVector(Ice.ObjectImpl iceObject) {
+		Vector<Object> extraInfo = new Vector<Object>();
+		Belief<dBelief> belief = Belief.create(dBelief.class,
+				(dBelief) iceObject);
+		String agentStatus = belief.getStatus().getClass().getSimpleName();
+		extraInfo.add("Status: " + agentStatus);
+		extraInfo.add("Type: " + belief.getType());
+
+		// try to convert into an IndependentFormulaDistributionsBelief
+		try {
+			CASTIndependentFormulaDistributionsBelief<dBelief> assumedBelief = CASTIndependentFormulaDistributionsBelief
+					.create(dBelief.class, (dBelief) iceObject);
+			extraInfo.add("duration="
+					+ assumedBelief.duration());
+			String distrStr = "";
+			for (Entry<String, FormulaDistribution> distr : assumedBelief
+					.getContent().entrySet()) {
+				distrStr += distr.getKey() + "={";
+				for (ProbFormula i : distr.getValue()) {
+					distrStr += i.getFormula().toString() + " ("
+							+ i.getProbability() + ") ";
+				}
+				distrStr += "} ";
+
+			}
+			extraInfo.add(distrStr);
+		} catch (ClassCastException e) {
+			// ignore this
+		}
+
+		// String offsprings = "";
+		// for (WorkingMemoryAddress offspringWMA :
+		// ((CASTBeliefHistory)belief.hist).offspring) {
+		// offsprings+=" "+offspringWMA.id;
+		// }
+		// extraInfo.add("offspring:" + offsprings);
+		// if (belief.content instanceof CondIndependentDistribs) {
+		// CondIndependentDistribs dist = (CondIndependentDistribs)
+		// belief.content;
+		// String features="";
+		// for (Entry<String, ProbDistribution> pd : dist.distribs.entrySet()) {
+		// if (pd.getValue() instanceof BasicProbDistribution) {
+		// BasicProbDistribution fvd = (BasicProbDistribution) pd.getValue();
+		// features+=pd.getKey()+"=[";
+		// for (FeatureValueProbPair fv : ((FeatureValues)fvd.values).values) {
+		// String featStr = toString(fv.val);
+		// features+=featStr+" ";
+		// }
+		// features+="] ";
+		// }
+		// }
+		// extraInfo.add("Features: " + features);
+		// }
+		// }
+		return extraInfo;
+	}
+
+	// /**
+	// * @param fv
+	// * @return
+	// */
+	// public static String toString(FeatureValue fv) {
+	// String featStr="*";
+	// if (fv instanceof IntegerValue)
+	// featStr=Integer.toString(((IntegerValue) fv).val);
+	// if (fv instanceof PointerValue)
+	// featStr=CASTUtils.toString(((PointerValue) fv).beliefId);
+	// if (fv instanceof StringValue)
+	// featStr=((StringValue) fv).val;
+	// if (fv instanceof FloatValue)
+	// featStr= Double.toString(((FloatValue) fv).val);
+	// if (fv instanceof BooleanValue)
+	// featStr=Boolean.toString(((BooleanValue) fv).val);
+	// return featStr;
+	// }
+
+}
---
+++ code/tools/cogxutils/trunk/src/java/eu/cogx/percepttracker/WMTracker.java
@@ -0,0 +1,373 @@
+package eu.cogx.percepttracker;
+
+import java.util.Map.Entry;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+import cast.CASTException;
+import cast.ConsistencyException;
+import cast.DoesNotExistOnWMException;
+import cast.PermissionException;
+import cast.SubarchitectureComponentException;
+import cast.UnknownSubarchitectureException;
+import cast.architecture.ChangeFilterFactory;
+import cast.architecture.ManagedComponent;
+import cast.architecture.WorkingMemoryWriterComponent;
+import cast.cdl.WorkingMemoryAddress;
+import cast.cdl.WorkingMemoryChange;
+import cast.cdl.WorkingMemoryOperation;
+import cast.cdl.WorkingMemoryPermissions;
+import cast.core.CASTUtils;
+import cast.interfaces.WorkingMemory;
+import castutils.castextensions.CASTHelper;
+import castutils.castextensions.PointerMap;
+import castutils.castextensions.WMEventQueue;
+import castutils.castextensions.WMView;
+import castutils.slice.WMTrackedBeliefMap;
+import de.dfki.lt.tr.beliefs.data.Belief;
+import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
+
+/**
+ * This implements a generic tracker for {@link Belief}s. It listens for the
+ * addition or overwrite of a "From" belief tries to match it to existing "To"
+ * beliefs and if find a matching one it updates the corresponding "To" belief.
+ * In order to do this job it requires a {@link MatcherFunction}.
+ * 
+ * @author marc
+ * 
+ * @param <From>
+ *            the generic type to listen for and generate from
+ * @param <To>
+ *            the generic that is generated and synchronized to the From type
+ */
+public class WMTracker<From extends dBelief, To extends dBelief> extends
+		CASTHelper implements Runnable {
+
+	/**
+	 * an interface to be implemented by classes that should serve as a
+	 * {@link MatcherFunction} for {@link WMTracker}.
+	 * 
+	 * @author marc
+	 * 
+	 * @param <From2>
+	 *            the generic type of the source of the transfer
+	 * @param <To2>
+	 *            the generic type of the sink of the transfer
+	 */
+	public interface MatcherFunction<From2 extends Ice.ObjectImpl, To2 extends Ice.ObjectImpl> {
+		/**
+		 * create a new "To" belief from an unmatched "From" belief.
+		 * 
+		 * @param idToCreate
+		 *            the new ID to create (@see
+		 *            {@link WorkingMemoryWriterComponent})
+		 * @param wmc
+		 *            the {@link WorkingMemoryChange} that caused the creation
+		 * @param from
+		 *            the element that is used to initialize the new one
+		 * @return the new instance, ready to be written to
+		 *         {@link WorkingMemory}
+		 * @throws IncompatibleAssignmentException
+		 */
+		public To2 create(WorkingMemoryAddress idToCreate,
+				WorkingMemoryChange wmc, From2 from)
+				throws IncompatibleAssignmentException;
+
+		/**
+		 * match a "From" to a "To" belief
+		 * 
+		 * @param wmc
+		 *            the {@link WorkingMemoryChange} that caused the match
+		 * @param from
+		 *            the belief that is compared
+		 * @param to
+		 *            the belief to compare to
+		 * @return a likelihood of matching between 0.0 and 1.0
+		 */
+		public double match(WorkingMemoryChange wmc, From2 from, To2 to);
+
+		/**
+		 * update an existing belief with a new observation
+		 * 
+		 * @param wmc
+		 *            the {@link WorkingMemoryChange} that caused the update
+		 * @param from
+		 *            the belief that is used to update the existing one
+		 * @param to
+		 *            the belief to be updated
+		 * @throws IncompatibleAssignmentException
+		 */
+		public void update(WorkingMemoryChange wmc, From2 from, To2 to)
+				throws IncompatibleAssignmentException;
+
+		/**
+		 * @param from
+		 * @return true if this matching function can actually handle this type
+		 *         of belief
+		 */
+		public boolean canHandle(From2 from);
+
+	}
+
+	/**
+	 * create new synchronizer only for given memory operations
+	 * 
+	 * @param <FromS>
+	 *            From type
+	 * @param <ToS>
+	 *            To type
+	 * @param component
+	 *            the underlying component employed for memory operations
+	 * @param fromType
+	 *            the from class object
+	 * @param toType
+	 *            the to class object
+	 * @param transferFunction
+	 *            an instance of a {@link MatcherFunction}
+	 * @param wm2wm
+	 * @return a new object
+	 */
+	public static <FromS extends dBelief, ToS extends dBelief> WMTracker<FromS, ToS> create(
+			ManagedComponent component, Class<FromS> fromType,
+			Class<ToS> toType, MatcherFunction<FromS, ToS> transferFunction,
+			PointerMap<?> wm2wm) {
+
+		return new WMTracker<FromS, ToS>(component, fromType, toType,
+				transferFunction, wm2wm);
+	}
+
+	private PointerMap<?> wm2wmMap;
+
+	/**
+	 * a synchronized event queue that ensure that any new insert notification
+	 * returns immediately while the worker thread can sequentially work on all
+	 * the events.
+	 */
+	private WMEventQueue entryQueue;
+
+	/**
+	 * the from type
+	 */
+	protected final Class<From> fromType;
+
+	private ExecutorService executor;
+
+	/**
+	 * the to type
+	 */
+	protected final Class<To> toType;
+
+	/** the registered TransferFunction for PerceptMonitor */
+	protected MatcherFunction<From, To> matcherFunction;
+
+	/**
+	 * a map to keep track of all the relations between From and To beliefs
+	 */
+	protected WMView<To> allTrackedBeliefs;
+
+	/**
+	 * create new synchronizer only for given memory operations
+	 * 
+	 * @param component
+	 * @param fromType
+	 * @param toType
+	 * @param transferFunction
+	 * @param ops
+	 */
+	protected WMTracker(ManagedComponent component, Class<From> fromType,
+			Class<To> toType, MatcherFunction<From, To> transferFunction,
+			PointerMap<?> wm2wmMap) {
+		super(component);
+		this.fromType = fromType;
+		this.toType = toType;
+		this.matcherFunction = transferFunction;
+		entryQueue = new WMEventQueue();
+		this.wm2wmMap = wm2wmMap;
+		this.allTrackedBeliefs = WMView.create(this.component, toType);
+		this.executor = Executors.newCachedThreadPool();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		log("register listeners for type " + fromType.getSimpleName());
+		component.addChangeFilter(ChangeFilterFactory.createTypeFilter(
+				fromType, WorkingMemoryOperation.ADD), entryQueue);
+		component.addChangeFilter(ChangeFilterFactory.createTypeFilter(
+				fromType, WorkingMemoryOperation.OVERWRITE), entryQueue);
+		component.addChangeFilter(ChangeFilterFactory.createTypeFilter(
+				fromType, WorkingMemoryOperation.DELETE), entryQueue);
+
+		try {
+			allTrackedBeliefs.start();
+		} catch (UnknownSubarchitectureException e1) {
+			logger.fatal("cannot start WMView", e1);
+			return;
+		}
+
+		start();
+		try {
+			while (true) {
+				try {
+					final WorkingMemoryChange ev = entryQueue.take();
+					switch (ev.operation) {
+					case ADD: {
+						handleNewObservation(ev);
+						break;
+					}
+					case OVERWRITE: {
+						WorkingMemoryAddress toWMA = wm2wmMap.get(ev.address);
+						if (toWMA == null)
+							handleNewObservation(ev);
+						else {
+							// we assume a static assignment here. if an
+							// observation has once been linked to a tracked
+							// belief, overwrite will only be propagated to
+							// this one!
+							handleOverwriteObservation(ev, toWMA);
+						}
+						break;
+					}
+					case DELETE: {
+						wm2wmMap.remove(ev.address);
+						break;
+					}
+					}
+				} catch (CASTException e) {
+					logger.error("in run: ", e);
+				} catch (IncompatibleAssignmentException e) {
+					logger.error("in run", e);
+				}
+			}
+		} catch (InterruptedException e) {
+			logger.warn("interrupted in run, leaving thread: ", e);
+			return;
+		} finally {
+			try {
+				component.removeChangeFilter(entryQueue);
+			} catch (SubarchitectureComponentException e) {
+				logger.error("while removing change filter: ", e);
+			}
+		}
+	}
+
+	protected WorkingMemoryAddress bestMatch(WorkingMemoryChange wmc, From from) {
+		double bestProb = 0.0;
+		WorkingMemoryAddress result = null;
+		for (Entry<WorkingMemoryAddress, To> i : allTrackedBeliefs.entrySet()) {
+			double m = matcherFunction.match(wmc, from, i.getValue());
+			if (m > bestProb) {
+				result = i.getKey();
+				bestProb = m;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * to be overwritten by any subclass if required. Is called after the
+	 * registration of ChangeReceivers and before actually entering the run loop
+	 * (@see {@link Runnable}).
+	 */
+	protected void start() {
+		// to be overwritten
+
+	}
+
+	private void handleNewObservation(final WorkingMemoryChange ev)
+			throws CASTException, IncompatibleAssignmentException {
+		final From from = component.getMemoryEntry(ev.address, fromType);
+		if (!matcherFunction.canHandle(from))
+			return;
+		final WorkingMemoryAddress matchingWMA = bestMatch(ev, from);
+		executor.submit(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+				if (matchingWMA == null) {
+					log("found no match for observation "
+							+ CASTUtils.toString(ev.address));
+					String id = component.newDataID();
+
+					WorkingMemoryAddress toWMA = new WorkingMemoryAddress(id, component
+							.getSubarchitectureID());
+					log("creating new tracked belief with " + CASTUtils.toString(toWMA));
+
+					To to = matcherFunction.create(toWMA, ev, from);
+					if (to != null) {
+						matcherFunction.update(ev, from, to);
+						component.addToWorkingMemory(toWMA, to);
+						wm2wmMap.put(ev.address, toWMA);
+					} else {
+						logger.warn("failed to create a corresponding belief for "
+								+ from.type);
+					}
+				} else {
+					try {
+						log("found match " + CASTUtils.toString(matchingWMA)
+								+ " for observation " + CASTUtils.toString(ev.address));
+						// make sure we have exclusive access
+						component.lockEntry(matchingWMA,
+								WorkingMemoryPermissions.LOCKEDODR);
+						To to = component.getMemoryEntry(matchingWMA, toType);
+						log("updating belief " + CASTUtils.toString(matchingWMA));
+						matcherFunction.update(ev, from, to);
+						component.overwriteWorkingMemory(matchingWMA, to);
+						wm2wmMap.put(ev.address, matchingWMA);
+					} finally {
+						component.unlockEntry(matchingWMA);
+					}
+				}
+				} catch (IncompatibleAssignmentException e) {
+					logger.error("during update:", e);
+				} catch (CASTException e) {
+					logger.error("during update:", e);
+				}
+			}
+		});
+
+		
+
+	}
+
+	private void handleOverwriteObservation(final WorkingMemoryChange ev,
+			final WorkingMemoryAddress matchingWMA) throws CASTException,
+			IncompatibleAssignmentException {
+		final From from = component.getMemoryEntry(ev.address, fromType);
+		if (!matcherFunction.canHandle(from))
+			return;
+
+		executor.submit(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					// make sure we have exclusive access
+					component.lockEntry(matchingWMA,
+							WorkingMemoryPermissions.LOCKEDODR);
+					To to = component.getMemoryEntry(matchingWMA, toType);
+					matcherFunction.update(ev, from, to);
+					component.overwriteWorkingMemory(matchingWMA, to);
+				} catch (IncompatibleAssignmentException e) {
+					logger.error("during update:", e);
+				} catch (CASTException e) {
+					logger.error("during update:", e);
+				} finally {
+					try {
+						component.unlockEntry(matchingWMA);
+					} catch (CASTException e) {
+						logger.error("during unlock of "+matchingWMA,e);
+					}
+				}
+			}
+		});
+
+	}
+
+}
