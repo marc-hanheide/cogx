f44d688690c0abf607d4633d554a852b5c5b4d8b
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-07-06 17:41:36 +0000

    support for a addr->addr map in WMs

---
+++ code/tools/castutils/trunk/src/java/castutils/viewer/plugins/WMMapInfo.java
@@ -0,0 +1,47 @@
+package castutils.viewer.plugins;
+
+import java.util.Vector;
+import java.util.Map.Entry;
+
+import castutils.slice.WMMap;
+
+import de.dfki.lt.tr.beliefs.data.Belief;
+import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
+import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
+import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
+import de.dfki.lt.tr.beliefs.util.ProbFormula;
+
+/**
+ * @author Marc Hanheide (marc@hanheide.de)
+ *
+ */
+public class WMMapInfo implements Plugin {
+
+	@Override
+	public Vector<Object> toVector(Ice.ObjectImpl iceObject) {
+		Vector<Object> extraInfo = new Vector<Object>();
+		WMMap map = (WMMap) iceObject;
+		extraInfo.add("size()="+map.map.size());
+		return extraInfo;
+	}
+
+	// /**
+	// * @param fv
+	// * @return
+	// */
+	// public static String toString(FeatureValue fv) {
+	// String featStr="*";
+	// if (fv instanceof IntegerValue)
+	// featStr=Integer.toString(((IntegerValue) fv).val);
+	// if (fv instanceof PointerValue)
+	// featStr=CASTUtils.toString(((PointerValue) fv).beliefId);
+	// if (fv instanceof StringValue)
+	// featStr=((StringValue) fv).val;
+	// if (fv instanceof FloatValue)
+	// featStr= Double.toString(((FloatValue) fv).val);
+	// if (fv instanceof BooleanValue)
+	// featStr=Boolean.toString(((BooleanValue) fv).val);
+	// return featStr;
+	// }
+
+}
