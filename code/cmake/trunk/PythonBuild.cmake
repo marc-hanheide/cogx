74e706cf577bc7c6035badd96ff33d4bb3726bf8
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-07-14 08:55:46 +0000

    cmake: python install procedure

--- code/cmake/trunk/PythonBuild.cmake
+++ code/cmake/trunk/PythonBuild.cmake
@@ -4,6 +4,10 @@
 # or when slice2py is not available.
 option(BUILD_PYTHON_COMPONENTS "Build Python Ice Interfaces and Components" YES)
 
-set(PYTHON_SLICEGEN_DIR  dist-slicegen)
+set(PYTHON_INSTALL_PREFIX     ${CMAKE_INSTALL_PREFIX}/python)
+set(PYTHON_INSTALL_PACKAGES   ${PYTHON_INSTALL_PREFIX}/dist-packages)
+set(PYTHON_INSTALL_SLICEGEN   ${PYTHON_INSTALL_PREFIX}/dist-slicegen)
+set(PYTHON_INSTALL_CASTMODULE ${PYTHON_INSTALL_PREFIX}/castmodule)
+
 add_subdirectory(tools/python)
 
