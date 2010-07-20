6fb23884aaa28f2268be58215990fe2727443766
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-07-20 10:09:10 +0000

    vision/sim: forms generated

--- subarchitectures/vision.sa/trunk/src/c++/vision/components/SimulatedVision/SimulatedVision.cpp
+++ subarchitectures/vision.sa/trunk/src/c++/vision/components/SimulatedVision/SimulatedVision.cpp
@@ -1,4 +1,22 @@
+/*
+ * Author: Marko Mahnič
+ * Created: July 2010
+ *
+ * © Copyright 2010 Marko Mahnič. 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include "SimulatedVision.h"
+#include "Exception.h"
+#include "StringFmt.h"
 
 extern "C"
 {
@@ -33,7 +51,131 @@ bool CVisionSimulator::CDisplayClient::getFormData(const std::string& id,
 
 void CVisionSimulator::CDisplayClient::createForms()
 {
-   setHtmlForm("Vision.Simulator", "Generator", "TODO: create the fields");
+   // Two forms:
+   //    - scene contents: a list of objects that are on the scene.
+   //       - multi-list or multiple drop-down boxes (configurable)
+   //       - create scene
+   //       - clear scene
+   //       - load/save scene
+   //       - show -> displays properties for object
+   //    - object properties
+   //       - color distribution (3 cbx+edit, unknown prob RO-calculated)
+   //       - shape distribution (-"-)
+   //       - label distribution (-"-)
+   //       - ambiguity/gain for each (meaning: what do we gain if we ask)
+   //       - load/save (submit saves data)
+   //       - cbx to select object
+   //       - (saliency, time, ...)
+   // Data:
+   //    - each object has a set of attributes (multiple sets?)
+   //    - list of known (possible) colors
+   //    - list of possible shapes
+   //    - list of possible labels; this can grow; (shared with ObjectRecognizerSrv?)
+   //    - list of known objects
+   //    - (lists could grow during execution -- learn new stuff)
+   
+   // scene form
+   {
+      std::ostringstream ss, objids;
+
+      // TODO: load object names from a list
+      for(int i = 0; i < 10; i++) {
+         objids << "<option>Object " << i << "</option>";
+      }
+      // up to 5 objects in a scene
+      for(int i = 0; i < 5; i++) {
+         ss << "<select name='sceneobj" << i << "' >";
+         ss << objids.str();
+         ss << "</select>" << "<br/>";
+      }
+
+      // Management buttons
+      ss << "<input type='submit' name='submit' value='Create Scene' />";
+      ss << "<input type='button' @@ONCLICK@@('scene.erase') value='Erase Scene' />";
+      ss << "<input type='button' @@ONCLICK@@('scene.load') value='Load Scene' />";
+      ss << "<input type='button' @@ONCLICK@@('scene.save') value='Save Scene' />";
+
+      setHtmlForm("Vision.Simulator", "002.sim.scene", ss.str());
+   }
+
+   // object form
+   {
+      struct _local_ {
+         static void valueDistrib(std::ostringstream& ss, const std::string& name,
+               const std::vector<std::string>& values, int count)
+         {
+            std::ostringstream opts;
+            for(int i = 0; i < values.size(); i++) {
+               opts << "<option>" << values[i] << "</option>";
+            }
+            for(int i = 0; i < count; i++) {
+               ss << "<tr><td>";
+               ss << "<select name='" << name << i << "' >";
+               ss << opts.str();
+               ss << "</select>";
+               ss << "</td><td>";
+               ss << "<input type='text' name='p" << name << i << "' style='width:5em;' />";
+               ss << "</td></tr>";
+            }
+            ss << "<tr><td>";
+            ss << "Unknown: </td><td> 1.0";
+            ss << "</td></tr>";
+         }
+         static void ambiguityGain(std::ostringstream& ss, const std::string& name, bool bLabel) {
+            ss << "<tr><td>" << (bLabel ? "Ambiguity:" : "&nbsp") << "</td><td>";
+            ss << "<input type='text' name='ambig" << name << "' style='width:5em;' />";
+            ss << "</td></tr>";
+            ss << "<tr><td>" << (bLabel ? "Gain:" : "&nbsp") << "</td><td>";
+            ss << "<input type='text' name='gain" << name << "' style='width:5em;' />";
+            ss << "</td></tr>";
+         }
+      };
+      std::ostringstream ss;
+      std::vector<std::string> clrs, shps, lbls;
+
+      ss << "<table><tr><td>";
+
+      // TODO: load color names from a list
+      for(int i = 0; i < 10; i++) {
+         //clrs.push_back( ((std::ostringstream&)(std::ostringstream() << "Color" << i)).str() );
+         clrs.push_back( std::string("Color") + _str_(i, "d:3:0") );
+      }
+
+      // up to 3 colors for an object
+      ss << "<table>";
+      _local_::valueDistrib(ss, "color", clrs, 3);
+      _local_::ambiguityGain(ss, "color", true);
+      ss << "</table>";
+
+      ss << "</td><td>";
+
+      // TODO: load shape names from a list
+      for(int i = 0; i < 3; i++) {
+         shps.push_back( std::string("Shape") + _str_(i, "d:3:0") );
+      }
+
+      // up to 3 shapes for an object
+      ss << "<table>";
+      _local_::valueDistrib(ss, "shape", shps, 3);
+      _local_::ambiguityGain(ss, "shape", true);
+      ss << "</table>";
+
+      ss << "</td><td>";
+
+      // TODO: load label names from a list
+      for(int i = 0; i < 3; i++) {
+         lbls.push_back( std::string("Label") + _str_(i, "d:3:0") );
+      }
+
+      // up to 3 labels for an object
+      ss << "<table>";
+      _local_::valueDistrib(ss, "label", lbls, 3);
+      _local_::ambiguityGain(ss, "label", true);
+      ss << "</table>";
+
+      ss << "</td></tr></table>";
+      setHtmlForm("Vision.Simulator", "001.sim.object", ss.str());
+   }
 }
 
 CVisionSimulator::CVisionSimulator()
---
+++ subarchitectures/vision.sa/trunk/src/c++/vision/components/SimulatedVision/StringFmt.h
@@ -0,0 +1,124 @@
+/*
+ * Author: Marko Mahnič
+ * Created: July 2010
+ *
+ * © Copyright 2010 Marko Mahnič. 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef STRINGFMT_DO03FM55
+#define STRINGFMT_DO03FM55
+
+template<typename T>
+std::string _str_(T i, short width=0, unsigned short precision=6, char fill=' ')
+{
+   std::ostringstream ss;
+   if (width != 0) {
+      if (width < 0) {
+         width = -width;
+         ss.setf(std::ios::left);
+      }
+      ss.width(width);
+      ss.fill(fill);
+   }
+   ss.precision(precision);
+   ss << i;
+   return ss.str();
+}
+
+template<typename T>
+std::string _str_(T i, short width=0, char fill=' ')
+{
+   return _str_(i, width, 6, fill);
+}
+
+// "", "d"  - generic representation
+// "x" - hex representation
+// "o" - oct representation
+// "f" - fixed representation
+// "s" - scientific representation
+// ":21" - width 21, right aligned
+// ":-4" - width 4, left aligned
+// ":+8" - width 8, justified
+// ":11.3" - width 11, precision 3
+// "s:10: " - scientific, width 10, fill with spaces
+template<typename T>
+std::string _str_(T i, const std::string& fmt)
+{
+   enum _stage_ { stype = 0, spos = 1, sfill = 2 };
+   std::ostringstream ss;
+   int stage = stype;
+   size_t len = fmt.size();
+   size_t pos = 0;
+   while(pos < len) {
+      char ch = fmt[pos];
+      if (stage == sfill) {
+         ss.fill(ch);
+         break;
+      }
+      if (ch == ':') {
+         stage++;
+         pos++;
+         continue;
+      }
+      if (stage == stype) {
+         switch (ch) {
+            case 'a': ss.setf(std::ios::boolalpha); break;
+            case 'b': ss.setf(std::ios::showbase); break;
+            case 'd': ss.setf(std::ios::dec, std::ios::basefield); break;
+            case 'o': ss.setf(std::ios::oct, std::ios::basefield); break;
+            case 'x': ss.setf(std::ios::hex, std::ios::basefield); break;
+            case 's': ss.setf(std::ios::scientific, std::ios::floatfield); break;
+            case 'f': ss.setf(std::ios::fixed, std::ios::floatfield); break;
+            default: break;
+         }
+      }
+      else if (stage == spos) {
+         size_t beg = pos;
+         if (ch == '-') { ss.setf(std::ios::left); pos++; }
+         else if (ch == '+') { ss.setf(std::ios::internal); pos++; }
+         size_t end = fmt.find(':', pos);
+         if (end < 0) end = len;
+         int n = 0;
+         int wp = 0;
+         while (pos < end) {
+            ch = fmt[pos];
+            if (ch >= '0' && ch <= '9') {
+               n = n * 10 + ch - '0';
+               pos++;
+               continue;
+            }
+            if (ch != '.') { // error, invalid char
+               n = 0;
+               break;
+            }
+            if (wp > 0) break; // error, second point
+            else {
+               wp++;
+               if (n > 0) ss.width(n);
+               n = 0;
+            }
+         }
+         if (n > 0) {
+            if (wp == 0) ss.width(n);
+            else if (wp == 1) ss.precision(n);
+         }
+         pos = end-1;
+      }
+
+      pos++;
+   }
+   ss << i;
+   return ss.str();
+}
+
+#endif /* end of include guard: STRINGFMT_DO03FM55 */
+// vim:sw=3:ts=8:et
