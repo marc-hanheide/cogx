f4f78072ee1223f5d0c3cd6a84908099ee6ce1ad
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-07-29 15:01:37 +0000

    moved scenario specific code here

---
+++ code/scenarios/dora/trunk/src/java/eu/cogx/perceptmediator/LocalizedVisualObjectTransferFunction.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package eu.cogx.perceptmediator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import SpatialData.Place;
+
+import VisionData.VisualObject;
+import cast.CASTException;
+import cast.architecture.ManagedComponent;
+import cast.cdl.WorkingMemoryAddress;
+import cast.cdl.WorkingMemoryChange;
+import castutils.castextensions.WMView;
+import de.dfki.lt.tr.beliefs.data.formulas.Formula;
+import de.dfki.lt.tr.beliefs.data.formulas.PropositionFormula;
+import de.dfki.lt.tr.beliefs.data.formulas.WMPointer;
+import de.dfki.lt.tr.beliefs.util.BeliefException;
+import eu.cogx.beliefs.slice.PerceptBelief;
+import eu.cogx.perceptmediator.transferfunctions.abstr.DependentDiscreteTransferFunction;
+import eu.cogx.perceptmediator.transferfunctions.helpers.PlaceMatchingFunction;
+import facades.SpatialFacade;
+
+/**
+ * @author marc
+ * 
+ */
+public class LocalizedVisualObjectTransferFunction extends
+		DependentDiscreteTransferFunction<VisualObject> {
+
+	public LocalizedVisualObjectTransferFunction(ManagedComponent component,
+			WMView<PerceptBelief> allBeliefs) {
+		super(component, allBeliefs, Logger
+				.getLogger(LocalizedVisualObjectTransferFunction.class));
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Map<String, Formula> getFeatureValueMapping(
+			WorkingMemoryChange wmc, VisualObject from) throws BeliefException {
+		assert (from != null);
+		Map<String, Formula> result = new HashMap<String, Formula>();
+		// TODO: we should use a DoubleValue here!
+		try {
+			Place currentPlace = SpatialFacade.get(component).getPlace();
+			WorkingMemoryAddress placeWMA = getReferredBelief(new PlaceMatchingFunction(
+					currentPlace.id));
+			result.put("ObjectId", PropositionFormula.create(wmc.address.id).getAsFormula());
+			result.put("label", PropositionFormula.create(from.label).getAsFormula());
+			result.put("is-in", WMPointer.create(placeWMA).getAsFormula());
+		} catch (BeliefException e) {
+			component.logException(e);
+		} catch (CASTException e) {
+			component.logException(e);
+		} catch (InterruptedException e) {
+			component.logException(e);
+		}
+
+		return result;
+	}
+
+}
