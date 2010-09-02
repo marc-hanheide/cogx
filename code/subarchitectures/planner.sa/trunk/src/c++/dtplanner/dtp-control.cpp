69074b0c6f02fe0540a1ba70a0fd3aa64f956189
grettonc <grettonc@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-09-02 16:06:44 +0000

    ...

--- code/subarchitectures/planner.sa/trunk/src/c++/dtplanner/dtp-control.cpp
+++ code/subarchitectures/planner.sa/trunk/src/c++/dtplanner/dtp-control.cpp
@@ -696,7 +696,8 @@ void DTPCONTROL::newTask(Ice::Int id,
     solvers[id] = new Planning::Solver(*actual_problem->second);//thread_to_problem[id]);
     solvers[id]->preprocess();
     QUERY_UNRECOVERABLE_ERROR(!solvers[id]->expansion_queue.size()
-                              , "The problem is NULL, there is no starting state.");
+                              , "The problem is NULL, there is no starting state."
+                              <<*actual_problem->second<<std::endl);
 //     solvers[id]->expand_belief_state_space();
     current_state[id] = solvers[id]->expansion_queue.front();
     if(solvers[id]->expand_belief_state_space()){
