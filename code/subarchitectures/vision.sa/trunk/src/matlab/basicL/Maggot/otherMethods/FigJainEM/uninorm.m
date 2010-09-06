ea6a4afb249ca55290893d5fe04cf631d23615cb
danijels <danijels@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-09-06 15:24:59 +0000

    Old Maggot deleted.

--- code/subarchitectures/vision.sa/trunk/src/matlab/basicL/Maggot/otherMethods/FigJainEM/uninorm.m
+++
@@ -1,9 +0,0 @@
-function y = uninorm(x,m,var)
-% Evaluates a multidimensional Gaussian
-% of mean m and variance var
-% at the array of points x
-%
-ff = ((2*pi*(var+realmin))^(-1/2));
-y = ff * exp((-1/(2*var))*(x-m).^2);
-
-
