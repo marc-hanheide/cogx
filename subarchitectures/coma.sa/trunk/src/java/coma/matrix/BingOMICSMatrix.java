5119564b5f2200c491ae184ed30227fa7c27e94a
zender <zender@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-09-07 20:34:47 +0000

    querying bing images

--- subarchitectures/coma.sa/trunk/src/java/coma/matrix/BingOMICSMatrix.java
+++ subarchitectures/coma.sa/trunk/src/java/coma/matrix/BingOMICSMatrix.java
@@ -1,24 +1,23 @@
 package coma.matrix;
 
-import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+
 import comadata.QueryResults;
 
 import de.dfki.lt.hfc.*;
 
-public class SimpleOMICSMatrix {
-
+public class BingOMICSMatrix {
 	public String namespaceFile="";
 	public String tupleFile="";
 	public String ruleFile = "";
-	
+
 	public String rowQuery = "";
 	public String colQuery = "";
 
@@ -27,13 +26,15 @@ public class SimpleOMICSMatrix {
 	private Query query;
 	private RuleStore ruleStore;
 	private ForwardChainer hfc;
+
+	private Matrix<Integer> matrix;
+
+	private WebsiteScanner bingImagesScanner;
 	
-	private Matrix<Boolean> matrix;
-	
-	public SimpleOMICSMatrix() {
-		
+	public BingOMICSMatrix() {
+
 	}
-	
+
 	public void init() {
 		namespaces = new de.dfki.lt.hfc.Namespace(namespaceFile);
 		tupleStore = new de.dfki.lt.hfc.TupleStore(namespaces, tupleFile);
@@ -46,15 +47,18 @@ public class SimpleOMICSMatrix {
 		System.out.println("created forward chainer object.");
 
 		matrix = null;
+		
+		bingImagesScanner = new WebsiteScanner("span", "class", "sb_count");
+		
 	}
-	
+
 	public QueryResults querySelect(String q) {
 		QueryResults _results = new QueryResults();
-		
+
 		if (q.toUpperCase().startsWith("SELECT")) {
 			de.dfki.lt.hfc.BindingTable bt;
 			_results.query = q;
-			
+
 			try {
 				bt = this.query.query(q);
 				if (bt == null) {
@@ -74,7 +78,7 @@ public class SimpleOMICSMatrix {
 						_results.varPosMap.put(_var, _posIndex);
 						_results.bt[0][_posIndex++] = _firstLine.get(_var);
 					}
-					
+
 					int _currLineIndex = 1;
 					for (Map<String,String> _currLine : _hfcAnswer) {
 						for (String _var : _currLine.keySet()) {
@@ -96,15 +100,15 @@ public class SimpleOMICSMatrix {
 			_results.bt = new String[0][0];
 			_results.varPosMap = new HashMap<String, Integer>();
 		}
-		
+
 		return _results;
 	}
-	
+
 	public void prepareMatrix() {
 		String[] rowLabels;
 		String[] colLabels;
 
-	
+
 		QueryResults rowResults = this.querySelect(this.rowQuery);
 		ArrayList<String> __rowLabels = new ArrayList<String>();
 		for (int i = 0; i < rowResults.bt.length; i++) {
@@ -112,12 +116,12 @@ public class SimpleOMICSMatrix {
 		}
 		rowLabels = new String[__rowLabels.size()]; 
 		rowLabels = (String[]) __rowLabels.toArray(rowLabels);
-		
+
 		System.out.println("rowLabels[] has a length of " + rowLabels.length + "and the following entries:");
 		for (int i = 0; i < rowLabels.length; i++) {
 			System.out.println("rowLabels["+i+"] = " + rowLabels[i]);
 		}
-		
+
 		QueryResults colResults = this.querySelect(this.colQuery);
 		ArrayList<String> __colLabels = new ArrayList<String>();
 		for (int i=0; i < colResults.bt.length; i++) {
@@ -125,44 +129,76 @@ public class SimpleOMICSMatrix {
 		}
 		colLabels = new String[__colLabels.size()];
 		colLabels = (String[]) __colLabels.toArray(colLabels);
-		
+
 		try {
-			this.matrix = new Matrix<Boolean>(colLabels, rowLabels, false);
+			this.matrix = new Matrix<Integer>(colLabels, rowLabels, 0);
 		} catch (NonUniqueLabelException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public void markCooccurrence(String coocQuery) {
-		
-		QueryResults qr = querySelect(coocQuery);
-		for (int i=0; i<qr.bt.length ; i++) {
-			String object = qr.bt[i][qr.varPosMap.get("?obj")];
-			String location = qr.bt[i][qr.varPosMap.get("?loc")];
-			System.out.println("Marking OMICS cooccurrence of " + object + " and " + location);
-			this.matrix.insertCell(new Boolean(true), object, location);
-		}
-	
-	}
-	
 
-	
+	public void markNumHits() {
+		FileOutputStream fstream;
+		try {
+			fstream = new FileOutputStream("bing-omics-locations-coocmatrix.nt");
+			PrintStream outstream = new PrintStream(fstream);
+
+			for (int a = 0; a < this.matrix.getNumCols() ; a++) {
+				for (int b = 0; b < this.matrix.getNumRows() ; b++) {
+
+					String object = this.matrix.getColLabel(a);
+					String location = this.matrix.getRowLabel(b);
+
+					String bingImagesQuery = "http://www.bing.com/images/search?q="+object.split("#")[1].replace(">", "").replace("_", "+")+"+in+the+"+location.split("#")[1].replace(">", "").replace("_", "+");
+
+					System.out.println(bingImagesQuery);
+					Integer numHits = bingImagesScanner.scanForHits(bingImagesQuery);
+
+					System.out.println("Marking cooccurrence of " + object + " and " + location + " with number of Bing images hits: " + numHits);
+					this.matrix.insertCell(numHits, a, b);
+
+					outstream.println(object + 
+							" <http://dora.cogx.eu/#in> " + location +
+							" " + numHits);
+				}
+			}
+			
+			outstream.close();
+			fstream.close();
+			System.out.println("Closed file streams.");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+
+
 	public static void main(String args[]) {
-		SimpleOMICSMatrix my_matrix = new SimpleOMICSMatrix();
+		// start in the dora directory using:
+		// % java -cp output/classes/:hfc-hz.jar:./subarchitectures/coma.sa/lib/trove-2.1.0.jar:subarchitectures/coma.sa/lib/xercesImpl.jar -Xms800m -Xmx6200m  coma.matrix.BingOMICSMatrix
+
 		
+		BingOMICSMatrix my_matrix = new BingOMICSMatrix();
+
 		my_matrix.namespaceFile = "dora.ns";
 		my_matrix.tupleFile = "/Users/zender/projects/resources/omics/omics-locations.nt";
 		my_matrix.ruleFile = "default.rdl";
-		
+
 		my_matrix.init();
-		
+
 		my_matrix.colQuery = "SELECT DISTINCT ?obj where ?obj <http://dora.cogx.eu/#omicsCooccurrence>  ?loc";
 		my_matrix.rowQuery = "SELECT DISTINCT ?loc where ?obj <http://dora.cogx.eu/#omicsCooccurrence>  ?loc";
-		
+
 		my_matrix.prepareMatrix();
-		
-		my_matrix.markCooccurrence("SELECT ?obj ?loc where ?obj <http://dora.cogx.eu/#omicsCooccurrence>  ?loc");
+
+		my_matrix.markNumHits();
+		// my_matrix.markCooccurrence("SELECT ?obj ?loc where ?obj <http://dora.cogx.eu/#omicsCooccurrence>  ?loc");
 
 		//try {
 		//	my_matrix.matrix.saveToFile("simpleOmicsMatrix.mtx");
@@ -172,31 +208,32 @@ public class SimpleOMICSMatrix {
 		//}
 
 		// save the matrix outcomes as nt file
-		int numCols = my_matrix.matrix.getNumCols();
-		int numRows = my_matrix.matrix.getNumRows();
-		
-		try{
-		    // Create file 
-		    FileOutputStream fstream = new FileOutputStream("omics-locations-coocmatrix.nt");
-		    PrintStream outstream = new PrintStream(fstream);
-		  //  BufferedWriter out = new BufferedWriter(fstream);
-		        for (int a = 0; a < numCols ; a++) {
-		        	for (int b = 0; b < numRows ; b++) {
-		        		boolean _coocDefault = my_matrix.matrix.getCell(a, b);
-		        		outstream.println(my_matrix.matrix.getColLabel(a) + 
-		        				" <http://dora.cogx.eu/#in> " + my_matrix.matrix.getRowLabel(b) +
-		        				" " + (_coocDefault ? "\"0.7\"^^<xsd:float>" : "\"0.1\"^^<xsd:float>"));
-		        	}
-		        }
-		    outstream.close();
-		    fstream.close();
-		    }catch (Exception e){//Catch exception if any
-		      System.err.println("Error: " + e.getMessage());
-		    }
-		System.out.println("Closed file streams.");
-		
+		//int numCols = my_matrix.matrix.getNumCols();
+		//int numRows = my_matrix.matrix.getNumRows();
+
+		//try{
+			// Create file 
+		//	FileOutputStream fstream = new FileOutputStream("bing-omics-locations-coocmatrix.nt");
+		//	PrintStream outstream = new PrintStream(fstream);
+		//	//  BufferedWriter out = new BufferedWriter(fstream);
+		//	for (int a = 0; a < numCols ; a++) {
+		//		for (int b = 0; b < numRows ; b++) {
+		//			Integer numHits = getNumHits(my_matrix.matrix.getColLabel(a), my_matrix.matrix.getRowLabel(b));
+		//				
+		//			outstream.println(my_matrix.matrix.getColLabel(a) + 
+		//					" <http://dora.cogx.eu/#in> " + my_matrix.matrix.getRowLabel(b) +
+		//					" " + numHits);
+		//		}
+		//	}
+		//	outstream.close();
+		//	fstream.close();
+		//}catch (Exception e){//Catch exception if any
+		//	System.err.println("Error: " + e.getMessage());
+		//}
+		//System.out.println("Closed file streams.");
+
 		System.exit(0);
-		
+
 	}
 
 }
--- subarchitectures/coma.sa/trunk/src/java/de/dfki/lt/hfc/BindingTableDecoder.java
+++ subarchitectures/coma.sa/trunk/src/java/de/dfki/lt/hfc/BindingTableDecoder.java
@@ -20,27 +20,30 @@ public final class BindingTableDecoder {
 	    ArrayList<Map<String, String>> decodedList = new ArrayList<Map<String,String>>();
 	    System.out.println("decode called, and arraylist initialized");
 	    for (int[] intResLine : bt.table) {
-	    	    
+
+	    	StringBuffer _outputLine = new StringBuffer();
+	    	
 	    	Map<String, String> _currDecodedLine = new HashMap<String, String>();
 	    	for (Integer intVarName: bt.nameToPos.keySet()) {
 	    		// the internal variable name is the first element (key) of the name2pos map
 	    		// iterating over the keys gives us all 'bound' array positions to check
 	    		// some logging:
-	    		System.out.println("BTD: current internal variable name: " + intVarName);
-	    		System.out.println("BTD: the external name of the variable is: " + bt.nameToExternalName.get(intVarName));
-	    		System.out.println("BTD: the associated array position is: " + bt.nameToPos.get(intVarName));
-	    		System.out.println("BTD: the current resolved line has the following int ID at this array position: " + intResLine[ bt.nameToPos.get(intVarName)]);
-	    		System.out.println("BTD: this int ID corresponds to the following resource: " + ts.getObject(intResLine[bt.nameToPos.get(intVarName)]));
+	    		//System.out.println("BTD: current internal variable name: " + intVarName);
+	    		//System.out.println("BTD: the external name of the variable is: " + bt.nameToExternalName.get(intVarName));
+	    		//System.out.println("BTD: the associated array position is: " + bt.nameToPos.get(intVarName));
+	    		//System.out.println("BTD: the current resolved line has the following int ID at this array position: " + intResLine[ bt.nameToPos.get(intVarName)]);
+	    		//System.out.println("BTD: this int ID corresponds to the following resource: " + ts.getObject(intResLine[bt.nameToPos.get(intVarName)]));
 	    		
     			String resource = ts.getObject(intResLine[bt.nameToPos.get(intVarName)]);
 				String extVarName = bt.nameToExternalName.get(intVarName);
 	    		
-				System.out.println("BTD: " + extVarName + "->" + resource);
+				_outputLine.append(extVarName + "->" + resource + " - ");
 				_currDecodedLine.put(extVarName, resource);
 			}
 	    	decodedList.add(_currDecodedLine);
+	    	System.out.println(_outputLine);
 	    }
-	    System.out.println("BTD: the decoded bindings list has a size of: " + decodedList.size());
+	    //System.out.println("BTD: the decoded bindings list has a size of: " + decodedList.size());
 		return decodedList;
 	}
 	
