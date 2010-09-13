3024b60a3dc7fe0725f781b935b5120cb3e7b425
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-09-13 14:31:49 +0000

    more exec niceness

--- code/subarchitectures/motivation.sa/trunk/src/java/spatial/motivation/TurnAndLookExecutor.java
+++ code/subarchitectures/motivation.sa/trunk/src/java/spatial/motivation/TurnAndLookExecutor.java
@@ -20,10 +20,10 @@ import execution.slice.Action;
 import execution.slice.TriBool;
 import execution.util.NonBlockingActionExecutor;
 
-public abstract class TurnAndLookExecutor extends NonBlockingActionExecutor {
+public abstract class TurnAndLookExecutor<ActionType extends Action> extends NonBlockingActionExecutor<ActionType> {
 
 	private final int m_detections;
-	protected final ManagedComponent m_component;
+
 	private final Stack<NavCommand> m_remainingCommands;
 	private final WorkingMemoryChangeReceiver m_afterDetect;
 
@@ -32,9 +32,11 @@ public abstract class TurnAndLookExecutor extends NonBlockingActionExecutor {
 
 	static Logger logger = Logger.getLogger(TurnAndLookExecutor.class);
 
-	public TurnAndLookExecutor(ManagedComponent _component,
+	public TurnAndLookExecutor(ManagedComponent _component, Class<ActionType> _actCls,
 			final int _detections) {
-		m_component = _component;
+		
+		super(_component, _actCls);
+		
 		m_detections = _detections;
 		m_remainingCommands = new Stack<NavCommand>();
 
@@ -54,17 +56,17 @@ public abstract class TurnAndLookExecutor extends NonBlockingActionExecutor {
 			@Override
 			public void workingMemoryChanged(WorkingMemoryChange _wmc)
 					throws CASTException {
-				m_component.removeChangeFilter(this);
+				getComponent().removeChangeFilter(this);
 				logger.debug("afterDetectListener triggered: "
 						+ CASTUtils.toString(_wmc));
 
 				if (!m_remainingCommands.empty()) {
-					m_navCmdID = m_component.newDataID();
-					m_component.addChangeFilter(ChangeFilterFactory
+					m_navCmdID = getComponent().newDataID();
+					getComponent().addChangeFilter(ChangeFilterFactory
 							.createIDFilter(m_navCmdID,
 									WorkingMemoryOperation.OVERWRITE),
 							m_afterTurn);
-					m_component.addToWorkingMemory(m_navCmdID,
+					getComponent().addToWorkingMemory(m_navCmdID,
 							m_remainingCommands.pop());
 				} else {
 					executionComplete(TriBool.TRITRUE);
@@ -82,31 +84,31 @@ public abstract class TurnAndLookExecutor extends NonBlockingActionExecutor {
 						+ CASTUtils.toString(_wmc));
 
 				// read in the nav cmd
-				m_component.lockEntry(_wmc.address,
+				getComponent().lockEntry(_wmc.address,
 						WorkingMemoryPermissions.LOCKEDODR);
 
-				NavCommand cmd = m_component.getMemoryEntry(_wmc.address,
+				NavCommand cmd = getComponent().getMemoryEntry(_wmc.address,
 						NavCommand.class);
 				logger.debug("nav command status: " + cmd.comp.name());
 
 				// if this command failed, fail the whole thing
 				if (cmd.comp == Completion.COMMANDFAILED) {
-					m_component.removeChangeFilter(this);
-					m_component.deleteFromWorkingMemory(_wmc.address);
+					getComponent().removeChangeFilter(this);
+					getComponent().deleteFromWorkingMemory(_wmc.address);
 					m_remainingCommands.clear();
 					executionComplete(TriBool.TRIFALSE);
 
 				} else if (cmd.comp == Completion.COMMANDSUCCEEDED) {
 					logger.debug("time to detect now, triggerDetection()");
 					m_navCmdID = null;
-					m_component.removeChangeFilter(this);
-					m_component.deleteFromWorkingMemory(_wmc.address);
+					getComponent().removeChangeFilter(this);
+					getComponent().deleteFromWorkingMemory(_wmc.address);
 
 					// now we've moved, trigger a detection
 					triggerDetection();
 				} else {
-					// m_component.log("command in progress: " + cmd.comp);
-					m_component.unlockEntry(_wmc.address);
+					// getComponent().log("command in progress: " + cmd.comp);
+					getComponent().unlockEntry(_wmc.address);
 				}
 			}
 		};
@@ -129,11 +131,7 @@ public abstract class TurnAndLookExecutor extends NonBlockingActionExecutor {
 	 */
 	protected abstract void triggerDetection();
 
-	@Override
-	public boolean accept(Action _action) {
-		return true;
-	}
-
+	
 	@Override
 	public void stopExecution() {
 		// remove overwrite receiver
@@ -142,24 +140,24 @@ public abstract class TurnAndLookExecutor extends NonBlockingActionExecutor {
 			logger.debug("aborting execution if not already stopped");
 
 			m_remainingCommands.clear();
-			m_component.removeChangeFilter(m_afterTurn);
+			getComponent().removeChangeFilter(m_afterTurn);
 
 			// if a nav command is happening:
 			if (m_navCmdID != null) {
 
 				// reread
-				m_component.lockEntry(m_navCmdID,
+				getComponent().lockEntry(m_navCmdID,
 						WorkingMemoryPermissions.LOCKEDODR);
-				NavCommand navCmd = m_component.getMemoryEntry(m_navCmdID,
+				NavCommand navCmd = getComponent().getMemoryEntry(m_navCmdID,
 						NavCommand.class);
 				navCmd.comp = Completion.COMMANDABORTED;
-				m_component.overwriteWorkingMemory(m_navCmdID, navCmd);
-				m_component.unlockEntry(m_navCmdID);
+				getComponent().overwriteWorkingMemory(m_navCmdID, navCmd);
+				getComponent().unlockEntry(m_navCmdID);
 				m_navCmdID = null;
 			}
 
 		} catch (SubarchitectureComponentException e) {
-			m_component.logException(e);
+			getComponent().logException(e);
 		}
 
 	}
