e389356d38d8ec41dc983b1f77fa23d3531db8a1
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-09-14 11:35:59 +0000

    moved from motivation to execution

---
+++ code/subarchitectures/execution.sa/trunk/src/java/execution/util/NonBlockingActionExecutor.java
@@ -0,0 +1,61 @@
+package execution.util;
+
+import cast.architecture.ManagedComponent;
+import execution.slice.Action;
+import execution.slice.TriBool;
+
+/**
+ * Helper class to simplify writing a non-blocking subclass of
+ * {@link ActionExecutor}. This contains a field to store the callback and
+ * handles error cases.
+ * 
+ * @author nah
+ * 
+ */
+public abstract class NonBlockingActionExecutor<ActionType extends Action>
+		extends ComponentActionExecutor {
+
+	private ExecutionCompletionCallback m_callback;
+	private final Class<ActionType> m_actCls;
+
+	public NonBlockingActionExecutor(ManagedComponent _component, Class<ActionType> _actCls) {
+		super(_component);
+		m_actCls = _actCls;
+	}
+
+	protected abstract boolean acceptAction(ActionType _action);
+
+	@Override
+	public boolean accept(Action _action) {
+		return acceptAction(m_actCls.cast(_action));
+	}
+
+	@Override
+	public TriBool execute() {
+		throw new RuntimeException("Blocking action should not be called");
+	}
+
+	@Override
+	public boolean isBlockingAction() {
+		return false;
+	}
+
+	@Override
+	public void execute(ExecutionCompletionCallback _callback) {
+		m_callback = _callback;
+		executeAction();
+	}
+
+	public abstract void executeAction();
+
+	/**
+	 * Should be called when execution has completed. Result is passed back to
+	 * callback.
+	 * 
+	 * @param _success
+	 */
+	protected void executionComplete(TriBool _success) {
+		m_callback.executionComplete(_success);
+	}
+
+}
