25389ba764d70db519368d832836a4ff8cf67c22
pronobis <pronobis@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-09-16 16:46:58 +0000

    .

--- code/subarchitectures/default.sa/trunk/src/c++/def/components/Tester/Tester.cc
+++ code/subarchitectures/default.sa/trunk/src/c++/def/components/Tester/Tester.cc
@@ -6,9 +6,6 @@
 
 // Default.SA
 #include "Tester.h"
-#include "TesterDialog.h"
-// Qt
-#include <QApplication>
 
 
 /** The function called to create a new instance of our component. */
@@ -24,6 +21,7 @@ namespace def
 {
 
 using namespace std;
+using namespace cast;
 using namespace DefaultData;
 
 
@@ -33,21 +31,19 @@ void Tester::configure(const map<string,string> & _config)
 	map<string,string>::const_iterator it;
 
 	// Forward chainer server name
-	if((it = _config.find("--test-hfc")) != _config.end())
+	if((it = _config.find("--hfcserver")) != _config.end())
 	{
 		_hfcServerName = it->second;
 	}
 
 	// QueryHandler name
-	if((it = _config.find("--test-qh")) != _config.end())
+	if((it = _config.find("--queryhandler")) != _config.end())
 	{
 		_queryHandlerName = it->second;
 	}
 
 	log("Configuration parameters:");
-	log("-> Test HFCServer: %s", (_hfcServerName.empty())?"No":"Yes");
 	log("-> HFCServer Name: %s", _hfcServerName.c_str());
-	log("-> Test QueryHandler: %s", (_queryHandlerName.empty())?"No":"Yes");
 	log("-> QueryHandler Name: %s", _queryHandlerName.c_str());
 }
 
@@ -55,33 +51,31 @@ void Tester::configure(const map<string,string> & _config)
 // -------------------------------------------------------
 void Tester::start()
 {
-	// Get the forward chainer interface proxy
-	if (!_hfcServerName.empty())
-		_hfcInterfacePrx = getIceServer<DefaultData::HFCInterface>(_hfcServerName);
+	// Get the HFCServer interface proxy
+	try
+	{
+		_hfcInterfacePrx =
+				getIceServer<DefaultData::HFCInterface>(_hfcServerName);
+		_hfcServerAvailable = true;
+	}
+	catch (CASTException e)
+	{}
+
 	// Get the QueryHandler interface proxy
-	if (!_queryHandlerName.empty())
-		_queryHandlerServerInterfacePrx= getIceServer<DefaultData::QueryHandlerServerInterface>(
-				_queryHandlerName);
+	try
+	{
+		_queryHandlerServerInterfacePrx =
+				getIceServer<DefaultData::QueryHandlerServerInterface>(_queryHandlerName);
+		_queryHandlerAvailable = true;
+	}
+	catch (CASTException e)
+	{}
 }
 
 
 // -------------------------------------------------------
 void Tester::runComponent()
 {
-	// Create application
-	_qApp = new QApplication(0,0);
-
-	// Start dialog
-	_dialog = new TesterDialog(this, !_hfcServerName.empty(), !_queryHandlerName.empty());
-	_dialog->exec();
-
-	// Thread safe delete
-	TesterDialog *dialog=_dialog;
-	QApplication *app=_qApp;
-	_dialog=0;
-	delete dialog;
-	_qApp=0;
-	delete app;
 }
 
 
@@ -92,11 +86,26 @@ void Tester::stop()
 
 
 // -------------------------------------------------------
-QdlQueryResults Tester::sendHFCQuery(std::string query)
+QdlQueryResults Tester::sendHFCServerQuery(const std::string &query) const
 {
-	println("Sending query to HFCServer %s", query.c_str());
-	return _hfcInterfacePrx->querySelect(query);
+	if (_hfcServerAvailable)
+		return _hfcInterfacePrx->querySelect(query);
+	else
+		return QdlQueryResults();
 }
+
+
+// -------------------------------------------------------
+ProbabilityDistribution Tester::sendQueryHandlerQuery(const std::string &query) const
+{
+	if (_queryHandlerAvailable)
+		return _queryHandlerServerInterfacePrx->query(query);
+	else
+		return ProbabilityDistribution();
+}
+
+
+
 
 
 
