5fca0c10dc23f19e49ee9f195594af72beef2fff
grettonc <grettonc@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-09-17 10:37:09 +0000

    Repaired syntax error.
     - Charles

--- subarchitectures/planner.sa/trunk/src/c++/dtplanner/dtpddl/LINALG/orthogonal.hpp
+++ subarchitectures/planner.sa/trunk/src/c++/dtplanner/dtpddl/LINALG/orthogonal.hpp
@@ -1,6 +1,6 @@
 /** \file orthogonal.hpp \brief The gmres algorithm. -*- c++ -*- */
 
-/          Copyright Gunter Winkler 2004 - 2007.
+//          Copyright Gunter Winkler 2004 - 2007.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
