a0f6ff20871bc1552516df89a03693b953d04d52
zender <zender@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-09-17 11:55:12 +0000

    fixed wmentry display for coma rooms without proper category

--- code/subarchitectures/coma.sa/trunk/src/java/castutils/viewer/plugins/ComaRoomInfo.java
+++ code/subarchitectures/coma.sa/trunk/src/java/castutils/viewer/plugins/ComaRoomInfo.java
@@ -18,11 +18,15 @@ public class ComaRoomInfo implements Plugin {
 		extraInfo.add("roomId=" + r.roomId);
 		extraInfo.add("seedPlaceInstance=" + r.seedPlaceInstance);
 		helperString = "categories=( ";
-		for (JointProbabilityValue jpv : r.categories.massFunction) {
-			StringRandomVariableValue rvv = (StringRandomVariableValue) jpv.variableValues[0];
-			String c = rvv.value;
-			float prob_c = jpv.probability;
-			helperString = helperString + c + "(p=" + prob_c + ") ";
+		if (r.categories!=null) {
+			if (r.categories.massFunction!=null) {
+				for (JointProbabilityValue jpv : r.categories.massFunction) {
+					StringRandomVariableValue rvv = (StringRandomVariableValue) jpv.variableValues[0];
+					String c = rvv.value;
+					float prob_c = jpv.probability;
+					helperString = helperString + c + "(p=" + prob_c + ") ";
+				}
+			}
 		}
 		extraInfo.add(helperString + ")");
 		helperString = "places=( ";
