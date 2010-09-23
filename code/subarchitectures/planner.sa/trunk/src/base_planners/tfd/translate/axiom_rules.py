e933f3d0a9a1bdb57e1bb8b8fdc2240592f09675
goebelbe <goebelbe@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-09-23 14:03:59 +0000

    Fix for tfd

--- code/subarchitectures/planner.sa/trunk/src/base_planners/tfd/translate/axiom_rules.py
+++ code/subarchitectures/planner.sa/trunk/src/base_planners/tfd/translate/axiom_rules.py
@@ -179,8 +179,10 @@ def negate(axioms):
     result = [pddl.PropositionalAxiom(axioms[0].name, [], axioms[0].effect.negate())]
     for axiom in axioms:
         condition = axiom.condition
-        assert len(condition) > 0, "Negated axiom impossible; cannot deal with that"
-        if len(condition) == 1: # Handle easy special case quickly.
+        #assert len(condition) > 0, "Negated axiom impossible; cannot deal with that"
+        if len(condition) == 0:
+            return []
+        elif len(condition) == 1: # Handle easy special case quickly.
             new_literal = condition[0].negate()
             for result_axiom in result:
                 result_axiom.condition.append(new_literal)
