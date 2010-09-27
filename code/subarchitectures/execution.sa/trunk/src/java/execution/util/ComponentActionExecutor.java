3e98ea233bf8479e9e5c42ed9b3b1d0591243dbe
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-09-27 08:05:49 +0000

    added methods to superclass

--- code/subarchitectures/execution.sa/trunk/src/java/execution/util/ComponentActionExecutor.java
+++ code/subarchitectures/execution.sa/trunk/src/java/execution/util/ComponentActionExecutor.java
@@ -6,6 +6,30 @@ public abstract class ComponentActionExecutor implements ActionExecutor {
 
 	private final ManagedComponent m_component;
 	
+	public void debug(Object _o, Throwable _t) {
+		m_component.debug(_o, _t);
+	}
+
+	public void debug(Object _o) {
+		m_component.debug(_o);
+	}
+
+	public void log(Object _o, Throwable _t) {
+		m_component.log(_o, _t);
+	}
+
+	public void logException(Object _message, Throwable _t) {
+		m_component.logException(_message, _t);
+	}
+
+	public void logException(Throwable _t) {
+		m_component.logException(_t);
+	}
+
+	public void println(Object _o, Throwable _t) {
+		m_component.println(_o, _t);
+	}
+
 	public ComponentActionExecutor(ManagedComponent _component) {
 		m_component = _component;
 	}
@@ -13,5 +37,18 @@ public abstract class ComponentActionExecutor implements ActionExecutor {
 	protected ManagedComponent getComponent() {
 		return m_component;
 	}
+
+	protected void log(Object _o) {
+		m_component.log(_o);
+	}
 	
+	protected void println(Object _o) {
+		m_component.println(_o);
+	}
+
+
+
+
 }
+
+
