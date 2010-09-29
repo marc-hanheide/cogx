bd0edc72c528c934b58223c0b0ab7136f985f79a
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-09-29 11:06:48 +0000

    messing with data types

--- code/subarchitectures/execution.sa/trunk/src/java/execution/util/DoNothingExecutor.java
+++ code/subarchitectures/execution.sa/trunk/src/java/execution/util/DoNothingExecutor.java
@@ -29,6 +29,7 @@ public class DoNothingExecutor extends NonBlockingCompleteOnOperationExecutor<Ac
 
 	@Override
 	public void executeAction() {
+		getComponent().println("Doing nothing for 3 seconds. I hope that's OK");
 		new CompleteAction(3000).start();
 	}
 
