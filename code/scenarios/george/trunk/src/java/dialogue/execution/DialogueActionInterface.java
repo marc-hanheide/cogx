e7c6d82f09e6431188cec8ee21d1c5348cf7aee2
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-09-30 08:16:10 +0000

    loop closed in highly simulated environment

--- code/scenarios/george/trunk/src/java/dialogue/execution/DialogueActionInterface.java
+++ code/scenarios/george/trunk/src/java/dialogue/execution/DialogueActionInterface.java
@@ -1,32 +1,160 @@
 package dialogue.execution;
 
+import javax.swing.JOptionPane;
+
+import cast.CASTException;
+import cast.ConsistencyException;
+import cast.DoesNotExistOnWMException;
+import cast.PermissionException;
+import cast.UnknownSubarchitectureException;
 import cast.architecture.ManagedComponent;
+import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
+import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
+import eu.cogx.beliefs.slice.GroundedBelief;
+import execution.slice.TriBool;
 import execution.slice.actions.AskForColour;
 import execution.slice.actions.AskForIdentity;
 import execution.slice.actions.AskForShape;
-import execution.util.DoNothingActionExecutorFactory;
+import execution.slice.actions.SingleBeliefAction;
+import execution.util.BlockingActionExecutor;
+import execution.util.ComponentActionFactory;
 import execution.util.LocalActionStateManager;
 
-
 /**
- * Receives actions from the execution system and interfaces with the rest of the dialogue system.
+ * Receives actions from the execution system and interfaces with the rest of
+ * the dialogue system.
  * 
  * @author nah
- *
+ * 
  */
 public class DialogueActionInterface extends ManagedComponent {
 	private LocalActionStateManager m_actionStateManager;
 
-	
+	public static class DirectColourAnswer extends
+			BlockingActionExecutor<AskForColour> {
+
+		public DirectColourAnswer(ManagedComponent _component) {
+			super(_component, AskForColour.class);
+		}
+
+		@Override
+		public TriBool execute() {
+			TriBool result = TriBool.TRIFALSE;
+			try {
+				((DialogueActionInterface)getComponent()).askForFeatureThenSetDirect("color",getAction());
+				result = TriBool.TRITRUE;
+
+			} catch (CASTException e) {
+				logException(e);
+			}
+
+			return result;
+		}
+
+	}
+
+	public static class DirectShapeAnswer extends
+			BlockingActionExecutor<AskForShape> {
+
+		public DirectShapeAnswer(ManagedComponent _component) {
+			super(_component, AskForShape.class);
+		}
+
+		@Override
+		public TriBool execute() {
+
+			TriBool result = TriBool.TRIFALSE;
+			try {
+				((DialogueActionInterface)getComponent()).askForFeatureThenSetDirect("shape",getAction());
+				result = TriBool.TRITRUE;
+
+			} catch (CASTException e) {
+				logException(e);
+			}
+
+			return result;
+
+		}
+
+	}
+
+	public static class DirectIdentityAnswer extends
+			BlockingActionExecutor<AskForIdentity> {
+
+		public DirectIdentityAnswer(ManagedComponent _component) {
+			super(_component, AskForIdentity.class);
+		}
+
+		@Override
+		public TriBool execute() {
+
+			TriBool result = TriBool.TRIFALSE;
+			try {
+				((DialogueActionInterface)getComponent()).askForFeatureThenSetDirect("ident",getAction());
+				result = TriBool.TRITRUE;
+
+			} catch (CASTException e) {
+				logException(e);
+			}
+
+			return result;
+
+		}
+
+	}
+
+	private void addFeatureDirectly(SingleBeliefAction _action, String _feature,
+			String _value) throws DoesNotExistOnWMException,
+			ConsistencyException, PermissionException,
+			UnknownSubarchitectureException {
+		GroundedBelief belief = getMemoryEntry(_action.beliefAddress,
+				GroundedBelief.class);
+		CASTIndependentFormulaDistributionsBelief<GroundedBelief> pb = CASTIndependentFormulaDistributionsBelief
+				.create(GroundedBelief.class, belief);
+
+		FormulaDistribution fd = FormulaDistribution.create();
+		fd.add(_value, 1);
+		pb.getContent().put(_feature, fd);
+		overwriteWorkingMemory(_action.beliefAddress, pb.get());
+	}
+
+	private static String askForFeatureValue(String _feature) {
+		return (String) JOptionPane.showInputDialog(null, "What " + _feature
+				+ " is the object?");
+	}
+
+	/**
+	 * @throws DoesNotExistOnWMException
+	 * @throws ConsistencyException
+	 * @throws PermissionException
+	 * @throws UnknownSubarchitectureException
+	 */
+	private void askForFeatureThenSetDirect(String _feature,
+			SingleBeliefAction _action) throws DoesNotExistOnWMException,
+			ConsistencyException, PermissionException,
+			UnknownSubarchitectureException {
+		addFeatureDirectly(_action, _feature, askForFeatureValue(_feature));
+	}
+
 	@Override
 	protected void start() {
 		m_actionStateManager = new LocalActionStateManager(this);
-		
-		DoNothingActionExecutorFactory derelictFactory = new DoNothingActionExecutorFactory(this);
-		
-		m_actionStateManager.registerActionType(AskForColour.class, derelictFactory);
-		m_actionStateManager.registerActionType(AskForShape.class, derelictFactory);
-		m_actionStateManager.registerActionType(AskForIdentity.class, derelictFactory);
-		
+
+		// DoNothingActionExecutorFactory derelictFactory = new
+		// DoNothingActionExecutorFactory(
+		// this);
+
+		m_actionStateManager.registerActionType(AskForColour.class,
+				new ComponentActionFactory<DirectColourAnswer>(this,
+						DirectColourAnswer.class));
+
+		m_actionStateManager.registerActionType(AskForShape.class,
+				new ComponentActionFactory<DirectShapeAnswer>(this,
+						DirectShapeAnswer.class));
+
+		m_actionStateManager.registerActionType(AskForIdentity.class,
+				new ComponentActionFactory<DirectIdentityAnswer>(this,
+						DirectIdentityAnswer.class));
+
 	}
 }
