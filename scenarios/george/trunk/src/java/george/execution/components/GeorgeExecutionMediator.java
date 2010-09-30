4782a740298f4eed776d68180a37c84a35ee5b44
Nick Hawes <nickh@robots.ox.ac.uk>
2010-09-30 08:16:10 +0000

    loop closed in highly simulated environment

--- scenarios/george/trunk/src/java/george/execution/components/GeorgeExecutionMediator.java
+++ scenarios/george/trunk/src/java/george/execution/components/GeorgeExecutionMediator.java
@@ -3,12 +3,18 @@ package george.execution.components;
 import autogen.Planner.Action;
 import cast.CASTException;
 import de.dfki.lt.tr.beliefs.data.formulas.WMPointer;
+import de.dfki.lt.tr.beliefs.data.genericproxies.GenericFormula;
+import de.dfki.lt.tr.beliefs.slice.logicalcontent.ElementaryFormula;
 import de.dfki.lt.tr.beliefs.slice.logicalcontent.dFormula;
 import execution.components.PlanExecutionMediator;
 import execution.slice.ActionExecutionException;
 import execution.slice.actions.AskForColour;
 import execution.slice.actions.AskForIdentity;
 import execution.slice.actions.AskForShape;
+import execution.slice.actions.BeliefPlusStringAction;
+import execution.slice.actions.LearnColour;
+import execution.slice.actions.LearnIdentity;
+import execution.slice.actions.LearnShape;
 import execution.slice.actions.SingleBeliefAction;
 import execution.util.ActionConverter;
 
@@ -38,84 +44,42 @@ public class GeorgeExecutionMediator extends PlanExecutionMediator implements
 		if (_plannedAction.name.equals("ask-for-an-objects-color-general")) {
 
 			assert _plannedAction.arguments.length == 2 : "ask-for-an-objects-color-general is expected to be of artity 2";
-			AskForColour action = newActionInstance(AskForColour.class);
-			return beliefPointerToBeliefAction(_plannedAction.arguments[1],
-					action);
+			return createSingleBeliefAction(AskForColour.class,
+					_plannedAction.arguments[1]);
 		} else if (_plannedAction.name
 				.equals("ask-for-an-objects-shape-general")) {
 
 			assert _plannedAction.arguments.length == 2 : "ask-for-an-objects-shape-general is expected to be of artity 2";
-			AskForShape action = newActionInstance(AskForShape.class);
-			return beliefPointerToBeliefAction(_plannedAction.arguments[1],
-					action);
+			return createSingleBeliefAction(AskForShape.class,
+					_plannedAction.arguments[1]);
 		} else if (_plannedAction.name
 				.equals("ask-for-an-objects-ident-general")) {
 
 			assert _plannedAction.arguments.length == 2 : "ask-for-an-objects-ident-general is expected to be of artity 2";
-			AskForIdentity action = newActionInstance(AskForIdentity.class);
-			return beliefPointerToBeliefAction(_plannedAction.arguments[1],
-					action);
+			return createSingleBeliefAction(AskForIdentity.class,
+					_plannedAction.arguments[1]);
+		}
+		else if (_plannedAction.name.equals("learn-color")) {
+
+			assert _plannedAction.arguments.length == 3 : "learn-color is expected to be of artity 3";
+			return createBeliefPlusStringAction(LearnColour.class,
+					_plannedAction.arguments[1], _plannedAction.arguments[2]);
+
+		}
+		else if (_plannedAction.name.equals("learn-shape")) {
+
+			assert _plannedAction.arguments.length == 3 : "learn-shape is expected to be of artity 3";
+			return createBeliefPlusStringAction(LearnShape.class,
+					_plannedAction.arguments[1], _plannedAction.arguments[2]);
+
+		}		else if (_plannedAction.name.equals("learn-ident")) {
+
+			assert _plannedAction.arguments.length == 3 : "learn-ident is expected to be of artity 3";
+			return createBeliefPlusStringAction(LearnIdentity.class,
+					_plannedAction.arguments[1], _plannedAction.arguments[2]);
+
 		}
 
-		//
-		//
-		//
-		// if (_plannedAction.name.equals("move")) {
-		// assert _plannedAction.arguments.length == 2 :
-		// "move action arity is expected to be 2";
-		//
-		// GoToPlace act = newActionInstance(GoToPlace.class);
-		// WMPointer beliefPtr = WMPointer.create(_plannedAction.arguments[1]);
-		//
-		// // read the belief from WM
-		// IndependentFormulaDistributionsBelief<dBelief> placeUnion =
-		// IndependentFormulaDistributionsBelief
-		// .create(dBelief.class, getMemoryEntry(beliefPtr.getVal(),
-		// dBelief.class));
-		//
-		// Formula placeProperty = placeUnion.getContent().get("PlaceId")
-		// .getDistribution().firstValue();
-		// act.placeID = placeProperty.getInteger();
-		//
-		// return act;
-		// } else
-		// // if (_plannedAction.name.equals("look-for-object")) {
-		// // assert _plannedAction.arguments.length == 1 :
-		// // "look-for-object action arity is expected to be 1";
-		// //
-		// // DetectObjects act = newActionInstance(DetectObjects.class);
-		// // act.labels = DEFAULT_LABELS;
-		// // return act;
-		// // } else if (_plannedAction.name.equals("look-for-people")) {
-		// // assert _plannedAction.arguments.length == 1 :
-		// // "look-for-people action arity is expected to be 1";
-		// //
-		// // DetectPeople act = newActionInstance(DetectPeople.class);
-		// // return act;
-		// // // } else if (_plannedAction.name.equals("ask-for-placename")) {
-		// // // assert _plannedAction.arguments.length == 2 :
-		// // // "ask-for-feature action arity is expected to be 2";
-		// // // String beliefID = ((PointerValue)
-		// // // _plannedAction.arguments[1]).beliefId.id;
-		// // // String featureID = "name";
-		// // // ComsysQueryFeature act =
-		// // // newActionInstance(ComsysQueryFeature.class);
-		// // // act.beliefID = beliefID;
-		// // // act.featureID = featureID;
-		// // // return act;
-		// // // } else if (_plannedAction.name.equals("verify-placename")) {
-		// // // assert _plannedAction.arguments.length == 3 :
-		// // // "ask-for-feature action arity is expected to be 2";
-		// // // String beliefID = ((PointerValue)
-		// // // _plannedAction.arguments[1]).beliefId.id;
-		// // // String featureID = "name";
-		// // // ComsysTestFeatureValue act =
-		// // // newActionInstance(ComsysTestFeatureValue.class);
-		// // // act.beliefID = beliefID;
-		// // // act.featureType = featureID;
-		// // // act.featureValue = _plannedAction.arguments[2];
-		// // // return act;
-		// // }
 		throw new ActionExecutionException("No conversion available for: "
 				+ _plannedAction.fullName);
 	}
@@ -125,11 +89,28 @@ public class GeorgeExecutionMediator extends PlanExecutionMediator implements
 	 * @param action
 	 * @return
 	 */
-	private SingleBeliefAction beliefPointerToBeliefAction(dFormula _argument,
-			SingleBeliefAction action) {
+	private <T extends SingleBeliefAction> SingleBeliefAction beliefPointerToBeliefAction(
+			dFormula _argument, T action) {
 		WMPointer beliefPtr = WMPointer.create(_argument);
 		action.beliefAddress = beliefPtr.getVal();
 		return action;
+	}
+
+	private <T extends SingleBeliefAction> T createSingleBeliefAction(
+			Class<T> _cls, dFormula _belief) throws CASTException {
+		T action = newActionInstance(_cls);
+		return (T) beliefPointerToBeliefAction(_belief, action);
+	}
+
+	private <T extends BeliefPlusStringAction> T createBeliefPlusStringAction(
+			Class<T> _cls, dFormula _belief, dFormula _value)
+			throws CASTException {
+		T action = newActionInstance(_cls);
+		ElementaryFormula formula = (ElementaryFormula) _value;
+		GenericFormula<ElementaryFormula> elementaryFormula = GenericFormula
+				.create(ElementaryFormula.class, formula);
+		action.value = elementaryFormula.getProposition();
+		return (T) beliefPointerToBeliefAction(_belief, action);
 	}
 
 	@Override
