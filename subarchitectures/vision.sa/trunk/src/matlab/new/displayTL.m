b3dfb861aeffccce70fef8f9f11b6229dc0598af
matejk <matejk@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-01 10:18:27 +0000

    added tables to html presentation.

--- subarchitectures/vision.sa/trunk/src/matlab/new/displayTL.m
+++ subarchitectures/vision.sa/trunk/src/matlab/new/displayTL.m
@@ -12,11 +12,18 @@ if Settings.Disp.TL
    for sc=1:numSC
       fprintf(fid,'<u>%s:</u> <BR>\n',Coma.SCnames(sc,:));
       fprintf(fid,'SC%d ( numC: %d,   numComp: %2.1f,    conf: %d )<BR>\n',sc,getc(mC,sc,'numC'),getc(mC,sc,'numComp'),getc(mC,sc,'conf'));
-      fprintf(fid,'concepts: '); fprintf(fid,'%d ',getc(mC,sc,0,'name')); fprintf(fid,'<BR>\n');
-      fprintf(fid,'numComp : '); fprintf(fid,'%2.1f ',getc(mC,sc,0,'numComp')); fprintf(fid,'<BR>\n');
-      fprintf(fid,'conf    : '); fprintf(fid,'%d ',getc(mC,sc,0,'conf')); fprintf(fid,'<BR>\n');
-      fprintf(fid,'Fb      : '); fprintf(fid,'%d ',getc(mC,sc,0,'Fb')); fprintf(fid,'<BR>\n');
-      fprintf(fid,'<BR>\n');
+      
+      
+      fprintf(fid, '<table border="1"> <tr>') ;fprintf(fid,'\n');
+      fprintf(fid,'<td>concepts: </td>'); fprintf(fid,'<td>%d</td>',getc(mC,sc,0,'name')); %fprintf(fid,'<BR>\n');
+      fprintf(fid,'\n<tr>\n') ;
+      fprintf(fid,'<td>numComp :</td>'); fprintf(fid,'<td>%2.1f</td>',getc(mC,sc,0,'numComp')); %fprintf(fid,'<BR>\n');
+      fprintf(fid,'\n<tr>\n') ;
+      fprintf(fid,'<td>conf    :</td>'); fprintf(fid,'<td>%d</td>',getc(mC,sc,0,'conf')); %fprintf(fid,'<BR>\n');
+      fprintf(fid,'\n<tr>\n') ;
+      fprintf(fid,'<td>Fb      :</td>'); fprintf(fid,'<td>%d</td>',getc(mC,sc,0,'Fb')); %fprintf(fid,'<BR>\n');
+      fprintf(fid, '</table> ') ;
+%       fprintf(fid,'<BR>\n');
    end
    
    fclose(fid);
