09dfade9f4e10fbe2fa50eebfcb106c6609fd7fd
goebelbe <goebelbe@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-05 16:42:55 +0000

    Try again to fix the "ngeated axiom" problem in tfd

--- code/subarchitectures/planner.sa/trunk/src/base_planners/tfd/translate/axiom_rules.py
+++ code/subarchitectures/planner.sa/trunk/src/base_planners/tfd/translate/axiom_rules.py
@@ -181,7 +181,8 @@ def negate(axioms):
         condition = axiom.condition
         #assert len(condition) > 0, "Negated axiom impossible; cannot deal with that"
         if len(condition) == 0:
-            return [pddl.PropositionalAxiom(axioms[0].name, [], axioms[0].effect.negate())]
+            return []
+            #return [pddl.PropositionalAxiom(axioms[0].name, [], axioms[0].effect.negate())]
         elif len(condition) == 1: # Handle easy special case quickly.
             new_literal = condition[0].negate()
             for result_axiom in result:
