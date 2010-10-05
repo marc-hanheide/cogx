c6dd9b1ada67b2bf02470416f1ac9f2114b267cf
Marc Hanheide <marc@hanheide.net>
2010-10-05 13:51:05 +0000

    - creation of goals from intentions up and running for the very first time

--- scenarios/george/trunk/src/java/eu/cogx/goals/george/IntentionGoalGenerator.java
+++ scenarios/george/trunk/src/java/eu/cogx/goals/george/IntentionGoalGenerator.java
@@ -1,31 +1,34 @@
 package eu.cogx.goals.george;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import motivation.components.generators.AbstractIntentionMotiveGenerator;
 import motivation.slice.GeneralGoalMotive;
 import nu.xom.Document;
-import nu.xom.Element;
 import nu.xom.Node;
 import nu.xom.Nodes;
-import VisionData.VisualObject;
+
+import org.cognitivesystems.binder.POINTERLABEL;
+
 import autogen.Planner.Goal;
-import cast.UnknownSubarchitectureException;
+import cast.CASTException;
 import cast.cdl.WorkingMemoryAddress;
 import castutils.castextensions.IceXMLSerializer;
+import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
+import de.dfki.lt.tr.beliefs.data.formulas.WMPointer;
+import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
 import de.dfki.lt.tr.beliefs.slice.epstatus.AttributedEpistemicStatus;
 import de.dfki.lt.tr.beliefs.slice.intentions.Intention;
-import eu.cogx.beliefs.slice.GroundedBelief;
-import eu.cogx.perceptmediator.transferfunctions.abstr.SimpleDiscreteTransferFunction;
+import de.dfki.lt.tr.beliefs.slice.logicalcontent.ModalFormula;
+import de.dfki.lt.tr.beliefs.slice.logicalcontent.PointerFormula;
+import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
 
 public class IntentionGoalGenerator extends
 		AbstractIntentionMotiveGenerator<GeneralGoalMotive, Intention> {
 
-	public final static String XPATH_SELECT_FORMULAS = "/*[estatus/attribagents/string/text()='human']/content/values/values/*/val/forms";
-	public final static String XPATH_SELECT_REFERRED_OBJECT = "*[op/text()='LingRef']/form/prop/text()";
-	public final static String XPATH_SELECT_COLOR = "*[op/text()='Color']/form/prop/text()";
-	public final static String XPATH_SELECT_SHAPE = "*[op/text()='Shape']/form/prop/text()";
+	public final static String XPATH_SELECT_PRECONDITIONS = "/*/content/*/preconditions/forms/*";
+	public final static String XPATH_SELECT_POSTCONDITIONS = "/*/content/*/preconditions/forms/*";
+	public final static String XPATH_SELECT_POINTER_PREFIX = "form/pointer";
+	private final static String[] CONCEPTS_TO_PARSE = new String[] { "color",
+			"shape" };
 
 	/*
 	 * (non-Javadoc)
@@ -49,63 +52,73 @@ public class IntentionGoalGenerator extends
 			// ... which object is referred to (the GroundedBelief)
 			// ... which concept is given (Colour, Shape)
 			// ... have to check whether this is new information or not
+			Nodes preconditions = xmlDoc.query(XPATH_SELECT_PRECONDITIONS);
+			WorkingMemoryAddress preBelief = findAttributedPrecondition(preconditions);
 
-			Element attributedForms = (Element) queryFirst(xmlDoc,
-					XPATH_SELECT_FORMULAS);
-			if (attributedForms == null) {
-				getLogger().warn(
-						"unable to find expected formulas in "
-								+ xmlDoc.toString());
-				return null;
-			}
-			log("found belief with forms in it: " + attributedForms.toString());
-			String objectId = queryFirst(attributedForms,
-					XPATH_SELECT_REFERRED_OBJECT).getValue();
-			if (objectId == null) {
-				getLogger().warn(
-						"unable to referred object in formulas "
-								+ attributedForms.toString());
-				return null;
-			}
-
-			// HACK TO FIND A VALID OBJECT
-			List<GroundedBelief> entries = new ArrayList<GroundedBelief>();
+			CASTIndependentFormulaDistributionsBelief<dBelief> preAttributedBelief;
 			try {
-				getMemoryEntries(GroundedBelief.class, entries,"binder");
-				for (GroundedBelief belief : entries) {
-					if (belief.type.equals(SimpleDiscreteTransferFunction
-							.getBeliefTypeFromCastType(VisualObject.class))) {
-						objectId = belief.id;
-						log("found an object grounded belief to fake reference resolution");
-					}
+				preAttributedBelief = CASTIndependentFormulaDistributionsBelief
+						.create(dBelief.class, getMemoryEntry(preBelief,
+								dBelief.class));
+				String concept = null;
+				for (String c : CONCEPTS_TO_PARSE) {
+					if (preAttributedBelief.getContent().get(c) != null)
+						concept = c;
 				}
-			} catch (UnknownSubarchitectureException e) {
+				if (concept==null) {
+					getLogger().warn("the concept in the attributed belief was none of the ones we know about.");
+					return null;
+				}
+				log("inferred concept from attributed belief: " + concept);
+				FormulaDistribution aboutPointerFD= preAttributedBelief.getContent().get(
+						POINTERLABEL.value);
+				if (aboutPointerFD==null) {
+					getLogger().warn("couldn't find any referring pointer, most likelz ref resolution didn't work");
+					return null;
+				}
+				WMPointer referredPrivate = WMPointer
+						.create(preAttributedBelief.getContent().get(
+								POINTERLABEL.value).getDistribution()
+								.getMostLikely().get());
+				log("got referred private belief: "
+						+ referredPrivate.getVal().id);
+
+				GeneralGoalMotive goal = new GeneralGoalMotive();
+				super.fillDefault(goal);
+				goal.referenceEntry = addr;
+				goal.goal = new Goal(-1, "(" + concept.toLowerCase()
+						+ "-learned '" + referredPrivate.getVal().id + ")",
+						false);
+				log("goal generated: " + goal.goal.goalString);
+				return goal;
+			} catch (CASTException e) {
 				logException(e);
 			}
-			// END OF HACK
 
-			String concept = "";
-			String prop = "";
+		}
+		return null;
+	}
 
-			Node color = queryFirst(attributedForms, XPATH_SELECT_COLOR);
-			Node shape = queryFirst(attributedForms, XPATH_SELECT_SHAPE);
-			if (color != null) {
-				concept = "Color";
-				prop = color.getValue();
-			} else if (shape != null) {
-				concept = "Shape";
-				prop = shape.getValue();
+	private WorkingMemoryAddress findAttributedPrecondition(Nodes preconditions) {
+		for (int i = 0; i < preconditions.size(); i++) {
+			Node n = preconditions.get(i);
+			ModalFormula mf = IceXMLSerializer.fromXMLString(n.toXML(),
+					ModalFormula.class);
+			if (!mf.op.equals("belief"))
+				continue;
+			WorkingMemoryAddress pf = ((PointerFormula) mf.form).pointer;
+			CASTIndependentFormulaDistributionsBelief<dBelief> preAttributedBelief;
+			try {
+				preAttributedBelief = CASTIndependentFormulaDistributionsBelief
+						.create(dBelief.class, getMemoryEntry(pf,
+								dBelief.class));
+				if (preAttributedBelief.isAttributed()) {
+					return pf;
+				}
+			} catch (CASTException e) {
+				logException(e);
+				return null;
 			}
-			log("attributed information to generate goal for concept "
-					+ concept + ", prop= " + prop);
-
-			GeneralGoalMotive goal = new GeneralGoalMotive();
-			super.fillDefault(goal);
-			goal.referenceEntry = addr;
-			goal.goal = new Goal(-1, "(been-used-for-learning-sample '"
-					+ objectId + "' " + concept + ")", false);
-			log("goal generated: " + goal.goal.goalString);
-			return goal;
 		}
 		return null;
 	}
@@ -127,9 +140,6 @@ public class IntentionGoalGenerator extends
 		motive.costs = -1;
 		motive.informationGain = 1;
 
-		motive.goal = new Goal(computeImportance(motive),
-				"(been-used-for-learning-sample '" + "DUMMY" + "' "
-						+ "CONCEPT_TO_BE_INFERRED_FROM_INTENTION" + ")", false);
 		log("updated goal to " + motive.goal.goalString);
 		return motive;
 	}
