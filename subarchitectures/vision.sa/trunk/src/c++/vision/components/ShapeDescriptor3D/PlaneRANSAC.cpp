d607aa171360f6ca576b0e92135d9d334f23586c
michaelz <michaelz@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-05 21:29:22 +0000

    added option to find only planes where inliers form a connected region, thus avoiding accidental inliers.

--- subarchitectures/vision.sa/trunk/src/c++/vision/components/ShapeDescriptor3D/PlaneRANSAC.cpp
+++ subarchitectures/vision.sa/trunk/src/c++/vision/components/ShapeDescriptor3D/PlaneRANSAC.cpp
@@ -5,30 +5,51 @@
 
 #include <cassert>
 #include <fstream>
+#include <highgui.h>
 #include "cogxmath.h"
+#include "AccessImage.h"
 #include "PlaneRANSAC.h"
 
 namespace cast
 {
+
+//#define PLANE_RANSAC_DEBUG
 
 using namespace std;
 using namespace cogx;
 using namespace cogx::Math;
 using namespace VisionData;
 
-PlaneRANSAC::PlaneRANSAC(double _thr, int _minPoints, int _maxIter)
+PlaneRANSAC::PlaneRANSAC(double _thr, bool connectedRegions, int _minPoints, int _maxIter)
 {
+  findConnectedRegions = connectedRegions;
   inlierThr = _thr;
   // TODO: get rid of this arbitrary tuning parameter
   maxIter = _maxIter;
   eta0 = 0.01;
   // TODO: get rid of this arbitrary tuning parameter
   minSupportPoints = _minPoints;
+  if(findConnectedRegions)
+  {
+    // HACK: this should be set from nearest neighbour distance, e.g. mean + 3 sigma
+    gridSize = 0.005;
+    voteImg = cvCreateImage(cvSize(400, 400), IPL_DEPTH_32F, 1);
+    maskImg = cvCreateImage(cvSize(voteImg->width+2, voteImg->height+2), IPL_DEPTH_8U, 1);
+  }
 
   assert(inlierThr > 0.);
   assert(maxIter > 0);
   assert(eta0 > 0.);
   assert(minSupportPoints > 0);
+}
+
+PlaneRANSAC::~PlaneRANSAC()
+{
+  if(findConnectedRegions)
+  {
+    cvReleaseImage(&voteImg);
+    cvReleaseImage(&maskImg);
+  }
 }
 
 // HACK: collect statistics for samples
@@ -127,7 +148,7 @@ bool PlaneRANSAC::selectSampleNear(const SurfacePointSeq &points, int nPoints,
   {
     double d, d_min = HUGE_VAL;
     int j_min = j;
-    for(int l = 0; l < 100; l++)
+    for(int l = 0; l < 10; l++)
     {
       j = rand()%nPoints;
       if(j != i)
@@ -149,7 +170,7 @@ bool PlaneRANSAC::selectSampleNear(const SurfacePointSeq &points, int nPoints,
   {
     double d, d_min = HUGE_VAL;
     int k_min = k;
-    for(int l = 0; l < 100; l++)
+    for(int l = 0; l < 10; l++)
     {
       k = rand()%nPoints;
       if(k != i && k != j)
@@ -183,9 +204,17 @@ bool PlaneRANSAC::selectSampleNear(const SurfacePointSeq &points, int nPoints,
  * @returns true if model could be computed (point were not in a degenerate position)
  */
 bool PlaneRANSAC::computeModel(const Vector3 &p, const Vector3 &q,
-    const Vector3 &r, Plane3 &plane)
+    const Vector3 &r, PlanarPatch &plane)
 {
-  return planeFromPoints(plane, p, q, r);
+  if(planeFromPoints(plane.plane, p, q, r))
+  {
+    // any point can be origin, take p
+    plane.pose.pos = p;
+    definePlaneAxes(plane.plane, plane.pose.rot);
+    return true;
+  }
+  else
+    return false;
 }
 
 /**
@@ -202,14 +231,14 @@ bool PlaneRANSAC::computeModel(const Vector3 &p, const Vector3 &q,
  * @return mean relative fit error
  */
 double PlaneRANSAC::fitError(const SurfacePointSeq &points, int nPoints,
-    const Plane3 &plane, int &nInliers)
+    const PlanarPatch &plane, int &nInliers)
 { 
   double sumErr = 0.;
   nInliers = 0;
   // note: using MSAC, i.e. summing errors instead of counting inliers
   for(int i = 0; i < nPoints; i++)
   {
-    double err = fabs(distPointToPlane(points[i].p, plane));
+    double err = fabs(distPointToPlane(points[i].p, plane.plane));
     if(err >= inlierThr)
       err = inlierThr;
     else
@@ -218,6 +247,171 @@ double PlaneRANSAC::fitError(const SurfacePointSeq &points, int nPoints,
   }
   sumErr /= (double)nPoints;
   return sumErr;
+}
+
+/**
+ * Fill vote image with 3D points projected on the plane and find connected
+ * region centered around projected plane origin.
+ * @return average error of points inside the connected region
+ */
+double PlaneRANSAC::vote(const SurfacePointSeq &points, int nPoints,
+    const PlanarPatch &plane, int &nInliers, bool saveImages)
+{
+  cvSet(voteImg, cvScalarAll(0));
+  cvSet(maskImg, cvScalarAll(0));
+  Video::AccessBwImageFloat grid(voteImg);
+  CvPoint seed = cvPoint(voteImg->width/2,
+                         voteImg->height/2);
+  for(int i = 0; i < nPoints; i++)
+  {
+    Vector3 q = transformInverse(plane.pose, points[i].p);
+    double err = fabs(distPointToPlane(points[i].p, plane.plane))/inlierThr;
+    int x = voteImg->width/2 + lrintf(q.x/gridSize);
+    int y = voteImg->height/2 + lrintf(q.y/gridSize);
+    // inliers (err < 1) have votes > 1 and <= 2, outliers (err >= 1) don't vote
+    if(err < 1.)
+      if(x >= 0 && x < voteImg->width && y >= 0 && y < voteImg->height)
+        grid[y][x] = 2. - err;
+  }
+#ifdef PLANE_RANSAC_DEBUG
+  if(saveImages)
+    cvSaveImage("ransac-vote-1.png", voteImg);
+#endif
+  CvConnectedComp connRegion;
+  cvFloodFill(
+      voteImg,
+      seed,
+      cvScalarAll(2.),  // fill value does not matter actually
+      cvScalarAll(1.),  // low diff
+      cvScalarAll(1.),  // up diff
+      &connRegion,
+      8 | CV_FLOODFILL_FIXED_RANGE | CV_FLOODFILL_MASK_ONLY,  // flags: 8 connectivity plus flags
+      maskImg
+      );
+  nInliers = (int)connRegion.area;
+  
+#ifdef PLANE_RANSAC_DEBUG
+  if(saveImages)
+  {
+    cvSaveImage("ransac-vote-2.png", voteImg);
+    cvSaveImage("ransac-mask.png", maskImg);
+    Video::AccessBwImage maskGrid(maskImg);
+    // note: mask image has a border of 1 pixel
+    printf("seed: %d %d = %lf / %d, ransc conn region area %lf, avg val %lf\n",
+      seed.x, seed.y, grid[seed.y][seed.x], maskGrid[seed.y+1][seed.x+1],
+      connRegion.area, 2. - connRegion.value.val[0]);
+  }
+#endif
+  return 2. - connRegion.value.val[0];
+}
+
+/**
+ * Returns the fit error of a connected region. This is meant to avoid accidental
+ * support from isolated points.
+ */
+double PlaneRANSAC::fitErrorConnectedRegion(const SurfacePointSeq &points, int nPoints,
+    const PlanarPatch &plane, int &nInliers)
+{
+  double avgErr = vote(points, nPoints, plane, nInliers);
+  return (nPoints - nInliers*avgErr)/nPoints;
+}
+
+void PlaneRANSAC::findConvexHull(const SurfacePointSeq &points, int nPoints,
+    PlanarPatch &plane, double thrFactor)
+{
+  std::vector<CvPoint2D32f> planePoints;
+  for(int j = 0; j < nPoints; j++)
+  {
+    if(fabs(distPointToPlane(points[j].p, plane.plane)) < inlierThr*thrFactor)
+    {
+      plane.surfPoints.push_back(points[j]);
+      Vector3 q = transformInverse(plane.pose, points[j].p);
+      planePoints.push_back(cvPoint2D32f(q.x, q.y));
+    }
+  }
+
+  int *hull = (int*)malloc(planePoints.size() * sizeof(hull[0]));
+  CvMat hullMat = cvMat(1, planePoints.size(), CV_32SC1, hull);
+  CvMat pointMat = cvMat(1, planePoints.size(), CV_32FC2, &planePoints[0]);
+  cvConvexHull2(&pointMat, &hullMat, CV_COUNTER_CLOCKWISE, 0);
+
+  for(int j = 0; j < hullMat.cols; j++)
+  {
+    Vector3 q = vector3(planePoints[hull[j]].x, planePoints[hull[j]].y, 0.);
+    plane.convexHull.push_back(transform(plane.pose, q));
+    plane.projectedConvexHull.push_back(vector2(q.x, q.y));
+  }
+
+  free(hull);
+}
+
+void PlaneRANSAC::findConvexHullConnectedRegion(const SurfacePointSeq &points, int nPoints,
+    PlanarPatch &plane)
+{
+  Video::AccessBwImage maskGrid(maskImg);
+  std::vector<CvPoint2D32f> planePoints;
+  int nInliers;
+
+  vote(points, nPoints, plane, nInliers);
+
+  for(int i = 0; i < nPoints; i++)
+  {
+    if(fabs(distPointToPlane(points[i].p, plane.plane)) < inlierThr)
+    {
+      Vector3 q = transformInverse(plane.pose, points[i].p);
+      int x = voteImg->width/2 + lrintf(q.x/gridSize);
+      int y = voteImg->height/2 + lrintf(q.y/gridSize);
+      // note: mask image has a border of 1 pixel
+      if(maskGrid[y+1][x+1] == 1)
+      {
+        plane.surfPoints.push_back(points[i]);
+        Vector3 q = transformInverse(plane.pose, points[i].p);
+        planePoints.push_back(cvPoint2D32f(q.x, q.y));
+      }
+    }
+  }
+
+  int *hull = (int*)malloc(planePoints.size() * sizeof(hull[0]));
+  CvMat hullMat = cvMat(1, planePoints.size(), CV_32SC1, hull);
+  CvMat pointMat = cvMat(1, planePoints.size(), CV_32FC2, &planePoints[0]);
+  cvConvexHull2(&pointMat, &hullMat, CV_COUNTER_CLOCKWISE, 0);
+
+  for(int i = 0; i < hullMat.cols; i++)
+  {
+    Vector3 q = vector3(planePoints[hull[i]].x, planePoints[hull[i]].y, 0.);
+    plane.convexHull.push_back(transform(plane.pose, q));
+    plane.projectedConvexHull.push_back(vector2(q.x, q.y));
+  }
+
+  free(hull);
+}
+
+/**
+ * Removes inliers from points.
+ * To pe precise: inliers are moved to end of points array and n decreased
+ * accordingly. So after call to this function points[0..n-1] are all outliers.
+ */
+void PlaneRANSAC::removeInliers(SurfacePointSeq &points, int &n,
+    const PlanarPatch &plane, double thrFactor)
+{
+  SurfacePoint t;
+  for(int i = 0; i < n; )
+  {
+    // if inlier
+    if(plane.isPointInside(points[i].p) &&
+       fabs(distPointToPlane(points[i].p, plane.plane)) < inlierThr*thrFactor)
+    {
+      // move point i to end of points and decrease n
+      t = points[i];
+      points[i] = points[n - 1];
+      points[n - 1] = t;
+      n--;
+    }
+    else
+    {
+      i++;
+    }
+  }
 }
 
 /**
@@ -234,7 +428,7 @@ double PlaneRANSAC::fitError(const SurfacePointSeq &points, int nPoints,
  * @returns true if any plane could be found
  */
 bool PlaneRANSAC::detectPlane(SurfacePointSeq &points, int &nPoints, const Vector3 &up,
-    bool horizontal, Plane3 &plane)
+    bool horizontal, PlanarPatch &plane)
 {
   // current estimate of inlier ratio, initial value
   double eps = 2./(double)nPoints;
@@ -255,14 +449,22 @@ bool PlaneRANSAC::detectPlane(SurfacePointSeq &points, int &nPoints, const Vecto
   while(pow(1. - pow(eps, 2.), (double)k) >= eta0 && k < maxIter)
   {
     Vector3 p, q, r;
-    if(selectSample(points, nPoints, up, horizontal, p, q, r))
-    //if(selectSampleNear(points, nPoints, p, q, r))
+    bool gotSample;
+    if(findConnectedRegions)
+      gotSample = selectSampleNear(points, nPoints, p, q, r);
+    else
+      gotSample = selectSample(points, nPoints, up, horizontal, p, q, r);
+    if(gotSample)
     {
-      Plane3 planeHyp;
+      PlanarPatch planeHyp;
       if(computeModel(p, q, r, planeHyp))
       {
         int nInliers = 0;
-        double error = fitError(points, nPoints, planeHyp, nInliers);
+        double error;
+        if(findConnectedRegions)
+          error = fitErrorConnectedRegion(points, nPoints, planeHyp, nInliers);
+        else
+          error = fitError(points, nPoints, planeHyp, nInliers);
         if(error < minError)
         {
           minError = error;
@@ -284,6 +486,14 @@ bool PlaneRANSAC::detectPlane(SurfacePointSeq &points, int &nPoints, const Vecto
   {
     // TODO: find least squares solution (and update inliers)
     // RANSAC_optimiseSolution();
+    
+    if(findConnectedRegions)
+      findConvexHullConnectedRegion(points, nPoints, plane);
+    else
+      findConvexHull(points, nPoints, plane);
+    // HACK
+    /*int nIn;
+    vote(points, nPoints, plane, nIn, true);*/
     return true;
   }
   else
--- subarchitectures/vision.sa/trunk/src/c++/vision/components/ShapeDescriptor3D/PlaneRANSAC.h
+++ subarchitectures/vision.sa/trunk/src/c++/vision/components/ShapeDescriptor3D/PlaneRANSAC.h
@@ -7,7 +7,9 @@
 #define PLANE_RANSAC_H
 
 #include <vector>
+#include <cv.h>
 #include <VisionData.hpp>
+#include "PlanarPatch.h"
 
 namespace cast
 {
@@ -33,15 +35,23 @@ private:
    * need at least this many points to accept a plane hypothesis
    */
   int minSupportPoints;
+  bool findConnectedRegions;
+  IplImage *voteImg;
+  IplImage *maskImg;
+  float gridSize;
 
   bool selectSample(const SurfacePointSeq &points, int n,
     const Vector3 &up, bool horizontal, Vector3 &p, Vector3 &q, Vector3 &r);
   bool selectSampleNear(const SurfacePointSeq &points, int nPoints,
       Vector3 &p, Vector3 &q, Vector3 &r);
   bool computeModel(const Vector3 &p, const Vector3 &q,
-    const Vector3 &r, Plane3 &plane);
+    const Vector3 &r, PlanarPatch &plane);
   double fitError(const SurfacePointSeq &points, int n,
-    const Plane3 &plane, int &nInliers);
+    const PlanarPatch &plane, int &nInliers);
+  double fitErrorConnectedRegion(const SurfacePointSeq &points, int nPoints,
+    const PlanarPatch &plane, int &nInliers);
+  double vote(const SurfacePointSeq &points, int nPoints,
+    const PlanarPatch &plane, int &nInliers, bool saveImages = false);
 
 public:
   /**
@@ -50,9 +60,17 @@ public:
    *        a very nasty threshold parameter ...
    * @param _maxIter maximum number of iterations before we give up
    */
-  PlaneRANSAC(double _thr, int _minPoints = 1000, int _maxIter = 1000);
+  PlaneRANSAC(double _thr, bool connectedRegions, int _minPoints = 1000, int _maxIter = 1000);
+  ~PlaneRANSAC();
   bool detectPlane(SurfacePointSeq &points, int &n, const Vector3 &up,
-    bool horizontal, Plane3 &plane);
+    bool horizontal, PlanarPatch &plane);
+  IplImage* getVoteImage() {return voteImg;}
+  void findConvexHull(const SurfacePointSeq &points, int nPoints,
+    PlanarPatch &plane, double thrFactor = 1.);
+  void findConvexHullConnectedRegion(const SurfacePointSeq &points, int nPoints,
+    PlanarPatch &plane);
+  void removeInliers(SurfacePointSeq &points, int &n,
+    const PlanarPatch &plane, double thrFactor = 1.);
 };
 
 }
