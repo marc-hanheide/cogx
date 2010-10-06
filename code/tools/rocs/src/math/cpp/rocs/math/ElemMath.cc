07c38beb9a6c64b6a56cc89746b68298c7f745a5
pronobis <pronobis@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-06 14:47:06 +0000

    Added initial version of ROCS to the SVN repository.

---
+++ code/tools/rocs/src/math/cpp/rocs/math/ElemMath.cc
@@ -0,0 +1,14 @@
+/*
+ * ElemMath.cc
+ *
+ */
+
+#include "rocs/math/ElemMath.h"
+
+namespace rocs {
+namespace math {
+
+//...
+
+} // end namespace math
+} // end namespace rocs
---
+++ code/tools/rocs/src/math/cpp/rocs/math/Vector.cc
@@ -0,0 +1,14 @@
+/*
+ * Vector.cpp
+ *
+ *  Created on: Jun 18, 2010
+ *      Author: arnaud
+ */
+
+#include "rocs/math/Vector.h"
+
+using namespace rocs::math;
+
+Vector::~Vector() {
+	// TODO Auto-generated destructor stub
+}
