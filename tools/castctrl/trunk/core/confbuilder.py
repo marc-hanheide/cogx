7ab84311d5ba1562b7c94fdc3726311b7cead8ec
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-06 08:05:32 +0000

    castctrl: minor

--- tools/castctrl/trunk/core/confbuilder.py
+++ tools/castctrl/trunk/core/confbuilder.py
@@ -153,7 +153,7 @@ class CCastConfig:
         """
         lines = []
         # TODO: Checking for CAST_HAS_SETVAR is temporary; remove when a new latest is published
-        # (after 2010-10-06; latest should point to trunk/rev >= 13671)
+        # (after 2010-10-06; latest should point to trunk/rev >= 13571)
         opts = options.getCastOptions()
         if opts.xe("$CAST_HAS_SETVAR") == "1":
             lines += [ "SETVAR CONFIG_DIR=%s" % os.path.dirname(os.path.abspath(filename)) ]
