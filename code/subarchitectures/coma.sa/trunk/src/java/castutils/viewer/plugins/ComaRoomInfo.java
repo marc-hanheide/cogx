91d1e94a95096d4ba02d6903e233b93b55d1cc98
zender <zender@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-07 16:58:52 +0000

    wasnt the obvious cause for the bug, so put the code back in

--- code/subarchitectures/coma.sa/trunk/src/java/castutils/viewer/plugins/ComaRoomInfo.java
+++ code/subarchitectures/coma.sa/trunk/src/java/castutils/viewer/plugins/ComaRoomInfo.java
@@ -18,16 +18,16 @@ public class ComaRoomInfo implements Plugin {
 		extraInfo.add("roomId=" + r.roomId);
 		extraInfo.add("seedPlaceInstance=" + r.seedPlaceInstance);
 		helperString = "categories=( ";
-		//if (r.categories!=null) {
-		//	if (r.categories.massFunction!=null) {
-		//		for (JointProbabilityValue jpv : r.categories.massFunction) {
-		//			StringRandomVariableValue rvv = (StringRandomVariableValue) jpv.variableValues[0];
-		//			String c = rvv.value;
-		//			float prob_c = jpv.probability;
-		//			helperString = helperString + c + "(p=" + prob_c + ") ";
-		//		}
-		//	}
-		//}
+		if (r.categories!=null) {
+			if (r.categories.massFunction!=null) {
+				for (JointProbabilityValue jpv : r.categories.massFunction) {
+					StringRandomVariableValue rvv = (StringRandomVariableValue) jpv.variableValues[0];
+					String c = rvv.value;
+					float prob_c = jpv.probability;
+					helperString = helperString + c + "(p=" + prob_c + ") ";
+				}
+			}
+		}
 		extraInfo.add(helperString + ")");
 		helperString = "places=( ";
 		for (long p : r.containedPlaceIds)
