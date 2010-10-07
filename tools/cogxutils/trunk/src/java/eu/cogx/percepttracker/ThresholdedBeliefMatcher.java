9846dd81dadf7744619805342737f3def95b781d
Marc Hanheide <marc@hanheide.net>
2010-10-07 18:40:31 +0000

    - refactored FormulaMatcher stuff

--- tools/cogxutils/trunk/src/java/eu/cogx/percepttracker/ThresholdedBeliefMatcher.java
+++ tools/cogxutils/trunk/src/java/eu/cogx/percepttracker/ThresholdedBeliefMatcher.java
@@ -8,7 +8,7 @@ import cast.cdl.WorkingMemoryAddress;
 import cast.cdl.WorkingMemoryChange;
 import castutils.castextensions.PointerMap;
 import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
-import de.dfki.lt.tr.beliefs.data.formulas.PropositionFormula;
+import de.dfki.lt.tr.beliefs.data.formulas.WMPointer;
 import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
 import de.dfki.lt.tr.beliefs.slice.epstatus.EpistemicStatus;
 import de.dfki.lt.tr.beliefs.slice.epstatus.SharedEpistemicStatus;
@@ -25,19 +25,21 @@ public class ThresholdedBeliefMatcher<From extends dBelief, To extends dBelief>
 	public ThresholdedBeliefMatcher(List<String> _types, PointerMap<?> _map,
 			Class<From> _fromCls, Class<To> _toCls) {
 		super(_types, _map, _fromCls, _toCls);
+		ignoreForPointerPropagationSet.add(SOURCE_BELIEF_ID);
 	}
 
 	public ThresholdedBeliefMatcher(List<String> _types, PointerMap<?> _map,
 			Collection<String> _ignoreKeys, Class<From> _fromCls,
 			Class<To> _toCls) {
 		super(_types, _map, _ignoreKeys, _fromCls, _toCls);
+		ignoreForPointerPropagationSet.add(SOURCE_BELIEF_ID);
 	}
 
 	protected EpistemicStatus getEpistemicStatus() {
 		SharedEpistemicStatus status = new SharedEpistemicStatus();
 		status.cgagents = new ArrayList<String>(2);
 		status.cgagents.add("robot");
-		status.cgagents.add("your mum");
+		status.cgagents.add("human");
 		return status;
 	}
 
@@ -50,23 +52,22 @@ public class ThresholdedBeliefMatcher<From extends dBelief, To extends dBelief>
 	}
 
 	@Override
-	public void update(WorkingMemoryChange wmc, From from, To to)
-			throws IncompatibleAssignmentException {
+	protected void fillBeliefs(WorkingMemoryChange wmc,
+			CASTIndependentFormulaDistributionsBelief<From> fromBelief,
+			CASTIndependentFormulaDistributionsBelief<To> toBelief) {
 
-		// add in specific things
-		CASTIndependentFormulaDistributionsBelief<From> fromProxy = createFromBeliefProxy(from);
+		super.fillBeliefs(wmc, fromBelief, toBelief);
+		
+//		// add pointer back to belief this was generated from
+//
+//		WMPointer formula = WMPointer.create(wmc.address);
+//		//PropositionFormula formula = PropositionFormula.create(wmc.address.id + " " + wmc.address.subarchitecture);	
+//		FormulaDistribution fd = FormulaDistribution.create();
+//		fd.add(formula.get(), 1d);
+//		fromBelief.getContent().put(SOURCE_BELIEF_ID, fd);
+//
+//		// update with rest
 
-		// add pointer back to belief this was generated from
-
-		//TODO put this back in and find out why it breaks things
-		//		WMPointer formula = WMPointer.create(wmc.address);
-		PropositionFormula formula = PropositionFormula.create(wmc.address.id + " " + wmc.address.subarchitecture);	
-		FormulaDistribution fd = FormulaDistribution.create();
-		fd.add(formula.get(), 1d);
-		fromProxy.getContent().put(SOURCE_BELIEF_ID, fd);
-
-		// update with rest
-		super.update(wmc, from, to);
 
 	}
 }
