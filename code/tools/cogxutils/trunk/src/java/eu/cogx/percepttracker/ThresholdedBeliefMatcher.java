9e7342f991cbcfba8a4d45d7205a009de204eccd
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-07 14:21:18 +0000

    oops

---
+++ code/tools/cogxutils/trunk/src/java/eu/cogx/percepttracker/ThresholdedBeliefMatcher.java
@@ -0,0 +1,70 @@
+package eu.cogx.percepttracker;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import cast.cdl.WorkingMemoryAddress;
+import cast.cdl.WorkingMemoryChange;
+import castutils.castextensions.PointerMap;
+import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
+import de.dfki.lt.tr.beliefs.data.formulas.WMPointer;
+import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
+import de.dfki.lt.tr.beliefs.slice.epstatus.EpistemicStatus;
+import de.dfki.lt.tr.beliefs.slice.epstatus.SharedEpistemicStatus;
+import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
+
+public class ThresholdedBeliefMatcher<From extends dBelief, To extends dBelief>
+		extends FormulaMatcher<From, To> {
+
+	/**
+	 * The belief from which this belief is derived
+	 */
+	public final String SOURCE_BELIEF_ID = "source-belief";
+
+	public ThresholdedBeliefMatcher(List<String> _types, PointerMap<?> _map,
+			Class<From> _fromCls, Class<To> _toCls) {
+		super(_types, _map, _fromCls, _toCls);
+	}
+
+	public ThresholdedBeliefMatcher(List<String> _types, PointerMap<?> _map,
+			Collection<String> _ignoreKeys, Class<From> _fromCls,
+			Class<To> _toCls) {
+		super(_types, _map, _ignoreKeys, _fromCls, _toCls);
+	}
+
+	protected EpistemicStatus getEpistemicStatus() {
+		SharedEpistemicStatus status = new SharedEpistemicStatus();
+		status.cgagents = new ArrayList<String>(2);
+		status.cgagents.add("robot");
+		status.cgagents.add("your mum");
+		return status;
+	}
+
+	@Override
+	public To create(WorkingMemoryAddress idToCreate, WorkingMemoryChange wmc,
+			From from) throws IncompatibleAssignmentException {
+		To create = super.create(idToCreate, wmc, from);
+		create.estatus = getEpistemicStatus();
+		return create;
+	}
+
+	@Override
+	public void update(WorkingMemoryChange wmc, From from, To to)
+			throws IncompatibleAssignmentException {
+
+		// add in specific things
+		CASTIndependentFormulaDistributionsBelief<From> fromProxy = createFromBeliefProxy(from);
+
+		// add pointer back to belief this was generated from
+		WMPointer formula = WMPointer.create(wmc.address);
+//		PropositionFormula formula = PropositionFormula.create(wmc.address.id + " " + wmc.address.subarchitecture);	
+		FormulaDistribution fd = FormulaDistribution.create();
+		fd.add(formula.get(), 1d);
+		fromProxy.getContent().put(SOURCE_BELIEF_ID, fd);
+
+		// update with rest
+		super.update(wmc, from, to);
+
+	}
+}
