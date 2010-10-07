463e495d48f715bc56d2fc9481224db75e81ded1
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-07 13:58:02 +0000

    hacking

--- code/tools/cogxutils/trunk/src/java/eu/cogx/perceptmediator/transferfunctions/abstr/SimpleDiscreteTransferFunction.java
+++ code/tools/cogxutils/trunk/src/java/eu/cogx/perceptmediator/transferfunctions/abstr/SimpleDiscreteTransferFunction.java
@@ -19,7 +19,7 @@ import castutils.castextensions.CASTHelper;
 import castutils.castextensions.WMEntrySynchronizer.TransferFunction;
 import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
 import de.dfki.lt.tr.beliefs.data.formulas.Formula;
-import de.dfki.lt.tr.beliefs.data.formulas.WMAddressFormula;
+import de.dfki.lt.tr.beliefs.data.formulas.WMPointer;
 import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
 import de.dfki.lt.tr.beliefs.data.specificproxies.IndependentFormulaDistributions;
 import de.dfki.lt.tr.beliefs.slice.distribs.CondIndependentDistribs;
@@ -157,7 +157,7 @@ public abstract class SimpleDiscreteTransferFunction<From extends Ice.ObjectImpl
 	 */
 	private void addUniversalMappings(WorkingMemoryChange _wmc, From _from,
 			Map<String, Formula> _mapping) {
-		_mapping.put(SOURCE_ADDR_ID, WMAddressFormula.create(_wmc.address).getAsFormula());
+		_mapping.put(SOURCE_ADDR_ID, WMPointer.create(_wmc.address).getAsFormula());
 	}
 
 	/**
