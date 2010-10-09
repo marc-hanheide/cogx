786ade16ded469947cb71c00648cbd9bb904ec76
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-09 22:45:56 +0000

    - now we generate more GroundedBeliefs directly to make our lives ways easier  - in Dora only Visual objects are now considered as percepts, all others are immediately grounded

--- code/scenarios/dora/trunk/src/java/eu/cogx/perceptmediator/dora/VisualObjectTransferFunction.java
+++ code/scenarios/dora/trunk/src/java/eu/cogx/perceptmediator/dora/VisualObjectTransferFunction.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 
 import SpatialData.Place;
-
 import VisionData.VisualObject;
 import cast.CASTException;
 import cast.architecture.ManagedComponent;
@@ -22,9 +21,13 @@ import de.dfki.lt.tr.beliefs.data.formulas.Formula;
 import de.dfki.lt.tr.beliefs.data.formulas.PropositionFormula;
 import de.dfki.lt.tr.beliefs.data.formulas.WMPointer;
 import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
+import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
 import de.dfki.lt.tr.beliefs.util.BeliefException;
+import eu.cogx.beliefs.slice.GroundedBelief;
 import eu.cogx.beliefs.slice.PerceptBelief;
-import eu.cogx.perceptmediator.transferfunctions.abstr.DependentDiscreteTransferFunction;
+import eu.cogx.perceptmediator.transferfunctions.LocalizedAgentTransferFunction;
+import eu.cogx.perceptmediator.transferfunctions.abstr.DependentLinkingDiscreteTransferFunction;
+import eu.cogx.perceptmediator.transferfunctions.helpers.AgentMatchingFunction;
 import eu.cogx.perceptmediator.transferfunctions.helpers.PlaceMatchingFunction;
 import facades.SpatialFacade;
 
@@ -32,13 +35,15 @@ import facades.SpatialFacade;
  * @author marc
  * 
  */
-public class VisualObjectTransferFunction extends
-		DependentDiscreteTransferFunction<VisualObject, PerceptBelief> {
+public class VisualObjectTransferFunction
+		extends
+		DependentLinkingDiscreteTransferFunction<VisualObject, PerceptBelief, GroundedBelief> {
 
 	public VisualObjectTransferFunction(ManagedComponent component,
-			WMView<PerceptBelief> allBeliefs) {
+			WMView<GroundedBelief> allBeliefs) {
 		super(component, allBeliefs, Logger
-				.getLogger(VisualObjectTransferFunction.class), PerceptBelief.class);
+				.getLogger(VisualObjectTransferFunction.class),
+				PerceptBelief.class);
 	}
 
 	@Override
@@ -72,15 +77,25 @@ public class VisualObjectTransferFunction extends
 		FormulaDistribution fd = FormulaDistribution.create();
 		Place currentPlace;
 		try {
-			currentPlace = SpatialFacade.get(component).getPlace();
-			WorkingMemoryAddress placeWMA = getReferredBelief(new PlaceMatchingFunction(
-					currentPlace.id));
-			WMPointer wmp = WMPointer.create(placeWMA, CASTUtils.typeName(this.beliefClass));
-			component.log("size of from.identDistrib: "+ from.identDistrib.length);
-			fd.add(wmp.get(), from.identDistrib[0]);
+			WorkingMemoryAddress agentWMA = getReferredBelief(new AgentMatchingFunction(
+					0));
+			CASTIndependentFormulaDistributionsBelief<dBelief> agent = CASTIndependentFormulaDistributionsBelief
+					.create(dBelief.class, allBeliefs.get(agentWMA));
+			Formula place=agent.getContent().get(LocalizedAgentTransferFunction.IS_IN)
+					.getDistribution().getMostLikely();
+//			currentPlace = SpatialFacade.get(component).getPlace();
+//			component.log("I am at place " + currentPlace.id
+//					+ " when I found that object");
+//			WorkingMemoryAddress placeWMA = getReferredBelief(new PlaceMatchingFunction(
+//					currentPlace.id));
+//			WMPointer wmp = WMPointer.create(placeWMA, CASTUtils
+//					.typeName(GroundedBelief.class));
+			component.log("size of from.identDistrib: "
+					+ from.identDistrib.length);
+			fd.add(place.get(), from.identDistrib[0]);
 			belief.getContent().put("is-in", fd);
-		} catch (CASTException e) {
-			component.logException(e);
+//		} catch (CASTException e) {
+//			component.logException(e);
 		} catch (InterruptedException e) {
 			component.logException(e);
 		} catch (BeliefException e) {
