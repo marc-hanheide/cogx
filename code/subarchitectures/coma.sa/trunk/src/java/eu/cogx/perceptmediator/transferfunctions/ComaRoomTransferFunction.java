6714809bb992e43d456ce548883dd72da04c8137
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-09 10:47:10 +0000

    - added possibility to interrupt from fillBelief

--- code/subarchitectures/coma.sa/trunk/src/java/eu/cogx/perceptmediator/transferfunctions/ComaRoomTransferFunction.java
+++ code/subarchitectures/coma.sa/trunk/src/java/eu/cogx/perceptmediator/transferfunctions/ComaRoomTransferFunction.java
@@ -8,14 +8,19 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 
+import SpatialProbabilities.JointProbabilityValue;
+
 import cast.architecture.ManagedComponent;
 import cast.cdl.WorkingMemoryChange;
 import castutils.castextensions.IceXMLSerializer;
 
 import comadata.ComaRoom;
 
+import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
 import de.dfki.lt.tr.beliefs.data.formulas.Formula;
 import de.dfki.lt.tr.beliefs.data.formulas.IntFormula;
+import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
+import de.dfki.lt.tr.beliefs.data.specificproxies.IndependentFormulaDistributions;
 import de.dfki.lt.tr.beliefs.util.BeliefException;
 import eu.cogx.beliefs.slice.PerceptBelief;
 import eu.cogx.perceptmediator.transferfunctions.abstr.SimpleDiscreteTransferFunction;
@@ -44,13 +49,35 @@ public class ComaRoomTransferFunction extends
 				.put(ROOM_ID, IntFormula.create((int) from.roomId)
 						.getAsFormula());
 
-		component.getLogger().info(IceXMLSerializer.toXMLString(from));
+		if (from.categories.massFunction == null) {
+			component.getLogger().info(
+					"Coma room without a category yet, not mediating!");
+			return null;
+		}
+		logger.info(IceXMLSerializer.toXMLString(from));
 		// BoolFormula isExplored =
 		// BoolFormula.create(from.status==PlaceStatus.TRUEPLACE);
 		// result.put("placestatus",
 		// PropositionFormula.create(from.status.name()).getAsFormula());
 		// result.put("explored", isExplored.getAsFormula());
 		return result;
+	}
+
+	@Override
+	protected void fillBelief(
+			CASTIndependentFormulaDistributionsBelief<PerceptBelief> belief,
+			WorkingMemoryChange wmc, ComaRoom from) {
+		super.fillBelief(belief, wmc, from);
+		logger.info("fill belief with coma categories");
+		IndependentFormulaDistributions distr = belief.getContent();
+		FormulaDistribution fd = FormulaDistribution.create();
+		for (JointProbabilityValue jp : from.categories.massFunction) {
+			String value = ((SpatialProbabilities.StringRandomVariableValue) (jp.variableValues[0])).value;
+			logger.info("adding " + value + " ("+jp.probability+")");
+			fd.add(value, jp.probability);
+		}
+		assert(fd.size()>0);
+		distr.put("category", fd);
 	}
 
 }
