fa88b079ca94e81322f615767b0f823aea401937
danijels <danijels@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-11 12:07:39 +0000

    All proxies fixed to handle masks of different sizes.

--- code/subarchitectures/vision.sa/trunk/src/matlab/cogxBuild/cpphelpers/cogxVisualLearner_update.m
+++ code/subarchitectures/vision.sa/trunk/src/matlab/cogxBuild/cpphelpers/cogxVisualLearner_update.m
@@ -2,6 +2,12 @@
 function cogxVisualLearner_update(avw,X,B,pts3d)
    global mC Coma Params
 
+   %resize mask to match image size
+   sizex=size(X);
+   if ~isequal(sizex(1:2),size(B))
+       B=imresize(B,sizex(1:2), 'nearest');
+   end
+
    B = double(B);
    B = (B==1);
    X = uint8(X);
