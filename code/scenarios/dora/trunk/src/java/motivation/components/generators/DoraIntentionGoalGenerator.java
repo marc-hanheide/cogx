346aa147462e3ec7f81600084883ece239c7d09e
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-12 16:36:29 +0000

    works now again with dialogue

--- code/scenarios/dora/trunk/src/java/motivation/components/generators/DoraIntentionGoalGenerator.java
+++ code/scenarios/dora/trunk/src/java/motivation/components/generators/DoraIntentionGoalGenerator.java
@@ -1,8 +1,7 @@
 package motivation.components.generators;
 
-import java.util.Map;
 import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util.Map;
 
 import motivation.slice.TutorInitiativeMotive;
 import nu.xom.Document;
@@ -16,7 +15,6 @@ import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
 import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
 import de.dfki.lt.tr.beliefs.slice.epstatus.AttributedEpistemicStatus;
 import de.dfki.lt.tr.beliefs.slice.intentions.Intention;
-import de.dfki.lt.tr.beliefs.slice.logicalcontent.ModalFormula;
 import de.dfki.lt.tr.beliefs.slice.logicalcontent.PointerFormula;
 import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
 
@@ -24,8 +22,8 @@ import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
 public class DoraIntentionGoalGenerator extends
 		AbstractIntentionMotiveGenerator<TutorInitiativeMotive, Intention> {
 
-	public final static String XPATH_SELECT_PRECONDITIONS = "/*/content/*/preconditions/forms/*";
-	public final static String XPATH_SELECT_POSTCONDITIONS = "/*/content/*/preconditions/forms/*";
+	public final static String XPATH_SELECT_PRECONDITIONS = "/*/content/a/*/preconditions/form";
+	public final static String XPATH_SELECT_CONCEPT = "//*/prop[text()='found']";
 	public final static String XPATH_SELECT_POINTER_PREFIX = "form/pointer";
 	private static final Map<String, String> DLG_TO_PLANNER_MAPPING = new HashMap<String, String>() {
 		{
@@ -51,50 +49,82 @@ public class DoraIntentionGoalGenerator extends
 			Document xmlDoc = IceXMLSerializer.toXomDom(newEntry);
 			// get intention
 
+			String concept = null;
+			Nodes validConcept = xmlDoc.query(XPATH_SELECT_CONCEPT);
+			if (validConcept.size() < 1) {
+				getLogger()
+						.warn("invalid concept asked for: " + xmlDoc.toXML());
+				return null;
+			} else {
+				concept = "location";
+			}
+
 			Nodes preconditions = xmlDoc.query(XPATH_SELECT_PRECONDITIONS);
-			WorkingMemoryAddress preBelief = findAttributedPrecondition(preconditions);
+			if (preconditions.size() < 1) {
+				getLogger()
+						.warn("invalid intention content: " + xmlDoc.toXML());
+				return null;
+			}
+			Node n = preconditions.get(0);
+			PointerFormula pf = IceXMLSerializer.fromXMLString(n.toXML(),
+					PointerFormula.class);
+			if (pf == null) {
+				getLogger().warn("invalid intention content: " + n.toXML());
+				return null;
+			}
+			WorkingMemoryAddress preBelief = pf.pointer;
 
 			CASTIndependentFormulaDistributionsBelief<dBelief> preAttributedBelief;
 			try {
 				preAttributedBelief = CASTIndependentFormulaDistributionsBelief
 						.create(dBelief.class, getMemoryEntry(preBelief,
 								dBelief.class));
-				String concept = null;
-				// look for "unknown" values in all the properties:
-				for (Entry<String, FormulaDistribution> f : preAttributedBelief
-						.getContent().entrySet()) {
-					String mostLikely = f.getValue().getDistribution()
-							.getMostLikely().getProposition();
-					if (mostLikely.equals("unknown")) {
-						concept = f.getKey();
-						break;
-					}
-				}
+				// String concept = null;
+				// // look for "unknown" values in all the properties:
+				// for (Entry<String, FormulaDistribution> f :
+				// preAttributedBelief
+				// .getContent().entrySet()) {
+				// String mostLikely = f.getValue().getDistribution()
+				// .getMostLikely().getProposition();
+				// if (mostLikely.equals("unknown")) {
+				// concept = f.getKey();
+				// break;
+				// }
+				// }
 				if (concept == null) {
 					getLogger()
 							.warn(
 									"the concept in the attributed belief was none of the ones we know about.");
 					return null;
 				}
-				log("inferred concept from attributed belief: " + concept);
+				log("inferred concept from intnention: " + concept);
 				if (concept.equals("location")) {
 					FormulaDistribution objecttypeFD = preAttributedBelief
 							.getContent().get("objecttype");
-					if (objecttypeFD == null) {
+					FormulaDistribution materialFD = preAttributedBelief
+							.getContent().get("material");
+					if ((objecttypeFD == null) && (materialFD == null)) {
 						getLogger()
 								.warn(
 										"couldn't find any referring pointer, most likelz ref resolution didn't work");
 						return null;
 					}
 
-					String objectType = objecttypeFD.getDistribution()
-							.getMostLikely().getProposition();
-
-					objectType = DLG_TO_PLANNER_MAPPING.get(objectType);
+					String objectType = "";
+					if (objecttypeFD != null) {
+						objectType = objecttypeFD.getDistribution()
+								.getMostLikely().getProposition();
+					} else if (materialFD != null) {
+						objectType = materialFD.getDistribution()
+								.getMostLikely().getProposition();
+					}
+					log("objectType="+objectType);
+					if (DLG_TO_PLANNER_MAPPING.get(objectType)!=null)
+						objectType = DLG_TO_PLANNER_MAPPING.get(objectType);
 
 					TutorInitiativeMotive goal = new TutorInitiativeMotive();
 					super.fillDefault(goal);
-					
+
 					goal.referenceEntry = addr;
 					// (and (exists (?o - visualobject) (and (= (label ?o)
 					// cereal_box) (kval dora (is-in ?o))))
@@ -104,6 +134,7 @@ public class DoraIntentionGoalGenerator extends
 									+ objectType + ") (kval '" + robotBel
 									+ "' (is-in ?o))))", false);
 					log("goal generated: " + goal.goal.goalString);
+					goal.informationGain=1.0;
 					return goal;
 				} else {
 					return null;
@@ -116,29 +147,30 @@ public class DoraIntentionGoalGenerator extends
 		return null;
 	}
 
-	private WorkingMemoryAddress findAttributedPrecondition(Nodes preconditions) {
-		for (int i = 0; i < preconditions.size(); i++) {
-			Node n = preconditions.get(i);
-			ModalFormula mf = IceXMLSerializer.fromXMLString(n.toXML(),
-					ModalFormula.class);
-			if (!mf.op.equals("belief"))
-				continue;
-			WorkingMemoryAddress pf = ((PointerFormula) mf.form).pointer;
-			CASTIndependentFormulaDistributionsBelief<dBelief> preAttributedBelief;
-			try {
-				preAttributedBelief = CASTIndependentFormulaDistributionsBelief
-						.create(dBelief.class,
-								getMemoryEntry(pf, dBelief.class));
-				if (preAttributedBelief.isAttributed()) {
-					return pf;
-				}
-			} catch (CASTException e) {
-				logException(e);
-				return null;
-			}
-		}
-		return null;
-	}
+	// private WorkingMemoryAddress findAttributedPrecondition(Nodes
+	// preconditions) {
+	// for (int i = 0; i < preconditions.size(); i++) {
+	// Node n = preconditions.get(i);
+	// ModalFormula mf = IceXMLSerializer.fromXMLString(n.toXML(),
+	// ModalFormula.class);
+	// if (!mf.op.equals("belief"))
+	// continue;
+	// WorkingMemoryAddress pf = ((PointerFormula) mf.form).pointer;
+	// CASTIndependentFormulaDistributionsBelief<dBelief> preAttributedBelief;
+	// try {
+	// preAttributedBelief = CASTIndependentFormulaDistributionsBelief
+	// .create(dBelief.class,
+	// getMemoryEntry(pf, dBelief.class));
+	// if (preAttributedBelief.isAttributed()) {
+	// return pf;
+	// }
+	// } catch (CASTException e) {
+	// logException(e);
+	// return null;
+	// }
+	// }
+	// return null;
+	// }
 
 	@Override
 	protected TutorInitiativeMotive checkForUpdate(Intention newEntry,
