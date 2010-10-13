c3bb2a80009b94faa420a1fe2fa2c491ed31577b
alen <alen@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-13 18:24:38 +0000

    Importing the affordance recognizer for the first time.

---
+++ subarchitectures/vision.sa/trunk/src/matlab/cogxAffordanceClassifier_recognise.m
@@ -0,0 +1,10 @@
+function Affordance = cogxAffordanceClassifier_recognise(Features)
+%
+% Note: This assumes the existence of a global BiModalLearner object named
+% AffordanceClassifier
+% 
+
+    FeaturesResults = AffordanceClassifier.classify(Features);
+    Affordance = FeaturesResults.Results.GroundTruthClassification{1};
+
+end
