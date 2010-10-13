66ec41a796a5437cb129b5f13205dc39e5518bda
Nick Hawes <nickh@robots.ox.ac.uk>
2010-10-13 20:14:48 +0000

    now intentions are deleted, provided they contain a locally correct id

--- subarchitectures/dialogue.sa/trunk/apis/cast/src/java/de/dfki/lt/tr/cast/dialogue/ReferenceMonitor.java
+++ subarchitectures/dialogue.sa/trunk/apis/cast/src/java/de/dfki/lt/tr/cast/dialogue/ReferenceMonitor.java
@@ -20,6 +20,15 @@
 
 package de.dfki.lt.tr.cast.dialogue;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+import cast.CASTException;
 import cast.SubarchitectureComponentException;
 import cast.architecture.ChangeFilterFactory;
 import cast.architecture.WorkingMemoryChangeReceiver;
@@ -41,27 +50,18 @@ import de.dfki.lt.tr.cast.ProcessingData;
 import de.dfki.lt.tr.dialogue.discourse.DialogueMoveTranslator;
 import de.dfki.lt.tr.dialogue.interpret.BeliefIntentionUtils;
 import de.dfki.lt.tr.dialogue.interpret.IntentionManagementConstants;
-import de.dfki.lt.tr.dialogue.slice.discourse.DialogueMove;
 import de.dfki.lt.tr.dialogue.ref.BeliefTranslator;
+import de.dfki.lt.tr.dialogue.slice.discourse.DialogueMove;
 import de.dfki.lt.tr.dialogue.util.DialogueException;
-import eu.cogx.beliefs.slice.GroundedBelief;
 import eu.cogx.beliefs.slice.SharedBelief;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
 
 /**
- *
+ * 
  * @author Miroslav Janicek
  */
-public class ReferenceMonitor
-extends AbstractDialogueComponent {
+public class ReferenceMonitor extends AbstractDialogueComponent {
 
-//	private AbductiveReferenceResolution arr;
+	// private AbductiveReferenceResolution arr;
 	private String dumpfile = "/dev/null";
 	private boolean testing = false;
 
@@ -74,70 +74,60 @@ extends AbstractDialogueComponent {
 	@Override
 	public void start() {
 		super.start();
-//		arr = new AbductiveReferenceResolution();
+		// arr = new AbductiveReferenceResolution();
 
 		rewriteRulefile(dumpfile);
 
-/*
-		addChangeFilter(
-				ChangeFilterFactory.createLocalTypeFilter(dBelief.class,  WorkingMemoryOperation.ADD),
-				new WorkingMemoryChangeReceiver() {
-					@Override
-					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
-						handleBeliefAddOverwrite(_wmc);
-					}
-				});
-
-		addChangeFilter(
-				ChangeFilterFactory.createLocalTypeFilter(dBelief.class,  WorkingMemoryOperation.OVERWRITE),
-				new WorkingMemoryChangeReceiver() {
-					@Override
-					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
-						handleBeliefAddOverwrite(_wmc);
-					}
-				});
-
-		addChangeFilter(
-				ChangeFilterFactory.createLocalTypeFilter(dBelief.class,  WorkingMemoryOperation.DELETE),
-				new WorkingMemoryChangeReceiver() {
-					@Override
-					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
-						handleBeliefDelete(_wmc);
-					}
-				});
-
- */
+		/*
+		 * addChangeFilter(
+		 * ChangeFilterFactory.createLocalTypeFilter(dBelief.class,
+		 * WorkingMemoryOperation.ADD), new WorkingMemoryChangeReceiver() {
+		 * 
+		 * @Override public void workingMemoryChanged(WorkingMemoryChange _wmc)
+		 * { handleBeliefAddOverwrite(_wmc); } });
+		 * 
+		 * addChangeFilter(
+		 * ChangeFilterFactory.createLocalTypeFilter(dBelief.class,
+		 * WorkingMemoryOperation.OVERWRITE), new WorkingMemoryChangeReceiver()
+		 * {
+		 * 
+		 * @Override public void workingMemoryChanged(WorkingMemoryChange _wmc)
+		 * { handleBeliefAddOverwrite(_wmc); } });
+		 * 
+		 * addChangeFilter(
+		 * ChangeFilterFactory.createLocalTypeFilter(dBelief.class,
+		 * WorkingMemoryOperation.DELETE), new WorkingMemoryChangeReceiver() {
+		 * 
+		 * @Override public void workingMemoryChanged(WorkingMemoryChange _wmc)
+		 * { handleBeliefDelete(_wmc); } });
+		 */
 		addBeliefChangeFilters(SharedBelief.class);
 
-		addChangeFilter(
-				ChangeFilterFactory.createLocalTypeFilter(DialogueMove.class,  WorkingMemoryOperation.ADD),
+		addChangeFilter(ChangeFilterFactory.createLocalTypeFilter(
+				DialogueMove.class, WorkingMemoryOperation.ADD),
 				new WorkingMemoryChangeReceiver() {
 					@Override
 					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
 						handleDialogueMove(_wmc);
 					}
 				});
-/*
-		addChangeFilter(
-				ChangeFilterFactory.createLocalTypeFilter(Intention.class, WorkingMemoryOperation.ADD),
-				new WorkingMemoryChangeReceiver() {
-					@Override
-					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
-						handleIntentionAdd(_wmc);
-					}
-				});
-
-		addChangeFilter(
-				ChangeFilterFactory.createLocalTypeFilter(Intention.class, WorkingMemoryOperation.DELETE),
-				new WorkingMemoryChangeReceiver() {
-					@Override
-					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
-						handleIntentionDelete(_wmc);
-					}
-				});
-*/
-		addChangeFilter(
-				ChangeFilterFactory.createLocalTypeFilter(CommunicativeIntention.class, WorkingMemoryOperation.ADD),
+		/*
+		 * addChangeFilter(
+		 * ChangeFilterFactory.createLocalTypeFilter(Intention.class,
+		 * WorkingMemoryOperation.ADD), new WorkingMemoryChangeReceiver() {
+		 * 
+		 * @Override public void workingMemoryChanged(WorkingMemoryChange _wmc)
+		 * { handleIntentionAdd(_wmc); } });
+		 * 
+		 * addChangeFilter(
+		 * ChangeFilterFactory.createLocalTypeFilter(Intention.class,
+		 * WorkingMemoryOperation.DELETE), new WorkingMemoryChangeReceiver() {
+		 * 
+		 * @Override public void workingMemoryChanged(WorkingMemoryChange _wmc)
+		 * { handleIntentionDelete(_wmc); } });
+		 */
+		addChangeFilter(ChangeFilterFactory.createLocalTypeFilter(
+				CommunicativeIntention.class, WorkingMemoryOperation.ADD),
 				new WorkingMemoryChangeReceiver() {
 					@Override
 					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
@@ -145,51 +135,48 @@ extends AbstractDialogueComponent {
 					}
 				});
 
-/*
-		addChangeFilter(
-				ChangeFilterFactory.createLocalTypeFilter(CommunicativeIntention.class, WorkingMemoryOperation.DELETE),
-				new WorkingMemoryChangeReceiver() {
-					@Override
-					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
-						handleCommIntentionDelete(_wmc);
-					}
-				});3
- */
-}
+		/*
+		 * addChangeFilter(
+		 * ChangeFilterFactory.createLocalTypeFilter(CommunicativeIntention
+		 * .class, WorkingMemoryOperation.DELETE), new
+		 * WorkingMemoryChangeReceiver() {
+		 * 
+		 * @Override public void workingMemoryChanged(WorkingMemoryChange _wmc)
+		 * { handleCommIntentionDelete(_wmc); } });3
+		 */
+	}
 
-	private void
-	addBeliefChangeFilters(Class cls_) {
-		addChangeFilter(
-				ChangeFilterFactory.createChangeFilter(cls_, WorkingMemoryOperation.ADD, "", "", "binder", FilterRestriction.ALLSA),
-				new WorkingMemoryChangeReceiver() {
-					@Override
-					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
-						handleBeliefAddOverwrite(_wmc);
-					}
-				});
+	private void addBeliefChangeFilters(Class cls_) {
+		addChangeFilter(ChangeFilterFactory.createChangeFilter(cls_,
+				WorkingMemoryOperation.ADD, "", "", "binder",
+				FilterRestriction.ALLSA), new WorkingMemoryChangeReceiver() {
+			@Override
+			public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+				handleBeliefAddOverwrite(_wmc);
+			}
+		});
 
-		addChangeFilter(
-				ChangeFilterFactory.createChangeFilter(cls_, WorkingMemoryOperation.OVERWRITE, "", "", "binder", FilterRestriction.ALLSA),
-				new WorkingMemoryChangeReceiver() {
-					@Override
-					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
-						handleBeliefAddOverwrite(_wmc);
-					}
-				});
+		addChangeFilter(ChangeFilterFactory.createChangeFilter(cls_,
+				WorkingMemoryOperation.OVERWRITE, "", "", "binder",
+				FilterRestriction.ALLSA), new WorkingMemoryChangeReceiver() {
+			@Override
+			public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+				handleBeliefAddOverwrite(_wmc);
+			}
+		});
 
-		addChangeFilter(
-				ChangeFilterFactory.createChangeFilter(cls_, WorkingMemoryOperation.DELETE, "", "", "binder", FilterRestriction.ALLSA),
-				new WorkingMemoryChangeReceiver() {
-					@Override
-					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
-						handleBeliefDelete(_wmc);
-					}
-				});
+		addChangeFilter(ChangeFilterFactory.createChangeFilter(cls_,
+				WorkingMemoryOperation.DELETE, "", "", "binder",
+				FilterRestriction.ALLSA), new WorkingMemoryChangeReceiver() {
+			@Override
+			public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+				handleBeliefDelete(_wmc);
+			}
+		});
 	}
 
 	@Override
-	public void configure(Map<String, String> _config)
-	{
+	public void configure(Map<String, String> _config) {
 		if (_config.containsKey("--dumpfile")) {
 			dumpfile = _config.get("--dumpfile");
 		}
@@ -201,11 +188,10 @@ extends AbstractDialogueComponent {
 	private void handleBeliefAddOverwrite(WorkingMemoryChange _wmc) {
 		try {
 			CASTData data = getWorkingMemoryEntry(_wmc.address);
-			dBelief belief = (dBelief)data.getData();
+			dBelief belief = (dBelief) data.getData();
 			bm.put(_wmc.address, belief);
 			triggerRulefileRewrite();
-		}
-		catch (SubarchitectureComponentException e) {
+		} catch (SubarchitectureComponentException e) {
 			e.printStackTrace();
 		}
 	}
@@ -218,68 +204,66 @@ extends AbstractDialogueComponent {
 	private void handleDialogueMove(WorkingMemoryChange _wmc) {
 		try {
 			CASTData data = getWorkingMemoryEntry(_wmc.address);
-			DialogueMove dm = (DialogueMove)data.getData();
+			DialogueMove dm = (DialogueMove) data.getData();
 			dst.push(dm);
 			triggerRulefileRewrite();
-		}
-		catch (SubarchitectureComponentException e) {
+		} catch (SubarchitectureComponentException e) {
 			e.printStackTrace();
 		}
 	}
 
-/*
-	private void handleIntentionDelete(WorkingMemoryChange _wmc) {
-		// somebody deleted it for us
-		if (_wmc.address.equals(qud_addr)) {
-			log("hmm... the question under discussion has been resolved by somebody else");
-			qud_addr = null;
-			qud = null;
-		}
-		triggerRulefileRewrite();
-	}
-
-	private void handleIntentionAdd(WorkingMemoryChange _wmc) {
-		try {
-			CASTData data = getWorkingMemoryEntry(_wmc.address);
-			Intention it = (Intention)data.getData();
-			log("got an intention!");
-			if (BeliefIntentionUtils.isRobotsPrivateIntention(it)) {
-				// this is the new QUD
-				if (qud != null) {
-					log("removing the old question under discussion");
-					deleteFromWorkingMemory(qud_addr);
-				}
-				log("adding new question under discussion");
-				qud_addr = _wmc.address;
-				qud = it;
-				triggerRulefileRewrite();
-			}
-			else {
-				log("ignoring the intention...");
-			}
-		}
-		catch (SubarchitectureComponentException e) {
-			e.printStackTrace();
-		}
-	}
-*/
+	/*
+	 * private void handleIntentionDelete(WorkingMemoryChange _wmc) { //
+	 * somebody deleted it for us if (_wmc.address.equals(qud_addr)) {
+	 * log("hmm... the question under discussion has been resolved by somebody else"
+	 * ); qud_addr = null; qud = null; } triggerRulefileRewrite(); }
+	 * 
+	 * private void handleIntentionAdd(WorkingMemoryChange _wmc) { try {
+	 * CASTData data = getWorkingMemoryEntry(_wmc.address); Intention it =
+	 * (Intention)data.getData(); log("got an intention!"); if
+	 * (BeliefIntentionUtils.isRobotsPrivateIntention(it)) { // this is the new
+	 * QUD if (qud != null) { log("removing the old question under discussion");
+	 * deleteFromWorkingMemory(qud_addr); }
+	 * log("adding new question under discussion"); qud_addr = _wmc.address; qud
+	 * = it; triggerRulefileRewrite(); } else {
+	 * log("ignoring the intention..."); } } catch
+	 * (SubarchitectureComponentException e) { e.printStackTrace(); } }
+	 */
 
 	private void handleCommIntentionDelete(WorkingMemoryChange _wmc) {
 		// somebody deleted it for us
 		if (_wmc.address.equals(qud_addr)) {
-			log("(communicative) hmm... the question under discussion has been resolved by somebody else");  // okay this won't work
-//			qud_addr = null;
-//			qud = null;
+			log("(communicative) hmm... the question under discussion has been resolved by somebody else"); // okay
+																											// this
+																											// won't
+																											// work
+			// qud_addr = null;
+			// qud = null;
 		}
 		triggerRulefileRewrite();
 	}
 
 	private void handleCommIntentionAdd(WorkingMemoryChange _wmc) {
 		try {
-			CASTData data = getWorkingMemoryEntry(_wmc.address);
-			CommunicativeIntention cit = (CommunicativeIntention)data.getData();
+			CommunicativeIntention cit = getMemoryEntry(_wmc.address,
+					CommunicativeIntention.class);
 			Intention it = cit.intent;
 			log("got an comm. intention!");
+
+			// HACK
+			// deleting previous intention here, this signals to the planner
+			// that the action has been finished (e.g. that the dialogue has
+			// been completed)
+
+			try {
+				log("deleting input intention");
+				deleteFromWorkingMemory(it.id);
+			} catch (CASTException e) {
+				logException("Problem when deleting input intention", e);
+			}
+
+			// END HACK
+
 			if (BeliefIntentionUtils.isRobotsPrivateIntention(it)) {
 				// this is the new QUD
 				if (qud != null) {
@@ -287,9 +271,10 @@ extends AbstractDialogueComponent {
 					deleteFromWorkingMemory(qud_addr);
 					try {
 						deleteFromWorkingMemory(qud.id, getSubarchitectureID());
-					}
-					catch (SubarchitectureComponentException e) {
-						log("hmm, something went wrong while trying to remove the intention: " + e.message);
+					} catch (SubarchitectureComponentException e) {
+						logException(
+								"hmm, something went wrong while trying to remove the intention",
+								e);
 					}
 					qud = null;
 					qud_addr = null;
@@ -298,18 +283,15 @@ extends AbstractDialogueComponent {
 					log("adding new question under discussion");
 					qud_addr = _wmc.address;
 					qud = it;
-				}
-				else {
+				} else {
 					log("this is not a question");
 				}
 				triggerRulefileRewrite();
-			}
-			else {
+			} else {
 				log("ignoring the intention...");
 			}
-		}
-		catch (SubarchitectureComponentException e) {
-			e.printStackTrace();
+		} catch (SubarchitectureComponentException e) {
+			logException(e);
 		}
 	}
 
@@ -319,7 +301,10 @@ extends AbstractDialogueComponent {
 			if (itc.postconditions instanceof ComplexFormula) {
 				ComplexFormula post = (ComplexFormula) itc.postconditions;
 				assert (post.op == BinaryOp.conj);
-				if (post.forms.size() > 0 && post.forms.get(0) instanceof ModalFormula && ((ModalFormula) post.forms.get(0)).op.equals(IntentionManagementConstants.stateModality)) {
+				if (post.forms.size() > 0
+						&& post.forms.get(0) instanceof ModalFormula
+						&& ((ModalFormula) post.forms.get(0)).op
+								.equals(IntentionManagementConstants.stateModality)) {
 					dFormula sfs = ((ModalFormula) post.forms.get(0)).form;
 					if (sfs instanceof ComplexFormula) {
 						ComplexFormula xf = (ComplexFormula) sfs;
@@ -327,7 +312,8 @@ extends AbstractDialogueComponent {
 						if (xf.forms.size() > 0) {
 							dFormula hohof = xf.forms.get(0);
 							if (hohof instanceof ElementaryFormula) {
-								return ((ElementaryFormula) hohof).prop.equals("question-answered");
+								return ((ElementaryFormula) hohof).prop
+										.equals("question-answered");
 							}
 						}
 					}
@@ -337,31 +323,21 @@ extends AbstractDialogueComponent {
 		return false;
 	}
 
-/*
-	private boolean toPostcondition(Intention it) {
-		if (it.content.size() > 0) {
-			IntentionalContent itc = it.content.get(0);
-			if (itc.postconditions instanceof ComplexFormula) {
-				ComplexFormula post = (ComplexFormula) itc.postconditions;
-				assert (post.op == BinaryOp.conj);
-				if (post.forms.size() > 0 && post.forms.get(0) instanceof ModalFormula && ((ModalFormula) post.forms.get(0)).op.equals(IntentionManagementConstants.stateModality)) {
-					dFormula sfs = ((ModalFormula) post.forms.get(0)).form;
-					if (sfs instanceof ComplexFormula) {
-						ComplexFormula xf = (ComplexFormula) sfs;
-						assert (xf.op == BinaryOp.conj);
-						if (xf.forms.size() > 0) {
-							dFormula hohof = xf.forms.get(0);
-							if (hohof instanceof ElementaryFormula) {
-								return ((ElementaryFormula) hohof).prop.equals("question-answered");
-							}
-						}
-					}
-				}
-			}
-		}
-		return false;
-	}
-*/
+	/*
+	 * private boolean toPostcondition(Intention it) { if (it.content.size() >
+	 * 0) { IntentionalContent itc = it.content.get(0); if (itc.postconditions
+	 * instanceof ComplexFormula) { ComplexFormula post = (ComplexFormula)
+	 * itc.postconditions; assert (post.op == BinaryOp.conj); if
+	 * (post.forms.size() > 0 && post.forms.get(0) instanceof ModalFormula &&
+	 * ((ModalFormula)
+	 * post.forms.get(0)).op.equals(IntentionManagementConstants.stateModality))
+	 * { dFormula sfs = ((ModalFormula) post.forms.get(0)).form; if (sfs
+	 * instanceof ComplexFormula) { ComplexFormula xf = (ComplexFormula) sfs;
+	 * assert (xf.op == BinaryOp.conj); if (xf.forms.size() > 0) { dFormula
+	 * hohof = xf.forms.get(0); if (hohof instanceof ElementaryFormula) { return
+	 * ((ElementaryFormula) hohof).prop.equals("question-answered"); } } } } } }
+	 * return false; }
+	 */
 
 	private void triggerRulefileRewrite() {
 		String taskID = newTaskID();
@@ -387,12 +363,12 @@ extends AbstractDialogueComponent {
 
 		String qud_str = "";
 		if (qud != null) {
-			String lines[] = BeliefIntentionUtils.intentionToString(qud).split("\n");
+			String lines[] = BeliefIntentionUtils.intentionToString(qud).split(
+					"\n");
 			for (String line : Arrays.asList(lines)) {
 				qud_str += "% " + line + "\n";
 			}
-		}
-		else {
+		} else {
 			qud_str = "% QUD = null\n";
 		}
 
@@ -403,8 +379,7 @@ extends AbstractDialogueComponent {
 			f.write("\n");
 			f.write(qud_str);
 			f.close();
-		}
-		catch (IOException ex) {
+		} catch (IOException ex) {
 			log("I/O error while writing the dumpfile");
 		}
 	}
