497a5e3dc78199da1fff1b39aa3ee7231f34b681
danijels <danijels@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-14 23:14:28 +0000

    minor.

--- code/subarchitectures/vision.sa/trunk/src/matlab/vis/showLmodels.m
+++ code/subarchitectures/vision.sa/trunk/src/matlab/vis/showLmodels.m
@@ -55,7 +55,7 @@ for i=1:numCall
    %   set(ha,'ButtonDownFcn',{'showOneModel',mC(i),Fnames,Cnames});
 end
 
-	
+txt=strFromIdxs(mC);	
 if length(txt)>1 && numCall>0%8
     a=axis(ha);
     text(-(mod( getc(mC,'numC')-1,5)+1)/(a(2)-a(1)),-1.5,txt,'FontSize',12,'HorizontalAlignment','center','Parent',ha);
