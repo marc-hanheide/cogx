071a9d1c7dc482c7102e8ebae999bd50ac831984
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-10-14 16:55:04 +0000

    improving the reference resolution

--- subarchitectures/dialogue.sa/trunk/resources/abduction/cogx/current/generate-suffix.pl
+++ subarchitectures/dialogue.sa/trunk/resources/abduction/cogx/current/generate-suffix.pl
@@ -3,19 +3,20 @@
 use warnings;
 use strict;
 
-if ($#ARGV != 1) {
-	print "Usage: generate-correlations.pl P_MATCH P_UNK < DOMAIN-FILE\n";
+if ($#ARGV != 2) {
+	print "Usage: generate-correlations.pl P-MATCH P-NOMATCH P-UNK < DOMAIN-FILE\n";
 	exit 1;
 }
 
 my $p_match = $ARGV[0];
-my $p_unk = $ARGV[1];
+my $p_nomatch = $ARGV[1];
+my $p_unk = $ARGV[2];
 
-my $p_distrib = 1.0 - $p_match - $p_unk;
-
-if ($p_distrib < 0.0) {
-	die "remaining probability mass lesser than zero";
-}
+#my $p_distrib = 1.0 - $p_match - $p_unk;
+#
+#if ($p_distrib < 0.0) {
+#	die "remaining probability mass lesser than zero";
+#}
 
 my $current_feat = "";
 my %val_to_feat = ();
@@ -47,8 +48,12 @@ my @cs = ();
 for my $i (keys(%val_to_feat)) {
 	my $f_i = $val_to_feat{$i};
 
+	push @cs, "(ling_to_observed($val_to_feat{$i}($i), unknown)) = p($p_unk)";
+
 	for my $j (keys(%val_to_feat)) {
 		my $f_j = $val_to_feat{$j};
+
+#		push @cs, "(ling_to_observed(unknown, $val_to_feat{$j}($j))) = p($p_unk)";
 
 		my $p = 0.0;
 
@@ -56,9 +61,10 @@ for my $i (keys(%val_to_feat)) {
 			$p = $p_match;
 		}
 		else {
-			if ($feat_counts{$f_i} eq $feat_counts{$f_j}) {
-				$p = (1.0 - $p_match) / $feat_counts{$f_i};
-			}
+			$p = $p_nomatch;
+#			if ($feat_counts{$f_i} eq $feat_counts{$f_j}) {
+#				$p = (1.0 - $p_match) / $feat_counts{$f_i};
+#			}
 		}
 
 		if ($p > 0.0) {
@@ -67,4 +73,5 @@ for my $i (keys(%val_to_feat)) {
 	}
 }
 
-print "correlation = [\n\t" . join(",\n\t", @cs) . "\n].\n";
+#print "correlation = [\n\t"
+print "\t" . join(",\n\t", @cs) . "\n].\n";
