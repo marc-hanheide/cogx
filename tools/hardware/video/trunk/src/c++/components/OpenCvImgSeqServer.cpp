7e036b8317d22cae718d8c9bb7f0e1a2b1ee3a22
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2010-11-15 15:08:32 +0000

    hw/video: ImgSeqServer can handle multiple sequences that can be switched from a DisplayServer while running

--- tools/hardware/video/trunk/src/c++/components/OpenCvImgSeqServer.cpp
+++ tools/hardware/video/trunk/src/c++/components/OpenCvImgSeqServer.cpp
@@ -1,6 +1,12 @@
 /**
  * @author Michael Zillich
  * @date October 2006
+ *
+ * Changes:
+ * 2010-11 Marko Mahnič:
+ *   - switch image sequence while running
+ *   - parser for INI files that describe available image sequences
+ *   - UI to select a sequence using the Display Server (requires Visualization SA)
  */
 
 #include <highgui.h>
@@ -8,6 +14,17 @@
 #include <cast/core/CASTUtils.hpp>
 #include <VideoUtils.h>
 #include "OpenCvImgSeqServer.h"
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+#ifdef FEAT_VISUALIZATION
+#include <fstream>
+#include <limits.h>
+#include <stdlib.h>
+#endif
 
 /**
  * The function called to create a new instance of our component.
@@ -25,6 +42,139 @@ namespace cast
 
 using namespace std;
 
+CSequenceInfo::CSequenceInfo()
+{
+  clear();
+}
+
+void CSequenceInfo::clear()
+{
+  fileTemplates = "";
+  start = 0;
+  end = 0;
+  step = 1;
+  repeatFrame = 1;
+  downsampleFactor = 1;
+  loop = true;
+}
+
+void CSequenceInfo::checkLimits()
+{
+  if (start < 0) start = 0;
+  if (end < 0) end = 0;
+  if (step <= 0) step = 1;
+  if (repeatFrame <= 0) repeatFrame = 1;
+  if(downsampleFactor <= 0) downsampleFactor = 1;
+}
+
+void CSequenceInfo::parseConfig(const map<string,string> & _config)
+{
+  map<string,string>::const_iterator it;
+
+  clear();
+
+  if((it = _config.find("--files")) != _config.end())
+  {
+    fileTemplates = it->second;
+  }
+  if((it = _config.find("--start")) != _config.end())
+  {
+    istringstream str(it->second);
+    str >> start;
+  }
+  if((it = _config.find("--end")) != _config.end())
+  {
+    istringstream str(it->second);
+    str >> end;
+  }
+  if((it = _config.find("--step")) != _config.end())
+  {
+    istringstream str(it->second);
+    str >> step;
+  }
+  if((it = _config.find("--repeatframe")) != _config.end())
+  {
+    istringstream str(it->second);
+    str >> repeatFrame;
+  }
+  if((it = _config.find("--noloop")) != _config.end())
+  {
+    loop = false;
+  }
+  if((it = _config.find("--downsample")) != _config.end())
+  {
+    istringstream str(it->second);
+    str >> downsampleFactor;
+  }
+  checkLimits();
+}
+
+void CSequenceInfo::parseConfig(const std::string& _configStr)
+{
+  map<string,string> params;
+  // TODO: split string, join quoted strings, add to map
+  vector<string> tokens, parts;
+  vector<string>::iterator it, itnx;
+  istringstream iss(_configStr);
+  copy(istream_iterator<string>(iss),
+      istream_iterator<string>(),
+      back_inserter<vector<string> >(tokens));
+
+  // Fix quoted parameters (escape sequences are not handled)
+  for(it = tokens.begin(); it != tokens.end(); it++) {
+    if (it->substr(0, 1) == "\"") {
+      string s = *it;
+      while (s.substr(s.length()-1, 1) != "\"") {
+        it++;
+        if (it == tokens.end()) break;
+        s = s + " " + *it;
+      }
+      parts.push_back(s.substr(1, s.length()-2));
+    }
+    else if (it->substr(0, 1) == "\'") {
+      string s = *it;
+      while (s.substr(s.length()-1, 1) != "\'") {
+        it++;
+        if (it == tokens.end()) break;
+        s = s + " " + *it;
+      }
+      parts.push_back(s.substr(1, s.length()-2));
+    }
+    else parts.push_back(*it);
+  }  
+
+  // create a map from the list
+  for(it = parts.begin(); it != parts.end(); it++) {
+    string& s = *it;
+    if (s.substr(0, 2) != "--") continue; // TODO: error - expecting parameter
+
+    string ns;
+    itnx = it+1;
+    if (itnx == tokens.end()) ns = "";
+    else {
+      ns = *itnx;
+      if (ns.substr(0, 2) == "--") ns = "";
+    }
+    params[s] = ns;
+    if (itnx->substr(0, 2) != "--") it++;
+  }
+
+  parseConfig(params);
+}
+
+void CSequenceInfo::setInfo(Video::VideoSequenceInfo& info)
+{
+  clear();
+  fileTemplates = info.fileTemplates;
+  start = info.start;
+  end = info.end;
+  step = info.step;
+  repeatFrame = info.repeatFrame;
+  loop = info.loop;
+  downsampleFactor = 1;
+  checkLimits();
+}
+
 // default framerate if none specified is 1 s
 static const int FRAMERATE_DEFAULT = 1000;
 
@@ -36,6 +186,9 @@ OpenCvImgSeqServer::OpenCvImgSeqServer()
   width = height = 0;
   frameRepeatCnt = 1;
   loopSequence = true;
+#ifdef FEAT_VISUALIZATION
+  sequenceIniFile = "";
+#endif
 }
 
 OpenCvImgSeqServer::~OpenCvImgSeqServer()
@@ -182,6 +335,8 @@ void OpenCvImgSeqServer::grabFramesInternal() throw(runtime_error)
 
 void OpenCvImgSeqServer::grabFrames()
 {
+  // TODO: if (VideoSequenceInfo present) switchSequence(seqInfo);
+
   // note that by just calling sleep(framrate) we actually do not really
   // get a fixed framerate, this would require setitimer() and pause()
   sleepComponent(framerateMillis);
@@ -248,7 +403,7 @@ void OpenCvImgSeqServer::retrieveFrame(int camId, int width, int height,
 
 void OpenCvImgSeqServer::retrieveHRFrames(std::vector<Video::Image> &frames)
 {
-	printf("OpenCvImgSeqServer::retrieveHRFrames: not yet implemented.\n");
+  printf("OpenCvImgSeqServer::retrieveHRFrames: not yet implemented.\n");
 }
 
 /**
@@ -276,48 +431,14 @@ int OpenCvImgSeqServer::getFramerateMilliSeconds()
 void OpenCvImgSeqServer::configure(const map<string,string> & _config)
   throw(runtime_error)
 {
-  int start = 0, end = 0, step = 1;
-  vector<string> fileTemplates;
+  //int start = 0, end = 0, step = 1;
+  //vector<string> fileTemplates;
   map<string,string>::const_iterator it;
 
   // first let the base class configure itself
   VideoServer::configure(_config);
 
-  if((it = _config.find("--files")) != _config.end())
-  {
-    istringstream str(it->second);
-    string file;
-    while(str >> file)
-      fileTemplates.push_back(file);
-  }
-  if((it = _config.find("--start")) != _config.end())
-  {
-    istringstream str(it->second);
-    str >> start;
-    if(start < 0)
-      start = 0;
-  }
-  if((it = _config.find("--end")) != _config.end())
-  {
-    istringstream str(it->second);
-    str >> end;
-    if(end < 0)
-      end = 0;
-  }
-  if((it = _config.find("--step")) != _config.end())
-  {
-    istringstream str(it->second);
-    str >> step;
-    if(step <= 0)
-      step = 1;
-  }
-  if((it = _config.find("--repeatframe")) != _config.end())
-  {
-    istringstream str(it->second);
-    str >> frameRepeatCnt;
-    if(frameRepeatCnt <= 0)
-      frameRepeatCnt = 1;
-  }
+  // The framerate can be configured at startup, but not when switching sequences
   if((it = _config.find("--framerate_ms")) != _config.end())
   {
     istringstream str(it->second);
@@ -325,20 +446,245 @@ void OpenCvImgSeqServer::configure(const map<string,string> & _config)
     if(framerateMillis <= 0)
       framerateMillis = FRAMERATE_DEFAULT;
   }
-  if((it = _config.find("--downsample")) != _config.end())
+
+  CSequenceInfo sequenceInfo;
+  sequenceInfo.parseConfig(_config);
+
+#ifdef FEAT_VISUALIZATION
+  if((it = _config.find("--sequences")) != _config.end())
   {
-    istringstream str(it->second);
-    str >> downsampleFactor;
-    if(downsampleFactor <= 0)
-      downsampleFactor = 1;
-  }
-  if((it = _config.find("--noloop")) != _config.end())
-  {
-    loopSequence = false;
+    sequenceIniFile = it->second;
+    if (! parseSequenceIniFile(sequenceIniFile)) {
+      sequenceIniFile = "";
+    }
   }
 
+  if((it = _config.find("--show_sequence")) != _config.end())
+  {
+    if (sequenceMap.find(it->second) == sequenceMap.end()) {
+        throw runtime_error(exceptionMessage(__HERE__,
+              "The sequence '%s' specified in --show_sequence does not exist in --sequences",
+              it->second.c_str()));
+    }
+    else {
+      sequenceInfo = sequenceMap[it->second];
+    }
+  }
+#endif
+
+  if (sequenceInfo.fileTemplates.length() < 1) {
+    throw runtime_error(exceptionMessage(__HERE__,
+          "No video sequence was configured. Use --files or --sequences/--show_sequence."));
+  }
+
+  switchSequence(sequenceInfo);
+
+#ifdef FEAT_VISUALIZATION
+  m_display.configureDisplayClient(_config);
+#endif
+}
+
+#ifdef FEAT_VISUALIZATION
+// Read definitions of image sequences from an INI file.
+//
+// INI example:
+//    [:global:]
+//    imagepath=%(INI_DIR)/images
+//
+//    [MyBlueMug]
+//    files=%(imagepath)/image%03-L.png %(imagepath)/image%03d-R.png
+//    in=--files "%(files)" --start 0 --end 10
+//    sequence=--files "%(files)" --start 11 --end 20
+//    out=--files "%(files)" --start 21 --end 30
+//
+// Sequences are defined with parameters accepted by CSequenceInfo::parseConfig.
+// Variable names INIPATH, in, out and sequence have a special meaning.
+//
+bool OpenCvImgSeqServer::parseSequenceIniFile(const std::string& fname)
+{
+  ifstream f(fname.c_str(), ifstream::in);
+  if (f.fail()) return false;
+
+  string section;
+  struct {
+    map<string, string> globalVar, localVar;
+    string expandVars(const string& str)
+    {
+      string::size_type pos = str.find("%(");
+      if (pos == string::npos) return str;
+
+      // Result is built incrementally
+      ostringstream oss;
+      oss << str.substr(0, pos);
+
+      // Position after the last position from which output was generated
+      string::size_type nextpos = pos;
+
+      while (pos != string::npos && pos < str.size()) {
+        string::size_type epos = str.find(")", pos);
+        if (epos == string::npos) break;
+
+        string var = str.substr(pos + 2, epos-(pos+2));
+
+        // Replace the %(variable) with value.
+        // If the variable is not defined, keep original string.
+        if (localVar.find(var) != localVar.end()) oss << localVar[var];
+        else if (globalVar.find(var) != globalVar.end()) oss << globalVar[var];
+        else {
+          oss << str.substr(pos, epos-pos+1);
+          // TODO: warning, variable not defined
+        }
+
+        nextpos = epos+1;
+        pos = str.find("%(", nextpos);
+        if (pos != string::npos) {
+          oss << str.substr(nextpos, pos-nextpos);
+          nextpos = pos;
+        }
+      }
+
+      if (nextpos != string::npos && nextpos < str.size()) {
+        oss << str.substr(nextpos);
+      }
+      return oss.str();
+    }
+  } state;
+
+  {
+    char cpath[PATH_MAX];
+    realpath(fname.c_str(), cpath);
+    string path(cpath);
+    string::size_type const pos = path.find_last_of("/");
+    if (pos == string::npos) state.globalVar["INI_DIR"] = "/";
+    else state.globalVar["INI_DIR"] = path.substr(0, pos);
+  }
+
+  while(f.good()) {
+    string line;
+    getline(f, line);
+
+    // strip ws from line
+    string::size_type pos = line.find_first_not_of(" \t\n");
+    line = ( pos == string::npos ) ? string() : line.substr(pos, line.find_last_not_of(" \t\n")-pos+1);
+
+    // skip empty lines and comments
+    if (line == "") continue;
+    if (line[0] == '#') continue;
+
+    if (line[0] == '[') {
+      // new image sequence - extract name
+      state.localVar.clear();
+      pos = line.find_first_not_of(" \t\n[");
+      section = ( pos == string::npos ) ? string() : line.substr(pos, line.find_last_not_of(" \t\n]")-pos+1);
+      if (section == "") section = "[null]"; // TODO: notify error
+      continue;
+    }
+    else {
+      if (section == "[null]") continue;
+      if (section == "") section = ":global:";
+      string::size_type const posEq = line.find("=");
+      if (posEq == string::npos) continue; // TODO: notify error
+
+      string var, value;
+      pos = line.find_last_not_of(" \t\n", posEq-1);
+      if (pos == string::npos) continue; // TODO: notify error
+      else var = line.substr(0, pos+1);
+
+      pos = line.find_first_not_of(" \t\n", posEq+1);
+      if (pos == string::npos) value = "";
+      else value = line.substr(pos);
+
+      if (section == ":global:") state.globalVar[var] = state.expandVars(value);
+      else {
+        if (var == "sequence") {
+          CSequenceInfo info;
+          info.parseConfig(state.expandVars(value));
+          sequenceMap[section] = info;
+        }
+        // transitions - may be supported some day
+        //else if (var == "in" || var == "out") {
+        //  CSequenceInfo info;
+        //  info.parseConfig(state.expandVars(value));
+        //  transitionMap[section + "-" + var] = info;
+        //}
+        else if (var == "INI_DIR") {
+          continue; // TODO: notify error: some names shouldn't be used
+        }
+        else {
+          state.localVar[var] = state.expandVars(value);
+        }
+      }
+    }
+  }
+  f.close();
+  return true;
+}
+
+#define ID_V11N_OBJECT "ImageServer.Sequence"
+#define IDCHUNK_SEQUENCES "SequenceButtons"
+
+void OpenCvImgSeqServer::start()
+{
+  // Enable Display only if configured with multiple sequences
+  if (sequenceIniFile != "" && sequenceMap.size() > 1) {
+    m_display.connectIceClient(*this);
+    m_display.installEventReceiver();
+    m_display.setClientData(this);
+
+    map<string,CSequenceInfo>::iterator it;
+    ostringstream ss;
+    for (it = sequenceMap.begin(); it != sequenceMap.end(); it++) {
+      ss << "<input type='button' value='Load sequence " << it->first << "' "
+         << "@@ONCLICK@@('" << it->first << "');\" /><br>";
+    }
+    m_display.setActiveHtml(ID_V11N_OBJECT, IDCHUNK_SEQUENCES, ss.str());
+  }
+
+  VideoServer::start();
+}
+
+void OpenCvImgSeqServer::CDisplayClient::handleEvent(const Visualization::TEvent& event)
+{
+  if (event.type == Visualization::evHtmlOnClick) {
+    log("evHtmlOnClick on " + event.sourceId + " (" + event.objectId + ":" + event.partId + ")");
+    string seqname = event.sourceId;
+    if (pComponent->sequenceMap.find(seqname) != pComponent->sequenceMap.end()) {
+      log("going to load sequence " + seqname);
+      pComponent->switchSequence(pComponent->sequenceMap[seqname]);
+    }
+  }
+
+}
+#endif
+
+void OpenCvImgSeqServer::switchSequence(CSequenceInfo& seq)
+{
+  seq.checkLimits();
+
+  // XXX: filenames are not cleared by constructFilenames().
+  // XXX: width & height are reset so that obtainImageSize() will work.
+  // TODO: frame size should remain unchanged when switching sequences (use downsampleFactor?)
+  filenames.clear();
+  width = 0;
+  height = 0;
+
+  vector<string> fileTemplates;
+  {
+    istringstream str(seq.fileTemplates);
+    string file;
+    while(str >> file)
+      fileTemplates.push_back(file);
+  }
   // do some initialisation based on configured items
-  init(fileTemplates, start, end, step);
+  init(fileTemplates, seq.start, seq.end, seq.step);
+
+  frameRepeatCnt = seq.repeatFrame;
+  if(frameRepeatCnt <= 0) frameRepeatCnt = 1;
+  loopSequence = seq.loop;
+
+  // Restart the sequence
+  frameRepeatPos = frameRepeatCnt;
+  frameCnt = 0;
 }
 
 /**
@@ -352,7 +698,7 @@ void OpenCvImgSeqServer::configure(const map<string,string> & _config)
  */
 void OpenCvImgSeqServer::changeFormat7Properties(int width, int height, int offsetX, int offsetY, int mode, int paketSize)
 {
-	log("only for the PointGrey server available: abort.");
+  log("only for the PointGrey server available: abort.");
 }
 
 /**
@@ -361,7 +707,7 @@ void OpenCvImgSeqServer::changeFormat7Properties(int width, int height, int offs
  */
 bool OpenCvImgSeqServer::inFormat7Mode()
 {
-	return false;
+  return false;
 }
 
 /**
@@ -370,8 +716,8 @@ bool OpenCvImgSeqServer::inFormat7Mode()
  */
 const std::string OpenCvImgSeqServer::getServerName()
 {
-	const std::string str("OpenCvImgSeqServer");
-	return str;
+  const std::string str("OpenCvImgSeqServer");
+  return str;
 }
 
 }
--- tools/hardware/video/trunk/src/c++/components/OpenCvImgSeqServer.h
+++ tools/hardware/video/trunk/src/c++/components/OpenCvImgSeqServer.h
@@ -8,13 +8,48 @@
 
 #include <string>
 #include <vector>
+#include <map>
 #include <stdexcept>
 #include <cv.h>
 //#include <opencv/cv.h>  -- this is incorrect according to the pkg-config flags
 #include "VideoServer.h"
 
+#ifdef FEAT_VISUALIZATION
+#include <CDisplayClient.hpp>
+#endif
+
 namespace cast
 {
+
+struct CSequenceInfo
+{
+  // List of filename templates, one for each camera.
+  // The list is space-delimited to be compatible with --files in configure().
+  std::string fileTemplates;
+
+  // Start index, end index, step.
+  int start;
+  int end;
+  int step;
+
+  // True, if the sequence should play continuously.
+  bool loop;
+
+  // How many times to repeat each frame.
+  // Framerate should be set in configure().
+  int repeatFrame;
+
+  // Integer factor for downsampling.
+  // NOTE: ignored for now!
+  int downsampleFactor;
+
+  CSequenceInfo();
+  void clear();
+  void checkLimits();
+  void parseConfig(const std::map<std::string,std::string> & _config);
+  void parseConfig(const std::string& _configStr);
+  void setInfo(Video::VideoSequenceInfo& info);
+};
 
 /**
  * Video device reading from stored files.
@@ -78,8 +113,9 @@ private:
   virtual void retrieveFrames(const std::vector<int> &camIds, int width, int height, std::vector<Video::Image> &frames);
   virtual void retrieveFrames(int width, int height, std::vector<Video::Image> &frames);
   virtual void retrieveFrame(int camId, int width, int height, Video::Image &frame);
-	virtual void retrieveHRFrames(std::vector<Video::Image> &frames);
+  virtual void retrieveHRFrames(std::vector<Video::Image> &frames);
 
+  void switchSequence(CSequenceInfo& seq);
 
 public:
   OpenCvImgSeqServer();
@@ -89,9 +125,32 @@ public:
   virtual void grabFrames();
   virtual void getImageSize(int &width, int &height);
   virtual int getFramerateMilliSeconds();
-	virtual void changeFormat7Properties(int width, int height, int offsetX, int offsetY, int mode, int paketSize);
-	virtual bool inFormat7Mode();
-	virtual const std::string getServerName();
+  virtual void changeFormat7Properties(int width, int height, int offsetX, int offsetY, int mode, int paketSize);
+  virtual bool inFormat7Mode();
+  virtual const std::string getServerName();
+
+private:
+#ifdef FEAT_VISUALIZATION
+  std::string sequenceIniFile;
+  std::map<std::string, CSequenceInfo> sequenceMap;
+  class CDisplayClient: public cogx::display::CDisplayClient
+  {
+    OpenCvImgSeqServer* pComponent;
+  public:
+    CDisplayClient() { pComponent = NULL; }
+    void setClientData(OpenCvImgSeqServer* pComponent) { this->pComponent = pComponent; }
+    void handleEvent(const Visualization::TEvent &event); /*override*/
+    //std::string getControlState(const std::string& ctrlId); [>override<]
+    //void handleForm(const std::string& id, const std::string& partId,
+    //  const std::map<std::string, std::string>& fields); [>override<]
+    //bool getFormData(const std::string& id, const std::string& partId,
+    //  std::map<std::string, std::string>& fields); [>override<]
+  };
+  CDisplayClient m_display;
+
+  virtual void start();
+  bool parseSequenceIniFile(const std::string& fname);
+#endif
 };
 
 }
