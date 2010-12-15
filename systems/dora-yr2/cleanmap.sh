3b62967f0c65ecf8ff0be6504942207c9b22075a
Marc Hanheide <marc@hanheide.net>
2010-12-15 12:09:02 +0000

    back to ubuntu defaults

--- systems/dora-yr2/cleanmap.sh
+++ systems/dora-yr2/cleanmap.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-zenity --question --text "reset mapping?"  && rm -f robotpose.ccf tmpmap.*
+#zenity --question --text "reset mapping?"  && cp instantiations/map_backup/alu_review_sim/* .
+rm -f robotpose.ccf tmpmap.*
+cp instantiations/map_backup/alu_review_sim/* .
+
