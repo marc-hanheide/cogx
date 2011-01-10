d2d5862a37fa6409311d403637700249bd7f9fc3
Marc Hanheide <marc@hanheide.net>
2011-01-10 16:17:08 +0000

    - changes for model status

--- scenarios/george/branches/post-review-yr2-11560/src/java/eu/cogx/perceptmediator/ModelStatusTransferFunction.java
+++ scenarios/george/branches/post-review-yr2-11560/src/java/eu/cogx/perceptmediator/ModelStatusTransferFunction.java
@@ -9,10 +9,9 @@ import java.util.Map;
 import VisionData.VisualConceptModelStatus;
 import cast.architecture.ManagedComponent;
 import cast.cdl.WorkingMemoryChange;
-import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
+import de.dfki.lt.tr.beliefs.data.formulas.BoolFormula;
 import de.dfki.lt.tr.beliefs.data.formulas.DoubleFormula;
 import de.dfki.lt.tr.beliefs.data.formulas.Formula;
-import de.dfki.lt.tr.beliefs.data.formulas.IntFormula;
 import de.dfki.lt.tr.beliefs.data.formulas.PropositionFormula;
 import de.dfki.lt.tr.beliefs.util.BeliefException;
 import eu.cogx.beliefs.slice.GroundedBelief;
@@ -29,6 +28,7 @@ public class ModelStatusTransferFunction
 	public static final String GAIN = "gain";
 	public static final String MOST_PROMISING = "most-promising";
 	public static final String CONCEPT_ID = "concept";
+	private static final String ASKED_FOR = "asked-for";
 
 	public ModelStatusTransferFunction(ManagedComponent component) {
 		super(component, component.getLogger(), GroundedBelief.class);
@@ -45,7 +45,7 @@ public class ModelStatusTransferFunction
 		double maxGain = 0.0;
 		String maxLabel = null;
 		for (int i = 0; i < from.gains.length; i++) {
-			if (from.gains[i] > maxGain) {
+			if (from.gains[i] > maxGain && !from.askedFor[i]) {
 				maxGain = from.gains[i];
 				maxLabel = from.labels[i];
 			}
@@ -55,7 +55,7 @@ public class ModelStatusTransferFunction
 		result.put(MOST_PROMISING, PropositionFormula.create(maxLabel)
 				.getAsFormula());
 		result.put(GAIN, DoubleFormula.create(maxGain).getAsFormula());
-
+		result.put(ASKED_FOR, BoolFormula.create(false).getAsFormula());
 		return result;
 	}
 
