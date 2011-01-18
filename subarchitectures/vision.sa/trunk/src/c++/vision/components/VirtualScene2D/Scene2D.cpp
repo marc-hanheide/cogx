36cd7ac57bee9f1d9a76c685750e4fc5e3345d41
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-01-18 12:37:54 +0000

    vision: virtual-scene-2D displays contours for WM visualobjects that have an associated protoobject

--- subarchitectures/vision.sa/trunk/src/c++/vision/components/VirtualScene2D/Scene2D.cpp
+++ subarchitectures/vision.sa/trunk/src/c++/vision/components/VirtualScene2D/Scene2D.cpp
@@ -15,10 +15,14 @@
  */
 #include "Scene2D.h"
 
-#include <VisionData.hpp>
 #include <cast/architecture/ChangeFilterFactory.hpp>
-#include <plotter.h> // libplot-dev
 #include <sstream>
+#include <iomanip>
+#include <iterator>
+#include <algorithm>
+#include <cv.h>
+
+#include <CSvgPlotter.hpp> // libplot-dev, wrapper
 
 extern "C"
 {
@@ -60,6 +64,10 @@ CScene2D::CScene2D()
 void CScene2D::configure(const std::map<std::string,std::string> & _config)
 {
    m_display.configureDisplayClient(_config);
+
+   // TODO: ADD PARAMETER. The size of the video image so that we can draw in the right scale
+   m_outputWidth = 640;
+   m_outputHeight = 480;
 }
 
 #define OBJ_VISUAL_OBJECTS "scene2d.VisualObjects"
@@ -84,6 +92,137 @@ void CScene2D::start()
    m_display.createView("VirtualScene2D", Visualization::VtGraphics, objects);
 }
 
+IplImage* wrapMask(const VisionData::SegmentMask &img)
+{
+   // HACK: Data shared between IplImage and vector
+   IplImage* pImg = cvCreateImageHeader(cvSize(img.width, img.height), IPL_DEPTH_8U, 1);
+   pImg->imageData = (char*) &(img.data[0]);
+   pImg->imageDataOrigin = pImg->imageData;
+   pImg->widthStep = img.width;
+   pImg->imageSize = pImg->widthStep * img.height;
+   return pImg;
+}
+
+void releaseWrappedImage(IplImage** pImagePtr)
+{
+   if (! pImagePtr) return;
+   if (*pImagePtr) {
+      // HACK: Data shared between IplImage and vector; the data will be released with Video::Image
+      (*pImagePtr)->imageData = NULL;
+      (*pImagePtr)->imageDataOrigin = NULL;
+      cvReleaseImage(pImagePtr);
+   }
+}
+
+#if 1
+#define YY(y) -(y)
+#else
+#define YY(y) y
+#endif
+void drawContours(Plotter& p, double x0, double y0, vector<vector<cv::Point> >&contours)
+{
+   vector<vector<cv::Point> >::iterator itpart;
+   vector<cv::Point>::iterator itpoint;
+
+   for (itpart = contours.begin(); itpart != contours.end(); itpart++) {
+      itpoint = itpart->begin();
+      p.fmove(itpoint->x + x0, YY(itpoint->y + y0));
+      itpoint++;
+      while (itpoint != itpart->end()) {
+         p.fcont(itpoint->x + x0, YY(itpoint->y + y0));
+         itpoint++;
+      }
+      itpoint = itpart->begin();
+      if (itpoint != itpart->end()) {
+         p.fcont(itpoint->x + x0, YY(itpoint->y + y0));
+      }
+      p.endsubpath();
+   }
+   p.endpath();
+}
+
+void CScene2D::drawVisualObject(const std::string& id, const VisualObjectPtr& pVisObj,
+      const ProtoObjectPtr& pProtoObj)
+{
+   struct _local_ {
+      static std::string getBestLabel(std::vector<std::string>& labels, std::vector<double>& values)
+      {
+         if (labels.size() < 1) return "";
+         if (values.size() < 1) return "";
+         std::vector<double>::iterator it = std::max_element(values.begin(), values.end());
+         int i = std::distance(values.begin(), it);
+         if (i > labels.size()) return "";
+         ostringstream ss;
+         ss << std::fixed << std::setprecision(2) << values[i] << " " << labels[i];
+         return ss.str();
+      }
+   };
+   std::string ident = _local_::getBestLabel(pVisObj->identLabels, pVisObj->identDistrib);
+   std::string color = _local_::getBestLabel(pVisObj->colorLabels, pVisObj->colorDistrib);
+   std::string shape = _local_::getBestLabel(pVisObj->shapeLabels, pVisObj->shapeDistrib);
+
+   vector<vector<cv::Point> > contours;
+   vector<cv::Vec4i> hierarchy;
+   IplImage* pImg = wrapMask(pProtoObj->mask);
+   cv::Mat imgMat(pImg); // XXX copy=true crashes; so we modify the original data.
+
+   // XXX: mask created in SOIFilter: 1-object, 2-background.
+   // findContours requires 0 for background, everything else is the object.
+   for (int i=0; i< pProtoObj->mask.height; i++) {
+     unsigned char *prow = imgMat.ptr(i);
+     unsigned char *pend = prow + pProtoObj->mask.width;
+     while(prow < pend) {
+        if (*prow != 1) *prow = 0;
+        prow++;
+     }
+   }
+   cv::findContours( imgMat, contours, hierarchy,
+         CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
+   releaseWrappedImage(&pImg);
+
+   double W = pProtoObj->sourceImageSize.x;
+   double H = pProtoObj->sourceImageSize.y;
+   double x0 = pProtoObj->imageOrigin.x;
+   double y0 = pProtoObj->imageOrigin.y;
+   double w0 = pProtoObj->mask.width;
+   double h0 = pProtoObj->mask.height;
+   double ps = W / m_outputWidth;
+   std::ostringstream ss;
+   cogx::display::CSvgStringPlotter p(ss);
+   p.openpl();
+   p.fscale(W / m_outputWidth, H / m_outputHeight);
+   p.flinewidth(ps * 1.0);
+
+   p.pencolorname("red");
+   drawContours(p, x0, y0, contours);
+
+   double true_size = p.fontsize(ps * 12);
+   double dy = 0;
+
+   p.fontname("sans-serif");
+   p.pencolorname("yellow");
+   p.fframedtext(x0, YY(y0+h0/2+dy), id);
+   dy += true_size;
+   if (ident != "") {
+      p.fframedtext(x0, YY(y0+h0/2+dy), ident);
+      dy += true_size;
+   }
+   if (color != "") {
+      p.fframedtext(x0, YY(y0+h0/2+dy), color);
+      dy += true_size;
+   }
+   if (shape != "") {
+      p.fframedtext(x0, YY(y0+h0/2+dy), shape);
+      dy += true_size;
+   }
+   p.endpath();
+
+   p.closepl();
+   std::string svg = p.getScreenSvg();
+
+   m_display.setObject(OBJ_VISUAL_OBJECTS, id, svg);
+}
+
 void CScene2D::onAdd_VisualObject(const cdl::WorkingMemoryChange & _wmc)
 {
    cdl::WorkingMemoryAddress addr = _wmc.address;
@@ -92,32 +231,56 @@ void CScene2D::onAdd_VisualObject(const cdl::WorkingMemoryChange & _wmc)
    VisualObjectPtr pVisObj;
    try {
       pVisObj = getMemoryEntry<VisualObject>(addr);
-      addr.id = pVisObj->protoObjectID;
    }
    catch(DoesNotExistOnWMException){
       //log("CScene2D: VisualObject %s deleted while working...", descAddr(addr).c_str());
       return;
    };
+   if (! pVisObj) return;
 
    // ProtoObject provides position and outline
    ProtoObjectPtr pProtoObj;
    try {
+      addr.id = pVisObj->protoObjectID;
       pProtoObj = getMemoryEntry<ProtoObject>(addr);
    }
    catch(DoesNotExistOnWMException){
       //log("CScene2D: ProtoObject %s deleted while working...", descAddr(addr).c_str());
       return; // we don't know where on the image to show the labels
    };
+   if (! pProtoObj) return;
 
-   std::ostringstream svg;
-   SVGPlotter p(std::cin, svg, std::cerr);
-   p.line(110, 110, 230, 230);
-
-   m_display.setObject(OBJ_VISUAL_OBJECTS, _wmc.address.id, svg.str());
+   drawVisualObject(_wmc.address.id, pVisObj, pProtoObj);
 }
 
 void CScene2D::onChange_VisualObject(const cdl::WorkingMemoryChange & _wmc)
 {
+   cdl::WorkingMemoryAddress addr = _wmc.address;
+
+   // VisualObject provides label values
+   VisualObjectPtr pVisObj;
+   try {
+      pVisObj = getMemoryEntry<VisualObject>(addr);
+   }
+   catch(DoesNotExistOnWMException){
+      //log("CScene2D: VisualObject %s deleted while working...", descAddr(addr).c_str());
+      return;
+   };
+   if (! pVisObj) return;
+
+   // ProtoObject provides position and outline
+   ProtoObjectPtr pProtoObj;
+   try {
+      addr.id = pVisObj->protoObjectID;
+      pProtoObj = getMemoryEntry<ProtoObject>(addr);
+   }
+   catch(DoesNotExistOnWMException){
+      //log("CScene2D: ProtoObject %s deleted while working...", descAddr(addr).c_str());
+      return; // we don't know where on the image to show the labels
+   };
+   if (! pProtoObj) return;
+
+   drawVisualObject(_wmc.address.id, pVisObj, pProtoObj);
 }
 
 void CScene2D::onDelete_VisualObject(const cdl::WorkingMemoryChange & _wmc)
