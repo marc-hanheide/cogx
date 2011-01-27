33d1beaa3a96fbf3802e55f5a7074b7f3dd6151e
goebelbe <goebelbe@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-01-27 09:23:33 +0000

    New system for AVS - switching planner development

---
+++ systems/avs-yr3/cleanmap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#zenity --question --text "reset mapping?"  && rm -f robotpose.ccf tmpmap.* && exit 0
+#zenity --question --text "restore start map?" && rm -f robotpose.ccf && cp instantiations/map_backup/bham_lab/* .
+rm -f robotpose.ccf tmpmap.*
+cp instantiations/map_backup/alu_review_sim/* .
+
