73bb4938eeb0f19a624f3a69b157d68f044715c8
goebelbe <goebelbe@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-01-27 09:17:10 +0000

    Merge changes from dora-yr2

--- code/subarchitectures/planner.sa/trunk/src/java/eu/cogx/planner/facade/ManualPlanningTaskFrame.java
+++ code/subarchitectures/planner.sa/trunk/src/java/eu/cogx/planner/facade/ManualPlanningTaskFrame.java
@@ -342,6 +342,8 @@ public class ManualPlanningTaskFrame extends JFrame {
 		this.setContentPane(getJContentPane());
 		this.setTitle("ManualPlanningTask");
 		this.pack();
+		jGoalTextEditField.setText("(exists (?o - visualobject) (and (= (label ?o) cornflakes) (position-reported ?o)))");
+		
 	}
 
 	/**
