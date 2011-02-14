e645a9ce8b0691b30929559b2f0ef847d39c4c5f
pronobis <pronobis@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-02-14 23:36:23 +0000

    Display of placeholders and placeholder properties. Refactored and cleaned up the code as well (first part).

--- code/subarchitectures/spatial.sa/branches/avs-iros11/src/c++/DisplayNavInPB.hpp
+++ code/subarchitectures/spatial.sa/branches/avs-iros11/src/c++/DisplayNavInPB.hpp
@@ -1,39 +1,37 @@
 //
 // = FILENAME
-//   DisplayNavInPB.hpp
+//   DisplaySpatialInPB.hpp
 //
 // = FUNCTION
 //
 // = AUTHOR(S)
-//    Patric Jensfelt
+//    Patric Jensfelt, Andrzej Pronobis
 //
 // = COPYRIGHT
-//    Copyright (c) 2008 Patric Jensfelt
+//    Copyright (c) 2010 Patric Jensfelt, Andrzej Pronobis
 //
 /*----------------------------------------------------------------------*/
 
-#ifndef DisplayNavInPB_hpp
-#define DisplayNavInPB_hpp
+#ifndef DisplaySpatialInPB_hpp
+#define DisplaySpatialInPB_hpp
 
-#include <cast/architecture/ManagedComponent.hpp>
-#include <NavData.hpp>
-#include <SpatialProbabilities.hpp>
-#include <string>
-#include <Scan2dReceiver.hpp>
-#include <SensorData/SensorPose.hh>
+#include "PTZ.hpp"
+#include "NavData.hpp"
+#include "Scan2dReceiver.hpp"
+#include "SpatialProbabilities.hpp"
 #include "SpatialProperties.hpp"
 
-#include <Transformation/Transformation3D.hh>
 #include <peekabot.hh>
 #include <peekabot/Types.hh>
-#include <PTZ.hpp>
+#include <cast/architecture/ManagedComponent.hpp>
+#include <SensorData/SensorPose.hh>
+#include <Transformation/Transformation3D.hh>
+#include <string>
 
-namespace navsa {
+namespace spatial
+{
 
 /**
- * This class will build the nav graph which is communicated 
- * to the conceptual map.
- *
  * Command line options:
  *
  * @param -c a CURE config file to optionally read the
@@ -57,164 +55,158 @@ namespace navsa {
  *
  * @author Patric Jensfelt
  */
-class DisplayNavInPB : public cast::ManagedComponent ,
-                       public Scan2dReceiver
+class DisplayNavInPB : public cast::ManagedComponent, public Scan2dReceiver
 {
+
 public:
-  DisplayNavInPB();
-  virtual ~DisplayNavInPB();
-  
-  virtual void runComponent();
-  virtual void start();
-  
+	DisplayNavInPB();
+	virtual ~DisplayNavInPB();
+	virtual void runComponent();
+	virtual void start();
+
 protected:
-
-  virtual void configure(const std::map<std::string, std::string>& _config);
-
-  void debugScanPush(const char* msg) {} //debug(msg); }
-  void logScanPush(const char* msg) {} // log(msg); }
+	virtual void configure(const std::map<std::string, std::string>& _config);
+	void debugScanPush(const char* msg) {} //debug(msg); }
+	void logScanPush(const char* msg) {} // log(msg); }
 
 private:
-  class PersonData {
-  public:
-    std::string m_WMid;
-    NavData::PersonPtr m_data;
-    bool m_Followed;
-  };
 
-  class Node {
-  public:
-    long m_Id;
-    bool m_Gateway;
-    long m_areaId;
-    double m_X;
-    double m_Y;
-    long m_AreaClassNo;
-  };
+	struct PersonData
+	{
+		std::string m_WMid;
+		NavData::PersonPtr m_data;
+		bool m_Followed;
+	};
 
+	struct Node
+	{
+		long m_Id;
+		bool m_Gateway;
+		long m_areaId;
+		double m_X;
+		double m_Y;
+		long m_AreaClassNo;
+	};
 
-  void displayPeople();
-  void receiveScan2d(const Laser::Scan2d &scan);
-  void newPointCloud(const cast::cdl::WorkingMemoryChange &objID);
-  #ifdef DISPLAY_COMA
-  void newComaRoom(const cast::cdl::WorkingMemoryChange &objID);
-  #endif
+	struct PlaceData
+	{
+		int placeId;
+		SpatialData::PlaceStatus placeStatus;
+		int nodeId;
+	};
 
-  void newShapeProperty(const cast::cdl::WorkingMemoryChange &objID);
-  void newAppearanceProperty(const cast::cdl::WorkingMemoryChange &objID);
+	void receiveScan2d(const Laser::Scan2d &scan);
+	void newPointCloud(const cast::cdl::WorkingMemoryChange &objID);
+	void newComaRoom(const cast::cdl::WorkingMemoryChange &objID);
 
-  void newVPlist(const cast::cdl::WorkingMemoryChange &objID);
-  void newArea(const cast::cdl::WorkingMemoryChange &objID);
-  void newRobotPose(const cast::cdl::WorkingMemoryChange &objID);
-  void newNavGraphNode(const cast::cdl::WorkingMemoryChange &objID);
-  void newPlace(const cast::cdl::WorkingMemoryChange &objID);
-  void newPlanePointCloud(const cast::cdl::WorkingMemoryChange &objID);
-  void newNavGraphEdge(const cast::cdl::WorkingMemoryChange &objID);
-  void newNavGraphObject(const cast::cdl::WorkingMemoryChange &objID);
-  void newLineMap(const cast::cdl::WorkingMemoryChange &objID);
-  //  void newConvexHull(const cast::cdl::WorkingMemoryChange &objID);
-  void newPerson(const cast::cdl::WorkingMemoryChange &objID);
-  void deletePerson(const cast::cdl::WorkingMemoryChange &objID);
-  void newPersonFollowed(const cast::cdl::WorkingMemoryChange &objID);
-  void addEdgeToList(long id1, long id2);
-  void displayEdge(const DisplayNavInPB::Node &node1, 
-                   const DisplayNavInPB::Node &node2);
-  void redisplayEdgesToNode(const DisplayNavInPB::Node &node);
-  void addDoorpost(double x, double y, double theta, 
-                   double width, peekabot::SphereProxy &node);
-  void connectPeekabot();
-  void createFOV(peekabot::GroupProxy &proxy,const char* path, double fovHorizAngle, double fovVertiAngle, double* color, double opacity, 
-				NavData::ViewPoint viewpoint,  bool robotfov = true);
-  void createRobotFOV();
-  void getColorByIndex(int id, float &r, float &g, float &b);
+	void newShapeProperty(const cast::cdl::WorkingMemoryChange &objID);
+	void newAppearanceProperty(const cast::cdl::WorkingMemoryChange &objID);
+	void newRoomCategoryPlaceholderProperty(const cast::cdl::WorkingMemoryChange &objID);
 
-  double getComaRoomCategoryProbabilityValue(const SpatialProbabilities::ProbabilityDistribution &pd,
+	void newVPlist(const cast::cdl::WorkingMemoryChange &objID);
+	void newRobotPose(const cast::cdl::WorkingMemoryChange &objID);
+	void newNavGraphNode(const cast::cdl::WorkingMemoryChange &objID);
+	void newPlace(const cast::cdl::WorkingMemoryChange &objID);
+	void deletePlace(const cast::cdl::WorkingMemoryChange &objID);
+	void newPlanePointCloud(const cast::cdl::WorkingMemoryChange &objID);
+	void newNavGraphEdge(const cast::cdl::WorkingMemoryChange &objID);
+	void newNavGraphObject(const cast::cdl::WorkingMemoryChange &objID);
+	void newLineMap(const cast::cdl::WorkingMemoryChange &objID);
+	void newPerson(const cast::cdl::WorkingMemoryChange &objID);
+	void deletePerson(const cast::cdl::WorkingMemoryChange &objID);
+	void newPersonFollowed(const cast::cdl::WorkingMemoryChange &objID);
+
+	void addEdgeToList(long id1, long id2);
+	void displayEdge(const DisplayNavInPB::Node &node1, const DisplayNavInPB::Node &node2);
+	void redisplayEdgesToNode(const DisplayNavInPB::Node &node);
+	void addDoorpost(double x, double y, double theta, double width, peekabot::SphereProxy &node);
+	void connectPeekabot();
+	void createFOV(peekabot::GroupProxy &proxy,const char* path, double fovHorizAngle,
+			double fovVertiAngle, double* color, double opacity,
+			NavData::ViewPoint viewpoint,  bool robotfov = true);
+	void createRobotFOV();
+	void getColorByIndex(int id, float &r, float &g, float &b);
+	double getProbabilityValue(const SpatialProbabilities::ProbabilityDistribution &pd,
 			std::string varValue);
- int GetPlaceIdFromNodeId(int nodeId);
- void addProperties(peekabot::SphereProxy &sp, int placeId);
+	double getProbabilityValue(const SpatialProperties::ProbabilityDistributionPtr pd,
+			int varValue);
+	int GetPlaceIdFromNodeId(int nodeId);
+	void addProperties(peekabot::SphereProxy &sp, int placeId);
+	void addRoomCategoryPlaceholderProperties(peekabot::CubeProxy &sp, int placeId);
+	void displayPeople();
+	Cure::Transformation3D getCameraToWorldTransform();
+
+private:
+	peekabot::PeekabotClient m_PeekabotClient;
+	peekabot::GroupProxy m_ProxyLabels;
+	peekabot::GroupProxy m_ProxyRobot;
+	peekabot::GroupProxy m_ProxyLaser;
+	peekabot::PointCloudProxy m_ProxyScan;
+	peekabot::GroupProxy m_ProxyGraph;
+	peekabot::GroupProxy m_ProxyNodes;
+	peekabot::GroupProxy m_ProxyEdges;
+	peekabot::GroupProxy m_ProxyObjects;
+	peekabot::GroupProxy m_ProxyObjectLabels;
+	peekabot::GroupProxy m_ProxyViewPoints;
+	peekabot::ObjectProxy m_ProxyCam;
+	peekabot::HingeProxy m_ProxyPan;
+	peekabot::HingeProxy m_ProxyTilt;
+
+	bool m_ShowWalls;
+	bool m_ShowGraph;
+	bool m_ShowPeople;
+	bool m_ShowPeopleId;
+	bool m_ShowRobot;
+	bool m_ShowScans;
+	bool m_ShowObjects;
+	bool m_ShowRoomId;
+	bool m_ShowProperties;
+	bool m_ShowPlaceholders;
+	bool m_ShowRoomCategory;
+	bool m_ShowRobotViewCone;
+	bool m_ShowPlanePoints;
+	bool m_ShowSOIs;
+	bool m_NonUniqueObjects;
+	int m_RetryDelay; // Seconds to retry if cannot connect. -1 means dont retry
+
+	double m_FovH; // horisontal fov in degs
+	double m_FovV; // vertical fov in degs
+	std::vector<double> previouscenter;
+	std::string m_LaserServerHost;
+	IceUtil::Mutex m_Mutex;
+	NavData::RobotPose2dPtr m_RobotPose;
+	NavData::LineMapPtr m_LineMap;
+	Laser::Scan2d m_Scan;
+	std::string m_PbHost;
+	int m_PbPort;
+	std::string m_PbRobotFile;
+	std::string m_PbRobotName;
+	std::string m_PbPersonFile;
+	bool m_NoPeopleModel;
+	double m_ScanAngFOV;
+	double m_ScanMaxRange;
+	int m_CurrPersonId;
 
 
-  Cure::Transformation3D getCameraToWorldTransform();
+	std::vector<PersonData> m_People; // The people that are currently in view
+	std::map<long, Node> m_Nodes;
+	std::list< std::pair<long,long> > m_Edges;
+	std::list< std::pair<long,long> > m_NewEdges;
+	std::map<std::string, PlaceData> _places;
 
-  bool m_ShowWalls;
-  bool m_ShowGraph;
-  bool m_ShowPeople;
-  bool m_ShowPeopleId;
-  bool m_ShowRobot;
-  bool m_ShowScans;
-  bool m_ShowObjects;
-  bool m_ShowNodeClass;
-  bool m_ShowProperties;
-  bool m_ShowAreaClass;
-  bool m_ShowRobotViewCone;
-  bool m_ShowPlanePoints;
-  bool m_ShowSOIs;
-  bool m_NonUniqueObjects;
-  int m_RetryDelay; // Seconds to retry if cannot connect. -1 means dont retry
+	bool m_LaserConnected;
+	bool m_ReadPTU;
+	ptz::PTZInterfacePrx m_PTUServer;
+	Cure::SensorPose m_CameraPoseR;
 
-  double m_FovH; // horisontal fov in degs
-  double m_FovV; // vertical fov in degs
-  std::vector<double> previouscenter;
-  std::string m_LaserServerHost;
-
-  IceUtil::Mutex m_Mutex;
-
-  NavData::RobotPose2dPtr m_RobotPose;
-  NavData::LineMapPtr m_LineMap;
-  Laser::Scan2d m_Scan;
-
-  std::string m_PbHost;
-  int m_PbPort;
-  std::string m_PbRobotFile;
-  std::string m_PbRobotName;
-  std::string m_PbPersonFile;
-
-  bool m_NoPeopleModel;
-
-  peekabot::PeekabotClient m_PeekabotClient;
-
-  //peekabot::ObjectProxy m_ProxyRoot;
-  
-  peekabot::GroupProxy m_ProxyLabels;
-  peekabot::GroupProxy m_ProxyRobot;
-  peekabot::GroupProxy m_ProxyLaser;
-  peekabot::PointCloudProxy m_ProxyScan;
-  double m_ScanAngFOV;
-  double m_ScanMaxRange; 
-  peekabot::GroupProxy m_ProxyGraph;
-  peekabot::GroupProxy m_ProxyNodes;
-  peekabot::GroupProxy m_ProxyEdges;
-  peekabot::GroupProxy m_ProxyObjects;
-  peekabot::GroupProxy m_ProxyObjectLabels;
-  peekabot::GroupProxy m_ProxyViewPoints;
-  peekabot::ObjectProxy m_ProxyCam;
-  peekabot::HingeProxy m_ProxyPan;
-  peekabot::HingeProxy m_ProxyTilt;
-  
-
-  // The people that are currently in view
-  std::vector<PersonData> m_People;
-  int m_CurrPersonId;
-
-  std::map<long, Node> m_Nodes;
-  std::list< std::pair<long,long> > m_Edges;
-  std::list< std::pair<long,long> > m_NewEdges;
-
-  bool m_LaserConnected;
-
-  bool m_ReadPTU;
-  ptz::PTZInterfacePrx m_PTUServer;
-
-  Cure::SensorPose m_CameraPoseR;
-
-  /** List of categories of rooms that we know about. */
-  std::vector<std::string> _roomCategories;
-
-  std::map<int, SpatialProperties::RoomShapePlacePropertyPtr> _shapeProps;
-  std::map<int, SpatialProperties::RoomAppearancePlacePropertyPtr> _appearanceProps;
+	/** List of categories of rooms that we know about. */
+	std::vector<std::string> _roomCategories;
+	std::map<int, SpatialProperties::RoomShapePlacePropertyPtr> _shapeProps;
+	std::map<int, SpatialProperties::RoomAppearancePlacePropertyPtr> _appearanceProps;
+	std::map<std::string, SpatialProperties::RoomCategoryPlaceholderPropertyPtr> _roomCatPlaceholderProps;
 
 };
 
-}; // namespace navsa
+}; // namespace spatial
 
-#endif // DisplayNavInPB_hpp
+#endif // DisplaySpatialInPB_hpp
