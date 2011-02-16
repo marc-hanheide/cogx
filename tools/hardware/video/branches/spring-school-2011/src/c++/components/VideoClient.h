13667c9720d439c7e3f4099ef0e59590c29e9708
michaelz <michaelz@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-02-16 15:19:46 +0000

    branch for 2011 spring school

---
+++ tools/hardware/video/branches/spring-school-2011/src/c++/components/VideoClient.h
@@ -0,0 +1,51 @@
+/**
+ * @author Michael Zillich
+ * @date February 2009
+ */
+
+#ifndef VIDEO_CLIENT_H
+#define VIDEO_CLIENT_H
+
+#include <stdexcept>
+#include <vector>
+#include <string>
+#include <map>
+#include <cast/core/CASTComponent.hpp>
+#include "Video.hpp"
+
+namespace cast
+{
+
+/**
+ * Client to a VideoServer.
+ * Inherit from this class if You want to connect to video servers.
+ */
+class VideoClient
+{
+protected:
+  class VideoClientI : virtual public Video::VideoClientInterface
+  {
+  private:
+    VideoClient *vidClt;
+
+  public:
+    VideoClientI(VideoClient *clt) : vidClt(clt) {}
+    virtual void receiveImages(const Video::ImageSeq &images, const Ice::Current&)
+    {
+      vidClt->receiveImages(images);
+    }
+  };
+
+public:
+  /**
+   * The callback function for images pushed by the image server.
+   * To be overwritten by derived classes.
+   */
+  virtual void receiveImages(const std::vector<Video::Image>& images) {}
+};
+
+}
+
+#endif
+
+
