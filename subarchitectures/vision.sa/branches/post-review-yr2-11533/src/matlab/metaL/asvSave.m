e3dc180015dbb6e1d614cb8491434ebbebfae527
danijels <danijels@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-03-09 12:50:24 +0000

    New models.

--- subarchitectures/vision.sa/branches/post-review-yr2-11533/src/matlab/metaL/asvSave.m
+++ subarchitectures/vision.sa/branches/post-review-yr2-11533/src/matlab/metaL/asvSave.m
@@ -4,15 +4,15 @@ global Settings
 
 global Figs
 global Dirs
-
+global mC
 
 if Settings.ASVon
-   
-   Settings.Params.ASVidx=Settings.Params.ASVidx+1;
-
-   Settings.Params.IPs=get(Figs.vmipH.lbipH,'String');
-
-   fname=[Dirs.asv 'asv' num2str(Settings.Params.ASVidx,'%04d') '.mat'];
-   save(fname,'mC','mFS','mDA','NUs','RSs','IPs');
-
+    
+    Settings.Params.ASVidx=Settings.Params.ASVidx+1;
+    Settings.Params.IPs=get(Figs.vmipH.lbipH,'String');
+    fname=[Dirs.asv 'asv' num2str(Settings.Params.ASVidx,'%04d') '.mat'];
+    
+    %   save(fname,'mC','mFS','mDA','NUs','RSs','IPs');
+    save(fname,'mC');
+    
 end
