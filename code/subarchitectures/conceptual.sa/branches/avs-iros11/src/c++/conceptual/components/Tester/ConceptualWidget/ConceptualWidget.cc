06ec18cfcd4fd0ed8f20275a7e081cc03abfc506
pronobis <pronobis@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-03-23 17:58:22 +0000

    .

--- code/subarchitectures/conceptual.sa/branches/avs-iros11/src/c++/conceptual/components/Tester/ConceptualWidget/ConceptualWidget.cc
+++ code/subarchitectures/conceptual.sa/branches/avs-iros11/src/c++/conceptual/components/Tester/ConceptualWidget/ConceptualWidget.cc
@@ -321,8 +321,7 @@ void ConceptualWidget::refreshWsButtonClicked()
 			{
 				QTreeWidgetItem *gatewayItem = new QTreeWidgetItem(placeholderItem, QStringList("Gateway Placeholder Property"));
 				gatewayItem->setIcon(0, QIcon(":/icons/icons/doorway.png"));
-				(new QTreeWidgetItem(gatewayItem, QStringList("Probability: "+QString::fromStdString(
-						lexical_cast<string>(phi.gatewayProperties[0].gatewayProbability)))))->
+				(new QTreeWidgetItem(gatewayItem, QStringList("Probability: "+QString::number(phi.gatewayProperties[0].gatewayProbability ))))->
 						setIcon(0, QIcon(":/icons/icons/flag-green.png"));
 			}
 			if (phi.associatedSpaceProperties.size())
