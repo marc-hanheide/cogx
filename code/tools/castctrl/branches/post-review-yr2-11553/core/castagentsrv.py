95933dc892db03c88a18c69bc20b89710274424e
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-03-28 07:17:16 +0000

    castctrl: merged trunk into postreview (many fixes)

--- code/tools/castctrl/branches/post-review-yr2-11553/core/castagentsrv.py
+++ code/tools/castctrl/branches/post-review-yr2-11553/core/castagentsrv.py
@@ -9,9 +9,9 @@ import stat
 import modice
 import icemodule.castcontrol.CastAgent as CastAgent
 # import icemodule.castcontrol.ProcessInfo as ProcessInfo
-from core import messages, options, procman
+from core import messages, logger, options, procman
 
-LOGGER = messages.CInternalLogger()
+LOGGER = logger.get()
 
 # Default port for the Ice server of a CastAgent.
 SLAVE_PORT=7832
@@ -71,16 +71,20 @@ class CAgentI(CastAgent.Agent):
         if p != None: p.stop()
         return 1 if p != None else 0
 
-    def setLog4jClientProperties(propText):
+    def setLog4jClientProperties(self, propText, current=None):
         # based on log4jutil.prepareClientConfig()
         # WARNING: if the client and the server are running from the same directory
         # the link to log4j properties will switch between two files
         logDir = os.path.abspath("./logs")
         logPropLink = "log4j.properties"
-        clientfile = os.path.join(ligDir, "ccatmp.log4client.conf")
+        clientfile = os.path.join(logDir, "ccatmp.log4client.conf")
+        if not os.path.exists(logDir):
+            os.makedirs(logDir)
+        f = open(clientfile, 'w')
+        f.write(propText)
+        f.close()
         if os.path.exists(logPropLink):
-            st = os.lstat(logPropLink)
-            if not stat.S_ISLNK(st.st_mode):
+            if not os.path.islink(logPropLink):
                 os.rename(logPropLink, os.tempnam(logDir, logPropLink))
             os.remove(logPropLink)
         os.symlink(clientfile, logPropLink)
@@ -115,7 +119,7 @@ class CCastSlave(threading.Thread):
         except:
             LOGGER.error(traceback.format_exc(20))
             status = 1
-        
+ 
         # Clean up
         try:
             if self._ic != None: self._ic.destroy()
@@ -171,7 +175,7 @@ def discoverRemoteHosts(hosts, tcpPort):
 
     for (haddr, chkr) in checker: chkr.start()
     for (haddr, chkr) in checker: chkr.join()
-    
+
     working = []
     for (haddr, chkr) in checker:
         if chkr.success: working.append(haddr)
