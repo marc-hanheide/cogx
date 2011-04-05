d0e459b7d5c253532d47b17e09118007078c6e0c
pronobis <pronobis@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-04-05 10:29:53 +0000

    Added new version of rocs to trunk.

---
+++ tools/rocs/trunk/src/vision/cpp/rocs/vision/Crfh/CrfhInterface.h
@@ -0,0 +1,145 @@
+// ==================================================================
+// ROCS - Toolkit for Robots Comprehending Space
+// Copyright (C) 2010  Arnaud Ramey, Andrzej Pronobis
+//
+// This file is part of ROCS.
+//
+// ROCS is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3
+// of the License, or (at your option) any later version.
+//
+// ROCS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ROCS. If not, see <http://www.gnu.org/licenses/>.
+// ==================================================================
+
+/*!
+ * \file CrfhInterface.h
+ *
+ * \date Oct 7, 2010
+ * \author Arnaud Ramey, Andrzej Pronobis
+ */
+
+#ifndef CRFHINTERFACE_H_
+#define CRFHINTERFACE_H_
+
+// rocs includes
+#include "rocs/cv/FeatureExtractor.h"
+#include "rocs/cv/Crfh/System.h"
+#include "rocs/cv/Crfh/Crfh.h"
+
+// STD includes
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+
+using namespace std;
+
+namespace rocs
+{
+namespace cv
+{
+
+class CrfhInterface: public rocs::cv::FeatureExtractor<Crfh>
+{
+public:
+	/*! ctor */
+	CrfhInterface()
+	{
+		rocsDebug3("CrfhInterface()");
+		setDefaultParams();
+	}
+	/*! dtor */
+	virtual ~CrfhInterface()
+	{
+	}
+
+	/*!
+	 * Initializes the system (creates descriptors and filters).
+	 * \param sysDescr Exemple string:
+	 * Lxx(8,28)+Lxy(8,28)+Lyy(8,28)+Lxx(2,28)+Lxy(2,28)+Lyy(2,28)
+	 *
+	 */
+	void defineSystem(string sysDescr)
+	{
+		rocsDebug1("defineSystem(sysDescr:'%s'", sysDescr.c_str());
+
+		/* Define a system */
+		//System _syst(sysDescr);
+		_syst.build(sysDescr);
+	}
+
+	/*!
+	 * set the minimal value for the histogram
+	 * \param minHistValue
+	 */
+	void setMinHistValue(double minHistValue)
+	{
+		rocsDebug3("setMinHistValue(%f)", minHistValue);
+		_minHistValue = minHistValue;
+	}
+
+	/*!
+	 * set the width of the border that is not analyzed in the image
+	 * \param skipBorderPixels
+	 */
+	void setSkipBorderPixels(int skipBorderPixels)
+	{
+		_skipBorderPixels = skipBorderPixels;
+	}
+
+	void setDefaultParams()
+	{
+		rocsDebug3("setDefaultParams()");
+		/* default params */
+		_minHistValue = 0;
+		_skipBorderPixels = 15;
+	}
+
+	void start()
+	{
+		rocsDebug3("start()");
+		/* reset timer */
+		_totalTime = 0;
+		_nbImagesTreated = 0;
+	}
+
+	void end()
+	{
+		rocsDebug3("end()");
+		// nothing to do
+	}
+
+	Crfh* processImage(const Img* image)
+	{
+		rocsDebug1("processImage(%s)", image->infoString().c_str());
+		_t.reset();
+
+		// make the actual computation
+		Crfh *crfh = _syst.computeHistogram(*image, _skipBorderPixels);
+		if (_minHistValue > 0)
+			crfh->filter(_minHistValue);
+		crfh->normalize();
+		// update timer
+		_totalTime += _t.getTimeMilliseconds();
+		_nbImagesTreated++;
+		return crfh;
+	}
+
+private:
+	System _syst;
+	double _minHistValue;
+	int _skipBorderPixels;
+};
+
+} // end namespace cv
+} // end namespace rocs
+
+#endif /* CRFHINTERFACE_H_ */
+
---
+++ tools/rocs/trunk/src/vision/cpp/rocs/vision/Img.cc
@@ -0,0 +1,97 @@
+// ==================================================================
+// ROCS - Toolkit for Robots Comprehending Space
+// Copyright (C) 2010  Arnaud Ramey, Andrzej Pronobis
+//
+// This file is part of ROCS.
+//
+// ROCS is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3
+// of the License, or (at your option) any later version.
+//
+// ROCS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ROCS. If not, see <http://www.gnu.org/licenses/>.
+// ==================================================================
+
+/*!
+ * \file Img.cc
+ *
+ * \date Jul 3, 2010
+ * \author Arnaud Ramey, Andrzej Pronobis
+ */
+
+#include "rocs/cv/Img.h"
+//#include "highgui.h"
+
+namespace rocs {
+namespace cv {
+
+Img::Img(int rows, int cols, int type) :
+	math::Matrix(rows, cols, type) {
+}
+
+Img::~Img() {
+}
+
+/*! Returns the intensity channel L as a matrix of doubles. */
+math::Matrix_<double> *Img::getL(math::Matrix_<double> *L /*= 0*/) const {
+	rocsDebug3("getL()");
+
+	/* convert to HLS */
+	// void cvtColor(const Mat& src, Mat& dst, int code, int dstCn=0)
+	opencv::Mat thisAsHls(nbRows(), nbCols(), CV_8U);
+	opencv::cvtColor(this->asConstOpenCvMat(), thisAsHls, CV_RGB2HLS);
+	//	debugPrintf_lvl3("thisAsHls after convert:depth=%i", thisAsHls.depth());
+	//	opencv::imshow("image_hls", thisAsHls);
+	//	opencv::waitKey();
+
+	/* get the wanted channel as uchar */
+	IplImage thisAsHlsAsIpl = thisAsHls;
+	math::Matrix_<uchar> channel(nbRows(), nbCols());
+	IplImage channelAsIpl = channel.asConstOpenCvMat();
+	cvSplit(&thisAsHlsAsIpl, 0, &channelAsIpl, 0, 0);
+	//	debugPrintf_lvl3("channel before return:%s, (1,1):%f", channel.infoCString(), (double) channel.get(1,1));
+	//	debugPrintf_lvl3("channel before return:%s", channel.toString().c_str());
+	//	opencv::imshow("test", channel.asConstOpenCvMat());
+	//	opencv::waitKey();
+
+	//	opencv::Mat thisAs32bits(nbRows(), nbCols(), CV_32SC3);
+	//	this->asConstOpenCvMat().convertTo(thisAs32bits, CV_32S);
+	//	debugPrintf_lvl3("thisAs32bits after convert:depth=%i", thisAs32bits.depth());
+	//	opencv::Mat thisAsHls(nbRows(), nbCols(), CV_32SC3);
+	//	opencv::cvtColor(thisAs32bits, thisAsHls, CV_RGB2HLS);
+	//	debugPrintf_lvl3("thisAsHls after convert:depth=%i", thisAsHls.depth());
+	//	opencv::imshow("image_hls", thisAsHls);
+	//	opencv::waitKey();
+	//
+	//	IplImage foo = thisAsHls;
+	//	math::Matrix_<uchar> test_m(nbRows(), nbCols());
+	//	IplImage test = test_m.asConstOpenCvMat();
+	//	cvSplit(&foo, &test, 0, 0, 0);
+	//	opencv::imshow("test", test_m.asConstOpenCvMat());
+	//	opencv::waitKey();
+
+	/* create the channel */
+	if (L == 0) {
+		rocsDebug3("Creating the L matrix.");
+		L = new math::Matrix_<double>(nbRows(), nbCols());
+	}
+
+	/* convert to double */
+	channel.copyTo_<uchar, double> (*L);
+	//	debugPrintf_lvl3("L before return:%s, (1,1):%f", L->infoCString(), L->get(1,1));
+	//	debugPrintf_lvl3("L before return:%s", L->toString().c_str());
+
+	rocsDebug3("getL() finished.");
+
+	/* return the wanted channel */
+	return L;
+}
+
+} // end namespace cv
+} // end namespace rocs
