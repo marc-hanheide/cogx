4139ea8c362a774fa059f5cfd8bf6aadbebf609d
pronobis <pronobis@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-04-06 12:43:09 +0000

    .

--- code/subarchitectures/conceptual.sa/branches/avs-iros11/src/c++/conceptual/components/Tester/MainDialog.cc
+++ code/subarchitectures/conceptual.sa/branches/avs-iros11/src/c++/conceptual/components/Tester/MainDialog.cc
@@ -3,16 +3,22 @@
 #include "AVSMainWidget.h"
 #include "NavWidget.h"
 #include "DefaultWidget.h"
+#include "DemoWidget.h"
+#include "CategoricalWidget.h"
 
 MainDialog::MainDialog(conceptual::Tester *component)
     : QDialog(0, Qt::WindowMinMaxButtonsHint), _component(component)
 {
 	ui.setupUi(this);
+	_demoWidget = new DemoWidget(this, component);
 	_conceptualWidget = new ConceptualWidget(this, component);
+	_categoricalWidget = new CategoricalWidget(this, component);
 	_defaultWidget = new DefaultWidget(this, component);
 	_avsMainWidget = new AVSMainWidget(this, component);
 	_navWidget = new NavWidget(this, component);
+	ui.tabWidget->addTab(_demoWidget, "Semantic Mapping Demo");
 	ui.tabWidget->addTab(_conceptualWidget, "Conceptual.SA Tester");
+	ui.tabWidget->addTab(_categoricalWidget, "Categorical.SA Tester");
 	ui.tabWidget->addTab(_defaultWidget, "Default.SA Tester");
 	ui.tabWidget->addTab(_avsMainWidget, "AVS Tester");
 	ui.tabWidget->addTab(_navWidget, "Naigation Tester");
