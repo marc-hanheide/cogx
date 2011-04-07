0854aaf72dd72d8321f00bbe005b5b5f19f255f4
michaelz <michaelz@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-04-07 22:16:48 +0000

    added --fixed option

--- subarchitectures/vision.sa/branches/spring-school-2011/src/c++/vision/components/CameraMount/CameraMount.cpp
+++ subarchitectures/vision.sa/branches/spring-school-2011/src/c++/vision/components/CameraMount/CameraMount.cpp
@@ -6,6 +6,9 @@
  * Oct 2009
  */
 
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
 #include "cogxmath.h"
 #include "Video.hpp"
 #include "VideoUtils.h"
@@ -26,15 +29,32 @@ extern "C"
   }
 }
 
+static Pose3 readPoseXML(const string &filename)
+{
+  cv::FileStorage poseFile(filename, cv::FileStorage::READ);
+  CvMat *t = (CvMat*)poseFile["tvec"].readObj();
+  CvMat *r = (CvMat*)poseFile["rvec"].readObj();
+  Pose3 pose;
+  fromRotVector(pose.rot, vector3(cvmGet(r, 0, 0), cvmGet(r, 1, 0), cvmGet(r, 2, 0)));
+  pose.pos = vector3(cvmGet(t, 0, 0), cvmGet(t, 1, 0), cvmGet(t, 2, 0));
+  return pose;
+}
+
 void CameraMount::configure(const map<string,string> & _config)
   throw(runtime_error)
 {
   map<string,string>::const_iterator it;
- 
+
+  setIdentity(ptZeroPose);
   if((it = _config.find("--pt_zero_pose")) != _config.end())
   {
     istringstream str(it->second);
     str >> ptZeroPose;
+  }
+  if((it = _config.find("--pt_zero_pose_xml")) != _config.end())
+  {
+    string filename = it->second;
+    camPoses.push_back(readPoseXML(filename));
   }
 
   if((it = _config.find("--cam_ids")) != _config.end())
@@ -47,36 +67,66 @@ void CameraMount::configure(const map<string,string> & _config)
 
   if((it = _config.find("--cam_poses")) != _config.end())
   {
-    Pose3 pose;
     istringstream str(it->second);
     for(size_t i = 0; i < camIds.size(); i++)
     {
+      Pose3 pose;
       str >> pose;
       camPoses.push_back(pose);
     }
+  }
+  if((it = _config.find("--cam_poses_xml")) != _config.end())
+  {
+    istringstream str(it->second);
+    for(size_t i = 0; i < camIds.size(); i++)
+    {
+      string filename;
+      str >> filename;
+      camPoses.push_back(readPoseXML(filename));
+    }
+  }
+
+  isFixed = false;
+  if((it = _config.find("--fixed")) != _config.end())
+  {
+    isFixed = true;
   }
 }
 
 void CameraMount::start()
 {
-  Ice::CommunicatorPtr ic = getCommunicator();
+  if(!isFixed)
+  {
+    Ice::CommunicatorPtr ic = getCommunicator();
 
-  Ice::Identity id;
-  id.name = "PTZServer";
-  id.category = "PTZServer";
+    Ice::Identity id;
+    id.name = "PTZServer";
+    id.category = "PTZServer";
 
-  std::ostringstream str;
-  str << ic->identityToString(id) 
-    << ":default"
-    << " -h localhost"
-    << " -p " << cast::cdl::CPPSERVERPORT;
+    std::ostringstream str;
+    str << ic->identityToString(id) 
+      << ":default"
+      << " -h localhost"
+      << " -p " << cast::cdl::CPPSERVERPORT;
 
-  Ice::ObjectPrx base = ic->stringToProxy(str.str());    
-  m_PTUServer = ptz::PTZInterfacePrx::uncheckedCast(base);
+    Ice::ObjectPrx base = ic->stringToProxy(str.str());    
+    m_PTUServer = ptz::PTZInterfacePrx::uncheckedCast(base);
+  }
 }
 
 void CameraMount::runComponent()
 {
+  vector<Pose3> camPosesToEgo;
+  ptz::PTZReading ptz;
+
+  // initial pose: pan, tilt = 0
+  ptz.pose.pan = 0.;
+  ptz.pose.tilt = 0.;
+  ptz.pose.zoom = 0.;
+  ptz.time = getCASTTime();
+  calculatePoses(ptz, camPosesToEgo);
+
+  // add camera parameters to WM, with initial poses
   camWMIds.resize(camPoses.size());
   for(size_t i = 0; i < camPoses.size(); i++)
   {
@@ -85,32 +135,36 @@ void CameraMount::runComponent()
     // only fill pose and time (let video server fill the rest)
     // we only know about poses
     camParms->cam.id = camIds[i];
-    camParms->cam.pose = camPoses[i];
-    camParms->cam.time = getCASTTime();
+    camParms->cam.pose = camPosesToEgo[i];
+    camParms->cam.time = ptz.time;
     camWMIds[i] = newDataID();
     addToWorkingMemory(camWMIds[i], camParms);
   }
 
-  while(isRunning())
+  // if not fixed, continuosly (with a nasty sleep) get pan, tilt values and
+  // update poses
+  if(!isFixed)
   {
-    vector<Pose3> camPosesToEgo;
-	  ptz::PTZReading ptz = m_PTUServer->getPose();
-    calculatePoses(ptz, camPosesToEgo);
-    for(size_t i = 0; i < camPoses.size(); i++)
+    while(isRunning())
     {
-      CameraParametersWrapperPtr camParms = new CameraParametersWrapper;
-      initCameraParameters(camParms->cam);
-      // only fill pose and time (let video server fill the rest)
-      // we only know about poses
-      camParms->cam.id = camIds[i];
-      camParms->cam.pose = camPosesToEgo[i];
-      camParms->cam.time = ptz.time;
-      overwriteWorkingMemory(camWMIds[i], camParms);
+	    ptz = m_PTUServer->getPose();
+      calculatePoses(ptz, camPosesToEgo);
+      for(size_t i = 0; i < camPoses.size(); i++)
+      {
+        CameraParametersWrapperPtr camParms = new CameraParametersWrapper;
+        initCameraParameters(camParms->cam);
+        // only fill pose and time (let video server fill the rest)
+        // we only know about poses
+        camParms->cam.id = camIds[i];
+        camParms->cam.pose = camPosesToEgo[i];
+        camParms->cam.time = ptz.time;
+        overwriteWorkingMemory(camWMIds[i], camParms);
+      }
+      // HACK: should get rid of need for sleep
+      // We will hardly be moving the pan tilt unit at first, so long update
+      // interval should not be a problem.
+      sleepComponent(1000);
     }
-    // HACK: should get rid of need for sleep
-    // We will hardly be moving the pan tilt unit at first, so long update
-    // interval should not be a problem.
-    sleepComponent(1000);
   }
 }
 
