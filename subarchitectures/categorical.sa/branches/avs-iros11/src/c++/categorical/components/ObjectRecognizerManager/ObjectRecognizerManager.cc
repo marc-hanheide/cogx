ab6f9e01d3a8ed93b5e3784a5b73e843840647be
pronobis <pronobis@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-04-11 16:04:26 +0000

    .

--- subarchitectures/categorical.sa/branches/avs-iros11/src/c++/categorical/components/ObjectRecognizerManager/ObjectRecognizerManager.cc
+++ subarchitectures/categorical.sa/branches/avs-iros11/src/c++/categorical/components/ObjectRecognizerManager/ObjectRecognizerManager.cc
@@ -7,7 +7,9 @@
 // Conceptual.SA
 #include "ObjectRecognizerManager.h"
 // System
+#include "ConceptualData.hpp"
 #include "VisionData.hpp"
+#include "SpatialProperties.hpp"
 // CAST
 #include <cast/architecture/ChangeFilterFactory.hpp>
 
@@ -29,7 +31,7 @@ using namespace cast;
 using namespace VisionData;
 
 // -------------------------------------------------------
-ObjectRecognizerManager::ObjectRecognizerManager()
+ObjectRecognizerManager::ObjectRecognizerManager() : _curRoomId(0)
 {
 }
 
@@ -69,6 +71,12 @@ void ObjectRecognizerManager::start()
 	addChangeFilter(createGlobalTypeFilter<VisionData::Recognizer3DCommand>(cdl::OVERWRITE),
 			new MemberFunctionChangeReceiver<ObjectRecognizerManager>(this,
 					&ObjectRecognizerManager::overwriteRecognizer3DCommand));
+	addChangeFilter(createGlobalTypeFilter<ConceptualData::WorldState>(cdl::OVERWRITE),
+			new MemberFunctionChangeReceiver<ObjectRecognizerManager>(this,
+					&ObjectRecognizerManager::worldStateChanged));
+	addChangeFilter(createGlobalTypeFilter<ConceptualData::WorldState>(cdl::ADD),
+			new MemberFunctionChangeReceiver<ObjectRecognizerManager>(this,
+					&ObjectRecognizerManager::worldStateChanged));
 
 	try
 	{
@@ -122,7 +130,28 @@ void ObjectRecognizerManager::overwriteRecognizer3DCommand(const cdl::WorkingMem
 	VisionData::Recognizer3DCommandPtr rec_cmd = getMemoryEntry<VisionData::Recognizer3DCommand>(_wmc.address);
 	if (rec_cmd->confidence>0.03)
 	{
-		println("Found %s %f", rec_cmd->label.c_str(), rec_cmd->confidence);
+		println("Visual detection of %s %f", rec_cmd->label.c_str(), rec_cmd->confidence);
+
+		// Current place
+		int placeId = getCurrentPlace();
+
+		// Check if we have it
+		bool exists = false;
+		for (ObjectPlaceList::iterator it = _objectInPlace.begin();
+				it!=_objectInPlace.end(); ++it)
+		{
+			if ( (it->first == rec_cmd->label) && (it->second == placeId))
+			{
+				exists = true;
+				break;
+			}
+		}
+
+		if (!exists)
+		{
+			println("Found new object %s!", rec_cmd->label.c_str());
+			addNewObject(rec_cmd->label, placeId, _curRoomId);
+		}
 	}
 }
 
@@ -136,6 +165,112 @@ int ObjectRecognizerManager::getCurrentPlace()
 	else
 		return -1;
 }
+
+
+void ObjectRecognizerManager::addNewObject(std::string label, int curPlaceId, int curRoomId)
+{
+	SpatialProperties::ObjectPlacePropertyPtr opp = new SpatialProperties::ObjectPlaceProperty();
+	opp->inferred = false;
+	opp->category = label;
+	opp->placeId = curPlaceId;
+	opp->relation = SpatialData::INROOM;
+	opp->supportObjectCategory = "";
+	opp->supportObjectId = "";
+	SpatialProperties::DiscreteProbabilityDistributionPtr pd = new SpatialProperties::DiscreteProbabilityDistribution();
+	SpatialProperties::ValueProbabilityPair vap1;
+		SpatialProperties::BinaryValuePtr bv1 = new SpatialProperties::BinaryValue();
+		bv1->value = true;
+		vap1.probability = 1.0;
+		vap1.value = bv1;
+	pd->data.push_back(vap1);
+	SpatialProperties::ValueProbabilityPair vap2;
+		SpatialProperties::BinaryValuePtr bv2 = new SpatialProperties::BinaryValue();
+		bv2->value = false;
+		vap2.probability = 0.0;
+		vap2.value = bv2;
+	pd->data.push_back(vap2);
+
+	opp->distribution = pd;
+		SpatialProperties::BinaryValuePtr bv3 = new SpatialProperties::BinaryValue();
+		bv3->value = true;
+	opp->mapValue = bv3;
+	opp->mapValueReliable = true;
+
+	addToWorkingMemory<SpatialProperties::ObjectPlaceProperty>(
+			newDataID(), "spatial.sa", opp);
+
+
+
+    // Check if we have such result already
+	bool exists = false;
+	vector< boost::shared_ptr< cast::CASTData< SpatialData::ObjectSearchResult > > > results;
+	getWorkingMemoryEntries<SpatialData::ObjectSearchResult> ("spatial.sa", 0 , results);
+	for (unsigned int i=0; i<results.size(); ++i)
+	{
+		if ((results[i]->getData()->searchedObjectCategory == label) &&
+			(results[i]->getData()->supportObjectId.empty()) &&
+			(results[i]->getData()->supportObjectCategory.empty()) &&
+			(results[i]->getData()->relation == SpatialData::INROOM) &&
+			(results[i]->getData()->roomId == curRoomId))
+		{
+			results[i]->getData()->beta = 1.0;
+			overwriteWorkingMemory<SpatialData::ObjectSearchResult>(results[i]->getID(), "spatial.sa", results[i]->getData());
+			exists=true;
+			break;
+		}
+	}
+	if (!exists)
+	{
+		SpatialData::ObjectSearchResultPtr osr = new SpatialData::ObjectSearchResult();
+		osr->beta = 1.0;
+		osr->roomId = curRoomId;
+		osr->searchedObjectCategory = label;
+		osr->supportObjectCategory = "";
+		osr->supportObjectId = "";
+		osr->relation = SpatialData::INROOM;
+
+		addToWorkingMemory<SpatialData::ObjectSearchResult>(
+				newDataID(), "spatial.sa", osr);
+	}
+
+}
+
+
+// -------------------------------------------------------
+void ObjectRecognizerManager::worldStateChanged(const cast::cdl::WorkingMemoryChange & wmChange)
+{
+	try
+	{
+		ConceptualData::WorldStatePtr worldStatePtr;
+		worldStatePtr = getMemoryEntry<ConceptualData::WorldState>(wmChange.address);
+
+		if (worldStatePtr)
+		{
+			int curPlaceId = getCurrentPlace();
+			for (size_t r=0; r<worldStatePtr->rooms.size(); ++r)
+			{
+				ConceptualData::ComaRoomInfo &room = worldStatePtr->rooms[r];
+				for (size_t p=0; p<room.places.size(); ++p)
+				{
+					ConceptualData::PlaceInfo &place = room.places[p];
+					if (place.placeId == curPlaceId)
+					{
+						_curRoomId = room.roomId;
+						break;
+					}
+				}
+			} // for
+		}
+	}
+	catch(CASTException &e)
+	{
+		log("Exception while reading world state from the WM!");
+	}
+}
+
+
+
+
 
 
 } // namespace def
