96956a5c719fff532cab599d8e69ce38d17dcd96
alper <alper@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-04-11 13:55:31 +0000

    getting there...

---
+++ subarchitectures/data.sa/trunk/src/c++/data/components/DataSaver/DataSaver.h
@@ -0,0 +1,20 @@
+/*
+ * DataSaver.h
+ *
+ *  Created on: Apr 5, 2011
+ *      Author: alper
+ */
+
+#ifndef DATASAVER_H_
+#define DATASAVER_H_
+
+
+#include <cast/architecture/ManagedComponent.hpp>
+
+class DataSaver: public cast::ManagedComponent {
+public:
+	DataSaver();
+	virtual ~DataSaver();
+};
+
+#endif /* DATASAVER_H_ */
