eceb6689fecf52f0237c708a8bbbd4bb168bed02
michaelz <michaelz@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-04-26 22:13:16 +0000

    fiddled with runComponent(), now behaves the same wither fixed or using PTZ

--- subarchitectures/vision.sa/branches/spring-school-2011/src/c++/vision/components/CameraMount/CameraMount.cpp
+++ subarchitectures/vision.sa/branches/spring-school-2011/src/c++/vision/components/CameraMount/CameraMount.cpp
@@ -82,10 +82,9 @@ void CameraMount::configure(const map<string,string> & _config)
   if((it = _config.find("--cam_poses_xml")) != _config.end())
   {
     istringstream str(it->second);
-    for(size_t i = 0; i < camIds.size(); i++)
+    string filename;
+    while(str >> filename)
     {
-      string filename;
-      str >> filename;
       Pose3 pose;
       readXML(filename, pose);
       camPoses.push_back(pose);
@@ -98,6 +97,9 @@ void CameraMount::configure(const map<string,string> & _config)
   {
     isFixed = true;
   }
+
+  if(camIds.size() != camPoses.size())
+    throw runtime_error("number of camera IDs must match number of camera poses");
 }
 
 void CameraMount::start()
@@ -123,35 +125,36 @@ void CameraMount::start()
 
 void CameraMount::runComponent()
 {
-  vector<Pose3> camPosesToEgo;
-  ptz::PTZReading ptz;
-
-  // initial pose: pan, tilt = 0
-  ptz.pose.pan = 0.;
-  ptz.pose.tilt = 0.;
-  ptz.pose.zoom = 0.;
-  ptz.time = getCASTTime();
-  calculatePoses(ptz, camPosesToEgo);
-
-  // add camera parameters to WM, with initial poses
-  camWMIds.resize(camPoses.size());
-  for(size_t i = 0; i < camPoses.size(); i++)
-  {
-    CameraParametersWrapperPtr camParms = new CameraParametersWrapper;
-    initCameraParameters(camParms->cam);
-    // only fill pose and time (let video server fill the rest)
-    // we only know about poses
-    camParms->cam.id = camIds[i];
-    camParms->cam.pose = camPosesToEgo[i];
-    camParms->cam.time = ptz.time;
-    camWMIds[i] = newDataID();
-    addToWorkingMemory(camWMIds[i], camParms);
-  }
-
-  // if not fixed, continuosly (with a nasty sleep) get pan, tilt values and
-  // update poses
   if(!isFixed)
   {
+    log("using PTU");
+    vector<Pose3> camPosesToEgo;
+    ptz::PTZReading ptz;
+
+    // initial pose: pan, tilt = 0
+    ptz.pose.pan = 0.;
+    ptz.pose.tilt = 0.;
+    ptz.pose.zoom = 0.;
+    ptz.time = getCASTTime();
+    calculatePoses(ptz, camPosesToEgo);
+
+    // add camera parameters to WM, with initial poses
+    camWMIds.resize(camPoses.size());
+    for(size_t i = 0; i < camPoses.size(); i++)
+    {
+      CameraParametersWrapperPtr camParms = new CameraParametersWrapper;
+      initCameraParameters(camParms->cam);
+      // only fill pose and time (let video server fill the rest)
+      // we only know about poses
+      camParms->cam.id = camIds[i];
+      camParms->cam.pose = camPosesToEgo[i];
+      camParms->cam.time = ptz.time;
+      camWMIds[i] = newDataID();
+      addToWorkingMemory(camWMIds[i], camParms);
+    }
+
+    // continuosly (with a nasty sleep) get pan, tilt values and
+    // update poses
     while(isRunning())
     {
 	    ptz = m_PTUServer->getPose();
@@ -164,12 +167,50 @@ void CameraMount::runComponent()
         // we only know about poses
         camParms->cam.id = camIds[i];
         camParms->cam.pose = camPosesToEgo[i];
+        log("cam pose in robot ego:" + toString(camParms->cam.pose));
         camParms->cam.time = ptz.time;
         overwriteWorkingMemory(camWMIds[i], camParms);
       }
       // HACK: should get rid of need for sleep
       // We will hardly be moving the pan tilt unit at first, so long update
       // interval should not be a problem.
+      sleepComponent(1000);
+    }
+  }
+  else
+  {
+    log("using fixed cam position and no PTU");
+    // add camera parameters to WM, with initial poses
+    camWMIds.resize(camPoses.size());
+    for(size_t i = 0; i < camPoses.size(); i++)
+    {
+      CameraParametersWrapperPtr camParms = new CameraParametersWrapper;
+      initCameraParameters(camParms->cam);
+      // only fill pose and time (let video server fill the rest)
+      // we only know about poses
+      camParms->cam.id = camIds[i];
+      camParms->cam.pose = camPoses[i];
+      camParms->cam.time = getCASTTime();
+      camWMIds[i] = newDataID();
+      addToWorkingMemory(camWMIds[i], camParms);
+    }
+
+    // continuosly (with a nasty sleep) update poses
+    while(isRunning())
+    {
+      for(size_t i = 0; i < camPoses.size(); i++)
+      {
+        CameraParametersWrapperPtr camParms = new CameraParametersWrapper;
+        initCameraParameters(camParms->cam);
+        // only fill pose and time (let video server fill the rest)
+        // we only know about poses
+        camParms->cam.id = camIds[i];
+        camParms->cam.pose = camPoses[i];
+        log("cam pose in robot ego:" + toString(camParms->cam.pose));
+        camParms->cam.time = getCASTTime();
+        overwriteWorkingMemory(camWMIds[i], camParms);
+      }
+      // HACK: should get rid of need for sleep
       sleepComponent(1000);
     }
   }
