4bfcf65377b36dbb64ec9828721e7f1454e8f728
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-05-01 19:40:02 +0000

    adding a skeleton of the execution monitoring loop

--- code/systems/css-2011-teams/red/subarchitectures/manipulation.sa/src/java/manipulation/planner/ManipulationPlanner.java
+++ code/systems/css-2011-teams/red/subarchitectures/manipulation.sa/src/java/manipulation/planner/ManipulationPlanner.java
@@ -1,5 +1,5 @@
 /**
- * @author Marc Hanheide (marc@hanheide.de)
+ * @author Team RED
  */
 
 package manipulation.planner;
@@ -30,6 +30,7 @@ import org.apache.log4j.Logger;
 
 import VisionData.Face;
 import VisionData.GeometryModel;
+import VisionData.GripperPose;
 import VisionData.Vertex;
 import VisionData.VisualObject;
 import VisionData.VisualObjectView;
@@ -37,6 +38,7 @@ import cast.AlreadyExistsOnWMException;
 import cast.CASTException;
 import cast.architecture.ChangeFilterFactory;
 import cast.architecture.WorkingMemoryChangeReceiver;
+import cast.cdl.WorkingMemoryOperation;
 import cast.cdl.CASTTime;
 import cast.cdl.WorkingMemoryAddress;
 import cast.cdl.WorkingMemoryChange;
@@ -47,6 +49,110 @@ import cogx.Math.Sphere3;
 import cogx.Math.Vector2;
 import cogx.Math.Vector3;
 
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ManipulationPlanner extends ManagedComponent {
+
+	private ManipulationCommand currentCommand = null;
+	private ConcurrentLinkedQueue<ManipulationCommand> currentPlan =
+			new ConcurrentLinkedQueue<ManipulationCommand>();
+
+	@Override
+	protected void configure(Map<String, String> config) {
+		// TODO: config
+	}
+
+	@Override
+	protected void start() {
+		// register GripperPose listener
+		addChangeFilter(ChangeFilterFactory.createTypeFilter(
+				GripperPose.class, WorkingMemoryOperation.ADD),
+				new WorkingMemoryChangeReceiver() {
+					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
+						handleGripperPose(_wmc);
+					}
+				});
+	}
+
+	@Override
+	protected void runComponent() {
+		while (this.isRunning()) {
+			this.sleepComponent(100);
+			if (currentCommand != null) {
+				if (currentCommand.comp == ManipulationCompletion.SUCCEEDED) {
+					log("command succeeded, will execute the next one");
+					removeCurrentCommand();
+
+					ManipulationCommand cmd = currentPlan.poll();
+					if (cmd != null) {
+						addCurrentCommand(cmd);
+					}
+					else {
+						log("plan empty");
+					}
+
+				}
+				else if (currentCommand.comp == ManipulationCompletion.FAILED) {
+					log("command failed, will empty the plan");
+					removeCurrentCommand();
+					currentPlan.clear();
+				}
+				else {
+					// do nothing
+				}
+			}
+			else {
+				ManipulationCommand cmd = currentPlan.poll();
+				if (cmd != null) {
+					log("starting a plan");
+					addCurrentCommand(cmd);
+				}
+			}
+		}
+	}
+
+	synchronized void addCurrentCommand(ManipulationCommand cmd) {
+		log("adding new command");
+		currentCommand = cmd;
+		String id = newDataID();
+
+		try {
+			addToWorkingMemory(id, cmd);
+		}
+		catch (AlreadyExistsOnWMException ex) {
+			log(ex);
+		}
+
+		// TODO: register the change filter for the cmd
+	}
+
+	synchronized void overwriteCurrentCommand(ManipulationCommand cmd) {
+		currentCommand = cmd;
+	}
+
+	synchronized void removeCurrentCommand() {
+		log("removing current command");
+
+		// TODO: unregister the change filter
+
+		currentCommand = null;
+	}
+
+	public void handleGripperPose(WorkingMemoryChange wmc) {
+		List<ManipulationCommand> newPlan = new LinkedList<ManipulationCommand>();
+
+		// TODO: generate the plan
+
+		log("adding plan of length " + newPlan.size() + " to the overall plan");
+		currentPlan.addAll(newPlan);
+	}
+
+	public void handleCommandOverwrite(WorkingMemoryChange wmc) {
+		log("detected a change on address [" + wmc.address.id + "]");
+		// TODO: overwrite currentCommand with the newly changed object
+	}
 
 }
