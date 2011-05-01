f284626311ee755eb68ce2ffd17d3a5d0bb50982
renaud.detry <renaud.detry@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-05-01 14:47:30 +0000

    Finite state machine for the blue team

---
+++ code/systems/css-2011-teams/blue/subarchitectures/manipulation.sa/src/c++/components/BlueFSM/BlueFSM.cpp
@@ -0,0 +1,62 @@
+/**
+ * @author Michael Zillich
+ * @date April 2011
+*/
+
+#include <iostream>
+#include <cast/architecture/ChangeFilterFactory.hpp>
+#include "BlueFSM.h"
+
+/**
+ * The function called to create a new instance of our component.
+ */
+extern "C"
+{
+  cast::CASTComponentPtr newComponent()
+  {
+    return new cogx::BlueFSM();
+  }
+}
+
+namespace cogx
+{
+
+  using namespace std;
+  using namespace cast;
+  
+  BlueFSM::BlueFSM()
+  {
+  }
+  
+  void BlueFSM::configure(const map<string, string> &_config)
+  {
+    map<string,string>::const_iterator it;
+    
+  }
+  
+  void BlueFSM::start()
+  {
+    addChangeFilter(createGlobalTypeFilter<VisionData::VisualObject>(cdl::OVERWRITE),
+                    new MemberFunctionChangeReceiver<BlueFSM>(this, &BlueFSM::objectPoseCallback));
+    addChangeFilter(createGlobalTypeFilter<VisionData::VisualObject>(cdl::ADD),
+                    new MemberFunctionChangeReceiver<BlueFSM>(this, &BlueFSM::objectPoseCallback));
+  }
+  
+  void BlueFSM::destroy()
+  {
+  }
+  
+  void BlueFSM::runComponent()
+  {
+  }
+  
+  void BlueFSM::objectPoseCallback(const cdl::WorkingMemoryChange &_wmc)
+  {
+  log("received objectPoseCallback");
+  VisionData::VisualObjectPtr cmd = getMemoryEntry<VisionData::VisualObject>(_wmc.address);
+
+  log("finished objectPoseCallback");
+}
+
+  
+}
