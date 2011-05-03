afff8a4ff08620b11d7954b101e016098831430b
krsj <krsj@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-05-03 12:15:26 +0000

    Whoa

--- systems/css-2011-teams/blue/subarchitectures/manipulation.sa/src/c++/components/BlueFSM/BlueFSM.h
+++ systems/css-2011-teams/blue/subarchitectures/manipulation.sa/src/c++/components/BlueFSM/BlueFSM.h
@@ -7,6 +7,16 @@
 #include <boost/thread/mutex.hpp>
 #include <Navigation/LocalGridMap.hh>
 #include <FrontierInterface.hpp>
+#include <Scan2dReceiver.hpp>
+#include <OdometryReceiver.hpp>
+#include <Navigation/LocalMap.hh>
+#include <Navigation/GridLineRayTracer.hh>
+#include <Map/TransformedOdomPoseProvider.hh>
+#include <SensorData/LaserScan2d.hh>
+#include <CureHWUtils.hpp>
+
+typedef Cure::LocalGridMap<unsigned char> CharMap ;
+typedef Cure::GridLineRayTracer<unsigned char> CharGridLineRayTracer;
 
 namespace cogx
 {
@@ -15,11 +25,14 @@ namespace cogx
   using namespace std;
   using namespace cast;
   
-  class BlueFSM : public ManagedComponent
+  class BlueFSM : public ManagedComponent,
+  		  public OdometryReceiver,
+		  public Scan2dReceiver
   {
     enum State {INIT,
       SPINNING,
       MOVE_TO_NEW_POS,
+      DECIDE_POSITION,
       LOOK_CANONICAL, //Pan-tilt in calibrated position
       LOOK_AROUND,    //Pan-tilt ambulatory
       DETECTING,
@@ -55,11 +68,18 @@ namespace cogx
     bool retract();
     bool grasp();
     bool release();
+    bool turn45Degrees();
+    bool turnTo(double theta);
+    bool moveTo(double x, double y, double theta = FLT_MAX);
+    bool movePTZ(double pan, double tilt);
     
     void objectPoseCallback(const cdl::WorkingMemoryChange &_wmc);
-    void simpleCallback(const cdl::WorkingMemoryChange &_wmc);
 
-    std::pair<Math::Pose3, double> nameless(const Math::Pose3& inRobotPose,
+    void simpleCallback(const cdl::WorkingMemoryChange &_wmc);
+    void navCallback(const cdl::WorkingMemoryChange &_wmc);
+    void ptzCallback(const cdl::WorkingMemoryChange &_wmc);
+
+    void nameless(const Math::Pose3& inRobotPose,
                                             const Math::Pose3& inObjectPose,
                                             const std::string& inObjectLabel,
                                             Math::Pose3& outPregraspPose,
@@ -69,11 +89,14 @@ namespace cogx
     State m_state;
     bool m_waiting;
 
+    bool m_dropTableDetected;
+
     int m_turnStep;
 
     boost::mutex mutex_;
 
     map<string, cogx::Math::Pose3> m_poses;
+    map<string, cogx::Math::Pose3> m_globalPoses;
     map<string, double> m_pose_confs;
 
     vector<string> m_lookForObjects;
@@ -82,17 +105,27 @@ namespace cogx
     cogx::Math::Pose3 m_pregraspPose;
     cogx::Math::Pose3 m_envelopingPose;
     cogx::Math::Pose3 m_currentArmPose;
-    
-//    PTZInterfacePrx m_ptzInterface;
+
+    //SLAM related
+    Cure::SensorPose m_LaserPoseR;
+    Cure::TransformedOdomPoseProvider m_TOPP;
+    CharMap* m_lgm;
+    CharGridLineRayTracer* m_Glrt;
+    Math::Pose3 m_CurrPose;
+
   private:
-    bool findGraspPoses(double objX, double objY, double theta, double halfLength,
-	double bestX, double bestY);
+    void receiveScan2d(const Laser::Scan2d &castScan);
+    void receiveOdometry(const Robotbase::Odometry &castOdom);
+    bool findBestGraspPose(const string &obj, double &bestX, double &bestY, 
+	double &bestTheta);
+    void findRandomPosition(double &x, double &y);
 
     FrontierInterface::PlaceInterfacePrx m_placeInterface;
     FrontierInterface::LocalMapInterfacePrx m_mapInterface;
 
   public:
     BlueFSM();
+    ~BlueFSM();
   };
   
 }
