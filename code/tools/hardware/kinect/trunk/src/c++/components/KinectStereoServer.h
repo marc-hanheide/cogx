a9dae602845db5b441b02ed61b5ea47744785c91
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-05-17 13:23:07 +0000

    kinect: getCameraParameters

--- code/tools/hardware/kinect/trunk/src/c++/components/KinectStereoServer.h
+++ code/tools/hardware/kinect/trunk/src/c++/components/KinectStereoServer.h
@@ -124,6 +124,7 @@ public:
   void getPoints(bool transformToGlobal, int imgWidth, std::vector<PointCloud::SurfacePoint> &points, bool complete);
   void getRectImage(int side, int imgWidth, Video::Image& image);
   void getDisparityImage(int imgWidth, Video::Image& image);
+  bool getCameraParameters(Ice::Int side, Video::CameraParameters& camPars);
   void receiveImages(const std::vector<Video::Image>& images);
 };
 
