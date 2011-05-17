2bae5d800491c8426c190e49d9f9498861506a8c
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-05-17 13:17:02 +0000

    v11n: use relative paths in cmake

--- subarchitectures/visualization.sa/trunk/cmake/v11n.cmake
+++ subarchitectures/visualization.sa/trunk/cmake/v11n.cmake
@@ -1,16 +1,24 @@
 # Include the visualization subarchitecture
 # To be included in the top-level CogX CMakeLists.txt
 
+macro(relative_path OUTPUT BASE TARGET)
+   string(REGEX REPLACE "^${BASE}/" "" _temp ${TARGET})
+   set(${OUTPUT} "${_temp}")
+endmacro(relative_path)
+
 option(BUILD_SA_V11N "Build visualization libraries (Requires QT4)" NO)
 if(BUILD_SA_V11N)
+   get_filename_component(VISUALIZATION_ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/.. ABSOLUTE)
+   relative_path(VISUALIZATION_ROOT_DIR_REL ${COGX_ROOT} ${VISUALIZATION_ROOT_DIR})
+
    set(VISUALIZATION_INCLUDE_DIRS
-      ${COGX_ROOT}/subarchitectures/visualization.sa/src/c++/autogen
-      ${COGX_ROOT}/subarchitectures/visualization.sa/src/c++/core/client
+      ${VISUALIZATION_ROOT_DIR}/src/c++/autogen
+      ${VISUALIZATION_ROOT_DIR}/src/c++/core/client
       )
    set(VISUALIZATION_LIBRARIES
       DisplayClient VisualizationData
       )
-   add_subdirectory(subarchitectures/visualization.sa)
+   add_subdirectory(${VISUALIZATION_ROOT_DIR_REL})
    add_definitions(-DFEAT_VISUALIZATION)
 endif(BUILD_SA_V11N)
 
