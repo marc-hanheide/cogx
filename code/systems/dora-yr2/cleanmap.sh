d147474d6a7cf599954c6c9c8980fb63df6ef721
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-05-23 14:55:48 +0000

    cleaned

--- code/systems/dora-yr2/cleanmap.sh
+++ code/systems/dora-yr2/cleanmap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #zenity --question --text "reset mapping?"  && rm -f robotpose.ccf tmpmap.* && exit 0
 #zenity --question --text "restore start map?" && rm -f robotpose.ccf && cp instantiations/map_backup/bham_lab/* .
-rm -f robotpose.ccf tmpmap.*
-cp instantiations/map_backup/alu_review_sim/* .
+rm -f robotpose.ccf tmpmap.* table+with+object.txt
+#cp instantiations/map_backup/bham_lab/* .
 
+#cp instantiations/map_backup/marcs_home/* .
