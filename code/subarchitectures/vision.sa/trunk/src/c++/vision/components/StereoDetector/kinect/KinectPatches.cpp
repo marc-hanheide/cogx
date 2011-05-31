8be5e9407609006ea1741c6713f3f9cb7ea5d570
ari <ari@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-05-31 14:16:41 +0000

    Calculations with Kinect data

---
+++ code/subarchitectures/vision.sa/trunk/src/c++/vision/components/StereoDetector/kinect/KinectPatches.cpp
@@ -0,0 +1,106 @@
+/**
+ * @file KinectPatches.cpp
+ * @author Andreas Richtsfeld
+ * @date May 2011
+ * @version 0.1
+ * @brief Kalculate 3D segments from Kinect data.
+ */
+
+#include "KinectCore.h"
+#include "KinectPatches.h"
+#include "VisionUtils.h"
+
+#include "PCLCommonHeaders.h"
+#include "Patch3D.h"
+
+namespace Z
+{
+
+
+/**
+ * @brief Constructor of class KinectPatches
+ * @param kc Kinect core
+ * @param vc Vision core
+ * @param iplI Ipl-Image
+ * @param p Point matrix
+ */
+KinectPatches::KinectPatches(KinectCore *kc, VisionCore *vc, IplImage *iplI, cv::Mat_<cv::Vec4f> &p) 
+                             : KinectBase(kc, vc, iplI, p)
+{
+  numPatches = 0;
+}
+
+/**
+ * @brief Clear all arrays.
+ */
+void KinectPatches::ClearResults()
+{
+  numPatches = 0;
+}
+
+
+/**
+ * @brief Calculate 3D patches from kinect data.
+ */
+void KinectPatches::Process()
+{
+  struct timespec start, current;
+  clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start);
+
+  pcl::PointCloud<pcl::PointXYZRGB> cloud;
+  pclU::Cv2PCLCloud(points, cloud);
+
+  // preprocess point cloud
+  bool useVoxelGrid = true;
+  double vg_size = 0.008;                 // 0.01 - 0.005
+  pclF::PreProcessPointCloud(cloud, useVoxelGrid, vg_size);
+
+  bool sac_optimal_distance = true;
+  double sac_optimal_weight_factor = 1.5;
+  double sac_distance = 0.005;            // 5mm
+  int sac_max_iterations = 100;
+  int sac_min_inliers = 25;
+  double ec_cluster_tolerance = 0.015;    // 15mm
+  int ec_min_cluster_size = 25;
+  int ec_max_cluster_size = 1000000;
+
+  std::vector< pcl::PointCloud<pcl::PointXYZRGB>::Ptr > pcl_plane_clouds;
+  std::vector< pcl::ModelCoefficients::Ptr > model_coefficients;
+  pclF::FitPlanesRecursive(cloud.makeShared(), pcl_plane_clouds, model_coefficients, sac_optimal_distance, sac_optimal_weight_factor, sac_distance, sac_max_iterations, 
+                           sac_min_inliers, ec_cluster_tolerance, ec_min_cluster_size, ec_max_cluster_size);
+                           
+//   pclF::FitPlanesRecursiveWithNormals(cloud.makeShared(), pcl_plane_clouds, model_coefficients, sac_optimal_distance, sac_optimal_weight_factor, sac_distance, sac_max_iterations, 
+//                            sac_min_inliers, ec_cluster_tolerance, ec_min_cluster_size, ec_max_cluster_size);
+
+  /// calculate convex hulls
+  std::vector< pcl::PointCloud<pcl::PointXYZRGB>::Ptr > pcl_hulls;
+  pclF::GetConvexHulls(pcl_plane_clouds, model_coefficients, pcl_hulls);
+
+  /// convert pcl point clouds to cv matrices
+  std::vector< cv::Mat_<cv::Vec4f> > cv_planes, cv_hulls;
+  pclU::PCLClouds2CvClouds(pcl_plane_clouds, cv_planes);
+  pclU::PCLClouds2CvClouds(pcl_hulls, cv_hulls, true);
+        
+  clock_gettime(CLOCK_THREAD_CPUTIME_ID, &current);
+  printf("Runtime for processing the point cloud: %4.3f\n", timespec_diff(&current, &start));
+  start=current;
+
+  /// now create Patch3D´s
+  for(unsigned i=0; i<cv_planes.size(); i++)
+  {
+    Z::Patch3D *p3d = new Z::Patch3D(cv_planes[i], cv_hulls[i]);
+    kcore->NewGestalt3D(p3d);
+    numPatches++;
+  }
+}
+
+
+}
+
+
+
+
+
+
+
+
---
+++ code/subarchitectures/vision.sa/trunk/src/c++/vision/components/StereoDetector/kinect/Patch3D.cpp
@@ -0,0 +1,95 @@
+/**
+ * @file Patch3D.cpp
+ * @author Andreas Richtsfeld
+ * @date June 2011
+ * @version 0.1
+ * @brief Base class for 3D plane patches.
+ */
+
+#include "Patch3D.h"
+
+namespace Z
+{
+
+/**
+ * @brief Constructor of class Patch3D for Kinect.
+ */
+Patch3D::Patch3D(cv::Mat_<cv::Vec4f> _p, cv::Mat_<cv::Vec4f> _h_p) : Gestalt3D(Gestalt3D::PATCH)
+{
+  points = _p;
+  hull_points = _h_p;
+  
+printf("Patch3D::Patch3D: Create Histogram\n");
+  int nr_bins = 10;
+  hist = new ColorHistogram(nr_bins, points);
+printf("Patch3D::Patch3D: Create Histogram done\n");
+}
+
+
+/**
+ * @brief Calculate significance for the stereo Gestalt.
+ * 2D horizontal lines are worser to match in 3D, therefore lower significance.
+ * 3D Lines with main direction in z-coordinte.
+ * @param angle2Dleft
+ * @param angle2Dright 
+ * @param angle3Dz Opening angle between z-axis and Line in 3D
+ */
+void Patch3D::CalculateSignificance(double angle2Dleft, double angle2Dright, double angle3Dz)
+{
+//   // Significance is between 0 and 1
+//   double sig2Dleft = fabs(angle2Dleft/(M_PI/2.));
+//   double sig2Dright = fabs(angle2Dright/(M_PI/2.));
+//   double sig3D = 1- angle3Dz/(M_PI/2.);	
+//   
+//   sig = 1- (1-sig2Dleft) * (1-sig2Dright) * (1-sig3D);
+}
+
+/**
+ * @brief Get the nodes vector for this 3D Gestalt.
+ * @param nodes Vector with n nodes, consisting of 3 position values.
+ * @return Returns true, if nodes from this 3D Gestalt are available.
+ */
+bool Patch3D::GetLinks(vector<GraphLink> &links)
+{
+printf("Segment3D::GetLinks: Not yet implemented!\n");
+//   GraphLink l;
+//   l.node[0].x = isct3D[0].p.x;
+//   l.node[0].y = isct3D[0].p.y;
+//   l.node[0].z = isct3D[0].p.z;
+//   l.node[1].x = isct3D[1].p.x;
+//   l.node[1].y = isct3D[1].p.y;
+//   l.node[1].z = isct3D[1].p.z;
+//   l.probability = sig;
+//   
+//   links.push_back(l);
+//   return true;
+  return false;
+}
+
+/**
+ * @brief Draw this 3D Gestalt to the TomGine render engine.
+ * @param tgRenderer Render engine
+ */
+void Patch3D::DrawGestalt3D(TGThread::TomGineThread *tgRenderer, bool randomColor)
+{
+  tgRenderer->AddPointCloud(points);
+  tgRenderer->AddConvexHull(hull_points);
+}
+
+/**
+ * @brief Print this 3D Gestalt to the console.
+ */
+void Patch3D::PrintGestalt3D()
+{
+  printf("\Patch3D: %u\n", id);
+  printf(" points (rows/cols): %u/%u\n", points.rows, points.cols);
+  
+  for(unsigned row=0; row<points.rows; row++)
+    for(unsigned col=0; col<points.cols; col++)
+      printf(" point: %4.3f / %4.3f / %4.3f\n", points(row, col)[0], points(row, col)[1], points(row, col)[2]);
+}
+
+
+}
+
+
