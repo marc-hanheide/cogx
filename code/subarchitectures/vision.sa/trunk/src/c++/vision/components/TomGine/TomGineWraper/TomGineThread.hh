787ff59eaed67fd6a7bfba0549d02e9053b8cb5f
ari <ari@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-05-31 14:14:24 +0000

    AddPointCloud(s), AddConvexHull(s)

--- code/subarchitectures/vision.sa/trunk/src/c++/vision/components/TomGine/TomGineWraper/TomGineThread.hh
+++ code/subarchitectures/vision.sa/trunk/src/c++/vision/components/TomGine/TomGineWraper/TomGineThread.hh
@@ -62,6 +62,7 @@ private:
   vector<Label> vLabel;                          // vector with all labels and poses
 
   bool drawImage, draw3D, drawPointCloud;        // draw image, 3D and point clouds
+  bool drawVPointCloud;                          // draw vec4f point cloud
   bool drawLabels;                               // draw labels
   bool useProbLevel;                             // use a probability level for pruning
   bool showCoordinateFrame;                      // show a coordinate frame
@@ -78,6 +79,8 @@ private:
   
   cv::Mat_<cv::Point3f> mCloud;                  // point cloud
   cv::Mat_<cv::Point3f> cCloud;                  // rgb color of point cloud
+  cv::Mat_<cv::Vec4f> vCloud;                    // point cloud with rgb color (float encoded)
+  cv::Mat_<cv::Vec4f> vClouds;                   // point clouds with rgb color (float encoded)
 
   vector<cv::Point3f> points3D;                  // points in 3D
   vector<cv::Point3f> colPoints3D;               // color of the 3D points
@@ -101,7 +104,8 @@ private:
   void SetTGCamera(unsigned width, unsigned height, double zFar, double zNear, Pose &pose,
          cv::Mat &intrinsic, TomGine::tgCamera &tgCam);
   
-  void DrawPointCloud(/*cv::Mat_<cv::Point3f> &pc*/);
+  void DrawPointCloud();
+  void DrawVPointCloud();
   void DrawPoints3D();
   void DrawLines3D();
   void DrawLabels3D(TomGine::tgEngine &render);
@@ -118,8 +122,8 @@ public:
   void SetRotationCenter(cv::Vec3d &_rotCenter);
   void SetShapeModel(TomGine::tgModel &tgmodel);
   void SetImage(cv::Mat &_img);
-  void SetPointCloud(cv::Mat_<cv::Point3f> &matCloud, cv::Mat_<cv::Point3f> &colCloud);
   void SetCoordinateFrame(double size = 0.5);
+  
   void AddPoint3D(double x, double y, double z, uchar r=0, uchar g=0, uchar b=0, double size=1);
   void AddLine3D(double x1, double y1, double z1, double x2, double y2, double z2, uchar r=0, uchar g=0, uchar b=0, 
 		 float probability = 1.0, string lLabel = "l", string lLabelS = "s", string lLabelE = "e");
@@ -128,7 +132,13 @@ public:
   void AddGraphModel(std::vector<cv::Point3d> first, std::vector<cv::Point3d> second, std::vector<double> probability, 
 		      std::vector<std::string> link, std::vector<std::string> node_0, std::vector<std::string> node_1,
 		      uchar r=0, uchar g=0, uchar b=0);
-
+          
+  void SetPointCloud(cv::Mat_<cv::Point3f> &matCloud, cv::Mat_<cv::Point3f> &colCloud);       // TODO  Remove this command => Use AddPointCloud instead!
+  void AddPointCloud(cv::Mat_<cv::Vec4f> &vecCloud);                                          // TODO Das sind Set Functionen, weil sie nur einmal verwendet werden können: Ändern
+  void AddPointClouds(vector< cv::Mat_<cv::Vec4f> > &vecClouds);                              // TODO Nur mehr eine Repräsentation für Punkte und die wird vergrößert!!
+  void AddConvexHull(cv::Mat_<cv::Vec4f> &vecHull);
+  void AddConvexHulls(vector< cv::Mat_<cv::Vec4f> > &vecHulls);
+  
   void Clear();
 };
 
