77eebd72e7727e4149d0e5b5f50f8487edcc75bd
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-06-01 19:00:29 +0000

    Reverse-merging r14951 through r11414

--- code/scenarios/george/trunk/src/java/eu/cogx/perceptmediator/ModelStatusTransferFunction.java
+++ code/scenarios/george/trunk/src/java/eu/cogx/perceptmediator/ModelStatusTransferFunction.java
@@ -9,9 +9,10 @@ import java.util.Map;
 import VisionData.VisualConceptModelStatus;
 import cast.architecture.ManagedComponent;
 import cast.cdl.WorkingMemoryChange;
-import de.dfki.lt.tr.beliefs.data.formulas.BoolFormula;
+import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
 import de.dfki.lt.tr.beliefs.data.formulas.DoubleFormula;
 import de.dfki.lt.tr.beliefs.data.formulas.Formula;
+import de.dfki.lt.tr.beliefs.data.formulas.IntFormula;
 import de.dfki.lt.tr.beliefs.data.formulas.PropositionFormula;
 import de.dfki.lt.tr.beliefs.util.BeliefException;
 import eu.cogx.beliefs.slice.GroundedBelief;
@@ -27,8 +28,7 @@ public class ModelStatusTransferFunction
 
 	public static final String GAIN = "gain";
 	public static final String MOST_PROMISING = "most-promising";
-	public static final String CONCEPT_ID = "concept";
-	private static final String ASKED_FOR = "asked-for";
+	private static final String CONCEPT_ID = "concept";
 
 	public ModelStatusTransferFunction(ManagedComponent component) {
 		super(component, component.getLogger(), GroundedBelief.class);
@@ -44,25 +44,18 @@ public class ModelStatusTransferFunction
 				.getAsFormula());
 		double maxGain = 0.0;
 		String maxLabel = null;
-		boolean maxAsked4 = false;
 		for (int i = 0; i < from.gains.length; i++) {
-			// if we have asked for it already we can skip it.
-			
-//				if (from.askedFor[i])
-//					continue;
-			if (from.gains[i] >= maxGain) {
+			if (from.gains[i] > maxGain) {
 				maxGain = from.gains[i];
 				maxLabel = from.labels[i];
-				if (from.askedFor.length>=i+1)
-					maxAsked4 = from.askedFor[i];
 			}
 		}
-		if (maxLabel == null || maxAsked4 == true)
+		if (maxLabel == null)
 			return null;
 		result.put(MOST_PROMISING, PropositionFormula.create(maxLabel)
 				.getAsFormula());
 		result.put(GAIN, DoubleFormula.create(maxGain).getAsFormula());
-		result.put(ASKED_FOR, BoolFormula.create(maxAsked4).getAsFormula());
+
 		return result;
 	}
 
