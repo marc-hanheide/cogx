b904b0838bba41ec369613827234033342ea3253
ari <ari@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-06-08 08:31:23 +0000

    Separated 3D Gestalts from stereo and kinect.

---
+++ subarchitectures/vision.sa/trunk/src/c++/vision/components/StereoDetector/Gestalts3D/Gestalt3D.h
@@ -0,0 +1,75 @@
+/**
+ * @file Gestalt3D.h
+ * @author Andreas Richtsfeld
+ * @date Januray 2011
+ * @version 0.1
+ * @brief Base class for stereo calculated Gestalts in 3D.
+ */
+
+#ifndef Z_GESTALT3D_HH
+#define Z_GESTALT3D_HH
+
+#include "TomGineThread.hh"
+#include "StereoTypes.h"
+#include "GraphLink.h"
+
+namespace Z
+{
+
+/**
+ * @class Gestalt3D
+ * @brief Base class for stereo calculated Gestalts in 3D.
+ */
+class Gestalt3D
+{
+public:
+  enum Type
+  {
+    PATCH,
+    SEGMENT,
+    COLLINEARITY,
+    LJUNCTION,
+    CORNER,
+    LINE,
+    CLOSURE,
+    RECTANGLE,
+    FLAP,
+    ELLIPSE,
+    MAX_TYPE,
+    UNDEF = MAX_TYPE
+  };
+
+protected:
+
+  Type type;            // Type of stereo Gestalt
+  unsigned id;          // Unique ID of the stereo Gestalt (with respect to the core)
+  
+  unsigned vs3ID;       // ID, if we got it from one vs3 Gestalt
+  unsigned vs3IDs[2];   // IDs of the left and right vs3 Gestalts				/// TODO für alle 3D Gestalts nachziehen
+  double sig;           // Significance value
+  unsigned rank;	      // Rank of the the stereo Gestalt
+
+public:
+  static const char* TypeName(Type t);
+  static const int TypeNameLength(Type t);
+  static Type EnumType(const char *type_name);
+
+
+public:
+  Gestalt3D(Type _type);
+  
+  Type GetType() const {return type;}
+  double GetSignificance() {return sig;}
+  unsigned GetVs3ID(unsigned side) {return vs3IDs[side];}
+  
+  void SetID(unsigned _id) {id = _id;}
+  unsigned GetID() {return id;}
+  
+  virtual bool GetLinks(vector<GraphLink> &links) {return false;}       /// TODO ARI: noch notwendig?
+  virtual void DrawGestalt3D(TGThread::TomGineThread *tgRenderer, bool randomColor = true) {}   // TODO pure virtual
+  virtual void PrintGestalt3D() {}                                     // TODO pure virtual
+};
+
+}
+
+#endif
