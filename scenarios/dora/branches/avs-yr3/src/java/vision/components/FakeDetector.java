c7e461a4c2756b5f0a525c9c8d8ea25295811115
Marc Hanheide <marc@hanheide.net>
2011-06-13 19:30:41 +0000

    - removed logging and cleaned  code - some more useful logging - a working FakeDetector for the first time ever

--- scenarios/dora/branches/avs-yr3/src/java/vision/components/FakeDetector.java
+++ scenarios/dora/branches/avs-yr3/src/java/vision/components/FakeDetector.java
@@ -5,6 +5,7 @@ package vision.components;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 
 import mathlib.Functions;
 import vision.VisionUtils;
@@ -12,9 +13,18 @@ import NavData.RobotPose2d;
 import Video.CameraParameters;
 import Video.CameraParametersWrapper;
 import VisionData.DetectionCommand;
+import VisionData.Face;
+import VisionData.GeometryModel;
 import VisionData.Post3DObject;
+import VisionData.Recognizer3DCommand;
+import VisionData.Recognizer3DCommandType;
+import VisionData.Vertex;
 import VisionData.VisualObject;
+import cast.AlreadyExistsOnWMException;
 import cast.CASTException;
+import cast.ConsistencyException;
+import cast.DoesNotExistOnWMException;
+import cast.PermissionException;
 import cast.architecture.ChangeFilterFactory;
 import cast.architecture.ManagedComponent;
 import cast.architecture.WorkingMemoryChangeReceiver;
@@ -22,21 +32,52 @@ import cast.cdl.WorkingMemoryChange;
 import cast.cdl.WorkingMemoryOperation;
 import cast.core.CASTUtils;
 import castutils.castextensions.WMEventQueue;
-import cogx.Math.Matrix33;
 import cogx.Math.Pose3;
+import cogx.Math.Vector2;
 import cogx.Math.Vector3;
 
 /**
+ * report VisualObject detections for pre-defined objects
+ * 
+ * <pre>
+ *  --geometry-hack 
+ *  --pos-table "4.0, -0.0, 0.0, 0.0" --dimensions-table "1.0, 2.0, 0.55" --visibility-area-table "1.0, -4.0, 4.0, -1.0"
+ * </pre>
+ * 
  * @author marc
  * 
  */
+
 public class FakeDetector extends ManagedComponent implements
 		WorkingMemoryChangeReceiver {
+	private static final String POS_CONFIG_PREFIX = "--pos-";
+	private static final int POS_CONFIG_PREFIX_LENGTH = POS_CONFIG_PREFIX
+			.length();
+	private static final String DIMENSION_CONFIG_PREFIX = "--dimensions-";
+	private static final int DIMENSION_CONFIG_PREFIX_LENGTH = DIMENSION_CONFIG_PREFIX
+			.length();
+	private static final String VISIBLEFROM_CONFIG_PREFIX = "--visibility-area-";
+	private static final int VISIBLEFROM_CONFIG_PREFIX_LENGTH = VISIBLEFROM_CONFIG_PREFIX
+			.length();
+
+	// FIXME: these values should come from the CameraParameters read from WM,
+	// but those are currently just wrong, so we copied values from the calib
+	// file directly here:
+	private static double PREDEF_CAM_FX = 816.208435;
+	private static double PREDEF_CAM_FY = 817.011902;
+	private static double PREDEF_CAM_CX = 300.978394;
+	private static double PREDEF_CAM_CY = 251.706039;
+	private static int PREDEF_CAM_WIDTH = 640;
+	private static int PREDEF_CAM_HEIGHT = 480;
 
 	private WMEventQueue recognitionCommandEvents = new WMEventQueue();
 	private CameraParameters currentCameraParameters;
-	private Map<String, Post3DObject> knowObjects = new HashMap<String, Post3DObject>();
+	private Map<String, Pose3> knowObjects = new HashMap<String, Pose3>();
+	private Map<String, Vector3> knownGeometry = new HashMap<String, Vector3>();
+	private Map<String, String> label2IdMap = new HashMap<String, String>();
 	private RobotPose2d currentRobotPose;
+	private boolean geometryHackEnabled = false;
+	private Map<String, Vector<Double>> knownVisibilityArea = new HashMap<String, Vector<Double>>();
 
 	/*
 	 * (non-Javadoc)
@@ -45,9 +86,79 @@ public class FakeDetector extends ManagedComponent implements
 	 */
 	@Override
 	protected void configure(Map<String, String> _config) {
-		Pose3 pose = new Pose3(new Vector3(0, 0, 0), new Matrix33(1, 0, 0, 0,
-				1, 0, 0, 0, 1));
-		knowObjects.put("table", new Post3DObject("table", pose));
+		// Pose3 pose = new Pose3(new Vector3(0, 0, 0), new Matrix33(1, 0, 0, 0,
+		// 1, 0, 0, 0, 1));
+		// knowObjects.put("table", pose);
+
+		if (_config.containsKey("--geometry-hack")) {
+			println("geometry hack is enabled... the GeometryModel is a BAD hack now to work with the ObjectRelationManager properly");
+			geometryHackEnabled = true;
+		}
+
+		for (String key : _config.keySet()) {
+			if (key.startsWith(POS_CONFIG_PREFIX)) {
+				configurePosition(_config, key);
+			}
+			if (key.startsWith(DIMENSION_CONFIG_PREFIX)) {
+				configureDimensions(_config, key);
+			}
+			if (key.startsWith(VISIBLEFROM_CONFIG_PREFIX)) {
+				configureVisibleArea(_config, key);
+			}
+		}
+
+	}
+
+	private void configurePosition(Map<String, String> _config, String key) {
+		String label = key.substring(POS_CONFIG_PREFIX_LENGTH);
+
+		String coordsJoined = _config.get(key);
+		String[] labels = coordsJoined.split(",");
+
+		assert (labels.length >= 4);
+		double x = Double.parseDouble(labels[0]);
+		double y = Double.parseDouble(labels[1]);
+		double z = Double.parseDouble(labels[2]);
+		double theta = Double.parseDouble(labels[3]) * Math.PI / 180.0;
+		println("predefined position configured: " + label + " " + x + " " + y
+				+ " " + theta);
+		Pose3 pose = Functions
+				.pose3FromEuler(new Vector3(x, y, z), 0, 0, theta);
+		knowObjects.put(label, pose);
+	}
+
+	private void configureDimensions(Map<String, String> _config, String key) {
+		String label = key.substring(DIMENSION_CONFIG_PREFIX_LENGTH);
+
+		String coordsJoined = _config.get(key);
+		String[] labels = coordsJoined.split(",");
+
+		assert (labels.length >= 3);
+
+		double dx = Double.parseDouble(labels[0]);
+		double dy = Double.parseDouble(labels[1]);
+		double dz = Double.parseDouble(labels[2]);
+		println("dimensions are provided: " + label + " " + dx + " " + dy + " "
+				+ dz);
+		knownGeometry.put(label, new Vector3(dx, dy, dz));
+
+	}
+
+	private void configureVisibleArea(Map<String, String> _config, String key) {
+		String label = key.substring(VISIBLEFROM_CONFIG_PREFIX_LENGTH);
+
+		String coordsJoined = _config.get(key);
+		String[] labels = coordsJoined.split(",");
+
+		assert (labels.length >= 4);
+
+		Vector<Double> geom = new Vector<Double>();
+		geom.add(Double.parseDouble(labels[0]));
+		geom.add(Double.parseDouble(labels[1]));
+		geom.add(Double.parseDouble(labels[2]));
+		geom.add(Double.parseDouble(labels[3]));
+		knownVisibilityArea.put(label, geom);
+
 	}
 
 	/*
@@ -61,19 +172,58 @@ public class FakeDetector extends ManagedComponent implements
 			try {
 				WorkingMemoryChange event = recognitionCommandEvents.take();
 				log("received detection command: " + CASTUtils.toString(event));
-				DetectionCommand dc = getMemoryEntry(event.address,
-						DetectionCommand.class);
-				for (String label : dc.labels) {
+				String[] labels;
+				DetectionCommand dc = null;
+				Recognizer3DCommand rc = null;
+				if (event.type.equals(CASTUtils
+						.typeName(DetectionCommand.class))) {
+
+					dc = getMemoryEntry(event.address, DetectionCommand.class);
+					labels = dc.labels;
+				} else {
+					rc = getMemoryEntry(event.address,
+							Recognizer3DCommand.class);
+					if (rc.cmd == Recognizer3DCommandType.RECOGNIZE)
+						labels = new String[] { rc.label };
+					else
+						continue;
+				}
+				for (String label : labels) {
 					if (knowObjects.containsKey(label)) {
 						log("checking detection of known object " + label);
-						Post3DObject fakeDetection = knowObjects.get(label);
-						Pose3 objPose = checkVisibility(fakeDetection);
+						Pose3 fakeObjectPoseInWorldCS = knowObjects.get(label);
+						Pose3 objPose = checkVisibility(
+								fakeObjectPoseInWorldCS, label);
 						if (objPose != null) {
-							log("fake object " + label
+							println("fake object " + label
 									+ " is visible. submitting to WM.");
-							Post3DObject postCmd = new Post3DObject(label,
-									objPose);
-							addToWorkingMemory(newDataID(), postCmd);
+							if (geometryHackEnabled) {
+								submitHackedVisualObjectToWM(label,
+										fakeObjectPoseInWorldCS, true);
+							} else {
+								submitVisualObjectToWM(label, objPose, true);
+							}
+						} else {
+							println("fake object " + label
+									+ " is NOT visible. submitting to WM.");
+							if (geometryHackEnabled) {
+//								submitHackedVisualObjectToWM(label, Functions
+//										.pose3FromEuler(new Vector3(0, 0, 0),
+//												0, 0, 0), false);
+								println("here we don't submit in case of failure");
+							} else {
+								submitVisualObjectToWM(label, Functions
+										.pose3FromEuler(new Vector3(0, 0, 0),
+												0, 0, 0), false);
+							}
+
+						}
+						if (dc != null) {
+							overwriteWorkingMemory(event.address, dc);
+						}
+						if (rc != null) {
+							rc.confidence = (objPose == null) ? 0.0 : 1.0;
+							overwriteWorkingMemory(event.address, rc);
 						}
 					}
 				}
@@ -85,23 +235,83 @@ public class FakeDetector extends ManagedComponent implements
 		}
 	}
 
-	private Pose3 checkVisibility(Post3DObject fakeDetection)
+	private void submitHackedVisualObjectToWM(String label,
+			Pose3 fakeObjectPoseInWorldCS, boolean b)
+			throws AlreadyExistsOnWMException, DoesNotExistOnWMException,
+			ConsistencyException, PermissionException {
+
+		VisualObject visualObject;
+
+		String oldId = label2IdMap.get(label);
+		if (oldId == null)
+			visualObject = VisionUtils.newVisualObject();
+		else
+			visualObject = getMemoryEntry(oldId, VisualObject.class);
+
+		visualObject.pose = fakeObjectPoseInWorldCS;
+		visualObject.detectionConfidence = (b ? 1.0 : 0.0);
+
+		visualObject.model = new GeometryModel(new Vertex[1], new Face[0]);
+		Vector3 predefDimensions = knownGeometry.get(label);
+		if (predefDimensions == null)
+			getLogger()
+					.error(
+							"couldn't find predefined dimension which is a prerequisite for the geometry hack.");
+		Vertex vert = new Vertex(predefDimensions, new Vector3(), new Vector2());
+		visualObject.model.vertices[0] = vert;
+		visualObject.identLabels = new String[] { label, "unknown" };
+		visualObject.identDistrib = new double[] {
+				visualObject.detectionConfidence,
+				1.0 - visualObject.detectionConfidence };
+		if (oldId == null) {
+			String id = newDataID();
+			label2IdMap.put(label, id);
+			addToWorkingMemory(id, visualObject);
+		} else {
+			overwriteWorkingMemory(oldId, visualObject);
+		}
+	}
+
+	private void submitVisualObjectToWM(String label, Pose3 objPose,
+			boolean positiveDetection) throws AlreadyExistsOnWMException {
+		Post3DObject postCmd = new Post3DObject(label, objPose,
+				positiveDetection);
+		addToWorkingMemory(newDataID(), postCmd);
+	}
+
+	private Pose3 checkVisibility(Pose3 objInWorldCS, String label)
 			throws InterruptedException {
 		RobotPose2d robotPose = getCurrentRobotPose();
 		log("checkVisibility: current robotPose is " + robotPose.x + ", "
 				+ robotPose.y);
+		Vector<Double> visibilityArea = knownVisibilityArea.get(label);
+		if (visibilityArea != null) {
+			double minx = visibilityArea.get(0);
+			double miny = visibilityArea.get(1);
+			double maxx = visibilityArea.get(2);
+			double maxy = visibilityArea.get(3);
+			println("");
+			if (robotPose.x < minx || robotPose.x > maxx || robotPose.y < miny
+					|| robotPose.y > maxy) {
+				println("object " + label + " cannot be visible from position "
+						+ robotPose.x + ", " + robotPose.y);
+				return null;
+			}
+		}
 		Pose3 robPose3D = Functions.pose3FromEuler(new Vector3(robotPose.x,
 				robotPose.y, 0.0), 0.0, 0.0, robotPose.theta);
 		log("checkVisibility: robot in world coords "
 				+ Functions.toString(robPose3D));
 		Pose3 objInRobotCoord = Functions.transformInverse(robPose3D,
-				fakeDetection.pose);
+				objInWorldCS);
+		Vector3 objCheckPos = (Vector3) objInRobotCoord.pos.clone();
+		// FIXME: this should really be read from geometry model...
+		objCheckPos.z += 0.225;
 		log("checkVisibility: object in robot coords is "
 				+ Functions.toString(objInRobotCoord));
 		log("checkVisibility: getCurrentCameraParameters().pose "
 				+ Functions.toString(getCurrentCameraParameters().pose));
-		if (VisionUtils.isVisible(getCurrentCameraParameters(),
-				objInRobotCoord.pos)) {
+		if (VisionUtils.isVisible(getCurrentCameraParameters(), objCheckPos)) {
 			return objInRobotCoord;
 		} else {
 			return null;
@@ -126,6 +336,9 @@ public class FakeDetector extends ManagedComponent implements
 		addChangeFilter(ChangeFilterFactory.createGlobalTypeFilter(
 				DetectionCommand.class, WorkingMemoryOperation.ADD),
 				recognitionCommandEvents);
+		addChangeFilter(ChangeFilterFactory.createGlobalTypeFilter(
+				Recognizer3DCommand.class, WorkingMemoryOperation.ADD),
+				recognitionCommandEvents);
 
 		addChangeFilter(ChangeFilterFactory
 				.createGlobalTypeFilter(RobotPose2d.class), this);
@@ -140,15 +353,20 @@ public class FakeDetector extends ManagedComponent implements
 			throws CASTException {
 		if (arg0.operation == WorkingMemoryOperation.DELETE)
 			return;
+		debug("relevant information changed: " + arg0.type);
 		if (arg0.type.equals(CASTUtils.typeName(RobotPose2d.class))) {
 			// new RobotPose
 			RobotPose2d robotPose = getMemoryEntry(arg0.address,
 					RobotPose2d.class);
+			debug("new robot pose: x=" + robotPose.x + " y=" + robotPose.y);
+
 			setCurrentRobotPose(robotPose);
 		} else {
 			// new CameraParams
 			CameraParametersWrapper cpw = getMemoryEntry(arg0.address,
 					CameraParametersWrapper.class);
+			debug("new camera information: fx=" + cpw.cam.fx + " fy="
+					+ cpw.cam.fy + "pose=" + Functions.toString(cpw.cam.pose));
 			setCurrentCameraParameters(cpw.cam);
 		}
 	}
@@ -160,6 +378,13 @@ public class FakeDetector extends ManagedComponent implements
 
 	protected synchronized void setCurrentCameraParameters(CameraParameters cam) {
 		currentCameraParameters = cam;
+		// FIXME: the following should not be necessary, but currently is...
+		currentCameraParameters.cx = PREDEF_CAM_CX;
+		currentCameraParameters.cy = PREDEF_CAM_CY;
+		currentCameraParameters.fx = PREDEF_CAM_FX;
+		currentCameraParameters.fy = PREDEF_CAM_FY;
+		currentCameraParameters.width = PREDEF_CAM_WIDTH;
+		currentCameraParameters.height = PREDEF_CAM_HEIGHT;
 		notifyAll();
 	}
 
