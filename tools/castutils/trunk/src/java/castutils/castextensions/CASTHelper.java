33e93cd32236e486ae26a70366813f096effd644
Marc Hanheide <marc@hanheide.net>
2011-06-24 13:16:16 +0000

    merged avs changes

--- tools/castutils/trunk/src/java/castutils/castextensions/CASTHelper.java
+++ tools/castutils/trunk/src/java/castutils/castextensions/CASTHelper.java
@@ -18,6 +18,10 @@ public class CASTHelper {
 	protected ManagedComponent component;
 	protected String name;
 	private Logger logger = null;
+	
+	protected Logger getLogger() {
+		return logger;
+	}
 
 	protected CASTHelper(ManagedComponent c) {
 		component = c;
