ae0d85e894c5eedfbc2c7cf39aaa4a819b7a16f7
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-07-05 15:39:08 +0000

    gisbert kapp settings

--- code/systems/dora-yr3/restoremap.sh
+++ code/systems/dora-yr3/restoremap.sh
@@ -2,4 +2,4 @@
 #zenity --question --text "reset mapping?"  && rm -f robotpose.ccf tmpmap.* && exit 0
 #zenity --question --text "restore start map?" && rm -f robotpose.ccf && cp instantiations/map_backup/bham_lab/* .
 rm -f robotpose.ccf tmpmap.*
-cp -v instantiations/map_backup/alu_review_sim/tmpmap* .
+cp -v instantiations/map_backup/gisbert_kapp1/tmpmap* .
