00f52b52e5c30c00442ff5d8a39e6a0ff1aaa055
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-07-06 10:01:15 +0000

    Updated ViewCone beliefs to point to ProtoObject beliefs rather than the objects themselves.

--- code/subarchitectures/vision.sa/trunk/src/java/eu/cogx/perceptmediator/components/ViewConeMediatorComponent.java
+++ code/subarchitectures/vision.sa/trunk/src/java/eu/cogx/perceptmediator/components/ViewConeMediatorComponent.java
@@ -1,19 +1,23 @@
 package eu.cogx.perceptmediator.components;
 
-import VisionData.ProtoObject;
 import VisionData.ViewCone;
 import eu.cogx.beliefs.slice.PerceptBelief;
 import eu.cogx.perceptmediator.PerceptBindingMediator;
-import eu.cogx.perceptmediator.components.abstr.PerceptMediatorComponent;
-import eu.cogx.perceptmediator.transferfunctions.ProtoObjectTransferFunction;
+import eu.cogx.perceptmediator.components.abstr.ReferringPerceptMediatorComponent;
 import eu.cogx.perceptmediator.transferfunctions.ViewConeTransferFunction;
 
-public class ViewConeMediatorComponent extends PerceptMediatorComponent {
+public class ViewConeMediatorComponent extends
+		ReferringPerceptMediatorComponent<PerceptBelief> {
+
+	public ViewConeMediatorComponent() {
+		super(PerceptBelief.class);
+	}
 
 	@Override
 	protected PerceptBindingMediator<ViewCone, PerceptBelief> getMediator() {
 		return PerceptBindingMediator.create(this, ViewCone.class,
-				PerceptBelief.class, new ViewConeTransferFunction(this));
+				PerceptBelief.class, new ViewConeTransferFunction(this,
+						this.allBeliefs));
 	}
 
 }
--- code/subarchitectures/vision.sa/trunk/src/java/eu/cogx/perceptmediator/transferfunctions/ViewConeTransferFunction.java
+++ code/subarchitectures/vision.sa/trunk/src/java/eu/cogx/perceptmediator/transferfunctions/ViewConeTransferFunction.java
@@ -10,45 +10,59 @@ import org.apache.log4j.Logger;
 
 import VisionData.ViewCone;
 import cast.architecture.ManagedComponent;
+import cast.cdl.WorkingMemoryAddress;
 import cast.cdl.WorkingMemoryChange;
 import cast.core.CASTUtils;
+import castutils.castextensions.WMView;
 import de.dfki.lt.tr.beliefs.data.formulas.DoubleFormula;
 import de.dfki.lt.tr.beliefs.data.formulas.Formula;
 import de.dfki.lt.tr.beliefs.data.formulas.PropositionFormula;
 import de.dfki.lt.tr.beliefs.data.formulas.WMPointer;
 import de.dfki.lt.tr.beliefs.util.BeliefException;
 import eu.cogx.beliefs.slice.PerceptBelief;
-import eu.cogx.perceptmediator.transferfunctions.abstr.SimpleDiscreteTransferFunction;
+import eu.cogx.perceptmediator.transferfunctions.abstr.DependentDiscreteTransferFunction;
+import eu.cogx.perceptmediator.transferfunctions.helpers.ProtoObjectMatchingFunction;
 
 /**
  * @author mmarko (copied from VisualObjectTransferFunction and adapted)
  * 
  */
 public class ViewConeTransferFunction extends
-		SimpleDiscreteTransferFunction<ViewCone, PerceptBelief> {
+		DependentDiscreteTransferFunction<ViewCone, PerceptBelief> {
 
 	public static final String VIEW_CONE_ID = "viewConeID";
 	static Logger logger = Logger.getLogger(ViewConeTransferFunction.class);
 
-	public ViewConeTransferFunction(ManagedComponent component) {
-		super(component, logger, PerceptBelief.class);
+	public ViewConeTransferFunction(ManagedComponent component,
+			WMView<PerceptBelief> allBeliefs) {
+		super(component, allBeliefs, logger, PerceptBelief.class);
 	}
 
 	@Override
 	protected Map<String, Formula> getFeatureValueMapping(
-			WorkingMemoryChange wmc, ViewCone from) throws InterruptedException,
-			BeliefException {
+			WorkingMemoryChange wmc, ViewCone from)
+			throws InterruptedException, BeliefException {
 
 		logger.info("Called with " + CASTUtils.toString(wmc));
+
+		// resolve the address of the proto object this cone targets
+		WorkingMemoryAddress poBelAddr = getReferredBelief(new ProtoObjectMatchingFunction(
+				from.target));
+
+		logger.debug("looking for place with belief address "
+				+ CASTUtils.toString(poBelAddr));
 
 		Map<String, Formula> result = new HashMap<String, Formula>();
 
 		result.put(VIEW_CONE_ID, PropositionFormula.create(wmc.address.id)
 				.getAsFormula());
 
-		result.put("proto-object", WMPointer.create(from.target)
-				.getAsFormula());
-		
+		//pointer to belief for proto object
+		result.put(
+				"proto-object",
+				WMPointer.create(poBelAddr,
+						CASTUtils.typeName(PerceptBelief.class)).getAsFormula());
+
 		result.put("anchor-x", DoubleFormula.create(from.anchor.x)
 				.getAsFormula());
 		result.put("anchor-y", DoubleFormula.create(from.anchor.y)
@@ -59,8 +73,7 @@ public class ViewConeTransferFunction extends
 		result.put("dy", DoubleFormula.create(from.y).getAsFormula());
 		result.put("dtheta", DoubleFormula.create(from.viewDirection)
 				.getAsFormula());
-		result.put("tilt", DoubleFormula.create(from.tilt)
-				.getAsFormula());
+		result.put("tilt", DoubleFormula.create(from.tilt).getAsFormula());
 
 		return result;
 	}
