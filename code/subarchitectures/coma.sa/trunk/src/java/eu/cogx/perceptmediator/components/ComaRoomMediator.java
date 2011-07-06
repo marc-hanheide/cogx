8ed83164c0eef767b2940d898fa0b2a1eab67c8a
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-07-06 06:38:16 +0000

    added discrete ComaRoom transfer function and introduced config switch --discrete to turn it on

--- code/subarchitectures/coma.sa/trunk/src/java/eu/cogx/perceptmediator/components/ComaRoomMediator.java
+++ code/subarchitectures/coma.sa/trunk/src/java/eu/cogx/perceptmediator/components/ComaRoomMediator.java
@@ -1,18 +1,48 @@
 package eu.cogx.perceptmediator.components;
 
+import java.util.Map;
+
 import comadata.ComaRoom;
 
 import eu.cogx.beliefs.slice.GroundedBelief;
 import eu.cogx.perceptmediator.PerceptBindingMediator;
 import eu.cogx.perceptmediator.components.abstr.PerceptMediatorComponent;
 import eu.cogx.perceptmediator.transferfunctions.ComaRoomTransferFunction;
+import eu.cogx.perceptmediator.transferfunctions.DiscreteComaRoomTransferFunction;
 
 public class ComaRoomMediator extends PerceptMediatorComponent {
+
+	private static final String DISCRETE_CONFIG = "--discrete";
+	private ComaRoomTransferFunction transferFunction = null;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * eu.cogx.perceptmediator.components.abstr.PerceptMediatorComponent#configure
+	 * (java.util.Map)
+	 */
+	@Override
+	protected void configure(Map<String, String> config) {
+
+		if (config.containsKey(DISCRETE_CONFIG))
+			transferFunction = new DiscreteComaRoomTransferFunction(this);
+		else
+			transferFunction = new ComaRoomTransferFunction(this);
+
+		println("using " + transferFunction.getClass().getSimpleName()
+				+ " as transfer function.");
+		/*
+		 * make sure the super class is called at the end, because it calls
+		 * getMediator(), which uses transferFunction, which was only set now!
+		 */
+		super.configure(config);
+	}
 
 	@Override
 	protected PerceptBindingMediator<ComaRoom, GroundedBelief> getMediator() {
 		return PerceptBindingMediator.create(this, ComaRoom.class,
-				GroundedBelief.class, new ComaRoomTransferFunction(this));
+				GroundedBelief.class, transferFunction);
 	}
 
 }
