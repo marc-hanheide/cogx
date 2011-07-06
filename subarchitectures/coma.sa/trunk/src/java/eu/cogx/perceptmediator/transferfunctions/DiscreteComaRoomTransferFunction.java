e2e420b68dfab203a3d355e9cadd1176b46c231a
Marc Hanheide <marc@hanheide.net>
2011-07-06 06:38:16 +0000

    added discrete ComaRoom transfer function and introduced config switch --discrete to turn it on

---
+++ subarchitectures/coma.sa/trunk/src/java/eu/cogx/perceptmediator/transferfunctions/DiscreteComaRoomTransferFunction.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package eu.cogx.perceptmediator.transferfunctions;
+
+import SpatialProbabilities.JointProbabilityValue;
+import cast.architecture.ManagedComponent;
+import cast.cdl.WorkingMemoryChange;
+
+import comadata.ComaRoom;
+
+import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
+import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
+import de.dfki.lt.tr.beliefs.data.specificproxies.IndependentFormulaDistributions;
+import eu.cogx.beliefs.slice.GroundedBelief;
+
+/**
+ * @author marc
+ * 
+ */
+public class DiscreteComaRoomTransferFunction extends ComaRoomTransferFunction {
+
+	public DiscreteComaRoomTransferFunction(ManagedComponent component) {
+		super(component);
+	}
+
+	@Override
+	protected void fillBelief(
+			CASTIndependentFormulaDistributionsBelief<GroundedBelief> belief,
+			WorkingMemoryChange wmc, ComaRoom from) {
+		super.fillBelief(belief, wmc, from);
+		if (from.categories.massFunction == null) {
+			component.getLogger().info(
+					"Coma room without a category yet, not mediating!");
+			return;
+		}
+
+		IndependentFormulaDistributions distr = belief.getContent();
+		FormulaDistribution fd = FormulaDistribution.create();
+		double maxProb = 0.0;
+		String maxLabel = null;
+		for (JointProbabilityValue jp : from.categories.massFunction) {
+			if (jp.probability > maxProb) {
+				maxProb = jp.probability;
+				maxLabel = ((SpatialProbabilities.StringRandomVariableValue) (jp.variableValues[0])).value;
+			}
+		}
+		assert (maxLabel != null);
+		fd.add(maxLabel, maxProb);
+		assert (fd.size() == 1);
+		distr.put(CATEGORY_ID, fd);
+	}
+
+}
