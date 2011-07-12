9fac70bb7c9c8a3a02fad752394c54ac058d8ccb
Nick Hawes <nickh@robots.ox.ac.uk>
2011-07-12 09:35:21 +0000

    added sa field to synchorniser

--- tools/cogxutils/trunk/src/java/eu/cogx/perceptmediator/components/abstr/PerceptMediatorComponent.java
+++ tools/cogxutils/trunk/src/java/eu/cogx/perceptmediator/components/abstr/PerceptMediatorComponent.java
@@ -5,10 +5,10 @@ package eu.cogx.perceptmediator.components.abstr;
 
 import java.util.Map;
 
-import Ice.ObjectImpl;
 import cast.architecture.ManagedComponent;
 import castutils.castextensions.WMEntrySynchronizer.TransferFunction;
 import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
+import eu.cogx.beliefs.slice.PerceptBelief;
 import eu.cogx.perceptmediator.PerceptBindingMediator;
 import eu.cogx.perceptmediator.transferfunctions.abstr.DependentDiscreteTransferFunction;
 import eu.cogx.perceptmediator.transferfunctions.abstr.SimpleDiscreteTransferFunction;
@@ -32,16 +32,20 @@ import eu.cogx.perceptmediator.transferfunctions.abstr.SimpleDiscreteTransferFun
  */
 public abstract class PerceptMediatorComponent extends ManagedComponent {
 
-	
 	/**
 	 * the mediator to be used by the component.
 	 */
 	PerceptBindingMediator<? extends Ice.ObjectImpl, ? extends dBelief> mediator = null;
-	
+
 	/**
 	 * the mediator runs in a separate thread.
 	 */
 	Thread mediatorThread = null;
+
+	/**
+	 * The subarchitecture that the mediates beliefs should be written to.
+	 */
+	protected String m_toSA;
 
 	/*
 	 * (non-Javadoc)
@@ -50,13 +54,21 @@ public abstract class PerceptMediatorComponent extends ManagedComponent {
 	 */
 	@Override
 	protected void configure(Map<String, String> config) {
-		mediator = getMediator();
+		m_toSA = config.get("--write-to-sa");
+		if (m_toSA == null) {
+			m_toSA = getSubarchitectureID();
+		}
+		mediator = getMediator(m_toSA);
 	}
 
-	/** to be implemented to specialize for certain types
+	/**
+	 * to be implemented to specialize for certain types * @param _toSA The
+	 * subarchitecture to which the mediator should write.
+	 * 
 	 * @return the {@link PerceptBindingMediator} to be used by this class.
 	 */
-	protected abstract PerceptBindingMediator<? extends Ice.ObjectImpl, ? extends dBelief> getMediator();
+	protected abstract PerceptBindingMediator<? extends Ice.ObjectImpl, ? extends dBelief> getMediator(
+			String _toSA);
 
 	/*
 	 * (non-Javadoc)
--- tools/cogxutils/trunk/src/java/eu/cogx/perceptmediator/components/abstr/ReferringPerceptMediatorComponent.java
+++ tools/cogxutils/trunk/src/java/eu/cogx/perceptmediator/components/abstr/ReferringPerceptMediatorComponent.java
@@ -3,26 +3,27 @@
  */
 package eu.cogx.perceptmediator.components.abstr;
 
-
+import Ice.ObjectImpl;
 import cast.UnknownSubarchitectureException;
 import castutils.castextensions.WMView;
 import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
+import eu.cogx.perceptmediator.PerceptBindingMediator;
 
 /**
  * @author marc
  * 
  */
-public abstract class ReferringPerceptMediatorComponent<To extends dBelief> extends
-		PerceptMediatorComponent {
+public abstract class ReferringPerceptMediatorComponent<To extends dBelief>
+		extends PerceptMediatorComponent {
 
 	final protected WMView<To> allBeliefs;
 	final protected Class<To> belClass;
-	
+
 	/**
 	 * 
 	 */
 	public ReferringPerceptMediatorComponent(Class<To> belClass) {
-		this.belClass=belClass;
+		this.belClass = belClass;
 		allBeliefs = WMView.create(this, belClass);
 	}
 
