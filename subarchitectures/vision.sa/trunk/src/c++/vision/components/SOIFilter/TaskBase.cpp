43e1b0d4ef035836cdb1b8623256e0b3a9b25ad5
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-07-19 11:02:54 +0000

    soif: refactor - TaskBase created

---
+++ subarchitectures/vision.sa/trunk/src/c++/vision/components/SOIFilter/TaskBase.cpp
@@ -0,0 +1,18 @@
+
+#include "TaskBase.h"
+#include <IceUtil/IceUtil.h>
+
+namespace cast {
+
+static long long g_order = 0;
+static IceUtil::Monitor<IceUtil::Mutex> g_OrderMonitor;
+
+long long WmEvent::getEventOrder()
+{
+  IceUtil::Monitor<IceUtil::Mutex>::Lock lock(g_OrderMonitor);
+  ++g_order;
+  return g_order;
+}
+
+} // namespace
+// vim: set sw=2 ts=8 sts=4 et :vim
