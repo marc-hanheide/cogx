7791b1ae3670d7f05c2695252392b558f54ef41f
Nick Hawes <nickh@robots.ox.ac.uk>
2011-07-26 08:12:39 +0000

    motives now removed

--- scenarios/george/trunk/src/java/eu/cogx/goals/george/ProtoObjectMotiveGenerator.java
+++ scenarios/george/trunk/src/java/eu/cogx/goals/george/ProtoObjectMotiveGenerator.java
@@ -10,6 +10,7 @@ import cast.cdl.WorkingMemoryAddress;
 import cast.core.CASTUtils;
 import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
 import eu.cogx.beliefs.slice.GroundedBelief;
+import eu.cogx.perceptmediator.transferfunctions.ProtoObjectTransferFunction;
 import eu.cogx.perceptmediator.transferfunctions.abstr.SimpleDiscreteTransferFunction;
 
 public class ProtoObjectMotiveGenerator extends
@@ -32,33 +33,40 @@ public class ProtoObjectMotiveGenerator extends
 
 		assert (_newEntry.type.equals(PO_TYPE));
 
-		log("checkForAddition(): check belief " + _newEntry.id + " for addition");
+		log("checkForAddition(): check belief " + _newEntry.id
+				+ " for addition");
 
 		CASTIndependentFormulaDistributionsBelief<GroundedBelief> belief = CASTIndependentFormulaDistributionsBelief
 				.create(GroundedBelief.class, _newEntry);
 
-		// if that is a place holder
-		log("place is explored, so it is a goal to patrol");
-		AnalyzeProtoObjectMotive result = new AnalyzeProtoObjectMotive();
+		AnalyzeProtoObjectMotive result = null;
 
-		result.created = getCASTTime();
-		result.updated = result.created;
-		result.correspondingUnion = "";
-		result.maxExecutionTime = MAX_EXECUTION_TIME;
-		result.maxPlanningTime = MAX_PLANNING_TIME;
-		result.priority = MotivePriority.UNSURFACE;
-		result.referenceEntry = _wma;
-		result.status = MotiveStatus.UNSURFACED;
+		if (!belief.getContent().containsKey(
+				ProtoObjectTransferFunction.VISUAL_OBJECT_LINK)) {
 
-		// (exists (?v - VisualObject) (= (po-is-associated-with ?p) ?v))
+			log("ProtoObject belief is not linked to VisualObject, so generating motive.");
 
-		result.goal = new Goal(100f,
-				"(exists (?v - VisualObject) (= (po-is-associated-with '"
-						+ belief.getId() + "') ?v))", false);
+			result = new AnalyzeProtoObjectMotive();
 
-		log("goal is " + result.goal.goalString + " with inf-gain "
-				+ result.informationGain);
+			result.created = getCASTTime();
+			result.updated = result.created;
+			result.correspondingUnion = "";
+			result.maxExecutionTime = MAX_EXECUTION_TIME;
+			result.maxPlanningTime = MAX_PLANNING_TIME;
+			result.priority = MotivePriority.UNSURFACE;
+			result.referenceEntry = _wma;
+			result.status = MotiveStatus.UNSURFACED;
 
+			// (exists (?v - VisualObject) (= (po-is-associated-with ?p) ?v))
+
+			result.goal = new Goal(100f,
+					"(exists (?v - VisualObject) (= (po-is-associated-with '"
+							+ belief.getId() + "') ?v))", false);
+
+			log("goal is " + result.goal.goalString + " with inf-gain "
+					+ result.informationGain);
+
+		}
 		return result;
 
 	}
@@ -66,8 +74,17 @@ public class ProtoObjectMotiveGenerator extends
 	@Override
 	protected AnalyzeProtoObjectMotive checkForUpdate(GroundedBelief _newEntry,
 			AnalyzeProtoObjectMotive _motive) {
-		//assume for now that no update effects the motive
-		return _motive;
-	}
 
+		CASTIndependentFormulaDistributionsBelief<GroundedBelief> belief = CASTIndependentFormulaDistributionsBelief
+				.create(GroundedBelief.class, _newEntry);
+
+		if (!belief.getContent().containsKey(
+				ProtoObjectTransferFunction.VISUAL_OBJECT_LINK)) {
+			log("ProtoObject belief is still not linked to VisualObject, so leaving motive unchanged.");
+			return _motive;
+		} else {
+			log("ProtoObject belief is now linked to VisualObject, so removing motive.");
+			return null;
+		}
+	}
 }
