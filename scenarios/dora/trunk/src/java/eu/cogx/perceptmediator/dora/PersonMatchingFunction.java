a924a83ca47099929c16292543ef330a7f54662e
Marc Hanheide <marc@hanheide.net>
2011-07-26 09:26:07 +0000

    new for person tracking

--- scenarios/dora/trunk/src/java/eu/cogx/perceptmediator/dora/PersonMatchingFunction.java
+++ scenarios/dora/trunk/src/java/eu/cogx/perceptmediator/dora/PersonMatchingFunction.java
@@ -3,28 +3,21 @@
  */
 package eu.cogx.perceptmediator.dora;
 
-import org.jaxen.function.StringFunction;
-
 import VisionData.Person;
-import cast.core.CASTUtils;
 import castutils.castextensions.WMContentWaiter.ContentMatchingFunction;
 import de.dfki.lt.tr.beliefs.data.formulas.Formula;
 import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
 import de.dfki.lt.tr.beliefs.data.specificproxies.IndependentFormulaDistributionsBelief;
-import de.dfki.lt.tr.beliefs.slice.distribs.BasicProbDistribution;
 import de.dfki.lt.tr.beliefs.slice.distribs.CondIndependentDistribs;
 import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
-import de.dfki.lt.tr.dialogue.slice.lf.Proposition;
-import eu.cogx.beliefs.slice.GroundedBelief;
-import eu.cogx.beliefs.slice.PerceptBelief;
 import eu.cogx.perceptmediator.transferfunctions.abstr.SimpleDiscreteTransferFunction;
 
 /**
  * @author marc
  * 
  */
-public class PersonMatchingFunction implements
-		ContentMatchingFunction<GroundedBelief> {
+public class PersonMatchingFunction<T extends dBelief> implements
+		ContentMatchingFunction<T> {
 	/** the identifier of the PlaceId attribute */
 	public static final String PLACE_ID = "PlaceId";
 
@@ -38,7 +31,7 @@ public class PersonMatchingFunction implements
 	}
 
 	@Override
-	public boolean matches(GroundedBelief r) {
+	public boolean matches(T r) {
 		if (r.type.equals(SimpleDiscreteTransferFunction
 				.getBeliefTypeFromCastType(Person.class))) {
 			assert (r.content instanceof CondIndependentDistribs);
--- scenarios/dora/trunk/src/java/eu/cogx/perceptmediator/dora/PersonRelationTransferFunction.java
+++ scenarios/dora/trunk/src/java/eu/cogx/perceptmediator/dora/PersonRelationTransferFunction.java
@@ -9,7 +9,7 @@ import VisionData.Person;
 import cast.architecture.ManagedComponent;
 import castutils.castextensions.WMView;
 import castutils.castextensions.WMContentWaiter.ContentMatchingFunction;
-import eu.cogx.beliefs.slice.GroundedBelief;
+import eu.cogx.beliefs.slice.PerceptBelief;
 import eu.cogx.perceptmediator.transferfunctions.abstr.LocalizedRelationTransferFunction;
 
 /**
@@ -17,19 +17,19 @@ import eu.cogx.perceptmediator.transferfunctions.abstr.LocalizedRelationTransfer
  * 
  */
 public class PersonRelationTransferFunction extends
-		LocalizedRelationTransferFunction<Person, GroundedBelief> {
+		LocalizedRelationTransferFunction<Person, PerceptBelief> {
 
 	public PersonRelationTransferFunction(ManagedComponent component,
-			WMView<GroundedBelief> allBeliefs) {
+			WMView<PerceptBelief> allBeliefs) {
 		super(component, allBeliefs, Logger
 				.getLogger(PersonRelationTransferFunction.class),
-				GroundedBelief.class);
+				PerceptBelief.class);
 	}
 
 	@Override
-	protected ContentMatchingFunction<GroundedBelief> getMatchingFunction(
+	protected ContentMatchingFunction<PerceptBelief> getMatchingFunction(
 			String id) {
-		return new PersonMatchingFunction(id);
+		return new PersonMatchingFunction<PerceptBelief>(id);
 	}
 
 }
