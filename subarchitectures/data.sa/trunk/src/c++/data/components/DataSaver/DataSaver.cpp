b9fbebe1d5bc6947b624da664e736e6c292e7ca2
alper <alper@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-08-03 16:17:27 +0000

    added get robotpose

--- subarchitectures/data.sa/trunk/src/c++/data/components/DataSaver/DataSaver.cpp
+++ subarchitectures/data.sa/trunk/src/c++/data/components/DataSaver/DataSaver.cpp
@@ -6,35 +6,94 @@
  */
 
 #include "DataSaver.h"
+#include <cast/architecture/ChangeFilterFactory.hpp>
+#include <NavData.hpp>
 
 using namespace cast;
 
-extern "C" {
-cast::CASTComponentPtr newComponent() {
-	return new DataSaver();
-}
-}
-
-DataSaver::DataSaver() {
-}
-
-void DataSaver::start() {
-	startPCCServerCommunication(*this);
+extern "C"
+{
+  cast::CASTComponentPtr
+  newComponent()
+  {
+    return new DataSaver();
+  }
 }
 
-void DataSaver::configure(const std::map<std::string, std::string>& _config) {
-	configureServerCommunication(_config);
+DataSaver::DataSaver(): saveRobotPose(false)
+{
 }
 
-DataSaver::~DataSaver() {
+void
+DataSaver::start()
+{
+  startPCCServerCommunication(*this);
+  if (saveRobotPose){
+    addChangeFilter(createLocalTypeFilter<NavData::RobotPose2d>(cdl::ADD),
+          new MemberFunctionChangeReceiver<DataSaver>(this,
+                      &DataSaver::newRobotPose));
+
+      addChangeFilter(createLocalTypeFilter<NavData::RobotPose2d>(cdl::OVERWRITE),
+          new MemberFunctionChangeReceiver<DataSaver>(this,
+                      &DataSaver::newRobotPose));
+
+  }
 }
 
+void DataSaver::newRobotPose(const cdl::WorkingMemoryChange &objID)
+{
+  NavData::RobotPose2dPtr oobj =
+    getMemoryEntry<NavData::RobotPose2d>(objID.address);
 
-void DataSaver::receiveScan2d(const Laser::Scan2d &castScan){
-	PointCloud::SurfacePointSeq points;
-	getPoints(false,640, points);
+
+  m_SlamRobotPose.setTime(Cure::Timestamp(oobj->time.s,
+                                           oobj->time.us));
+  m_SlamRobotPose.setX(oobj->x);
+  m_SlamRobotPose.setY(oobj->y);
+  m_SlamRobotPose.setTheta(oobj->theta);
+
+  Cure::Pose3D cp = m_SlamRobotPose;
 }
 
-void DataSaver::receiveOdometry(const Robotbase::Odometry &castOdom){
+void
+DataSaver::configure(const std::map<std::string, std::string>& _config)
+{
+  configureServerCommunication(_config);
+
+  Ice::CommunicatorPtr ic = getCommunicator();
+
+  Ice::Identity id;
+  id.name = "PTZServer";
+  id.category = "PTZServer";
+
+  std::ostringstream str;
+  str << ic->identityToString(id) << ":default" << " -h localhost" << " -p "
+      << cast::cdl::CPPSERVERPORT;
+
+  Ice::ObjectPrx base = ic->stringToProxy(str.str());
+  m_ptzInterface = ptz::PTZInterfacePrx::uncheckedCast(base);
+}
+
+DataSaver::~DataSaver()
+{
+}
+
+void
+DataSaver::receiveScan2d(const Laser::Scan2d &castScan)
+{
+  PointCloud::SurfacePointSeq points;
+  getPoints(false, 640, points);
+}
+
+void
+DataSaver::receiveOdometry(const Robotbase::Odometry &castOdom)
+{
+
+}
+
+void
+DataSaver::saveToFile(PointCloud::SurfacePointSeq& points,
+    Laser::Scan2d &castScan)
+{
 
 }
--- subarchitectures/data.sa/trunk/src/c++/data/components/DataSaver/DataSaver.h
+++ subarchitectures/data.sa/trunk/src/c++/data/components/DataSaver/DataSaver.h
@@ -8,27 +8,54 @@
 #ifndef DATASAVER_H_
 #define DATASAVER_H_
 
-
 #include <cast/architecture/ManagedComponent.hpp>
 #include "Laser.hpp"
 #include <Scan2dReceiver.hpp>
 #include <OdometryReceiver.hpp>
 #include <PointCloudClient.h>
 #include <PointCloud.hpp>
+#include <PTZ.hpp>
+#include <SensorData/SensorPose.hh>
 
-class DataSaver: public Scan2dReceiver,
-			     public cast::ManagedComponent,
-				 public OdometryReceiver,
-				 public cast::PointCloudClient {
+class DataSaver : public Scan2dReceiver,
+    public cast::ManagedComponent,
+    public OdometryReceiver,
+    public cast::PointCloudClient
+{
 public:
-	DataSaver();
-	virtual ~DataSaver();
-	void start();
-	void configure(const std::map<std::string, std::string>& _config);
+  DataSaver();
+
+  virtual
+  ~DataSaver();
+
+  void
+  start();
+
+  void
+  configure(const std::map<std::string, std::string>& _config);
 protected:
-	 void receiveScan2d(const Laser::Scan2d &castScan);
-	 void receiveOdometry(const Robotbase::Odometry &castOdom);
-	 void getPoints(bool transformToGlobal, int imgWidth, PointCloud::SurfacePointSeq& points);
+  void
+  receiveScan2d(const Laser::Scan2d &castScan);
+
+  void
+  receiveOdometry(const Robotbase::Odometry &castOdom);
+
+  void
+  getPoints(bool transformToGlobal, int imgWidth,
+      PointCloud::SurfacePointSeq& points);
+
+  void
+  saveToFile();
+
+  void
+  saveToFile(PointCloud::SurfacePointSeq& points, Laser::Scan2d &castScan);
+
+  void
+  newRobotPose(const cast::cdl::WorkingMemoryChange &objID);
+private:
+  ptz::PTZInterfacePrx m_ptzInterface;
+  bool saveRobotPose;
+  Cure::Pose3D m_SlamRobotPose;
 };
 
 #endif /* DATASAVER_H_ */
