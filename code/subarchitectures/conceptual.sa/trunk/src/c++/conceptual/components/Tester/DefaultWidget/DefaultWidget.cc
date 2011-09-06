6f2114c8a65947a005eb357c0ce64f324c4f7661
pronobis <pronobis@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-09-06 15:55:43 +0000

    Extended the widget to show human assertions.

--- code/subarchitectures/conceptual.sa/trunk/src/c++/conceptual/components/Tester/DefaultWidget/DefaultWidget.cc
+++ code/subarchitectures/conceptual.sa/trunk/src/c++/conceptual/components/Tester/DefaultWidget/DefaultWidget.cc
@@ -7,6 +7,10 @@ DefaultWidget::DefaultWidget(QWidget *parent, conceptual::Tester *component)
 {
 	ui.setupUi(this);
 
+	DefaultData::StringSeq humanAssertions = component->getHumanAssertions();
+	for (unsigned int i=0; i<humanAssertions.size(); ++i)
+		ui.humanAssertionsListWidget->addItem(QString::fromStdString(humanAssertions[i]));
+
 	DefaultData::StringSeq appearances = component->getAppearances();
 	for (unsigned int i=0; i<appearances.size(); ++i)
 		ui.appearancesListWidget->addItem(QString::fromStdString(appearances[i]));
@@ -14,6 +18,10 @@ DefaultWidget::DefaultWidget(QWidget *parent, conceptual::Tester *component)
 	DefaultData::StringSeq shapes = component->getShapes();
 	for (unsigned int i=0; i<shapes.size(); ++i)
 		ui.shapesListWidget->addItem(QString::fromStdString(shapes[i]));
+
+	DefaultData::StringSeq sizes = component->getSizes();
+	for (unsigned int i=0; i<sizes.size(); ++i)
+		ui.sizesListWidget->addItem(QString::fromStdString(sizes[i]));
 
 	DefaultData::StringSeq rooms = component->getRoomCategories();
 	for (unsigned int i=0; i<rooms.size(); ++i)
