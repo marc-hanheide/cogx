b48711e78980ae3e3576e63b72e8cb9b0b64fb9c
Nick Hawes <nickh@robots.ox.ac.uk>
2011-09-09 12:07:12 +0000

    renamed eigen find to clarify difference in versions

--- cmake/trunk/FindEigen2.cmake
+++ cmake/trunk/FindEigen2.cmake
@@ -1,16 +1,5 @@
-FIND_PATH(
-  EIGEN_INCLUDE_DIRS Eigen/Array
-  /usr/include/eigen2
-  /usr/local/include/eigen2
-  /opt/local/include/eigen2 
-)
-
-IF(EIGEN_INCLUDE_DIRS)
-  INCLUDE_DIRECTORIES(${EIGEN_INCLUDE_DIRS})
-  MESSAGE(STATUS "Found Eigen: ${EIGEN_INCLUDE_DIRS}")
-ELSE(EIGEN_INCLUDE_DIRS)
-  MESSAGE(FATAL_ERROR "Eigen not found")
-ENDIF(EIGEN_INCLUDE_DIRS)
+find_package(PkgConfig REQUIRED)
+pkg_search_module(EIGEN2 REQUIRED eigen2)
 
 FIND_PATH(
   UMFPACK_INCLUDE_DIRS umfpack.h
