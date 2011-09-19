9729316145cb78318c1f1b75824c6c90f8ff683c
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-09-19 10:45:17 +0000

    soif: simple generation of vc-s in look-around-command

--- code/subarchitectures/vision.sa/trunk/src/c++/vision/components/SOIFilter/TaskMoveToViewCone.cpp
+++ code/subarchitectures/vision.sa/trunk/src/c++/vision/components/SOIFilter/TaskMoveToViewCone.cpp
@@ -6,6 +6,7 @@
 
 #include "TaskMoveToViewCone.h"
 #include "SOIFilter.h"
+#include <cmath>
 
 #include "../../VisionUtils.h"
 
@@ -87,22 +88,37 @@ void WmTaskExecutor_MoveToViewCone::lookAround(WmEvent *pEvent)
   }
   debug("move_to: GOT A LookAroundCommand");
 
-  if (! pSoiFilter->hasPtz())
-    cmd.fail();
-  else {
-    // XXX: This command only moves the PTU, but should also move the robot
-    //try {
-    //  ViewConePtr pBetterVc = pSoiFilter->getMemoryEntry<VisionData::ViewCone>(cmd.pcmd->target->address);
-    //  double pan = pBetterVc->viewDirection - pBetterVc->anchor.z;
-    //  double tilt = pBetterVc->tilt;
-    //  pSoiFilter->movePtz(pan, tilt);
-    //  cmd.succeed();
-    //}
-    //catch (cast::DoesNotExistOnWMException) {
-    //  pSoiFilter->debug("move_to: ViewCone deleted while working.");
-    //  cmd.fail();
-    //}
+  long count = 3;
+  while (count > 0) {
+    double pan  = (rand() % 200 - 100) / 100.0 * (M_PI / 2);
+    //double tilt = (rand() % 20 - 10) / 100.0 * (M_PI);
+    if (fabs(pSoiFilter->m_RobotPose.pan - pan) * 180 / M_PI < 20)
+      continue;
+
+    bool retry = false;
+    for (int i = 0; i < cmd.pcmd->viewCones.size(); i++) {
+      ViewConePtr &pvc = cmd.pcmd->viewCones[i];
+      double vcpan = pvc->viewDirection - pvc->anchor.z;
+      if (fabs(vcpan - pan) * 180 / M_PI < 20) {
+        retry = true;
+        break;
+      }
+    }
+    if (retry) continue;
+
+    ViewConePtr pCurVc = createViewCone();
+    pCurVc->anchor.x = pSoiFilter->m_RobotPose.x;
+    pCurVc->anchor.y = pSoiFilter->m_RobotPose.y;
+    pCurVc->anchor.z = pSoiFilter->m_RobotPose.theta;
+    pCurVc->x = 0;
+    pCurVc->y = 0;
+    pCurVc->viewDirection = pSoiFilter->m_RobotPose.theta + pan;
+    pCurVc->tilt = pSoiFilter->m_RobotPose.tilt;
+
+    cmd.pcmd->viewCones.push_back(pCurVc);
+    --count;
   }
+  cmd.succeed();
 }
 
 
