b9235d9b7b8e876240910929fcd4665395c41283
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-10-04 16:54:06 +0000

    adding forgotten files

--- tools/abducer/trunk/api-java/src/de/dfki/lt/tr/infer/abducer/proof/AbstractAssertionSolver.java
+++ tools/abducer/trunk/api-java/src/de/dfki/lt/tr/infer/abducer/proof/AbstractAssertionSolver.java
@@ -1,16 +1,18 @@
 package de.dfki.lt.tr.infer.abducer.proof;
 
-import de.dfki.lt.tr.infer.abducer.lang.ModalisedAtom;
-
 public abstract class AbstractAssertionSolver<T> implements AssertionSolver {
 
-	public abstract T parseFromModalisedAtom(ModalisedAtom matom);
+	private ModalisedAtomMatcher<T> matcher;
+
+	public AbstractAssertionSolver(ModalisedAtomMatcher<T> matcher) {
+		this.matcher = matcher;
+	}
 
 	public abstract ContextUpdate solveFromParsed(T a);
 
 	@Override
 	public final ContextUpdate solve(Assertion a) {
-		T parsedAssertion = parseFromModalisedAtom(a.getModalisedAtom());
+		T parsedAssertion = matcher.match(a.getModalisedAtom());
 		if (parsedAssertion == null) {
 			return null;
 		}
