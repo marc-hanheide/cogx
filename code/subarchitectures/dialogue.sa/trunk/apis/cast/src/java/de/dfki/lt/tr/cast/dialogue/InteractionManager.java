7db39827e1acf1660d259603b04db325beaf2e50
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-10-06 21:55:41 +0000

    adding an interaction management component

---
+++ code/subarchitectures/dialogue.sa/trunk/apis/cast/src/java/de/dfki/lt/tr/cast/dialogue/InteractionManager.java
@@ -0,0 +1,18 @@
+package de.dfki.lt.tr.cast.dialogue;
+
+import java.util.Map;
+
+public class InteractionManager
+extends AbstractDialogueComponent {
+
+	@Override
+	public void onConfigure(Map<String, String> args) {
+		super.onConfigure(args);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+	}
+
+}
