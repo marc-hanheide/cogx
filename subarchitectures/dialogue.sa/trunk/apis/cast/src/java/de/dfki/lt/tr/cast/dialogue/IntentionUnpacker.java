d049f4481fde6990cd66bbe3306bf8d67767834c
Nick Hawes <nickh@robots.ox.ac.uk>
2011-10-11 14:51:13 +0000

    refactored unpacker for reuse

--- subarchitectures/dialogue.sa/trunk/apis/cast/src/java/de/dfki/lt/tr/cast/dialogue/IntentionUnpacker.java
+++ subarchitectures/dialogue.sa/trunk/apis/cast/src/java/de/dfki/lt/tr/cast/dialogue/IntentionUnpacker.java
@@ -1,7 +1,11 @@
 package de.dfki.lt.tr.cast.dialogue;
 
+import cast.AlreadyExistsOnWMException;
+import cast.DoesNotExistOnWMException;
 import cast.SubarchitectureComponentException;
+import cast.UnknownSubarchitectureException;
 import cast.architecture.ChangeFilterFactory;
+import cast.architecture.ManagedComponent;
 import cast.architecture.WorkingMemoryChangeReceiver;
 import cast.cdl.WorkingMemoryAddress;
 import cast.cdl.WorkingMemoryChange;
@@ -11,57 +15,97 @@ import de.dfki.lt.tr.beliefs.slice.intentions.PossibleInterpretedIntentions;
 import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
 import de.dfki.lt.tr.dialogue.interpret.InterpretedUserIntention;
 import de.dfki.lt.tr.dialogue.util.BeliefIntentionUtils;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.Set;
 
-public class IntentionUnpacker
-extends AbstractDialogueComponent {
+public class IntentionUnpacker extends AbstractDialogueComponent {
 
 	@Override
 	public void onStart() {
 		super.onStart();
 
 		addChangeFilter(
-				ChangeFilterFactory.createLocalTypeFilter(PossibleInterpretedIntentions.class, WorkingMemoryOperation.ADD),
+				ChangeFilterFactory.createLocalTypeFilter(
+						PossibleInterpretedIntentions.class,
+						WorkingMemoryOperation.ADD),
 				new WorkingMemoryChangeReceiver() {
 					@Override
 					public void workingMemoryChanged(WorkingMemoryChange _wmc) {
 						handlePossibleInterpretedIntentions(_wmc);
 					}
-		});
+				});
 	}
 
 	private void handlePossibleInterpretedIntentions(WorkingMemoryChange _wmc) {
-		addTask(new ProcessingTaskWithData<WorkingMemoryAddress>(_wmc.address) {
+		addTask(new ProcessingTaskWithDataAndComponent<WorkingMemoryAddress, IntentionUnpacker>(
+				_wmc.address, this) {
 
 			@Override
 			public void execute(WorkingMemoryAddress addr) {
 				try {
-					PossibleInterpretedIntentions pii = getMemoryEntry(addr, PossibleInterpretedIntentions.class);
-
-					WorkingMemoryAddress iintAddr = mostConfidentIntentionAddress(pii);
-					InterpretedIntention iint = pii.intentions.get(iintAddr);
-					
-					for (WorkingMemoryAddress belAddr : iint.addressContent.values()) {
-						if (pii.beliefs.containsKey(belAddr)) {
-							dBelief bel = pii.beliefs.get(belAddr);
-							getLogger().info("adding a dBelief to " + wmaToString(belAddr) + ": " + BeliefIntentionUtils.beliefToString(bel));
-							addToWorkingMemory(belAddr, bel);
-						}
-					}
-					getLogger().info("adding an InterpretedIntention to " + wmaToString(iintAddr) + ":\n" + InterpretedUserIntention.interpretedIntentionToString(iint));
-					addToWorkingMemory(iintAddr, iint);
-				}
-				catch (SubarchitectureComponentException ex) {
+					unpackIntention(getComponent(), addr);
+				} catch (SubarchitectureComponentException ex) {
 					getLogger().error("component exception", ex);
 				}
 			}
 
 		});
 	}
+	
+	/**
+	 * Unpack the most confident intention.
+	 * 
+	 * @param _component
+	 * @param addr
+	 * @throws DoesNotExistOnWMException
+	 * @throws UnknownSubarchitectureException
+	 * @throws AlreadyExistsOnWMException
+	 */
+	public static void unpackIntention(ManagedComponent _component,
+			WorkingMemoryAddress addr) throws DoesNotExistOnWMException,
+			UnknownSubarchitectureException, AlreadyExistsOnWMException {
+		PossibleInterpretedIntentions pii = _component.getMemoryEntry(addr,
+				PossibleInterpretedIntentions.class);
 
-	public static WorkingMemoryAddress mostConfidentIntentionAddress(PossibleInterpretedIntentions pii) {
+		WorkingMemoryAddress iintAddr = mostConfidentIntentionAddress(pii);
+		unpackIntention(_component, iintAddr, pii);
+	}
+
+	/**
+	 * Unpack the intention with the given address.
+	 * 
+	 * @param _component
+	 * @param _intentionAddr
+	 * @param _interpretations
+	 * @throws AlreadyExistsOnWMException
+	 * @throws DoesNotExistOnWMException
+	 * @throws UnknownSubarchitectureException
+	 */
+	public static void unpackIntention(ManagedComponent _component,
+			WorkingMemoryAddress _intentionAddr,
+			PossibleInterpretedIntentions _interpretations)
+			throws AlreadyExistsOnWMException, DoesNotExistOnWMException,
+			UnknownSubarchitectureException {
+		InterpretedIntention iint = _interpretations.intentions.get(_intentionAddr);
+
+		for (WorkingMemoryAddress belAddr : iint.addressContent.values()) {
+			if (_interpretations.beliefs.containsKey(belAddr)) {
+				dBelief bel = _interpretations.beliefs.get(belAddr);
+				_component.getLogger().info(
+						"adding a dBelief to " + wmaToString(belAddr) + ": "
+								+ BeliefIntentionUtils.beliefToString(bel));
+				_component.addToWorkingMemory(belAddr, bel);
+			}
+		}
+		_component.getLogger().info(
+				"adding an InterpretedIntention to "
+						+ wmaToString(_intentionAddr)
+						+ ":\n"
+						+ InterpretedUserIntention
+								.interpretedIntentionToString(iint));
+		_component.addToWorkingMemory(_intentionAddr, iint);
+	}
+
+	public static WorkingMemoryAddress mostConfidentIntentionAddress(
+			PossibleInterpretedIntentions pii) {
 		WorkingMemoryAddress bestAddr = null;
 		double best = -1.0;
 
@@ -72,7 +116,7 @@ extends AbstractDialogueComponent {
 				best = iint.confidence;
 			}
 		}
-		
+
 		return bestAddr;
 	}
 
