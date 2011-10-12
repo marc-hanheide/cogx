c00877c2297c1eb85e7398585849f071a5d6a717
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-10-12 17:08:42 +0000

    Turn off median filter for virtual laser scan

--- code/subarchitectures/categorical.sa/trunk/src/c++/categorical/components/DataProvider/DataProvider.cpp
+++ code/subarchitectures/categorical.sa/trunk/src/c++/categorical/components/DataProvider/DataProvider.cpp
@@ -21,6 +21,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <algorithm>
 
 
 using namespace std;
@@ -1150,7 +1151,9 @@ void CategoricalDataProvider::getGridmapScan(std::vector<double> &ranges, double
 {
   SpatialData::MapInterfacePrx mapPrx(getIceServer<SpatialData::MapInterface>("spatial.control"));
   vector<double> virtScan = mapPrx->getGridmapRaytrace(startAngle, angleStep, beamCount);
-	medianFilter(virtScan, ranges, 51);
+//	medianFilter(virtScan, ranges, 51);
+  ranges.clear();
+  ranges.insert(ranges.end(), virtScan.begin(), virtScan.end());
 }
 
 void CategoricalDataProvider::medianFilter(const std::vector<double> &in, std::vector<double> &out, unsigned int order)
