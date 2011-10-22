de29d8e1a67ca2cf49fe3c2043ed1a198dfe06b2
krsj <krsj@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-10-22 10:27:54 +0000

    Random on-ness point cloud sampling makes a comeback

--- subarchitectures/spatial.sa/trunk/src/c++/ObjectRelationTester.hpp
+++ subarchitectures/spatial.sa/trunk/src/c++/ObjectRelationTester.hpp
@@ -62,6 +62,13 @@ protected:
   void updatePosesFromPB();
 
   DensitySampler m_sampler;
+
+  void sampleOnnessForObject(const spatial::Object *objectS, spatial::Object *objectO);
+  void sampleRecursively(const vector<spatial::Object*> &objects, 
+    int currentLevel, unsigned int nSamplesPerStep, unsigned int nMaxSamples,
+    vector<Vector3> &outPoints, spatial::Object *supportObject
+//    , const vector<Vector3> &triangle
+    );
 }; 
 };
 #endif //ObjectRelationTester_hpp
