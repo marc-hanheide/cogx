ba0bd99793b339a1b5c8849b7215b610916e7506
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-11-08 20:35:42 +0000

    added

--- code/systems/dora-yr3/build-jenkins.sh
+++ code/systems/dora-yr3/build-jenkins.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-mkdir -pv "$WORKSPACE/BUILD"
-cd "$WORKSPACE/BUILD" \
+
+if [ "$1" ]; then
+	workdir="$1"
+else
+	workdir=`pwd`
+fi
+
+mkdir -pv "$workdir/BUILD"
+cd "$workdir/BUILD" \
 	&& (../tools/scripts/cmake-apply . ../cmake-caches/dora-yr3.txt || cmake -C ../cmake-caches/dora-yr3.txt ..) \
 	&& make -j4 install \
-	&& cd "$WORKSPACE" \
+	&& cd "$workdir" \
 	&& ant \
 	&& echo done
