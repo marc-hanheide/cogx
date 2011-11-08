49f13ba32e5794a7745365d7369d924a483bf9be
Marc Hanheide <marc@hanheide.net>
2011-11-08 20:38:31 +0000

    always fresh cmake

--- systems/dora-yr3/build-jenkins.sh
+++ systems/dora-yr3/build-jenkins.sh
@@ -8,7 +8,7 @@ fi
 
 mkdir -pv "$workdir/BUILD"
 cd "$workdir/BUILD" \
-	&& (../tools/scripts/cmake-apply . ../cmake-caches/dora-yr3.txt || cmake -C ../cmake-caches/dora-yr3.txt ..) \
+	&& cmake -C ../cmake-caches/dora-yr3.txt .. \
 	&& make -j4 install \
 	&& cd "$workdir" \
 	&& ant \
