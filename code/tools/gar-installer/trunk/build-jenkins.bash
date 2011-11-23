680733046d9a250ab9eee0099395941b0ff108dd
cogx <cogx@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-11-23 19:35:43 +0000

    added SVNUSER

--- code/tools/gar-installer/trunk/build-jenkins.bash
+++ code/tools/gar-installer/trunk/build-jenkins.bash
@@ -1,11 +1,11 @@
-export http_proxy=http://webcache:3128
-export https_proxy=http://webcache:3128
+export http_proxy=http://webcache:3128/
+export https_proxy=http://webcache:3128/
 
 dname=`dirname $0`
 
-cd $dname
+export http_proxy
+export https_proxy
+export SVNUSER=cogx
+env
 
-echo "make sure you have this in a file in /etc/sudoers.d:"
-echo %jenkins	ALL=NOPASSWD:SETENV:/usr/bin/make -C $dname dora
-
-sudo /usr/bin/make -C $dname dora
+/usr/bin/make -C $dname dora
