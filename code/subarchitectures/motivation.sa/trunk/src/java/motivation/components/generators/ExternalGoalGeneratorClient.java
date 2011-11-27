95094fc635538297620f386549282b2afab62722
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2011-11-27 22:56:53 +0000

    - added main method to call from shell

--- code/subarchitectures/motivation.sa/trunk/src/java/motivation/components/generators/ExternalGoalGeneratorClient.java
+++ code/subarchitectures/motivation.sa/trunk/src/java/motivation/components/generators/ExternalGoalGeneratorClient.java
@@ -3,7 +3,8 @@
  */
 package motivation.components.generators;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import motivation.slice.ExternalGoalServer;
 import motivation.slice.ExternalGoalServerPrx;
 import motivation.slice.MotiveStatus;
@@ -24,24 +25,25 @@ import cast.core.CASTUtils;
  * 
  * <pre>
  * 	<target name="goaltest">
-		<junit fork="no">
-			<sysproperty key="goal" value="${test.goal}"/>
-		  	<classpath>
-				<fileset dir="${jar.dir}">
-  					<include name="*.jar"/>
-				</fileset>
-    			<pathelement location="${output.dir}"/>
-    			<pathelement location="${cast.jar}"/>
-    			<pathelement location="${ice.jar}"/>
-  			</classpath>
-
-    		<formatter type="xml"/> 
-  			
-			<test name="motivation.components.generators.ExternalGoalGeneratorClient" haltonfailure="yes" outfile="result" />
-		</junit>
-	</target>
-
+ * 		<junit fork="no">
+ * 			<sysproperty key="goal" value="${test.goal}"/>
+ * 		  	<classpath>
+ * 				<fileset dir="${jar.dir}">
+ *   					<include name="*.jar"/>
+ * 				</fileset>
+ *     			<pathelement location="${output.dir}"/>
+ *     			<pathelement location="${cast.jar}"/>
+ *     			<pathelement location="${ice.jar}"/>
+ *   			</classpath>
+ * 
+ *     		<formatter type="xml"/> 
+ *   			
+ * 			<test name="motivation.components.generators.ExternalGoalGeneratorClient" haltonfailure="yes" outfile="result" />
+ * 		</junit>
+ * 	</target>
+ * 
  * </pre>
+ * 
  * @author marc
  * 
  */
@@ -49,7 +51,7 @@ public class ExternalGoalGeneratorClient {
 
 	/** timeout of 30 minutes */
 	private static final long TIMEOUT = 30 * 60 * 1000;
-	Logger logger = Logger.getLogger(ExternalGoalGeneratorClient.class);
+	static Logger logger = Logger.getLogger(ExternalGoalGeneratorClient.class);
 	private String serverHost;
 	private String serverName;
 	private ExternalGoalServerPrx server;
@@ -71,20 +73,20 @@ public class ExternalGoalGeneratorClient {
 		logger.info("calling the server with goal " + goalString);
 		MotiveStatus result = server.submitGoal(goalString, importance);
 		logger.info("server returned with " + result);
-		if (result == MotiveStatus.ACTIVE)
+		if (result == MotiveStatus.ACTIVE || result == MotiveStatus.COMPLETED)
 			return true;
 		else
 			return false;
 	}
 
-	String getGoal() {
+	String getGoalFromProperty() {
 		String g = System.getProperty("goal");
 		if (g == null)
 			g = "(and )";
 		return g;
 	}
 
-	float getImportance() {
+	float getImportanceFromProperty() {
 		String i = System.getProperty("importance");
 		if (i == null)
 			i = "-1";
@@ -100,10 +102,12 @@ public class ExternalGoalGeneratorClient {
 			fail("test failed from exception: " + e.message);
 		}
 
-		String goal = getGoal();
-		float importance = getImportance();
+		String goal = getGoalFromProperty();
+		float importance = getImportanceFromProperty();
 		logger.info("got goal " + goal + ", got importance " + importance);
-		assertTrue("the goal " + goal + " has not been achieved, but has now been removed.", call(goal, importance));
+		assertTrue("the goal " + goal
+				+ " has not been achieved, but has now been removed.",
+				call(goal, importance));
 	}
 
 	public ExternalGoalGeneratorClient() {
@@ -120,8 +124,32 @@ public class ExternalGoalGeneratorClient {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if (args.length < 1) {
+			System.err
+					.println("command line parameters: <goal> [<importance>]");
+			System.exit(1);
+		}
 
+		ExternalGoalGeneratorClient client = new ExternalGoalGeneratorClient();
+		try {
+			client.init();
+		} catch (CASTException e) {
+			logger.error("exception in testGoal", e);
+			System.exit(1);
+		}
+
+		String goal = args[0];
+		float importance = -1;
+		if (args.length > 1)
+			importance = Float.parseFloat(args[1]);
+		logger.info("got goal " + goal + ", got importance " + importance);
+		if (client.call(goal, importance)) {
+			logger.info("goal " + goal + " has been achieved.");
+			System.exit(0);
+		} else {
+			logger.warn("goal " + goal + " has failed.");
+			System.exit(1);
+		}
 	}
 
 }
