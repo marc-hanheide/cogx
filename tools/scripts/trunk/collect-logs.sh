da091dab6ff65322a521c4578521a2a1a072d120
Marc Hanheide <marc@hanheide.net>
2011-12-07 15:32:40 +0000

    collect-logs will now store the user name and the svn info, too

--- tools/scripts/trunk/collect-logs.sh
+++ tools/scripts/trunk/collect-logs.sh
@@ -2,7 +2,11 @@
 
 
 dirPrfx=`pwd | sed "s@.*/\(.*\)@\1@"`
-zipname=${dirPrfx}_`date +"%Y-%m-%d_%H-%M-%S"`
+zipname=logs/${USER}_${dirPrfx}_`date +"%Y-%m-%d_%H-%M-%S"`
 
-zip -j "$zipname" logs/log.xml subarchitectures/planner.sa/src/python/standalone/tmp/static_dir_for_debugging/* tmpmap.* conceptual.fg conceptual.info
+svn info . > logs/svn-info.txt
+svn st -q . >> logs/svn-info.txt
+
+
+zip -j "$zipname" logs/log.xml logs/svn-info.txt subarchitectures/planner.sa/src/python/standalone/tmp/static_dir_for_debugging/* tmpmap.* conceptual.fg conceptual.info
 
