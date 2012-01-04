0fa00bab1543bc43321ab4c55aa687e9ed5bcd81
Nick Hawes <nickh@robots.ox.ac.uk>
2012-01-04 22:57:30 +0000

    c++ server now fits axis-aligned bounding box too. this is returned as a fake object

--- scenarios/cdsr/trunk/src/cpp/RoomCoordinateFrameTransformer.cpp
+++ scenarios/cdsr/trunk/src/cpp/RoomCoordinateFrameTransformer.cpp
@@ -1,6 +1,6 @@
 #include <CGAL/Cartesian.h>
 #include <CGAL/bounding_box.h>
-
+#include <CGAL/linear_least_squares_fitting_2.h>
 
 #include <iostream>
 
@@ -29,6 +29,8 @@ typedef double              FT;
 typedef CGAL::Cartesian<FT> K;
 typedef K::Point_2          Point_2;
 typedef K::Point_3          Point_3;
+typedef K::Line_2            Line_2;
+
 
 using namespace std;
 
@@ -61,13 +63,56 @@ public:
       cout << "----------------" << endl;
       
       points.push_back(Point_2(next_wall.start().x(), next_wall.start().y()));
-      points.push_back(Point_2(next_wall.end().x(), next_wall.end().y()));
+      // points.push_back(Point_2(next_wall.end().x(), next_wall.end().y()));
     }
     
-    // axis-aligned bounding box of 2D points
-    K::Iso_rectangle_2 c2 = CGAL::bounding_box(points.begin(), points.end());
-    std::cout << c2 << std::endl;
+    // add an axis-aligned bounding box of 2D points as an extra object
 
+    K::Iso_rectangle_2 c2 = CGAL::bounding_box(points.begin(), points.end());
+    // std::cout << c2 << std::endl;
+
+	::cdsr::rsb::SensedObject* newObject = room->add_object();
+	newObject->set_id("fake object");
+	newObject->set_type("bounding box");
+
+	
+	::cdsr::rsb::Line* newSide = newObject->add_side();
+	newSide->mutable_start()->set_x(c2.xmin());
+	newSide->mutable_start()->set_y(c2.ymin());
+	newSide->mutable_end()->set_x(c2.xmin());
+	newSide->mutable_end()->set_y(c2.ymax());
+
+	newSide = newObject->add_side();
+	newSide->mutable_start()->set_x(c2.xmin());
+	newSide->mutable_start()->set_y(c2.ymax());
+	newSide->mutable_end()->set_x(c2.xmax());
+	newSide->mutable_end()->set_y(c2.ymax());
+	
+	newSide = newObject->add_side();
+	newSide->mutable_start()->set_x(c2.xmax());
+	newSide->mutable_start()->set_y(c2.ymax());
+	newSide->mutable_end()->set_x(c2.xmax());
+	newSide->mutable_end()->set_y(c2.ymin());
+	
+	newSide = newObject->add_side();
+	newSide->mutable_start()->set_x(c2.xmax());
+	newSide->mutable_start()->set_y(c2.ymin());
+	newSide->mutable_end()->set_x(c2.xmin());
+	newSide->mutable_end()->set_y(c2.ymin());
+	
+	
+	
+	//add a 2D regression line to the room as an extra wall
+	
+	//fit line to 2D points
+   Line_2 line;
+   linear_least_squares_fitting_2(points.begin(),points.end(),line,CGAL::Dimension_tag<0>());
+
+	::cdsr::rsb::Line* newWall = room->mutable_room()->add_wall();	
+	newWall->mutable_start()->set_x(-1);
+	newWall->mutable_start()->set_y(line.y_at_x(newWall->start().x()));
+	newWall->mutable_end()->set_x(10);
+	newWall->mutable_end()->set_y(line.y_at_x(newWall->end().x()));
     return room;
   }
 };
--- scenarios/cdsr/trunk/src/java/cdsr/rsb/DataViewer.java
+++ scenarios/cdsr/trunk/src/java/cdsr/rsb/DataViewer.java
@@ -1,6 +1,9 @@
 package cdsr.rsb;
 
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import rsb.Factory;
@@ -10,6 +13,8 @@ import rsb.patterns.RemoteServer;
 import cdsr.gui.StandaloneViewer;
 import cdsr.marshall.CDSRMarshaller;
 import cdsr.objects.ProblemSet;
+import cdsr.objects.Room;
+import cdsr.objects.SensedObject;
 import cdsr.rsb.CdsrMessages.RoomWithObjects;
 
 public class DataViewer extends StandaloneViewer {
@@ -18,9 +23,40 @@ public class DataViewer extends StandaloneViewer {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private static final Logger LOG = Logger.getLogger(DataViewer.class
 			.getName());
+
+	private static Room createTestRoom() {
+		ArrayList<Line2D.Double> walls = new ArrayList<Line2D.Double>();
+		double[] x = { 0, 0, 5, 5 };
+		double[] y = { 0, 2, 4, 2 };
+		for (int i = 0; i < 4; i++) {
+			walls.add(new Line2D.Double(new Point2D.Double(x[i], y[i]),
+					new Point2D.Double(x[(i + 1) % 4], y[(i + 1) % 4])));
+		}
+
+		Room room = new Room("room1", walls);
+		return room;
+	}
+
+	private static ArrayList<SensedObject> createTestSensedObjects() {
+		ArrayList<SensedObject> sensed_objects = new ArrayList<SensedObject>();
+
+		ArrayList<Line2D.Double> sides = new ArrayList<Line2D.Double>();
+		double[] x = { 10, 10, 20, 20 };
+		double[] y = { 10, 20, 20, 10 };
+		for (int i = 0; i < 4; i++) {
+			sides.add(new Line2D.Double(new Point2D.Double(x[i], y[i]),
+					new Point2D.Double(x[(i + 1) % 4], y[(i + 1) % 4])));
+		}
+		sensed_objects.add(new SensedObject("obj1", sides, "desk"));
+		return sensed_objects;
+	}
+	
+	private static ProblemSet createTestProblemSet() {
+		return new ProblemSet(createTestRoom(), createTestSensedObjects(), null);
+	}
 
 	/**
 	 * @param args
@@ -37,28 +73,30 @@ public class DataViewer extends StandaloneViewer {
 			return;
 		}
 
-		ProblemSet originalPS = CDSRMarshaller.loadProblemSet(args[0]);
-
+//		ProblemSet originalPS = CDSRMarshaller.loadProblemSet(args[0]);
+		ProblemSet originalPS = createTestProblemSet();
+		
 		DataViewer viewer = new DataViewer();
-//		viewer.addProblemSet(originalPS);
+		// viewer.addProblemSet(originalPS);
 
 		try {
 
 			CDSRRSBUtils.registerRoomWithObjectsConverter();
-			
-			RoomWithObjects room = CDSRRSBUtils.createRoomWithObjectsMessage(originalPS,
-					"Classroom");
+
+			RoomWithObjects room = CDSRRSBUtils.createRoomWithObjectsMessage(
+					originalPS, "Classroom");
 
 			RemoteServer server = Factory.getInstance().createRemoteServer(
 					"/cdsr/rooms");
 			server.activate();
 
 			LOG.info("making call");
-			RoomWithObjects transformedPS = server.call("standardiseCoordinateFrame", room);
+			RoomWithObjects transformedPS = server.call(
+					"standardiseCoordinateFrame", room);
 			viewer.addProblemSet(CDSRRSBUtils.toProblemSet(transformedPS));
-			
+
 			LOG.info("done");
-			
+
 			server.deactivate();
 		} catch (RSBException e) {
 			e.printStackTrace();
