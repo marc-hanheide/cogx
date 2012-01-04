886157cef724e4ac3442c217fdc9fe7ea4d49979
gsh148 <gsh148@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-01-04 12:01:46 +0000

    Created dora-yr4 system from dora-yr3

---
+++ systems/dora-yr4/cleanmap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#zenity --question --text "reset mapping?"  && rm -f robotpose.ccf tmpmap.* && exit 0
+#zenity --question --text "restore start map?" && rm -f robotpose.ccf && cp instantiations/map_backup/bham_lab/* .
+rm -f robotpose.ccf tmpmap.* NodeGridMaps.txt
+dc1394_reset_bus
+
