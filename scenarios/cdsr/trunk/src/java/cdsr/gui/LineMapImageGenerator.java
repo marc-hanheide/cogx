b8cc029cf83959b69db112f3ba6ac50a2400a845
Nick Hawes <nickh@robots.ox.ac.uk>
2012-01-20 09:53:29 +0000

    ground truth scaling now takes in a file in Kate's format

--- scenarios/cdsr/trunk/src/java/cdsr/gui/LineMapImageGenerator.java
+++ scenarios/cdsr/trunk/src/java/cdsr/gui/LineMapImageGenerator.java
@@ -19,12 +19,18 @@ import cdsr.objects.Room;
  */
 public class LineMapImageGenerator {
 
+	public static final int DEFAULT_IMAGE_MARGIN = 20;
+	public static final int DEFAULT_PIXELS_PER_METRE = 80;
 	private int m_width;
 	private int m_height;
 	private int m_xOffset;
 	private int m_yOffset;
 
 	private final LineMapPainter m_painter;
+
+	public LineMapImageGenerator(ProblemSet _ps) {
+		this(_ps, DEFAULT_PIXELS_PER_METRE, DEFAULT_IMAGE_MARGIN);
+	}
 
 	public LineMapImageGenerator(ProblemSet _ps, int _pixelsPerMetre,
 			int _imageMarginPixel) {
@@ -37,11 +43,33 @@ public class LineMapImageGenerator {
 		m_painter.setHeight(m_height);
 		m_painter.updateObjects(_ps.getObjects());
 		m_painter.updateRoom(_ps.getRoom());
-		
-		
+
 		System.out.println(_pixelsPerMetre + " pixels/m");
-		System.out.println("origin at pixel " + m_xOffset + ", " + (m_height - m_yOffset) + " measured from top left");
+		System.out.println("origin at pixel " + getXOriginOffset() + ", "
+				+ getYOriginOffset() + " measured from top left");
 		System.out.println("image height: " + m_height);
+	}
+
+	public int getImageHeight() {
+		return m_height;
+	}
+
+	/**
+	 * Offset from top left of image
+	 * 
+	 * @return
+	 */
+	public int getXOriginOffset() {
+		return m_xOffset;
+	}
+
+	/**
+	 * Offset from top left of image
+	 * 
+	 * @return
+	 */
+	public int getYOriginOffset() {
+		return (m_height - m_yOffset);
 	}
 
 	private void calculateImageDimensions(Room _room, int _pixelsPerMetre,
@@ -89,10 +117,10 @@ public class LineMapImageGenerator {
 			m_yOffset = 0;
 		}
 
-		//extend offset by margin
+		// extend offset by margin
 		m_xOffset += _imageMarginPixel;
-		m_yOffset += _imageMarginPixel;		
-		
+		m_yOffset += _imageMarginPixel;
+
 		m_width = ((int) ((maxX - minX) * _pixelsPerMetre))
 				+ (2 * _imageMarginPixel);
 		m_height = ((int) ((maxY - minY) * _pixelsPerMetre))
@@ -127,7 +155,7 @@ public class LineMapImageGenerator {
 		}
 
 		ProblemSet ps = CDSRMarshaller.loadProblemSet(args[0]);
-		LineMapImageGenerator generator = new LineMapImageGenerator(ps, 80, 20);
+		LineMapImageGenerator generator = new LineMapImageGenerator(ps);
 
 		if (!generator.generateImage(args[1])) {
 			System.err.println("image generation failed");
