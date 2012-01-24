d2ea52824176f78b68b1f8346105214acf41fe63
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-01-24 12:59:02 +0000

    systems/george-y4

---
+++ code/systems/george-y4/doant.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+SCRIPT=$(readlink -f $0)
+COGX_ROOT=$(dirname $SCRIPT)
+
+CAST_DIR=/usr/local
+
+COGX_BUILD_DIR=${COGX_ROOT}/BUILD
+COGX_LIB_DIR=${COGX_ROOT}/output/lib
+COGX_PY_DIR=${COGX_ROOT}/output/python
+COGX_CLASS_DIR=${COGX_ROOT}/output/classes
+
+CAST_INSTALL_ROOT=${CAST_DIR}
+CAST_ROOT=${CAST_INSTALL_ROOT}
+
+CAST_BIN_PREFIX=bin
+CAST_BIN_DIR=${CAST_INSTALL_ROOT}/${CAST_BIN_PREFIX}
+
+CAST_LIB_PREFIX=lib/cast
+CAST_LIB_DIR=${CAST_INSTALL_ROOT}/${CAST_LIB_PREFIX}
+CAST_PY_DIR=${CAST_LIB_DIR}/python
+
+CAST_CONFIG_PATH=share/cast/config/cast_ice_config
+CAST_ICE_CONFIG=${CAST_INSTALL_ROOT}/${CAST_CONFIG_PATH}
+
+ICE_CONFIG=${CAST_ICE_CONFIG}
+
+CAST_JAR=${CAST_INSTALL_ROOT}/share/java/cast.jar
+ICE_JARS=/usr/share/java/Ice.jar:/usr/share/java/ant-ice.jar
+LOG4_JAR=/usr/share/java/log4j-1.2.jar
+CLASSPATH=$ICE_JARS:$LOG4_JAR:$CAST_JAR:$CLASSPATH
+echo $CLASSPATH
+
+ant $*
