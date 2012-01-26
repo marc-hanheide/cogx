fd50ac0108cb041971c30f737925915cccb3c450
gsh148 <gsh148@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-01-26 15:05:04 +0000

    Added optional flag to allow images to be saved

--- tools/scripts/trunk/collect-logs.sh
+++ tools/scripts/trunk/collect-logs.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
-
 dirPrfx=`pwd | sed "s@.*/\(.*\)@\1@"`
 zipname=logs/${USER}_${dirPrfx}_`date +"%Y-%m-%d_%H-%M-%S"`
 
 svn info . > logs/svn-info.txt
 svn st -q . >> logs/svn-info.txt
 
-
 zip -j "$zipname" logs/*.xml logs/*core* logs/svn-info.txt subarchitectures/planner.sa/src/python/standalone/tmp/static_dir_for_debugging/* subarchitectures/planner.sa/src/python/history-*.pddl tmpmap.* conceptual.fg conceptual.info
+
+# A string with command options
+options=$@
+
+# An array with all the arguments
+arguments=($options)
+
+# Loop index
+index=0
+
+for argument in $options
+  do
+    # Incrementing index
+    index=`expr $index + 1`
+
+    # The conditions
+    case $argument in
+      --images) zip "$zipname-images" images/*;;
+      #--other) echo "key $argument value ${arguments[index]}" ;;
+    esac
+  done
+
+
+
 
