d5142fd4b7423656da15c59a800d4af4219fc197
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-02-15 14:12:40 +0000

    castctrl: add search in log widows

--- tools/castctrl/trunk/qtui/cctextedit.py
+++ tools/castctrl/trunk/qtui/cctextedit.py
@@ -7,6 +7,8 @@ class CCastTextEdit(QtGui.QTextEdit):
     def __init__(self, parent):
         QtGui.QTextEdit.__init__(self, parent)
         self.contextActions = []
+        self.createActions()
+        self.searchString = ""
 
     def contextMenuEvent(self, event):
         cur = self.textCursor()
@@ -18,4 +20,35 @@ class CCastTextEdit(QtGui.QTextEdit):
             # TODO: if act.isValid(self)
             menu.addAction(act)
         action = menu.exec_(event.globalPos())
-    pass
+
+    def createActions(self):
+        actFind = QtGui.QAction("Find...", self)
+        actFind.setShortcutContext(QtCore.Qt.WidgetShortcut)
+        #actFind.setShortcut("Ctrl+F") # XXX disabled, doesn't work
+        self.connect(actFind, QtCore.SIGNAL("triggered()"), self.onFind)
+        self.contextActions.append(actFind)
+
+        actFindAgain = QtGui.QAction("Find again", self)
+        actFind.setShortcutContext(QtCore.Qt.WidgetShortcut)
+        #actFindAgain.setShortcut("Ctrl+G") # XXX disabled, doesn't work
+        self.connect(actFindAgain, QtCore.SIGNAL("triggered()"), self.onFindAgain)
+        self.contextActions.append(actFindAgain)
+
+    def doFind(self, string):
+        rv = self.find(string)
+        if not rv:
+            self.moveCursor(QtGui.QTextCursor.Start);
+            rv = self.find(self.searchString)
+        if not rv:
+            reply = QtGui.QMessageBox.question(self, 'Message',
+                    "'%s' not found." % string,
+                    QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
+
+    def onFind(self):
+        text, ok = QtGui.QInputDialog.getText(self, "Find", "String:", QtGui.QLineEdit.Normal, self.searchString)
+        if ok:
+            self.searchString = text
+            self.doFind(self.searchString)
+
+    def onFindAgain(self):
+        self.doFind(self.searchString)
