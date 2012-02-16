8be91a9743e97e6bcf4c817f034c9667a9ac70ee
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-02-16 13:52:04 +0000

    gar: job parameter for build-jenkins

--- tools/gar-installer/trunk/build-jenkins.bash
+++ tools/gar-installer/trunk/build-jenkins.bash
@@ -3,10 +3,16 @@ export https_proxy=http://webcache:3128/
 
 dname=`dirname $0`
 
+jobname="$1"
+
+if [ -z "$jobname" ]; then
+   jobname="dora"
+fi
+
 export http_proxy
 export https_proxy
 export SVNUSER=cogx
 env
 
 /usr/bin/make -C $dname clean
-/usr/bin/make -C $dname dora SVNUSER=cogx
+/usr/bin/make -C $dname $jobname SVNUSER=cogx
