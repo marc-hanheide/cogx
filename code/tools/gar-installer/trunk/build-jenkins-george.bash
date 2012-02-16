74f9b31e405d4b0e3f5e80802e5f25f8c733e123
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-02-16 13:04:24 +0000

    gar: build-jenkins-george

---
+++ code/tools/gar-installer/trunk/build-jenkins-george.bash
@@ -0,0 +1,12 @@
+export http_proxy=http://webcache:3128/
+export https_proxy=http://webcache:3128/
+
+dname=`dirname $0`
+
+export http_proxy
+export https_proxy
+export SVNUSER=cogx
+env
+
+/usr/bin/make -C $dname clean
+/usr/bin/make -C $dname george SVNUSER=cogx
