143475070187f4a213293b3edf3585ca50b95f09
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-02-17 15:37:36 +0000

    v11n: reverted java utf8

--- code/subarchitectures/visualization.sa/trunk/src/java/si/unilj/fri/cogx/v11n/test/V11nJavaTestComponent.java
+++ code/subarchitectures/visualization.sa/trunk/src/java/si/unilj/fri/cogx/v11n/test/V11nJavaTestComponent.java
@@ -1,8 +1,9 @@
 /*
- * author: Marko Mahnic
+ * Author: Marko Mahnič
  * created: 2010-07-11
  *
- * (c) Copyright 2010 Marko Mahnic. 
+ * © Copyright 2010 Marko Mahnič. 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
