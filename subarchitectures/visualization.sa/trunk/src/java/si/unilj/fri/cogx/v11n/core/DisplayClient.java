61a1d4501671068e94987297f2bddd77a73f164a
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-02-17 08:33:48 +0000

    v11n: removed utf8 from java source

--- subarchitectures/visualization.sa/trunk/src/java/si/unilj/fri/cogx/v11n/core/DisplayClient.java
+++ subarchitectures/visualization.sa/trunk/src/java/si/unilj/fri/cogx/v11n/core/DisplayClient.java
@@ -1,8 +1,8 @@
 /*
- * author: Marko Mahnič
+ * author: Marko Mahnic
  * created: 2010-07-11
  *
- * © Copyright 2010 Marko Mahnič. 
+ * (c) Copyright 2010 Marko Mahnic. 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
