3698ef03c32ca173516b55b693408629166a1755
cogx <cogx@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-03-07 08:33:36 +0000

    creating static demo system

---
+++ code/systems/dora-yr3-demo/run-test.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+plannerDebugDir="subarchitectures/planner.sa/src/python/standalone/tmp/static_dir_for_debugging"
+
+function storePlannerLogs {
+	if [ -r "$plannerDebugDir" ]; then
+		zip -j logs/planner-debug.zip "$plannerDebugDir"/*
+	else
+		touch logs/no-planner-logs-available
+	fi
+}
+
+function storeCoreDump {
+	if [ -e "core" ]; then
+		zip logs/core.zip core
+	else
+		touch logs/no-core-dump
+	fi
+}
+
+function waitForTrigger {
+	while [ ! -e $STOPTRIGGERFILE ]; do
+		sleep 1
+	done
+}
+
+
+if [ "$1" ]; then
+    configFile="$1"
+else
+    configFile="instantiations/dora-test-search-base.cast"
+fi
+
+if [ "$2" ]; then
+    STOPTRIGGERFILE="$2"
+else
+    STOPTRIGGERFILE="logs/result.xml"
+fi
+
+
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$DIR"
+
+JARS=`find "$DIR/output/jar" -name "*.jar" | tr "\n" ":"`
+
+export CLASSPATH=$CLASSPATH:/usr/local/share/java/cast.jar:/usr/share/java/Ice.jar:/usr/share/java/log4j-1.2.jar:/opt/local/share/java/cast.jar:/opt/local/share/java/Ice.jar:$JARS
+echo $CLASSPATH
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$DIR/output/lib:/usr/local/lib/cast:/opt/local/lib/cast"
+export DYLD_LIBRARY_PATH="$LD_LIBRARY_PATH"
+
+trap 'kill -2 $PIDS; sleep 5; kill -9 $PIDS; exit 1' INT TERM PIPE QUIT ABRT HUP 
+
+PIDS=""
+
+mkdir -p logs
+# clear old logs
+rm -rf logs/*
+cat > logs/log4j.properties <<EOF
+log4j.rootLogger=DEBUG,srvXmlFile,srvConsole
+log4j.appender.srvXmlFile=org.apache.log4j.FileAppender
+log4j.appender.srvXmlFile.Threshold=DEBUG
+log4j.appender.srvXmlFile.File=log.xml
+log4j.appender.srvXmlFile.Append=true
+log4j.appender.srvXmlFile.layout=org.apache.log4j.xml.XMLLayout
+
+
+log4j.appender.srvConsole=org.apache.log4j.ConsoleAppender
+log4j.appender.srvConsole.Threshold=INFO
+log4j.appender.srvConsole.layout=org.apache.log4j.PatternLayout
+log4j.appender.srvConsole.layout.ConversionPattern=[%p %c: %m]%n
+EOF
+
+rm -f log4j.properties
+cat > log4j.properties <<EOF
+log4j.rootLogger=TRACE,cliSocketApp
+#+ Section: LOG4J.client.IceAppender
+#+ The clients are configured to send messages to an IceAppender (CAST logger)
+log4j.loggerFactory=cast.core.logging.ComponentLoggerFactory
+log4j.appender.cliSocketApp=cast.core.logging.IceAppender
+log4j.appender.cliSocketApp.Host=localhost
+EOF
+
+
+#if which vncsnapshot; then
+#	xterm -title "make snapshots" -e bash -c "vncsnapshot -fps 2 -count 10000 -passwd $HOME/.vnc/passwd $DISPLAY logs/snapshot.jpg" &
+#	PIDS="$PIDS $!"
+#else
+#	echo "snapshot is not possible, install vncsnapshot to make it work" >& 2
+#fi
+
+DIM=`xdpyinfo | grep dimension | cut -f7 -d" "`
+ffmpeg -f x11grab -s $DIM  -r 1  -i $DISPLAY logs/screencast.mov 2>&1  &
+PIDS="$PIDS $!"
+
+
+
+xterm -title "log server" -e bash -c "cd logs; cast-log-server" &
+PIDS="$PIDS $!"
+sleep 2
+
+xterm -e player instantiations/stage/alu/alu.cfg &
+PIDS="$PIDS $!"
+
+rm -f  robotpose.ccf tmpmap.*
+rm -f core
+
+#xterm -e peekabot &
+#PIDS="$PIDS $!"
+
+xterm -title "CAST server" -e bash -c "ulimit -c unlimited; output/bin/cast-server-start 2>&1 | tee logs/server.log" &
+SERVERPID="$!"
+PIDS="$PIDS $SERVERPID"
+
+xterm -title "Display server" -e bash -c "ulimit -c unlimited; output/bin/display-server 2>&1 | tee logs/display-server.log" &
+DISPLAYSERVERPID="$!"
+PIDS="$PIDS $DISPLAYSERVERPID"
+
+
+xterm -title "Abducer" -e bash -c "ulimit -c unlimited; tools/abducer/bin/abducer-server -n AbducerServer -e \"default -p 9100\" -l \"$DIR/log4j.properties\" -a \"$DIR/tools/abducer/bin/abducer-engine-pb\" -x --silent 2>&1 | tee logs/abducer.log" &
+ABDUCERPID="$!"
+PIDS="$PIDS $ABDUCERPID"
+
+sleep 10
+
+xterm -title "CAST client: $configFile" -e bash -c "ulimit -c unlimited; output/bin/cast-client-start $configFile  2>&1 | tee logs/client.log" &
+PIDS="$PIDS $!"
+
+
+# in the future we will wait for the junit result here... for now, let's run the system for 60 seconds
+#waitForTrigger
+sleep 60
+
+storePlannerLogs
+storeCoreDump
+
+# check if the C++ server is still running after this time!
+if ps ax | grep  cast-server-c++ | grep -qv "grep"; then RES=0; else RES=1; fi
+
+
+kill -2 $PIDS >/dev/null 2>&1
+sleep 5; 
+kill -9 $PIDS  >/dev/null 2>&1
+
+exit $RES
+
+
