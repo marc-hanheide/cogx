a0e505ead0a97819309c0a91f12af038db983a44
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-03-22 12:07:59 +0000

    soif: automated resource build

---
+++ code/subarchitectures/vision.sa/trunk/src/c++/vision/components/SOIFilter/res/res2c11.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# vim:set fileencoding=utf-8 sw=4 ts=8 et:vim
+
+import os, sys
+outfile = "ptuctrl.inc"
+infiles = ["ptucontroller.ui", "ptucontroller.js"]
+
+f = open(outfile, "w")
+
+f.write("// C++11 include file\n")
+f.write("// --------------------------------------------------\n\n")
+
+for fn in infiles:
+    fnfull = fn
+    lines = open(fnfull).readlines()
+    lines = [ "" if ln.strip().startswith("//")
+             else ln.rstrip()
+             for ln in lines
+            ]
+    var = "res_" + fn.replace(".", "_")
+    f.write("// Generated from %s\n" % fn)
+    f.write("const char %s[] = R\"((" % var)
+    for ln in lines:
+        f.write('%s\n' % ln)
+    f.write("))\"; // %s\n" % var)
+    f.write("// --------------------------------------------------\n\n")
+
+f.close()
+
