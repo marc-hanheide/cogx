4bc5eed842a493b5b940683ec0ac6287e25c48b7
alex8b <alex8b@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-03-23 15:51:54 +0000

    Small changes

--- code/systems/dora-yr4/cleanmap.sh
+++ code/systems/dora-yr4/cleanmap.sh
@@ -2,5 +2,3 @@
 #zenity --question --text "reset mapping?"  && rm -f robotpose.ccf tmpmap.* && exit 0
 #zenity --question --text "restore start map?" && rm -f robotpose.ccf && cp instantiations/map_backup/bham_lab/* .
 rm -f robotpose.ccf tmpmap.* NodeGridMaps.txt GridMap.txt HeightMap.txt
-dc1394_reset_bus
-
