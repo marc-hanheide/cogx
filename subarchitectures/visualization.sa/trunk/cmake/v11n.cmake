5461ad5da9a98df98b5ad531983aedafaac86a2d
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-03-27 08:57:52 +0000

    v11n-cmake: set variables even if v11n is not built

--- subarchitectures/visualization.sa/trunk/cmake/v11n.cmake
+++ subarchitectures/visualization.sa/trunk/cmake/v11n.cmake
@@ -7,16 +7,36 @@ set(v11n_subdir subarchitectures/visualization.sa)
 # Conditionally included subdirectory
 cogx_add_subdir(BUILD_SA_V11N ${v11n_subdir})
 
-if(BUILD_SA_V11N)
-   set(VISUALIZATION_INCLUDE_DIRS
-      ${COGX_ROOT}/${v11n_subdir}/src/c++/autogen
-      ${COGX_ROOT}/${v11n_subdir}/src/c++/core/client
-      )
-   set(VISUALIZATION_LIBRARIES
-      DisplayClient VisualizationData
-      )
-   add_definitions(-DFEAT_VISUALIZATION)
-   add_definitions(-DHAVE_COGX_MATH)
-   add_definitions(-DHAVE_COGX_VIDEO)
-endif(BUILD_SA_V11N)
+set(VISUALIZATION_INCLUDE_DIRS
+   ${COGX_ROOT}/${v11n_subdir}/src/c++/autogen
+   ${COGX_ROOT}/${v11n_subdir}/src/c++/core/client
+   )
+set(VISUALIZATION_LIBRARIES
+   DisplayClient VisualizationData
+   )
+add_definitions(-DFEAT_VISUALIZATION)
+
+if (1)
+  add_definitions(-DHAVE_COGX_MATH)
+  set(VISUALIZATION_INCLUDE_DIRS ${VISUALIZATION_INCLUDE_DIRS}
+    ${COGX_ROOT}/tools/math/src/c++/autogen
+    )
+  set(VISUALIZATION_LIBRARIES ${VISUALIZATION_LIBRARIES}
+    # Math
+    )
+endif()
+
+if (1)
+  add_definitions(-DHAVE_COGX_VIDEO)
+  set(VISUALIZATION_INCLUDE_DIRS ${VISUALIZATION_INCLUDE_DIRS}
+    ${COGX_ROOT}/tools/hardware/video/src/c++/autogen
+    )
+  set(VISUALIZATION_LIBRARIES ${VISUALIZATION_LIBRARIES}
+    # Video
+    )
+endif()
+
+if(NOT BUILD_SA_V11N)
+   set(VISUALIZATION_LIBRARY_DIRS ${COGX_ROOT}/output/lib)
+endif()
 
