2b5abc32ae797ce397c30f2e30b4417905995753
demmeln <demmeln@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-03-28 14:25:43 +0000

    Added comment explaining PlacePropertySaver.

--- subarchitectures/spatial.sa/trunk/src/c++/PlacePropertySaver/PlacePropertySaver.h
+++ subarchitectures/spatial.sa/trunk/src/c++/PlacePropertySaver/PlacePropertySaver.h
@@ -1,9 +1,31 @@
+/**
+ * Author: Nikolaus Demmel
+ * Date:  23.03.2012
+ **/
 
 #ifndef __SPATIAL_SA_PLACE_PROPERTY_SAVER_H__
 #define __SPATIAL_SA_PLACE_PROPERTY_SAVER_H__
 
 #include <cast/architecture/ManagedComponent.hpp>
 
+/**
+ * The PlacePropertySaver component can save observerd place properties to a
+ * file either periodically or at the end of execution. It can also load this
+ * information back into the working memory at the beginning of a new run. It
+ * should save all information about observations such that conceptual/Observer
+ * can recreate the conceptual world state. If compnents that provide these
+ * place-properties to the working memory fail if there are already such
+ * properties loaded at start-up time by PlacePropertySaver, it is considered a
+ * bug in the providing component. (An example would be a component adding
+ * object oservations too often, because it keeps track of the number in the
+ * internal state of itself and does not recognize object observations added by
+ * PlacePropertyManager. This would the be a bug in the object observation
+ * component).
+ *
+ * For serialization the Ice-generated serializers are used. This means the
+ * result is a non-human-readable byte string. We might want to write a better
+ * text based and human-readable output format that would aid debugging.
+ */
 class PlacePropertySaver: public cast::ManagedComponent 
 {
 public:
