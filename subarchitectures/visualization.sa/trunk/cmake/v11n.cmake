f850baa6e1e192a67608063ff34685ea958deb5e
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-03-28 07:04:44 +0000

    cmake: enable_cogx_visualization macro

--- subarchitectures/visualization.sa/trunk/cmake/v11n.cmake
+++ subarchitectures/visualization.sa/trunk/cmake/v11n.cmake
@@ -7,36 +7,70 @@ set(v11n_subdir subarchitectures/visualization.sa)
 # Conditionally included subdirectory
 cogx_add_subdir(BUILD_SA_V11N ${v11n_subdir})
 
-set(VISUALIZATION_INCLUDE_DIRS
-   ${COGX_ROOT}/${v11n_subdir}/src/c++/autogen
-   ${COGX_ROOT}/${v11n_subdir}/src/c++/core/client
-   )
-set(VISUALIZATION_LIBRARIES
-   DisplayClient VisualizationData
-   )
-add_definitions(-DFEAT_VISUALIZATION)
+# params: ON/OFF [NOMATH NOVIDEO OPENCV]
+macro(ENABLE_COGX_VISUALIZATION onoff)
+   set (onoff ${onoff})
+   if(NOT onoff)
+      message("***DISABLED*** _COGX_VISUALIZATION")
+      remove_definitions(-DFEAT_VISUALIZATION  -DFEAT_VISUALIZATION_OPENCV)
+      set(VISUALIZATION_INCLUDE_DIRS "")
+      set(VISUALIZATION_LIBRARY_DIRS "")
+      set(VISUALIZATION_LIBRARIES "")
+   else()
+      message("ENABLE_COGX_VISUALIZATION")
+      set (__havemath 1)
+      set (__havevideo 1)
+      set (__haveopencv 0)
+      foreach(arg ${ARGN})
+         if(arg STREQUAL "NOMATH")
+            set (__havemath 0)
+            message("NOMATH")
+         elseif(arg STREQUAL "NOVIDEO")
+            set (__havevideo 0)
+            message("NOVIDEO")
+         elseif(arg STREQUAL "OPENCV")
+            set (__haveopencv 1)
+            message("OPENCV")
+         endif()
+      endforeach()
+      set(VISUALIZATION_INCLUDE_DIRS
+         ${COGX_ROOT}/${v11n_subdir}/src/c++/autogen
+         ${COGX_ROOT}/${v11n_subdir}/src/c++/core/client
+         )
+      set(VISUALIZATION_LIBRARIES
+         DisplayClient VisualizationData
+         )
+      add_definitions(-DFEAT_VISUALIZATION)
+      if (__haveopencv)
+         add_definitions(-DFEAT_VISUALIZATION_OPENCV)
+      endif()
 
-if (1)
-  add_definitions(-DHAVE_COGX_MATH)
-  set(VISUALIZATION_INCLUDE_DIRS ${VISUALIZATION_INCLUDE_DIRS}
-    ${COGX_ROOT}/tools/math/src/c++/autogen
-    )
-  set(VISUALIZATION_LIBRARIES ${VISUALIZATION_LIBRARIES}
-    # Math
-    )
-endif()
+      if (__havemath)
+        add_definitions(-DHAVE_COGX_MATH)
+        set(VISUALIZATION_INCLUDE_DIRS ${VISUALIZATION_INCLUDE_DIRS}
+          ${COGX_ROOT}/tools/math/src/c++/autogen
+          )
+        set(VISUALIZATION_LIBRARIES ${VISUALIZATION_LIBRARIES}
+          # Math
+          )
+      endif()
 
-if (1)
-  add_definitions(-DHAVE_COGX_VIDEO)
-  set(VISUALIZATION_INCLUDE_DIRS ${VISUALIZATION_INCLUDE_DIRS}
-    ${COGX_ROOT}/tools/hardware/video/src/c++/autogen
-    )
-  set(VISUALIZATION_LIBRARIES ${VISUALIZATION_LIBRARIES}
-    # Video
-    )
-endif()
+      if (__havevideo)
+        add_definitions(-DHAVE_COGX_VIDEO)
+        set(VISUALIZATION_INCLUDE_DIRS ${VISUALIZATION_INCLUDE_DIRS}
+          ${COGX_ROOT}/tools/hardware/video/src/c++/autogen
+          )
+        set(VISUALIZATION_LIBRARIES ${VISUALIZATION_LIBRARIES}
+          # Video
+          )
+      endif()
 
-if(NOT BUILD_SA_V11N)
-   set(VISUALIZATION_LIBRARY_DIRS ${COGX_ROOT}/output/lib)
-endif()
+      if(NOT BUILD_SA_V11N)
+         set(VISUALIZATION_LIBRARY_DIRS ${COGX_ROOT}/output/lib)
+      endif()
+
+      include_directories(${VISUALIZATION_INCLUDE_DIRS})
+      link_directories(${VISUALIZATION_LIBRARY_DIRS})
+   endif()
+endmacro()
 
