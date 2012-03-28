905e972219c5400d3c4e0fa5549c6b32a906ec5a
demmeln <demmeln@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-03-28 15:13:18 +0000

    Added some scripts to quickly store away the map and reload it again. Also there is a script to restore the robotpose to 0. You could symlink the scripts to the dora-yr4 repository root for extra convenience if you want.

---
+++ code/tools/scripts/trunk/map-save-local.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILES="robotpose.ccf
+		   NodeGridMaps.txt
+		   GridMap.txt
+       HeightMap.txt
+		   tmpmap.graph
+		   tmpmap.metric
+       place_properties.bin"
+
+for f in $FILES ; do
+	cp $f local-saved-map/
+done
