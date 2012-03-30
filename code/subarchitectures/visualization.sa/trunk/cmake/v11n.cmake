96687d3f06201bed30f6ed8703d14e3fb4bafd69
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-03-30 13:36:54 +0000

    hack: path to libDisplayClient

--- code/subarchitectures/visualization.sa/trunk/cmake/v11n.cmake
+++ code/subarchitectures/visualization.sa/trunk/cmake/v11n.cmake
@@ -7,6 +7,19 @@ set(v11n_subdir subarchitectures/visualization.sa)
 # Conditionally included subdirectory
 cogx_add_subdir(BUILD_SA_V11N ${v11n_subdir})
 
+set (__v11n_binary_dir ${CMAKE_CURRENT_BINARY_DIR})
+set (__v11n_source_dir ${CMAKE_CURRENT_SOURCE_DIR})
+set (__v11n_list_file ${CMAKE_CURRENT_LIST_FILE})
+#message(" *** ${__v11n_binary_dir}")
+#message(" *** ${__v11n_source_dir}")
+#message(" *** ${__v11n_list_file}")
+string(REPLACE "${__v11n_source_dir}" "" __v11n_rel_list_file "${__v11n_list_file}")
+#message(" *** ${__v11n_rel_list_file}")
+string(REPLACE "/cmake/v11n.cmake" "" __v11n_sa_dir "${__v11n_rel_list_file}")
+#message(" *** ${__v11n_sa_dir}")
+set (__v11n_client_binary_dir "${__v11n_binary_dir}${__v11n_sa_dir}/scr/c++/core")
+#message(" *** ${__v11n_client_binary_dir}")
+
 # params: ON/OFF [NOMATH NOVIDEO OPENCV]
 macro(ENABLE_COGX_VISUALIZATION onoff)
    set (onoff ${onoff})
@@ -65,8 +78,9 @@ macro(ENABLE_COGX_VISUALIZATION onoff)
           )
       endif()
 
+      set(VISUALIZATION_LIBRARY_DIRS ${__v11n_client_binary_dir})
       if(NOT BUILD_SA_V11N)
-         set(VISUALIZATION_LIBRARY_DIRS ${COGX_ROOT}/output/lib)
+         set(VISUALIZATION_LIBRARY_DIRS ${VISUALIZATION_LIBRARY_DIRS} ${COGX_ROOT}/output/lib)
       endif()
 
       include_directories(${VISUALIZATION_INCLUDE_DIRS})
