625804eed6c4385e09f1ae2e033e4e6d05d203a9
demmeln <demmeln@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-04-02 17:30:44 +0000

    Update collect-logs.sh to contain all the map files.

--- code/tools/scripts/trunk/collect-logs.sh
+++ code/tools/scripts/trunk/collect-logs.sh
@@ -6,7 +6,7 @@ zipname=logs/${USER}_${dirPrfx}_`date +"%Y-%m-%d_%H-%M-%S"`
 svn info . > logs/svn-info.txt
 svn st -q . >> logs/svn-info.txt
 
-zip -j "$zipname" logs/*.xml logs/*core* logs/svn-info.txt subarchitectures/planner.sa/src/python/standalone/tmp/static_dir_for_debugging/* subarchitectures/planner.sa/src/python/history-*.pddl tmpmap.* conceptual.fg conceptual.info
+zip -j "$zipname" logs/*.xml logs/*core* logs/svn-info.txt subarchitectures/planner.sa/src/python/standalone/tmp/static_dir_for_debugging/* subarchitectures/planner.sa/src/python/history-*.pddl tmpmap.* conceptual.fg conceptual.info place_properties.bin *Map.txt Places.txt
 
 # A string with command options
 options=$@
