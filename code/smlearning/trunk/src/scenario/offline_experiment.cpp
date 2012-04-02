470997f5ee6a77d3b8f24e6ad7a9be96ec275842
sergio <sergio@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-04-02 20:29:04 +0000

    adding missing files

---
+++ code/smlearning/trunk/src/scenario/offline_experiment.cpp
@@ -0,0 +1,34 @@
+/** @file offline_experiment.cpp
+ *
+ * 
+ *
+ * Program which moves the arm along a straight line
+ * using reactive trajectory planner and collision detection
+ * simulating a pushing action on a polyflap
+ * (also see DemoReacPlannerPhys and DemoRobotFinger).
+ * 
+ * Program can be run in two modes:
+ * - the first uses real Katana arm
+ * - the second runs the arm simulators
+ *
+ * data are stored that can be used for offline training RNNs
+ *
+ * @author	Marek Kopicki (see copyright.txt),
+ * 			<A HREF="http://www.cs.bham.ac.uk/~msk">The University Of Birmingham</A>
+ * @author      Sergio Roa - DFKI
+ * @author      Jan Hanzelka - DFKI
+ * @version 1.0
+ *
+ */
+
+#include "scenario/PushingApplication.h"
+
+
+using namespace smlearning;
+
+int main(int argc, char *argv[]) {
+
+	PushingApplication<Scenario, Scenario::Desc>().main(argc, argv);
+
+	return 0;
+}
