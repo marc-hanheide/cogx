041c8c456d6f30c293c8a7d6020fc5d19b465b0b
demmeln <demmeln@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-04-02 17:44:09 +0000

    Hide side pane in peekabot-start.sh script.

--- tools/scripts/trunk/peekabot-start.sh
+++ tools/scripts/trunk/peekabot-start.sh
@@ -4,6 +4,7 @@ peekabot &
 sleep 1
 window_id=$(wmctrl -l | grep "peekabot$" | sed "s/ .*$//");
 echo $window_id
-xdotool windowactivate $window_id key alt+F5
-xdotool windowsize $window_id 40% 60%
+xdotool windowactivate $window_id key alt+F5  # unmaximize window
+xdotool windowsize $window_id 40% 60%         # resize window
+xdotool windowactivate $window_id key F9      # hide side pane
 
