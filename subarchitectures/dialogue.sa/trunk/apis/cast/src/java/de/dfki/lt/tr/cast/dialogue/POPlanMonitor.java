bf20b1ec2de120c7789a1ff5b7908a737471efdc
farnold <farnold@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-04-19 13:07:52 +0000

    added history

--- subarchitectures/dialogue.sa/trunk/apis/cast/src/java/de/dfki/lt/tr/cast/dialogue/POPlanMonitor.java
+++ subarchitectures/dialogue.sa/trunk/apis/cast/src/java/de/dfki/lt/tr/cast/dialogue/POPlanMonitor.java
@@ -1,11 +1,10 @@
 package de.dfki.lt.tr.cast.dialogue;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import autogen.Planner.Action;
-import autogen.Planner.Link;
 import autogen.Planner.POPlan;
 
 import cast.CASTException;
@@ -16,21 +15,28 @@ import cast.architecture.ManagedComponent;
 import cast.architecture.WorkingMemoryChangeReceiver;
 import cast.cdl.WorkingMemoryChange;
 import cast.cdl.WorkingMemoryOperation;
-import cast.core.CASTUtils;
-import de.dfki.lt.tr.beliefs.slice.logicalcontent.BooleanFormula;
-import de.dfki.lt.tr.beliefs.slice.logicalcontent.ElementaryFormula;
-import de.dfki.lt.tr.beliefs.slice.logicalcontent.FloatFormula;
-import de.dfki.lt.tr.beliefs.slice.logicalcontent.PointerFormula;
-import de.dfki.lt.tr.beliefs.slice.logicalcontent.dFormula;
 import de.dfki.lt.tr.cast.dialogue.util.POPlanUtils;
+import de.dfki.lt.tr.cast.dialogue.util.VerbalisationUtils;
 
 public class POPlanMonitor extends ManagedComponent {
 
+	private Map<Integer,List<POPlan>> runningMap = new HashMap<Integer,List<POPlan>>();
+	private Map<Integer,List<POPlan>> finishedMap = new HashMap<Integer,List<POPlan>>();
+	private String pddldomain = "";
+	private String grammarpath = "";
+	
 	protected void configure(Map<String, String> args) {
 		// TODO Auto-generated method stub
 		super.configure(args);
+		if (args.containsKey("--pddldomain")) {
+			pddldomain = args.get("--pddldomain");
+			log(pddldomain);
+		}
+		if (args.containsKey("--grammarpath")) {
+			grammarpath = args.get("--grammarpath");
+			log(grammarpath);
+		}
 	}
-
 
 	protected void start() {
 		// add CFs
@@ -64,6 +70,15 @@ public class POPlanMonitor extends ManagedComponent {
 			return;
 		}
 		log("ADDED POPlan: " + poplanToString(_newPOPlan));
+		VerbalisationUtils.verbaliseString(this, "Got a new POPlan");
+		if (_newPOPlan.status.name().equals("RUNNING")) {
+			runningMap.put(_newPOPlan.taskID, new LinkedList<POPlan>());
+			runningMap.get(_newPOPlan.taskID).add(_newPOPlan);
+		}
+		else {
+			finishedMap.put(_newPOPlan.taskID, new LinkedList<POPlan>());
+			finishedMap.get(_newPOPlan.taskID).add(_newPOPlan);
+		}
 	}
 	
 	private void processOverwrittenPOPlan(WorkingMemoryChange _wmc) {
@@ -78,6 +93,26 @@ public class POPlanMonitor extends ManagedComponent {
 			return;
 		}
 		log("OVERWRITTEN POPlan: " + poplanToString(_oldPOPlan));
+		VerbalisationUtils.verbaliseString(this, "Got an overwritten POPlan");
+		
+		if (_oldPOPlan.status.name().equals("RUNNING")) {
+			runningMap.get(_oldPOPlan.taskID).add(_oldPOPlan);
+		}
+		else {
+			finishedMap.get(_oldPOPlan.taskID).add(_oldPOPlan);
+		}
+		
+		/*for (Map.Entry<Integer, List<POPlan>> entry : runningMap.entrySet()) {
+			for (POPlan _plan : entry.getValue()) {
+				log(entry.getKey() + ":" + poplanToString(_plan));
+			}
+		}
+		
+		for (Map.Entry<Integer, List<POPlan>> entry : finishedMap.entrySet()) {
+			for (POPlan _plan : entry.getValue()) {
+				log(entry.getKey() + ":" + poplanToString(_plan));
+			}
+		}*/
 	}
 
 	private String poplanToString(POPlan _poPlan) {
@@ -91,3 +126,9 @@ public class POPlanMonitor extends ManagedComponent {
 	}
 
 }
+
+/*
+ * Behavior: 1. Goal: Goto-Place ( (= (is-in ROBOT) Place) ) : Adds a new task, does what it should and that's it. No confirmation after task succeeded.
+ * 			 2. Give a new Goal while old one is still active: Adds a new Task and only the newer task is overwritten, updated, etc.
+ */
+
