1770aeb11296aab41522d2ae9773a3f5b2e3fcd4
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-04-19 11:34:48 +0000

    ppo: moved v11n code

--- subarchitectures/vision.sa/trunk/src/c++/vision/components/PlanePopOutPc/PlanePopOut.cpp
+++ subarchitectures/vision.sa/trunk/src/c++/vision/components/PlanePopOutPc/PlanePopOut.cpp
@@ -857,6 +857,10 @@ void PlanePopOut::runComponent()
 	    }
 	    try {
 		GetPlaneAndSOIs();
+#ifdef FEAT_VISUALIZATION
+		if (m_bSendSois)
+		    SendSOIs(pcl_sois);
+#endif
 	    }
 	    catch (exception& e) {
 		error(" *** PPO GetPlaneANdSOIs HAS CRASHED *** with: '%s'", e.what());
@@ -1095,11 +1099,6 @@ void PlanePopOut::GetPlaneAndSOIs()
 
 	if(ppo_ok)
 	{
-    #ifdef FEAT_VISUALIZATION
-	    // NOTE: not nice having visualisiaton code here, but ok
-	    if (m_bSendSois)
-		SendSOIs(pcl_sois);
-    #endif
 	    // fill our dominant plane structure
 	    dominantPlane.init(pcl_cloud, planepoints, pcl_domplane, tablehull);
 	    if(dominantPlane.valid)
