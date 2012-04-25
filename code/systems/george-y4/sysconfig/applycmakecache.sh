64fd03ebbe7e023f4fc7cd5a384cfc6f13f6bddb
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-04-25 16:16:12 +0000

    george: added script applycmakecache.sh

---
+++ code/systems/george-y4/sysconfig/applycmakecache.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# workaround for missing "readlink -f"
+cd -P -- "$(dirname -- "$0")" &&
+SCRIPT=$(printf '%s\n' "$(pwd -P)/$(basename -- "$0")")
+
+SCRIPT_DIR=$(dirname $SCRIPT)
+
+COGX_ROOT=$SCRIPT_DIR/..
+
+TOOLS=$COGX_ROOT/tools/scripts
+
+
+if [ ! -f $COGX_ROOT/BUILD/CMakeCache.txt ]; then
+   echo "BUILD/CMakeCache.txt does not exsit! Won't continue."
+   exit 0
+fi
+
+cd $COGX_ROOT/BUILD
+
+cmake -Wno-dev $COGX_ROOT > /dev/null
+$TOOLS/cmake-apply  $COGX_ROOT/BUILD  $SCRIPT_DIR/cmakecache/george-y4.txt
+cmake -Wno-dev $COGX_ROOT
+
