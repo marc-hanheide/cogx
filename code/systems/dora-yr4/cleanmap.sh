b72f66d45d9edbcc7356b8f98595229bb8ddc27a
alex8b <alex8b@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-05-16 13:39:40 +0000

    Remove a slam debug file

--- code/systems/dora-yr4/cleanmap.sh
+++ code/systems/dora-yr4/cleanmap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #zenity --question --text "reset mapping?"  && rm -f robotpose.ccf tmpmap.* && exit 0
 #zenity --question --text "restore start map?" && rm -f robotpose.ccf && cp instantiations/map_backup/bham_lab/* .
-rm -f robotpose.ccf tmpmap.* NodeGridMaps.txt GridMap.txt HeightMap.txt Places.txt Connectivities.txt place_properties.bin rooms.xml odom.tdf scans.tdf
+rm -f robotpose.ccf tmpmap.* NodeGridMaps.txt GridMap.txt HeightMap.txt Places.txt Connectivities.txt place_properties.bin rooms.xml odom.tdf scans.tdf slaminputdata.txt
