d1fb3eabf15d0a75cdf5685b37d68c51ad423f16
Marc Hanheide <marc@hanheide.net>
2012-05-18 14:29:01 +0000

    for DAM

--- systems/dora-yr4/restoremap.sh
+++ systems/dora-yr4/restoremap.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+MAP=cs-2-small-furniture2-DAM
 #zenity --question --text "reset mapping?"  && rm -f robotpose.ccf tmpmap.* && exit 0
 #zenity --question --text "restore start map?" && rm -f robotpose.ccf && cp instantiations/map_backup/bham_lab/* .
-rm -f robotpose.ccf tmpmap.*
+./cleanmap.sh
+echo -n "restore '$MAP'..."
 #cp -v instantiations/map_backup/socs/tmpmap* .
 #cp -v instantiations/map_backup/tro_sim/tmpmap* .
 #cp -v instantiations/map_backup/socs_small/tmpmap* .
-cp -v instantiations/map_backup/cs-2-small/tmpmap* .
+cp instantiations/map_backup/$MAP/* .
+echo " done"
