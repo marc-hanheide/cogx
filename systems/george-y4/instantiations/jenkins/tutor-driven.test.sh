382f7f12c4853bf76f4784b3f57993c5878f09e9
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-05-31 18:49:00 +0000

    george: a script that should run the system simulation in jenkins

---
+++ systems/george-y4/instantiations/jenkins/tutor-driven.test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+root="."
+runner="python tools/castctrl/castrunner.py"
+
+abducer="--abducer"
+#cleanup="--cleanup-script=svnst.sh"
+#develop="--build --rsync"
+display="--display-server"
+gazebo="--gazebo-world=$root/instantiations/gazebo/test1.world"
+#golem="--golem=dummy"
+#peekabot="--peekabot"
+player="--player=$root/instantiations/player/cogx-platform-sim.cfg"
+#speech="--text2speech"
+system="--cast-file=$root/instantiations/george/run-george-full-eval.cast"
+
+$runner $develop $player $gazebo $golem $cleanup $peekabot $abducer $display $speech $system
