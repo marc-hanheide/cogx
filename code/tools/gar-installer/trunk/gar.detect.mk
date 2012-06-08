6c477dd71620245b4107b539466d28210d962071
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-06-08 08:14:42 +0000

    gar: added hghttps extension

--- code/tools/gar-installer/trunk/gar.detect.mk
+++ code/tools/gar-installer/trunk/gar.detect.mk
@@ -5,9 +5,11 @@ SYSTEM_UNAME:=$(shell uname -s)
 ifeq ($(SYSTEM_UNAME),Linux)
    SYSTEM_NAME:=$(shell $(LSB_RELEASE_BIN) -si)
    SYSTEM_VERSION:=$(shell $(LSB_RELEASE_BIN) -sr)
+   SYSTEM_CODENAME:=$(shell $(LSB_RELEASE_BIN) -sc)
 else
    SYSTEM_NAME:=$(SYSTEM_UNAME)
    SYSTEM_VERSION=x
+   SYSTEM_CODENAME=Mountain Lion
 endif
 SYSTEM_RELEASE:=$(SYSTEM_NAME)-$(SYSTEM_VERSION)
 
