af7a62742abbb336f9584f1c17c8e67002296907
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-06-08 11:38:40 +0000

    gar: default gazebo build for u11.10

--- tools/gar-installer/trunk/contrib/gazebo/mkmake.sh
+++ tools/gar-installer/trunk/contrib/gazebo/mkmake.sh
@@ -19,7 +19,7 @@ for version in $VERSIONS; do
    if [ "$version" == "Ubuntu-11.04" ]; then
       wrkdir='gazebo-0.9'
    elif [ "$version" == "Ubuntu-11.10" ]; then
-      wrkdir='gazebo-1.0'
+      wrkdir='gazebo-trunk-1.0'
    elif [ "$version" == "Darwin-x" ]; then
       wrkdir='gazebo-trunk-0.10'
    else
