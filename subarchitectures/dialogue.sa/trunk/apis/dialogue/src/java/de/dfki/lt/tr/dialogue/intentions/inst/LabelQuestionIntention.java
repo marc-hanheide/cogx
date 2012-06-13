d488e29b8014068fe53af0a6eed25b08446d6ff7
farnold <farnold@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-06-13 13:47:57 +0000

    adding intention structs for Dora

---
+++ subarchitectures/dialogue.sa/trunk/apis/dialogue/src/java/de/dfki/lt/tr/dialogue/intentions/inst/LabelQuestionIntention.java
@@ -0,0 +1,53 @@
+package de.dfki.lt.tr.dialogue.intentions.inst;
+
+import de.dfki.lt.tr.beliefs.slice.intentions.BaseIntention;
+import de.dfki.lt.tr.dialogue.intentions.AbstractBaseIntentionTranscoder;
+import de.dfki.lt.tr.dialogue.intentions.DecodingException;
+import de.dfki.lt.tr.dialogue.intentions.DecodingUtils;
+
+public class LabelQuestionIntention extends QuestionIntention {
+
+    protected final String label;
+
+    public LabelQuestionIntention(String label) {
+        super();
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+
+    public static class Transcoder
+            extends AbstractBaseIntentionTranscoder<LabelQuestionIntention> {
+
+        public static final Transcoder INSTANCE = new Transcoder();
+
+        public final String SKEY_LABEL = "label";
+
+        @Override
+        protected void encodeContent(LabelQuestionIntention rich, BaseIntention poor) {
+            QuestionIntention.Transcoder.INSTANCE.encodeContent(rich, poor);
+            poor.stringContent.put(SKEY_LABEL, rich.getLabel());
+        }
+
+        @Override
+        public LabelQuestionIntention tryDecode(BaseIntention poor) {
+            try {
+                QuestionIntention qint = QuestionIntention.Transcoder.INSTANCE.tryDecode(poor);
+                if (qint != null) {
+                    String feature = DecodingUtils.stringGet(poor, SKEY_LABEL);
+                    return new LabelQuestionIntention(feature);
+                }
+                else {
+                    return null;
+                }
+            }
+            catch (DecodingException ex) {
+                return null;
+            }
+        }
+    }
+
+}
