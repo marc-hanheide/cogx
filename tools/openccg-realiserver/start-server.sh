15a746ee2871b1a8d56960da7f996f1ccbf061e8
zender <zender@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-06-14 15:23:28 +0000

    switched standard port to 4321 or use ./start-server.sh <port> to use a different port

--- tools/openccg-realiserver/start-server.sh
+++ tools/openccg-realiserver/start-server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp `find . -name '*.jar' | tr "\n" ':'` de.dfki.lt.tr.realiserver.OpenCCGRealisationServer
+java -cp `find . -name '*.jar' | tr "\n" ':'` de.dfki.lt.tr.realiserver.OpenCCGRealisationServer $1
