e5f3d42d57110f291c1dc2320689f05022e6f3af
farnold <farnold@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-06-15 16:02:57 +0000

    added GBelief history

---
+++ code/subarchitectures/dialogue.sa/trunk/apis/cast/src/java/de/dfki/lt/tr/cast/dialogue/GBeliefMemory.java
@@ -0,0 +1,85 @@
+package de.dfki.lt.tr.cast.dialogue;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import cast.cdl.CASTTime;
+import cast.cdl.WorkingMemoryAddress;
+import castutils.CASTTimeUtil;
+import eu.cogx.beliefs.slice.GroundedBelief;
+
+public class GBeliefMemory implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Map<WorkingMemoryAddress,Map<CASTTime,GroundedBelief>> gbhistory = new HashMap<WorkingMemoryAddress,Map<CASTTime,GroundedBelief>>();
+	
+	public void addGBelief(WorkingMemoryAddress _wma, CASTTime ct, GroundedBelief _newBelief) {
+	
+		if (gbhistory.containsKey(_wma)) {
+			gbhistory.get(_wma).put(ct, _newBelief);
+		} else {
+			gbhistory.put(_wma, new LinkedHashMap<CASTTime,GroundedBelief>());
+			gbhistory.get(_wma).put(ct, _newBelief);
+		}
+	}
+	
+	public GroundedBelief getGBelief (WorkingMemoryAddress _wma, CASTTime ct) {
+		
+		ListIterator<Map.Entry<CASTTime,GroundedBelief>> iter =
+				new ArrayList(gbhistory.get(_wma).entrySet()).listIterator(gbhistory.get(_wma).size());
+		
+		while (iter.hasPrevious()) {
+			Map.Entry<CASTTime, GroundedBelief> entry = iter.previous();
+			if (CASTTimeUtil.diff(ct, entry.getKey()) >= 0) {
+				return entry.getValue();
+			}
+		}
+		return null;
+	}
+	
+	public GroundedBelief getValidGBelief (WorkingMemoryAddress _wma, CASTTime ct) {
+		
+		ListIterator<Map.Entry<CASTTime,GroundedBelief>> iter =
+				new ArrayList(gbhistory.get(_wma).entrySet()).listIterator(gbhistory.get(_wma).size());
+			
+		while (iter.hasPrevious()) {
+			Map.Entry<CASTTime, GroundedBelief> entry = iter.previous();
+			if (CASTTimeUtil.diff(ct, entry.getKey()) >= 0) {
+				if (entry.getValue() != null) {
+					  return entry.getValue();
+				}
+			}
+		}
+		return null;
+	}
+	
+    public Map<CASTTime,GroundedBelief> getAllGBelief (WorkingMemoryAddress _wma) {
+		
+		if (gbhistory.containsKey(_wma)) {
+			return gbhistory.get(_wma);
+		} else {
+			return null;
+		}
+	}
+    
+    public Map<CASTTime,GroundedBelief> getAllGBeliefBefore (WorkingMemoryAddress _wma, CASTTime ct) {
+    	
+    	Map<CASTTime,GroundedBelief> returnMap = new LinkedHashMap<CASTTime,GroundedBelief>();
+			
+	    for (Map.Entry<CASTTime,GroundedBelief> entry : gbhistory.get(_wma).entrySet()) {
+				
+			if (CASTTimeUtil.diff(ct, entry.getKey()) >= 0) {
+				returnMap.put(entry.getKey(), entry.getValue());
+			}
+			else {
+				break;
+			}
+		}
+		return returnMap;
+	}
+}
