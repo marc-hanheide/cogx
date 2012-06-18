0d6c2fed1779ee54848d39ee4a3a4f4c7ede37de
hanheidm <hanheidm@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-06-18 14:42:39 +0000

    allow configure of ExternalGoalgenerator

--- code/subarchitectures/motivation.sa/trunk/src/java/motivation/components/generators/ExternalGoalGenerator.java
+++ code/subarchitectures/motivation.sa/trunk/src/java/motivation/components/generators/ExternalGoalGenerator.java
@@ -18,7 +18,11 @@ import motivation.slice._ExternalGoalServerOperations;
 import motivation.slice._ExternalGoalServerTie;
 import Ice.Current;
 import autogen.Planner.Goal;
+import cast.AlreadyExistsOnWMException;
 import cast.CASTException;
+import cast.DoesNotExistOnWMException;
+import cast.SubarchitectureComponentException;
+import cast.UnknownSubarchitectureException;
 import cast.architecture.ChangeFilterFactory;
 import cast.architecture.ManagedComponent;
 import cast.architecture.WorkingMemoryChangeReceiver;
@@ -37,41 +41,23 @@ import eu.cogx.perceptmediator.transferfunctions.LocalizedAgentTransferFunction;
 public class ExternalGoalGenerator extends ManagedComponent implements
 		_ExternalGoalServerOperations {
 	public static final String ROBOT_TAG = "ROBOT";
-	private static final double DEFAULT_CONSTANT_GAIN = 1.0;
-	private static final float DEFAULT_CONSTANT_COSTS = (float) 1.0;
+	public static final double DEFAULT_CONSTANT_GAIN = 1.0;
+	public static final float DEFAULT_CONSTANT_COSTS = (float) 1.0;
 
 	// give planning at maximum 60 seconds
-	private static final int DEFAULT_MAX_PLANNING_TIME = 120;
+	public static final int DEFAULT_MAX_PLANNING_TIME = 120;
 	// give the max at max 60 minutes to reach goal
-	private static final int DEFAULT_MAX_EXECUTION_TIME = 60 * 60;
+	public static final int DEFAULT_MAX_EXECUTION_TIME = 60 * 60;
 
-	private double constantGain = DEFAULT_CONSTANT_GAIN;
-	private float constantCosts = DEFAULT_CONSTANT_COSTS;
-	protected int maxPlanningTime = DEFAULT_MAX_PLANNING_TIME;
-	protected int maxExecutionTime = DEFAULT_MAX_EXECUTION_TIME;
+	public double constantGain = DEFAULT_CONSTANT_GAIN;
+	public float constantCosts = DEFAULT_CONSTANT_COSTS;
+	public int maxPlanningTime = DEFAULT_MAX_PLANNING_TIME;
+	public int maxExecutionTime = DEFAULT_MAX_EXECUTION_TIME;
 
 	Map<String, WorkingMemoryAddress> goalAddresses = new HashMap<String, WorkingMemoryAddress>();
 	Set<WorkingMemoryAddress> beliefWmas = Collections
 			.synchronizedSet(new HashSet<WorkingMemoryAddress>());
 	private String robotID = null;
-
-	// /*
-	// * (non-Javadoc)
-	// *
-	// * @see
-	// * motivation.components.generators.AbstractMotiveGenerator#checkMotive(
-	// * motivation.slice.Motive)
-	// */
-	// @Override
-	// protected boolean checkMotive(Motive motive) throws CASTException {
-	// PlannerFacade pf = PlannerFacade.get(this);
-	// println("check if goal " + motive.goal.goalString +
-	// " is already achieved.");
-	// boolean isAchieved= pf.isGoalAchieved(motive.goal.goalString);
-	// println("  => " + motive.goal.goalString + " is already achieved? " +
-	// isAchieved);
-	// return !isAchieved;
-	// }
 
 	/*
 	 * (non-Javadoc)
@@ -132,48 +118,11 @@ public class ExternalGoalGenerator extends ManagedComponent implements
 	public MotiveStatus submitGoal(String goalString, float importance,
 			Current __current) {
 		try {
+
 			log("received command to submit new goal " + goalString
 					+ " with importance " + importance);
-			WorkingMemoryAddress wma = new WorkingMemoryAddress(newDataID(),
-					getSubarchitectureID());
-			GeneralGoalMotive ggm = MotiveFactory.createGeneralGoalMotive(wma);
-			ggm.thisEntry=wma;
-			goalAddresses.put(goalString, wma);
-
-			ggm.goal = new Goal(importance, -1, substituteRobotBelief(goalString),
-					false);
-			ggm.informationGain = constantGain;
-			ggm.costs = constantCosts;
-			WMEventQueue queue = new WMEventQueue();
-			addChangeFilter(ChangeFilterFactory.createAddressFilter(wma), queue);
-			addToWorkingMemory(wma, ggm);
-			println("motive has been created and submitted to WM with address "
-					+ CASTUtils.toString(wma));
-
-			WorkingMemoryOperation op = WorkingMemoryOperation.WILDCARD;
-			MotiveStatus status = MotiveStatus.WILDCARD;
-			// wait until that motive has been removed or flagged completed and update in other cases
-			while (op != WorkingMemoryOperation.DELETE && status != MotiveStatus.COMPLETED) {
-				try {
-					WorkingMemoryChange event = queue.take();
-					op = event.operation;
-					if (op != WorkingMemoryOperation.DELETE) {
-						ggm = getMemoryEntry(event.address,
-								GeneralGoalMotive.class);
-						status = ggm.status;
-						println("current status of motive "
-								+ ggm.goal.goalString + " after op " + op
-								+ " is " + status);
-					}
-				} catch (CASTException e) {
-					getLogger().warn(e);
-				}
-			}
-			removeChangeFilter(queue);
-			println("final status of motive " + ggm.goal.goalString + ": "
-					+ ggm.status);
-
-			return ggm.status;
+			WMEventQueue queue = submitGoalToWM(goalString, importance);
+			return waitForCompletion(queue);
 
 		} catch (CASTException e) {
 			getLogger().error(e);
@@ -183,7 +132,80 @@ public class ExternalGoalGenerator extends ManagedComponent implements
 		return MotiveStatus.IMPOSSIBLE;
 	}
 
-	private String getRobotBeliefID() throws CASTException {
+	/**
+	 * @param queue
+	 * @return
+	 * @throws InterruptedException
+	 * @throws SubarchitectureComponentException
+	 */
+	protected MotiveStatus waitForCompletion(WMEventQueue queue)
+			throws InterruptedException, SubarchitectureComponentException {
+		GeneralGoalMotive updatedGgm = null;
+		WorkingMemoryOperation op = WorkingMemoryOperation.WILDCARD;
+		MotiveStatus status = MotiveStatus.WILDCARD;
+		// wait until that motive has been removed or flagged completed and
+		// update in other cases
+		while (op != WorkingMemoryOperation.DELETE
+				&& status != MotiveStatus.COMPLETED) {
+			try {
+				WorkingMemoryChange event = queue.take();
+				op = event.operation;
+				if (op != WorkingMemoryOperation.DELETE) {
+					updatedGgm = getMemoryEntry(event.address,
+							GeneralGoalMotive.class);
+					status = updatedGgm.status;
+					println("current status of motive "
+							+ updatedGgm.goal.goalString + " after op " + op
+							+ " is " + status);
+				}
+			} catch (CASTException e) {
+				getLogger().warn(e);
+			}
+		}
+		removeChangeFilter(queue);
+		if (updatedGgm != null) {
+			println("final status of motive " + updatedGgm.goal.goalString
+					+ ": " + updatedGgm.status);
+
+			return updatedGgm.status;
+		} else {
+			return MotiveStatus.IMPOSSIBLE;
+		}
+
+	}
+
+	/**
+	 * @param goalString
+	 * @param importance
+	 * @param queue
+	 * @return
+	 * @throws CASTException
+	 * @throws AlreadyExistsOnWMException
+	 * @throws DoesNotExistOnWMException
+	 * @throws UnknownSubarchitectureException
+	 */
+	protected WMEventQueue submitGoalToWM(String goalString, float importance)
+			throws CASTException, AlreadyExistsOnWMException,
+			DoesNotExistOnWMException, UnknownSubarchitectureException {
+		WMEventQueue queue = new WMEventQueue();
+		WorkingMemoryAddress wma = new WorkingMemoryAddress(newDataID(),
+				getSubarchitectureID());
+		GeneralGoalMotive ggm = MotiveFactory.createGeneralGoalMotive(wma);
+		ggm.thisEntry = wma;
+		goalAddresses.put(goalString, wma);
+
+		ggm.goal = new Goal(importance, -1, substituteRobotBelief(goalString),
+				false);
+		ggm.informationGain = constantGain;
+		ggm.costs = constantCosts;
+		addChangeFilter(ChangeFilterFactory.createAddressFilter(wma), queue);
+		addToWorkingMemory(wma, ggm);
+		println("motive has been created and submitted to WM with address "
+				+ CASTUtils.toString(wma));
+		return queue;
+	}
+
+	protected String getRobotBeliefID() throws CASTException {
 		if (robotID == null) {
 			println("the ID of the robot's belief is not yet known, so we have to search for it...");
 			for (WorkingMemoryAddress wma : beliefWmas) {
@@ -200,7 +222,7 @@ public class ExternalGoalGenerator extends ManagedComponent implements
 		return robotID;
 	}
 
-	private String substituteRobotBelief(String goalString)
+	protected String substituteRobotBelief(String goalString)
 			throws CASTException {
 		if (goalString.contains(ROBOT_TAG)) {
 			String id = getRobotBeliefID();
@@ -208,6 +230,11 @@ public class ExternalGoalGenerator extends ManagedComponent implements
 		} else {
 			return goalString;
 		}
+	}
+
+	@Override
+	public void config(Map<String, String> cfg, Current __current) {
+		this.configure(cfg);
 	}
 
 }
