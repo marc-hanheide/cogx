86d1fe634739a1b7e463b1b5d25f59309f87bce1
gsh148 <gsh148@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-06-19 20:29:20 +0000

    GUI to trigger plan verbalisation

---
+++ code/scenarios/dora/trunk/src/java/verbalisation/PlanVerbalisationControllerFrame.java
@@ -0,0 +1,170 @@
+package verbalisation;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton; 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+
+import autogen.Planner.PlanningTask;
+
+/** GUI to keep track of PlanningTasks and trigger verbalisation.
+ * @author Graham Horn
+*/
+public class PlanVerbalisationControllerFrame extends JFrame
+{
+  private PlanVerbalisationController planVerbalisationController;
+  private JTable tasksTable;
+  private PlanningTaskTableModel tasksTableModel;
+  
+  private JPanel controlsPanel;
+  private JButton verbaliseButton;
+  private JTextField taskIdField;
+  
+  public PlanVerbalisationControllerFrame(PlanVerbalisationController plan_verbalisation_controller)
+  {
+    super("Plan Verbalisation Control");
+    planVerbalisationController = plan_verbalisation_controller;
+    getContentPane().setLayout(new BorderLayout());
+    
+    tasksTableModel = new PlanningTaskTableModel();
+    tasksTable = new JTable(tasksTableModel);   
+    tasksTable.setModel(tasksTableModel);
+    tasksTable.getColumnModel().getColumn(0).setPreferredWidth(8);
+    tasksTable.getColumnModel().getColumn(2).setPreferredWidth(12);
+    tasksTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+    tasksTable.getSelectionModel().addListSelectionListener(new ListSelectionListener()
+    {    
+      public void valueChanged(ListSelectionEvent e)
+      {
+        if (tasksTable.getSelectedRow() >= 0)
+        {
+          taskIdField.setText(""+tasksTable.getSelectedRow());
+        }
+      }
+    });
+    
+    JScrollPane scroll_pane = new JScrollPane(tasksTable);
+    tasksTable.setFillsViewportHeight(true);
+
+    controlsPanel = new JPanel();
+    taskIdField = new JTextField("  ");
+    verbaliseButton = new JButton("Verbalise");
+    controlsPanel.add(verbaliseButton);
+    controlsPanel.add(new JLabel(" task id "));
+    controlsPanel.add(taskIdField);
+    verbaliseButton.addActionListener(new ActionListener()
+    {    
+      public void actionPerformed(ActionEvent action_event) 
+      {
+        try
+        {
+          int planning_task_id = Integer.parseInt(taskIdField.getText());
+          
+          boolean trigger_result = planVerbalisationController.triggerPlanVerbalisation(planning_task_id);
+          
+          if (trigger_result == false)
+          {
+            System.err.println("PlanVerbalisationController: Invalid task id");
+          }
+        }
+        catch (NumberFormatException e)
+        {
+          // invalid content
+        }
+      }
+    });
+    getContentPane().add(scroll_pane, BorderLayout.CENTER);
+    getContentPane().add(controlsPanel, BorderLayout.SOUTH);
+  }
+  
+  public void addPlanningTask(PlanningTask new_planning_task)
+  {
+    tasksTableModel.updatePlanningTask(new_planning_task);
+  }
+
+  public void updatePlanningTask(PlanningTask planning_task)
+  {
+    tasksTableModel.updatePlanningTask(planning_task);
+  }
+  
+  private class PlanningTaskTableModel extends AbstractTableModel
+  {
+    private String[] columnNames = { "Task ID", "Goal", "Execution Status"};
+    private ArrayList<PlanningTask> planningTasks;
+    
+    public PlanningTaskTableModel() {
+      planningTasks = new ArrayList<PlanningTask>();
+    }
+
+    public int getColumnCount() {
+      return columnNames.length;
+  }
+
+  public int getRowCount() {
+      return planningTasks.size();
+  }
+
+  public String getColumnName(int col) {
+      return columnNames[col];
+  }
+
+  public Object getValueAt(int row, int col) {
+      PlanningTask planning_task = planningTasks.get(row);
+      switch (col)
+      {
+      case 0:
+      {
+        return planning_task.id;
+      }
+      case 1:
+        {
+          return PlanVerbalisationController.goalToString(planning_task.goals[0]);
+        }
+      case 2:
+        {
+          return PlanVerbalisationController.planningTaskExecutionStatusToString(planning_task);
+        }
+      }
+      return null;
+  }
+  
+    
+    public void updatePlanningTask(PlanningTask planning_task)
+    {
+      boolean found_id = false;
+
+      for (PlanningTask stored_planning_task : planningTasks)
+      {
+        if (stored_planning_task.id == planning_task.id)
+        {
+          stored_planning_task.executionStatus = planning_task.executionStatus;
+          found_id = true;
+        }
+      }
+      
+      if (found_id == false)
+      {
+        planningTasks.add(planning_task);
+      }
+      else
+      {
+        
+      }
+      fireTableDataChanged();
+    }
+  }
+  
+}
