6c5acfd984bb5641747d45e20e0d1b19acbfe299
gsh148 <gsh148@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-06-21 18:13:07 +0000

    added logging and break statements

--- code/tools/castutils/branches/dora-yr4-20120620/src/java/castutils/castextensions/WMContentInjector.java
+++ code/tools/castutils/branches/dora-yr4-20120620/src/java/castutils/castextensions/WMContentInjector.java
@@ -25,22 +25,35 @@ public class WMContentInjector extends CASTHelper {
 	}
 
 	public void inject(String xmlStr) {
-		WMInjection wmi = IceXMLSerializer.fromXMLString(xmlStr,
+		component.log("WM Entry to inject:\n" + xmlStr);
+		WMInjection wmi = null;
+                try {
+			wmi = IceXMLSerializer.fromXMLString(xmlStr,
 				WMInjection.class);
+		} catch (Exception e) {
+			component.logException(e);
+			return;
+		}
+		
 		// if the id is empty, generate a new one
 		if (wmi.adr.id.length() == 0)
 			wmi.adr.id = component.newDataID();
 		// is SA is empty, use the SA this component is running in
 		if (wmi.adr.subarchitecture.length() == 0)
 			wmi.adr.subarchitecture = component.getSubarchitectureID();
+		component.log("WM Entry to inject:\n" + IceXMLSerializer.toXMLString(wmi));
 		try {
 			switch (wmi.op) {
 			case ADD:
 				component.addToWorkingMemory(wmi.adr, wmi.content);
+				break;
 			case DELETE:
 				component.deleteFromWorkingMemory(wmi.adr);
+				break;
 			case OVERWRITE:
 				component.overwriteWorkingMemory(wmi.adr, wmi.content);
+				break;
+			default:
 			}
 		} catch (CASTException e) {
 			component.logException(e);
