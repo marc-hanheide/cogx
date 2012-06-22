c37d93103f5fb7d4ee72c4e51e9ec02f57d0b32e
gsh148 <gsh148@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-06-22 13:25:55 +0000

    wait for verbalisation

--- code/systems/dora-yr4/tests/dora-explore-test.sh
+++ code/systems/dora-yr4/tests/dora-explore-test.sh
@@ -6,7 +6,8 @@
 CAST_CFG="instantiations/dora-yr4-bham-jenkins-sim.cast"
 GOAL="(forall (?p - place) (= (placestatus ?p) trueplace))"
 PLAYER_CFG="instantiations/stage/BHAM/cs-2-small-furniture2.cfg"
-TEST_CFG="--wmcheck-allplacesexplored \\\"sa=spatial.sa;xpath=//status[node()='TRUEPLACE'];type=SpatialData.Place;qualifier=ALL\\\""
+#TEST_CFG="--wait 30 --wmcheck-allplacesexplored \\\"sa=spatial.sa;xpath=//status[node()='TRUEPLACE'];type=SpatialData.Place;qualifier=ALL\\\""
+TEST_CFG="--inject-after-goal \\\"<castutils.castextensions.WMInjection><op>ADD</op><adr><id></id><subarchitecture>dialogue</subarchitecture></adr><content class=\\\"de.dfki.lt.tr.dialogue.production.PlanVerbalizationRequest\\\"><taskID>1</taskID></content></castutils.castextensions.WMInjection>\\\" --wait 30 --wmcheck-allplacesexplored \\\"sa=spatial.sa;xpath=//status[node()='TRUEPLACE'];type=SpatialData.Place;qualifier=ALL\\\""
 ################################################################################
 
 export NOVNC
