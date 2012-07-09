e54042d9fa8e81c7fecf196205e22b0e50b91b67
nah <nah@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-07-09 10:26:22 +0000

    models status works with new goal string and manual filter keeps these goals out

--- code/scenarios/george/trunk/src/java/eu/cogx/goals/george/ModelStatusMotiveGenerator.java
+++ code/scenarios/george/trunk/src/java/eu/cogx/goals/george/ModelStatusMotiveGenerator.java
@@ -1,6 +1,8 @@
 package eu.cogx.goals.george;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import motivation.components.generators.AbstractBeliefMotiveGenerator;
 import motivation.slice.MotivePriority;
@@ -8,7 +10,9 @@ import motivation.slice.MotiveStatus;
 import motivation.slice.RobotNonSituatedMotive;
 import VisionData.VisualConceptModelStatus;
 import autogen.Planner.Goal;
+import cast.UnknownSubarchitectureException;
 import cast.cdl.WorkingMemoryAddress;
+import cast.core.CASTData;
 import cast.core.CASTUtils;
 import de.dfki.lt.tr.beliefs.data.CASTIndependentFormulaDistributionsBelief;
 import de.dfki.lt.tr.beliefs.data.specificproxies.FormulaDistribution;
@@ -27,19 +31,15 @@ public class ModelStatusMotiveGenerator extends
 
 	private static final int MAX_PLANNING_TIME = 30;
 
-	private static final double GAIN_THRESHOLD = 0.2;
+	private static final double GAIN_THRESHOLD = 0.1;
 
+	// keeps track of the current most promising
+	private final HashMap<String, String> m_conceptValues;
 
-	//keeps track of the current most promising 
-	private final HashMap<String,String> m_conceptValues;
-
-	
 	public ModelStatusMotiveGenerator() {
 		super(MS_TYPE, RobotNonSituatedMotive.class, MergedBelief.class);
 		m_conceptValues = new HashMap<String, String>(2);
 	}
-	
-	
 
 	@Override
 	protected RobotNonSituatedMotive checkForAddition(
@@ -54,24 +54,28 @@ public class ModelStatusMotiveGenerator extends
 				.create(MergedBelief.class, _newEntry);
 
 		double gain = getModelGain(belief);
-		
+
 		RobotNonSituatedMotive result = null;
-		
+
+		log("gain: " + gain);
+
 		if (gain > GAIN_THRESHOLD) {
 
-			
 			String concept = getConcept(belief);
 			String value = getMostPromising(belief);
 
-			//HACK
-			if(concept.equals("shape")) {
-				return null;
-			}
-			//END HACK
+			// HACK
+//			if (concept.equals("shape")) {
+//				getLogger().warn("ignoring shape model status",
+//						getLogAdditions());
+//				return null;
+//			} else {
+//				log("generating for: " + concept);
+//			}
+			// END HACK
 
-			
 			m_conceptValues.put(concept, value);
-			
+
 			result = new RobotNonSituatedMotive();
 			result.created = getCASTTime();
 			result.updated = result.created;
@@ -93,35 +97,60 @@ public class ModelStatusMotiveGenerator extends
 
 	}
 
+	// TODO HACK sanitise between Dora and George
+	private WorkingMemoryAddress m_robotBeliefAddr;
 
+	// TODO HACK sanitise between Dora and George
+	@Override
+	protected WorkingMemoryAddress getRobotBeliefAddr() {
+		while (m_robotBeliefAddr == null) {
+			List<CASTData<MergedBelief>> groundedBeliefs = new ArrayList<CASTData<MergedBelief>>();
+			try {
+				getMemoryEntriesWithData(MergedBelief.class, groundedBeliefs,
+						"binder", 0);
+			} catch (UnknownSubarchitectureException e) {
+				logException(e);
+				return null;
+			}
 
-	private String createGoalString(String concept, String value) {
-		String goal = "(object-of-desired-" + concept + "-available "
-				+ value + ")";
-		return goal;
+			for (CASTData<MergedBelief> beliefEntry : groundedBeliefs) {
+				if (beliefEntry.getData().type.equalsIgnoreCase("Robot")) {
+					m_robotBeliefAddr = new WorkingMemoryAddress(
+							beliefEntry.getID(), "binder");
+					break;
+				}
+			}
+			if (m_robotBeliefAddr == null) {
+				getLogger().warn("unable to find belief '" + "Robot" + "'");
+				sleepComponent(200);
+			}
+		}
+		return m_robotBeliefAddr;
+
 	}
 
+	private String createGoalString(String concept, String value) {
+		String predicate = "object-of-desired-" + concept + "-available";
 
+		return VisualObjectMotiveGenerator.beliefFunctionGoal(predicate,
+				getRobotBeliefAddr().id, value);
+
+	}
 
 	private String getMostPromising(
 			CASTIndependentFormulaDistributionsBelief<MergedBelief> belief) {
 		FormulaDistribution fd;
-		fd = belief.getContent().get(
-				ModelStatusTransferFunction.MOST_PROMISING);
-		String value = fd.getDistribution().getMostLikely()
-				.getProposition();
+		fd = belief.getContent()
+				.get(ModelStatusTransferFunction.MOST_PROMISING);
+		String value = fd.getDistribution().getMostLikely().getProposition();
 		return value;
 	}
-
-
 
 	private String getConcept(
 			CASTIndependentFormulaDistributionsBelief<MergedBelief> belief) {
 		FormulaDistribution fd;
-		fd = belief.getContent()
-				.get(ModelStatusTransferFunction.CONCEPT_ID);
-		String concept = fd.getDistribution().getMostLikely()
-				.getProposition();
+		fd = belief.getContent().get(ModelStatusTransferFunction.CONCEPT_ID);
+		String concept = fd.getDistribution().getMostLikely().getProposition();
 		return concept;
 	}
 
@@ -144,32 +173,32 @@ public class ModelStatusMotiveGenerator extends
 		String concept = getConcept(belief);
 		String value = getMostPromising(belief);
 		double gain = getModelGain(belief);
-		
+
 		RobotNonSituatedMotive result = null;
-		
-		if(gain > GAIN_THRESHOLD) {
-			
+
+		if (gain > GAIN_THRESHOLD) {
+
 			result = _motive;
-			
+
 			String previousValue = m_conceptValues.get(concept);
-			
-			if(previousValue == null) {
-				getLogger().warn("I'm not sure this should be happening", getLogAdditions());
+
+			if (previousValue == null) {
+				getLogger().warn("I'm not sure this should be happening",
+						getLogAdditions());
 				return null;
 			}
-			
+
 			// gain has increased enough so we can do something
-			if(!previousValue.equals(value)) {
-				log("changed model status goal from " + previousValue + " to " + value );
+			if (!previousValue.equals(value)) {
+				log("changed model status goal from " + previousValue + " to "
+						+ value);
 				m_conceptValues.put(concept, value);
 				result.goal.goalString = createGoalString(concept, value);
 				result.status = MotiveStatus.UNSURFACED;
 			}
-			
+
 		}
-		
-		
-		
+
 		return result;
 	}
 }
