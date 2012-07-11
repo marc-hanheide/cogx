d079fa7be81fbb740a43679154b061acab0d31fa
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-07-11 19:35:32 +0000

    proper handling of removed visual objects in discourse ref resolution

--- code/subarchitectures/dialogue.sa/trunk/apis/cast/src/java/de/dfki/lt/tr/cast/dialogue/DiscourseReferenceResolution.java
+++ code/subarchitectures/dialogue.sa/trunk/apis/cast/src/java/de/dfki/lt/tr/cast/dialogue/DiscourseReferenceResolution.java
@@ -6,12 +6,15 @@ import cast.architecture.WorkingMemoryChangeReceiver;
 import cast.cdl.WorkingMemoryAddress;
 import cast.cdl.WorkingMemoryChange;
 import cast.cdl.WorkingMemoryOperation;
+import de.dfki.lt.tr.beliefs.slice.distribs.*;
 import de.dfki.lt.tr.beliefs.slice.epstatus.EpistemicStatus;
 import de.dfki.lt.tr.beliefs.slice.intentions.BaseIntention;
 import de.dfki.lt.tr.beliefs.slice.intentions.IntentionToAct;
 import de.dfki.lt.tr.beliefs.slice.intentions.InterpretedIntention;
+import de.dfki.lt.tr.beliefs.slice.logicalcontent.ElementaryFormula;
 import de.dfki.lt.tr.beliefs.slice.logicalcontent.PointerFormula;
 import de.dfki.lt.tr.beliefs.slice.logicalcontent.dFormula;
+import de.dfki.lt.tr.beliefs.slice.sitbeliefs.dBelief;
 import de.dfki.lt.tr.cast.dialogue.DiscourseReferenceResolution.LastMentionedReferenceResolver;
 import de.dfki.lt.tr.dialogue.interpret.IntentionManagementConstants;
 import de.dfki.lt.tr.dialogue.ref.EpistemicReferenceHypothesis;
@@ -76,6 +79,40 @@ extends AbstractReferenceResolutionComponent<LastMentionedReferenceResolver> {
 		return lastMentioned;
 	}
 
+        public static boolean isVisible(dBelief bel) {
+            if ("visualobject".equals(bel.type)) {
+                if (bel.content instanceof CondIndependentDistribs) {
+                    CondIndependentDistribs d = (CondIndependentDistribs) bel.content;
+                    ProbDistribution pd = d.distribs.get("presence");
+                    if (pd != null && pd instanceof BasicProbDistribution) {
+                        BasicProbDistribution presd = (BasicProbDistribution) pd;
+                        if (presd.values instanceof FormulaValues) {
+                            FormulaValues fvs = (FormulaValues) presd.values;
+                            if (fvs.values.size() == 1) {
+                                FormulaProbPair fpp = fvs.values.get(0);
+                                if (fpp.val instanceof ElementaryFormula) {
+                                    ElementaryFormula formula = (ElementaryFormula) fpp.val;
+                                    return "visible".equals(formula.prop);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private boolean isVisibleVisualObject(WorkingMemoryAddress wma) {
+            try {
+                dBelief bel = getMemoryEntry(wma, dBelief.class);
+                return isVisible(bel);
+            }
+            catch (SubarchitectureComponentException ex) {
+                logException(ex);
+            }
+            return false;
+        }
+        
 	public class LastMentionedReferenceResolver implements ReferenceResolver {
 
 		private final EpistemicStatus epst = EpistemicStatusFactory.newSharedEpistemicStatus(IntentionManagementConstants.thisAgent, IntentionManagementConstants.humanAgent);
@@ -85,7 +122,7 @@ extends AbstractReferenceResolutionComponent<LastMentionedReferenceResolver> {
 			ReferenceResolutionResult result = ReferenceUtils.newEmptyResolutionResult(rr, origin, "last-mention");
 			if (rr.sort.equals(ReferenceResolver.SORT_DISCOURSE)) {
 				WorkingMemoryAddress qudAboutAddr = getLastMentioned();
-				if (qudAboutAddr != null) {
+				if (qudAboutAddr != null && isVisibleVisualObject(qudAboutAddr)) {
 					dFormula referent = new PointerFormula(0, qudAboutAddr, "");
 					double score = 10.0;
 					result.hypos.add(new EpistemicReferenceHypothesis(epst, referent, score));
