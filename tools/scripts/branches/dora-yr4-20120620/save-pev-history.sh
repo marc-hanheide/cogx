63daa43c61b7f33d45447a8375a72449bc0bf5f1
zender <zender@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-07-11 15:02:29 +0000

    PEV save script now also handles messages and raw text files

--- tools/scripts/branches/dora-yr4-20120620/save-pev-history.sh
+++ tools/scripts/branches/dora-yr4-20120620/save-pev-history.sh
@@ -5,5 +5,7 @@ mydir="subarchitectures/dialogue.sa/resources/pev-test-data/$timestamp"
 mkdir -p $mydir
 
 cp GBeliefHistory.xml $mydir
+cp PlanVerbMessages.xml $mydir
+cp PEV-rawtext.xml $mydir
 mv subarchitectures/planner.sa/src/python/history-*.pddl $mydir
 cp logs/log.xml $mydir
