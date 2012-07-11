526628241e59e5a830d086aa4d27f1b69413c184
mmarko <mmarko@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-07-11 14:55:48 +0000

    don't store paths with logfiles

--- code/systems/george-y4/logs/pack.sh
+++ code/systems/george-y4/logs/pack.sh
@@ -2,4 +2,4 @@
 
 rm logs.zip
 cd ..
-zip logs/logs.zip logs/*.html logs/log.xml subarchitectures/planner.sa/src/python/planner-log*.log
+zip -Dj logs/logs.zip logs/*.html logs/log.xml subarchitectures/planner.sa/src/python/planner-log*.log
