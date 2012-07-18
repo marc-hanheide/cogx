6fc24d814063bd8f04cdb148e04e379b3641bda1
krsj <krsj@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-07-18 15:40:22 +0000

    purge out of step queues in Integrators

--- subarchitectures/categorical.sa/branches/dora-yr4-20120620/src/c++/categorical/components/SizeIntegrator/SizeIntegrator.cpp
+++ subarchitectures/categorical.sa/branches/dora-yr4-20120620/src/c++/categorical/components/SizeIntegrator/SizeIntegrator.cpp
@@ -264,6 +264,23 @@ bool CategoricalSizeIntegrator::readyToAccumulate()
 	return true;
 }
 
+// ------------------------------------------------------
+void CategoricalSizeIntegrator::ensureQueueIntegrity()
+{
+	if ( (_resultsQueue.empty()) || (_odometryQueue.empty()) )
+		return;
+
+	if (_resultsQueue.front()->frameNo > _odometryQueue.front()->frameNo)
+	{
+	  getLogger()->warn("Queues out of sync; dropping one odometry frame!");
+	  _odometryQueue.pop_front();
+	}
+	if (_resultsQueue.front()->frameNo < _odometryQueue.front()->frameNo)
+	{
+	  getLogger()->warn("Queues out of sync; dropping one laser result frame!");
+	  _resultsQueue.pop_front();
+	}
+}
 
 // ------------------------------------------------------
 void CategoricalSizeIntegrator::runComponent()
@@ -283,6 +300,8 @@ ts.tv_nsec = 0;
 		pthread_mutex_lock(&_dataSignalMutex);
 		if (!readyToAccumulate())
 			pthread_cond_timedwait(&_dataSignalCond, &_dataSignalMutex, &ts);
+
+		ensureQueueIntegrity();
 
 		// Handle signal if signal arrived
 		if ((!isRunning()) || (!readyToAccumulate()))
