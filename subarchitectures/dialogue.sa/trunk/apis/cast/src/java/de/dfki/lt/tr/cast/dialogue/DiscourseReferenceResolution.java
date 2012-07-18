e7d8051d7db806b5434a3c3879f0e61ab1704817
janicek <janicek@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-07-18 10:12:59 +0000

    adding a very primitive interpretation of "other"

--- subarchitectures/dialogue.sa/trunk/apis/cast/src/java/de/dfki/lt/tr/cast/dialogue/DiscourseReferenceResolution.java
+++ subarchitectures/dialogue.sa/trunk/apis/cast/src/java/de/dfki/lt/tr/cast/dialogue/DiscourseReferenceResolution.java
@@ -1,11 +1,13 @@
 package de.dfki.lt.tr.cast.dialogue;
 
 import cast.SubarchitectureComponentException;
+import cast.UnknownSubarchitectureException;
 import cast.architecture.ChangeFilterFactory;
 import cast.architecture.WorkingMemoryChangeReceiver;
 import cast.cdl.WorkingMemoryAddress;
 import cast.cdl.WorkingMemoryChange;
 import cast.cdl.WorkingMemoryOperation;
+import castutils.castextensions.WMView;
 import de.dfki.lt.tr.beliefs.slice.distribs.*;
 import de.dfki.lt.tr.beliefs.slice.epstatus.EpistemicStatus;
 import de.dfki.lt.tr.beliefs.slice.intentions.BaseIntention;
@@ -79,33 +81,10 @@ extends AbstractReferenceResolutionComponent<LastMentionedReferenceResolver> {
 		return lastMentioned;
 	}
 
-        public static boolean isVisible(dBelief bel) {
-            if ("visualobject".equals(bel.type)) {
-                if (bel.content instanceof CondIndependentDistribs) {
-                    CondIndependentDistribs d = (CondIndependentDistribs) bel.content;
-                    ProbDistribution pd = d.distribs.get("presence");
-                    if (pd != null && pd instanceof BasicProbDistribution) {
-                        BasicProbDistribution presd = (BasicProbDistribution) pd;
-                        if (presd.values instanceof FormulaValues) {
-                            FormulaValues fvs = (FormulaValues) presd.values;
-                            if (fvs.values.size() == 1) {
-                                FormulaProbPair fpp = fvs.values.get(0);
-                                if (fpp.val instanceof ElementaryFormula) {
-                                    ElementaryFormula formula = (ElementaryFormula) fpp.val;
-                                    return "visible".equals(formula.prop);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return false;
-        }
-
         private boolean isVisibleVisualObject(WorkingMemoryAddress wma) {
             try {
                 dBelief bel = getMemoryEntry(wma, dBelief.class);
-                return isVisible(bel);
+                return CogXReferringUtils.canBeReferredTo(bel);
             }
             catch (SubarchitectureComponentException ex) {
                 logException(ex);
