4f20fb60a1a6182c96f6192e2f2932f92b39fbec
gsh148 <gsh148@9dca7cc1-ec4f-0410-aedc-c33437d64837>
2012-11-30 15:02:53 +0000

    config for systems paper

--- systems/branches/dora-yr4-systems-paper/cleanmap.sh
+++ systems/branches/dora-yr4-systems-paper/cleanmap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #zenity --question --text "reset mapping?"  && rm -f robotpose.ccf tmpmap.* && exit 0
 #zenity --question --text "restore start map?" && rm -f robotpose.ccf && cp instantiations/map_backup/bham_lab/* .
-rm -f robotpose.ccf tmpmap.* NodeGridMaps.txt GridMap.txt HeightMap.txt Places.txt Connectivities.txt place_properties.bin rooms.xml odom.tdf scans.tdf slaminputdata.txt conceptual.*
+rm -f robotpose.ccf tmpmap.* NodeGridMaps.txt GridMap.txt HeightMap.txt Places.txt Connectivities.txt place_properties.bin rooms.xml odom.tdf scans.tdf slaminputdata.txt conceptual.* subarchitectures/planner.sa/src/python/history-*.pddl subarchitectures/planner.sa/src/python/planner-log-*.log subarchitectures/planner.sa/src/python/standalone/tmp/static_dir_for_debugging/*
